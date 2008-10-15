b4130bdf56005299f5e23d25bbd3b2bc787e9e29
Robert Love <rlove@google.com>
2008-10-15 15:35:44 -0400

    ANDROID: net: Paranoid network.
    With CONFIG_ANDROID_PARANOID_NETWORK, require specific uids/gids to
    instantiate ipv4 and ipv6 network sockets.
    
    Change-Id: I879d7181f07532784499ef152288d12a03ab6354
    Signed-off-by: Robert Love <rlove@google.com>
    Signed-off-by: JP Abgrall <jpa@google.com>
    [AmitP: Folded following android-4.9 commit changes into this patch
            a2624d7b9d73 ("ANDROID: Add android_aid.h")]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    [astrachan: Folded the following changes into this patch:
                3d76291d7143 ("net: paranoid: security: Add AID_NET_RAW and AID_NET_ADMIN capability check in cap_capable().")
                ddde0de4b4e3 ("net: paranoid: Replace AID_NET_RAW checks with capable(CAP_NET_RAW).")
                19484f170f8e ("net: paranoid: Only NET_ADMIN is allowed to fully control TUN interfaces.")]
    [astrachan: Removed the group-based/implicit capability grants]
    [astrachan: Removed AF_BLUETOOTH socket handling; done in sepolicy]
    [astrachan: Removed TUN interface handling; done in sepolicy]
    [astrachan: Removed bogus reversion of network namespace check]
    Signed-off-by: Alistair Strachan <astrachan@google.com>

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -66,6 +66,10 @@
 #include <linux/uaccess.h>
 #include <linux/mroute6.h>
 
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+#include <linux/android_aid.h>
+#endif
+
 #include "ip6_offload.h"
 
 MODULE_AUTHOR("Cast of dozens");
@@ -121,6 +125,11 @@ static int inet6_create(struct net *net, struct socket *sock, int protocol,
 
 	if (protocol < 0 || protocol >= IPPROTO_MAX)
 		return -EINVAL;
+
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+	if (!in_egroup_p(AID_INET) && !ns_capable(net->user_ns, CAP_NET_RAW))
+		return -EACCES;
+#endif
 
 	/* Look for the requested type/protocol pair. */
 lookup_protocol:
