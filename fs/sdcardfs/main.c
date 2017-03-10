cb4ac8498285606da70d5815d3153e24e5ef30be
Daniel Rosenberg <drosen@google.com>
2017-03-09 20:56:05 -0800

    ANDROID: sdcardfs: update module info
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I958c7c226d4e9265fea8996803e5b004fb33d8ad

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -472,9 +472,11 @@ static void __exit exit_sdcardfs_fs(void)
 }
 
 MODULE_AUTHOR("Erez Zadok, Filesystems and Storage Lab, Stony Brook University"
-	      " (http://www.fsl.cs.sunysb.edu/)");
-MODULE_DESCRIPTION("Wrapfs " SDCARDFS_VERSION
-		   " (http://wrapfs.filesystems.org/)");
+	" (http://www.fsl.cs.sunysb.edu/)");
+MODULE_AUTHOR("Woojoong Lee, Daeho Jeong, Kitae Lee, Yeongjin Gil"
+        " System Memory Lab., Samsung Electronics");
+MODULE_AUTHOR("Daniel Rosenberg, Google");
+MODULE_DESCRIPTION("Sdcardfs " SDCARDFS_VERSION);
 MODULE_LICENSE("GPL");
 
 module_init(init_sdcardfs_fs);
