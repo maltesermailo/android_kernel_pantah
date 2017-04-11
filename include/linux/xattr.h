ccebed9ec2028c76c5d0798373824640179be837
Daniel Rosenberg <drosen@google.com>
2017-04-11 15:51:43 -0700

    vfs: Switch to inode_permission2
    Switch remaining vfs users of inode_permission to
    use inode_permission2 instead, which passes down mount
    information.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I3252bf0e921116c8a74db7d55855be92fce36b49
    Bug: 35848445

--- include/linux/xattr.h
+++ include/linux/xattr.h
@@ -38,10 +38,13 @@ struct xattr {
 
 ssize_t xattr_getsecurity(struct inode *, const char *, void *, size_t);
 ssize_t vfs_getxattr(struct dentry *, const char *, void *, size_t);
+ssize_t vfs_getxattr2(struct vfsmount *, struct dentry *, const char *, void *, size_t);
 ssize_t vfs_listxattr(struct dentry *d, char *list, size_t size);
 int __vfs_setxattr_noperm(struct dentry *, const char *, const void *, size_t, int);
 int vfs_setxattr(struct dentry *, const char *, const void *, size_t, int);
+int vfs_setxattr2(struct vfsmount *, struct dentry *, const char *, const void *, size_t, int);
 int vfs_removexattr(struct dentry *, const char *);
+int vfs_removexattr2(struct vfsmount *, struct dentry *, const char *);
 
 ssize_t generic_getxattr(struct dentry *dentry, const char *name, void *buffer, size_t size);
 ssize_t generic_listxattr(struct dentry *dentry, char *buffer, size_t buffer_size);
@@ -49,7 +52,11 @@ int generic_setxattr(struct dentry *dentry, const char *name, const void *value,
 int generic_removexattr(struct dentry *dentry, const char *name);
 ssize_t vfs_getxattr_alloc(struct dentry *dentry, const char *name,
 			   char **xattr_value, size_t size, gfp_t flags);
+ssize_t vfs_getxattr_alloc2(struct vfsmount *, struct dentry *dentry, const char *name,
+			   char **xattr_value, size_t size, gfp_t flags);
 int vfs_xattr_cmp(struct dentry *dentry, const char *xattr_name,
+		  const char *value, size_t size, gfp_t flags);\
+int vfs_xattr_cmp2(struct vfsmount *, struct dentry *dentry, const char *xattr_name,
 		  const char *value, size_t size, gfp_t flags);
 
 struct simple_xattrs {
