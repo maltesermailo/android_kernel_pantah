dc8cb4a401248ea81784fc0fff8b17d06ea812d4
Patrick Bellasi <patrick.bellasi@arm.com>
2017-07-17 15:52:28 +0100

    sched/fair: increase chances to use energy_diff
    For non latency sensitive tasks we wanna optimize for a better
    power/performance trade-off. Thus, let's use find_best_target to always
    (possibly) pick a candidate target CPU which is different than the CPU
    where the task executed previously.
    
    This should increases the chances to use energy_diff to select the
    better CPU where the task will be scheduled.
    
    Change-Id: I27ee5e2f8435221a85fc06b024aff8fc4de3b51f
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6277,7 +6277,7 @@ static int start_cpu(bool boosted)
 }
 
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
-				   bool boosted, bool prefer_idle)
+				   int prev_cpu, bool boosted, bool prefer_idle)
 {
 	unsigned long best_idle_min_cap_orig = ULONG_MAX;
 	unsigned long min_util = boosted_task_util(p);
@@ -6421,6 +6421,20 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				best_active_cpu = i;
 				continue;
 			}
+
+			/*
+			 * Enforce energy_diff
+			 *
+			 * For non latency sensitive tasks, skip the task's
+			 * previous CPU.
+			 *
+			 * The goal here is to try hard to find another
+			 * possible candidate and use energy_diff to find out
+			 * if it's more energy efficient to move the task
+			 * there.
+			 */
+			if (i == prev_cpu)
+				continue;
 
 			/*
 			 * Case B) Non latency sensitive tasks on IDLE CPUs.
@@ -6601,7 +6615,7 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
 
 	sd = rcu_dereference(per_cpu(sd_ea, prev_cpu));
 	/* Find a cpu with sufficient capacity */
-	tmp_target = find_best_target(p, &tmp_backup, boosted, prefer_idle);
+	tmp_target = find_best_target(p, &tmp_backup, prev_cpu, boosted, prefer_idle);
 
 	if (!sd)
 		goto unlock;
