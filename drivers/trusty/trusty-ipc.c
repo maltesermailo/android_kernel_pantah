0aabf9a3219ccf43a247c6ab278efb4d788c4ae8
Michael Ryleev <gmar@google.com>
2017-07-31 13:23:56 -0700

    trusty: memrefs: Fixup for 4.9
    Change-Id: I2ac1c443b259bbe1e60f67bd116606ba6cc6ad6a
    Signed-off-by: Michael Ryleev <gmar@google.com>

--- drivers/trusty/trusty-ipc.c
+++ drivers/trusty/trusty-ipc.c
@@ -730,7 +730,7 @@ static int append_user_memref(struct tipc_msg_mref_hdr *hdr,
 			      struct tipc_shmem *shm)
 {
 	long ret;
-	bool write;
+	unsigned int gup_flags;
 	unsigned int i;
 	unsigned int pg_num;
 	unsigned long pg_start;
@@ -740,7 +740,7 @@ static int append_user_memref(struct tipc_msg_mref_hdr *hdr,
 	if (!vmas)
 		return -ENOMEM;
 
-	write = !!(shm->flags & TIPC_MEMREF_PERM_RW);
+	gup_flags = (shm->flags & TIPC_MEMREF_PERM_RW) ? FOLL_WRITE : 0;
 
 	down_read(&current->mm->mmap_sem);
 
@@ -752,8 +752,7 @@ static int append_user_memref(struct tipc_msg_mref_hdr *hdr,
 		if (!pg_start || !pg_num)
 			continue;
 
-		ret = get_user_pages(current, current->mm,
-				     pg_start, pg_num, write, 0,
+		ret = get_user_pages(pg_start, pg_num, gup_flags,
 				     mref->pages + mref->pg_pinned,
 				     vmas + mref->pg_pinned);
 		if (ret < 0)
