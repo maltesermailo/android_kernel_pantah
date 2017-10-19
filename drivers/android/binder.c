ff9ec1a0d98c79cfdf40309caeb87be547442330
Martijn Coenen <maco@android.com>
2017-10-19 15:04:46 +0200

    ANDROID: binder: Add todo_nowake queue.
    This queue contains thread work for which we don't
    immediately have to wake up or return to userspace.
    The work will only be processed once some work is pushed
    to the regular todo list.
    
    The ordering with respect to the regular todo list is that
    all work in the _nowake queue will be handled first, then
    the regular todo work will be handled.
    
    The prime usecase for this is improving the performance
    of synchronous transactions: all synchronous transactions
    post a BR_TRANSACTION_COMPLETE to the calling thread,
    but there's no reason to return that command to userspace
    right away - userspace anyway needs to wait for the reply.
    
    Likewise, a synchronous transaction that contains a binder
    object can cause a BC_ACQUIRE/BC_INCREFS to be returned to
    userspace; since the caller must anyway hold a strong/weak
    ref for the duration of the call, postponing these commands
    until the reply comes in is not a problem.
    
    Before patch:
    ------------------------------------------------------------------
    Benchmark                           Time           CPU Iterations
    ------------------------------------------------------------------
    BM_sendVec_binderize/4          45959 ns      20288 ns      34351
    BM_sendVec_binderize/8          45603 ns      20080 ns      34909
    BM_sendVec_binderize/16         45528 ns      20113 ns      34863
    BM_sendVec_binderize/32         45551 ns      20122 ns      34881
    BM_sendVec_binderize/64         45701 ns      20183 ns      34864
    BM_sendVec_binderize/128        45824 ns      20250 ns      34576
    BM_sendVec_binderize/256        45695 ns      20171 ns      34759
    BM_sendVec_binderize/512        45743 ns      20211 ns      34489
    BM_sendVec_binderize/1024       46169 ns      20430 ns      34081
    
    After patch:
    ------------------------------------------------------------------
    Benchmark                           Time           CPU Iterations
    ------------------------------------------------------------------
    BM_sendVec_binderize/4          42939 ns      17262 ns      40653
    BM_sendVec_binderize/8          42823 ns      17243 ns      40671
    BM_sendVec_binderize/16         42898 ns      17243 ns      40594
    BM_sendVec_binderize/32         42838 ns      17267 ns      40527
    BM_sendVec_binderize/64         42854 ns      17249 ns      40379
    BM_sendVec_binderize/128        42881 ns      17288 ns      40427
    BM_sendVec_binderize/256        42917 ns      17297 ns      40429
    BM_sendVec_binderize/512        43184 ns      17395 ns      40411
    BM_sendVec_binderize/1024       43119 ns      17357 ns      40432
    
    Change-Id: Ia70287066d62aba64e98ac44ff1214e37ca75693
    
    Signed-off-by: Martijn Coenen <maco@android.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -601,6 +601,8 @@ enum {
  *                        (protected by @proc->inner_lock)
  * @todo:                 list of work to do for this thread
  *                        (protected by @proc->inner_lock)
+ * @todo_nowake:          work still to do that we don't wake up the thread for
+ *                        (protected by @proc->inner_lock)
  * @return_error:         transaction errors reported by this thread
  *                        (only accessed by this thread)
  * @reply_error:          transaction errors reported by target thread
@@ -627,6 +629,7 @@ struct binder_thread {
 	bool looper_need_return; /* can be written by other thread */
 	struct binder_transaction *transaction_stack;
 	struct list_head todo;
+	struct list_head todo_nowake;
 	struct binder_error return_error;
 	struct binder_error reply_error;
 	wait_queue_head_t wait;
@@ -2412,6 +2415,7 @@ static int binder_translate_binder(struct flat_binder_object *fp,
 	struct binder_node *node;
 	struct binder_proc *proc = thread->proc;
 	struct binder_proc *target_proc = t->to_proc;
+	struct list_head *target_list;
 	struct binder_ref_data rdata;
 	int ret = 0;
 
@@ -2434,9 +2438,14 @@ static int binder_translate_binder(struct flat_binder_object *fp,
 		goto done;
 	}
 
+	if (t->flags & TF_ONE_WAY)
+		target_list = &thread->todo;
+	else /* The reply will wake us up, so queue to _nowake */
+		target_list = &thread->todo_nowake;
+
 	ret = binder_inc_ref_for_node(target_proc, node,
 			fp->hdr.type == BINDER_TYPE_BINDER,
-			&thread->todo, &rdata);
+			target_list, &rdata);
 	if (ret)
 		goto done;
 
@@ -3258,10 +3267,10 @@ static void binder_transaction(struct binder_proc *proc,
 		}
 	}
 	tcomplete->type = BINDER_WORK_TRANSACTION_COMPLETE;
-	binder_enqueue_work(proc, tcomplete, &thread->todo);
 	t->work.type = BINDER_WORK_TRANSACTION;
 
 	if (reply) {
+		binder_enqueue_work(proc, tcomplete, &thread->todo);
 		binder_inner_proc_lock(target_proc);
 		if (target_thread->is_dead) {
 			binder_inner_proc_unlock(target_proc);
@@ -3277,6 +3286,8 @@ static void binder_transaction(struct binder_proc *proc,
 	} else if (!(t->flags & TF_ONE_WAY)) {
 		BUG_ON(t->buffer->async_transaction != 0);
 		binder_inner_proc_lock(proc);
+		BUG_ON(!binder_worklist_empty_ilocked(&thread->todo));
+		binder_enqueue_work_ilocked(tcomplete, &thread->todo_nowake);
 		t->need_reply = 1;
 		t->from_parent = thread->transaction_stack;
 		thread->transaction_stack = t;
@@ -3290,6 +3301,7 @@ static void binder_transaction(struct binder_proc *proc,
 	} else {
 		BUG_ON(target_node == NULL);
 		BUG_ON(t->buffer->async_transaction != 1);
+		binder_enqueue_work(proc, tcomplete, &thread->todo);
 		if (!binder_proc_transaction(t, target_proc, NULL))
 			goto err_dead_proc_or_thread;
 	}
@@ -3977,7 +3989,9 @@ retry:
 		struct binder_thread *t_from;
 
 		binder_inner_proc_lock(proc);
-		if (!binder_worklist_empty_ilocked(&thread->todo))
+		if (!binder_worklist_empty_ilocked(&thread->todo_nowake))
+			list = &thread->todo_nowake;
+		else if (!binder_worklist_empty_ilocked(&thread->todo))
 			list = &thread->todo;
 		else if (!binder_worklist_empty_ilocked(&proc->todo) &&
 			   wait_for_proc_work)
@@ -4355,6 +4369,7 @@ static struct binder_thread *binder_get_thread_ilocked(
 	atomic_set(&thread->tmp_ref, 0);
 	init_waitqueue_head(&thread->wait);
 	INIT_LIST_HEAD(&thread->todo);
+	INIT_LIST_HEAD(&thread->todo_nowake);
 	rb_link_node(&thread->rb_node, parent, p);
 	rb_insert_color(&thread->rb_node, &proc->threads);
 	thread->looper_need_return = true;
@@ -4399,6 +4414,7 @@ static void binder_free_proc(struct binder_proc *proc)
 
 static void binder_free_thread(struct binder_thread *thread)
 {
+	BUG_ON(!list_empty(&thread->todo_nowake));
 	BUG_ON(!list_empty(&thread->todo));
 	binder_stats_deleted(BINDER_STAT_THREAD);
 	binder_proc_dec_tmpref(thread->proc);
@@ -4466,6 +4482,7 @@ static int binder_thread_release(struct binder_proc *proc,
 
 	if (send_reply)
 		binder_send_failed_reply(send_reply, BR_DEAD_REPLY);
+	binder_release_work(proc, &thread->todo_nowake);
 	binder_release_work(proc, &thread->todo);
 	binder_thread_dec_tmpref(thread);
 	return active_transactions;
@@ -5228,6 +5245,10 @@ static void print_binder_thread_ilocked(struct seq_file *m,
 					"    bad transaction", t);
 			t = NULL;
 		}
+	}
+	list_for_each_entry(w, &thread->todo_nowake, entry) {
+		print_binder_work_ilocked(m, thread->proc, "    ",
+					  "    pending transaction", w);
 	}
 	list_for_each_entry(w, &thread->todo, entry) {
 		print_binder_work_ilocked(m, thread->proc, "    ",
