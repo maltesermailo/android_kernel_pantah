7c81d16b7b1d3fc9413034804a32cdf2aff9f42c
Nathan Harold <nharold@google.com>
2018-01-05 15:19:53 -0800

    Allow SA mark to be updated using UPDSA
    Allowing UPDSA to change mark and output_mark
    permits policy separation of packet routing
    decisions from SA keying.
    
    This patch allows the SA mark to be updated using
    UPSDA as long as the new mark is a narrower subset
    of the previous mark. Thus if the mark was previously
    un-set, then any mark may be set, in effect permitting
    late-binding of SA to policies.
    
    In addition, the output_mark, used as a routing mark
    for outbound packets, is also made update-able which
    allows routing decisions to be handled independently
    of keying/SA creation.
    
    Signed-off-by: Nathan Harold <nharold@google.com>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -1349,10 +1349,17 @@ out:
 			memcpy(&x1->sel, &x->sel, sizeof(x1->sel));
 		memcpy(&x1->lft, &x->lft, sizeof(x1->lft));
 		x1->km.dying = 0;
-
 		tasklet_hrtimer_start(&x1->mtimer, ktime_set(1, 0), HRTIMER_MODE_REL);
 		if (x1->curlft.use_time)
 			xfrm_state_check_expire(x1);
+		if (memcmp(&x1->mark, &x->mark, sizeof(x1->mark)) ||
+		    x1->props.output_mark != x->props.output_mark) {
+			spin_lock_bh(&net->xfrm.xfrm_state_lock);
+			__xfrm_state_bump_genids(x1);
+			memcpy(&x1->mark, &x->mark, sizeof(x1->mark));
+			x1->props.output_mark = x->props.output_mark;
+			spin_unlock_bh(&net->xfrm.xfrm_state_lock);
+		}
 
 		err = 0;
 		x->km.state = XFRM_STATE_DEAD;
