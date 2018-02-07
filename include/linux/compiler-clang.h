bdef6f7b514e5736a178a87b3c719a53ebe5cb97
Sami Tolvanen <samitolvanen@google.com>
2018-02-07 09:09:34 -0800

    add support for clang SafeStack
    This change adds support for clang SafeStack, which protects function
    return addresses against attacks based on stack buffer overflows:
    
      https://clang.llvm.org/docs/SafeStack.html
    
    SafeStack splits the kernel stack into safe and unsafe parts. Safe
    stack is used only for data that's at compile time determined to be
    only accessed in a safe way. This prevents a buffer overflow in the
    unsafe stack from overwriting any data in the safe stack, including
    the return address.
    
    Bug: 67506682
    Bug: 67507327
    Change-Id: I6ce8dbf80dfd8a97d16e049531bba900eb8699b3
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -24,3 +24,5 @@
 
 #define __nocfi		__attribute__((no_sanitize("cfi")))
 #endif
+
+#define __nosafestack	__attribute__((no_sanitize("safe-stack")))
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -75,6 +75,7 @@
 #include <linux/compiler.h>
 #include <linux/frame.h>
 #include <linux/prefetch.h>
+#include <linux/safestack.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -5286,6 +5287,7 @@ static const char stat_nam[] = TASK_STATE_TO_CHAR_STR;
 void sched_show_task(struct task_struct *p)
 {
 	unsigned long free = 0;
+	unsigned long unsafe_free = 0;
 	int ppid;
 	unsigned long state = p->state;
 
@@ -5299,15 +5301,21 @@ void sched_show_task(struct task_struct *p)
 		printk(KERN_CONT "  running task    ");
 #ifdef CONFIG_DEBUG_STACK_USAGE
 	free = stack_not_used(p);
+	unsafe_free = unsafe_stack_not_used(p);
 #endif
 	ppid = 0;
 	rcu_read_lock();
 	if (pid_alive(p))
 		ppid = task_pid_nr(rcu_dereference(p->real_parent));
 	rcu_read_unlock();
-	printk(KERN_CONT "%5lu %5d %6d 0x%08lx\n", free,
-		task_pid_nr(p), ppid,
-		(unsigned long)task_thread_info(p)->flags);
+	if (IS_ENABLED(CONFIG_SAFESTACK))
+		printk(KERN_CONT "%5lu %5lu %5d %6d 0x%08lx\n", free, unsafe_free,
+			task_pid_nr(p), ppid,
+			(unsigned long)task_thread_info(p)->flags);
+	else
+		printk(KERN_CONT "%5lu %5d %6d 0x%08lx\n", free,
+			task_pid_nr(p), ppid,
+			(unsigned long)task_thread_info(p)->flags);
 
 	print_worker_info(KERN_INFO, p);
 	show_stack(p, NULL);
@@ -5318,12 +5326,22 @@ void show_state_filter(unsigned long state_filter)
 {
 	struct task_struct *g, *p;
 
+#ifdef CONFIG_SAFESTACK
+#if BITS_PER_LONG == 32
+	printk(KERN_INFO
+		"  task                PC stack   unsafe  pid father\n");
+#else
+	printk(KERN_INFO
+		"  task                        PC stack   unsafe  pid father\n");
+#endif
+#else
 #if BITS_PER_LONG == 32
 	printk(KERN_INFO
 		"  task                PC stack   pid father\n");
 #else
 	printk(KERN_INFO
 		"  task                        PC stack   pid father\n");
+#endif
 #endif
 	rcu_read_lock();
 	for_each_process_thread(g, p) {
