33ba71e4db9cf23870af4f34a0de394af8cf7b9c
Dmitry Shmidt <dimitrysh@google.com>
2018-03-07 10:44:02 -0800

    Allow task_struct comm field to be copied to bigger buffer
    Currently __get_task_comm() copies buf_size, therefore call
    __get_task_comm(buf, sizeof(buf), tsk) will copy from tsk->comm
    much more than it is needed. And original patch author
    restricted this by BUILD_BUG_ON(sizeof(buf) != TASK_COMM_LEN);
    
    Change-Id: Ie0f93a9799708af38c89d8bdaa04052f6b0d1eb1
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1558,8 +1558,8 @@ static inline void set_task_comm(struct task_struct *tsk, const char *from)
 
 extern char *__get_task_comm(char *to, size_t len, struct task_struct *tsk);
 #define get_task_comm(buf, tsk) ({			\
-	BUILD_BUG_ON(sizeof(buf) != TASK_COMM_LEN);	\
-	__get_task_comm(buf, sizeof(buf), tsk);		\
+	BUILD_BUG_ON(sizeof(buf) < sizeof(tsk->comm));	\
+	__get_task_comm(buf, sizeof(tsk->comm), tsk);	\
 })
 
 #ifdef CONFIG_SMP
