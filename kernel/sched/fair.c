8691b88b2fdc7d55268318b1d5f3f8e1fd5c2d49
Ionela Voinescu <ionela.voinescu@arm.com>
2018-03-19 18:19:12 +0000

    ANDROID: sched/fair: always start with the biggest CPU
    find_best_target iterates though all CPUs and tries to either maximise
    or minimise the capacity of the selected CPU based on the combination
    of prefer_idle and boosted information it is provided with.
    
    While the order in which CPUs are iterated though is no longer
    necessary, it is important to start with the biggest CPU in order to
    cater to the needs of one type of task: a prefer_idle and boosted task.
    For this task we have to implement a preference towards big reserved
    CPUs that will result in low wakeup latency and lower preemption
    probability for this type of tasks.
    
    Therefore, this code removes start_cpu and sets the start point always
    to the CPU with the highest capacity_orig. Given that for other cases
    we already iterate though all CPU to favour the most energy efficient
    ones, always starting with the biggest CPU will not impact the result.
    
    Change-Id: I2ac036226c21855e28e8b065f7182f2eadaa0004
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Reviewed-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6875,16 +6875,10 @@ static inline int task_fits_capacity(struct task_struct *p, long capacity)
 	return capacity * 1024 > boosted_task_util(p) * capacity_margin;
 }
 
-static int start_cpu(bool boosted)
-{
-	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
-
-	return boosted ? rd->max_cap_orig_cpu : rd->min_cap_orig_cpu;
-}
-
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				   bool boosted, bool prefer_idle)
 {
+	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
 	unsigned long min_util = boosted_task_util(p);
 	unsigned long target_capacity = ULONG_MAX;
 	unsigned long min_wake_util = ULONG_MAX;
@@ -6913,8 +6907,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	if (prefer_idle && boosted)
 		target_capacity = 0;
 
-	/* Find start CPU based on boost value */
-	cpu = start_cpu(boosted);
+	/*
+	 * Start from the first maximum capacity CPU.
+	 * This will increase the chances to select a reserved CPU for
+	 * prefer_idle and boosted tasks while not impacting the selection
+	 * of the most energy efficient CPUs in the other cases.
+	 */
+	cpu = rd->max_cap_orig_cpu;
 	if (cpu < 0)
 		return -1;
 
