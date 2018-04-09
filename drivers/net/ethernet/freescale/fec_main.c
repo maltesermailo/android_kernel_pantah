39525d69c2d16134297b26c3f57e582e8ff0d19c
Amit Pundir <amit.pundir@linaro.org>
2018-04-09 13:32:30 +0530

    net: fec: Fix implicit declaration of pm_runtime_{put,disable}()
    Include linux/pm_runtime.h to declare pm_runtime_put() and
    pm_runtime_disable() functions.
    
    Build breakage reported at kernelci:
    https://kernelci.org/build/android/branch/android-3.18/kernel/ASB-2018-04-05_3.18-96-gc1d1d0d82a3a/
    
    Fixes: linux-3.18.y commit f4d8124a4ade ("net: fec: Fix unbalanced PM runtime calls")
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -24,6 +24,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
+#include <linux/pm_runtime.h>
 #include <linux/ptrace.h>
 #include <linux/errno.h>
 #include <linux/ioport.h>
