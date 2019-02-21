95a406b7230119b345abad87699f670b09c89a4d
Patrick Bellasi <patrick.bellasi@arm.com>
2019-02-21 16:45:10 +0000

    ANDROID: sched/fair: Add uclamp support to select_max_spare_cap_cpus()
    Bug: 120440300
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: I02a4d6981de340560dcf1a788f0ede045fe32f62

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6976,6 +6976,10 @@ static void select_cpu_candidates(struct sched_domain *sd, cpumask_t *cpus,
 	unsigned int min_exit_lat = UINT_MAX;
 	int cpu, max_spare_cap_cpu;
 	struct cpuidle_state *idle;
+	unsigned long min_cap;
+
+	/* Minimum capacity required by the task */
+	min_cap = uclamp_task(p);
 
 	for (; pd; pd = pd->next) {
 		max_spare_cap_cpu = -1;
@@ -6985,10 +6989,13 @@ static void select_cpu_candidates(struct sched_domain *sd, cpumask_t *cpus,
 			if (!cpumask_test_cpu(cpu, &p->cpus_allowed))
 				continue;
 
-			/* Skip CPUs that will be overutilized. */
+			/*
+			 * Skip CPUs that will be overutilized or not
+			 * providing the min capacity required by the task.
+			 */
 			util = cpu_util_next(cpu, p, cpu);
 			cpu_cap = capacity_of(cpu);
-			if (cpu_cap * 1024 < util * capacity_margin)
+			if (cpu_cap * 1024 < max(min_cap, util) * capacity_margin)
 				continue;
 
 			/*
