24f75bb72bc31606c2f3d52befb1efbc0beccb90
Yiwei Zhang <zzyiwei@google.com>
2019-08-23 13:49:12 -0700

    [GPU TRACE] Add GPU sched trace points
    This change adds the below gpu scheduling trace points:
    gpu_sched_enqueue
    gpu_sched_submit
    gpu_sched_complete
    
    Bug: 139323730
    Test: insert into kmd and enable ftrace tracing to verify
    Signed-off-by: Yiwei Zhang <zzyiwei@google.com>
    Change-Id: I9190fe872f811b5e4d7a18269a7a27de79a13e12

---
+++ kernel/trace/gpu-traces.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * GPU trace points
+ *
+ * Copyright (C) 2013-2019 Google, Inc.
+ */
+
+#include <linux/module.h>
+
+#define CREATE_TRACE_POINTS
+#include <trace/events/gpu.h>
+
+EXPORT_TRACEPOINT_SYMBOL(gpu_sched_enqueue);
+EXPORT_TRACEPOINT_SYMBOL(gpu_sched_submit);
+EXPORT_TRACEPOINT_SYMBOL(gpu_sched_complete);
