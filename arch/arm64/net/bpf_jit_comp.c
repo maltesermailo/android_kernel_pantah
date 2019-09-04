70468cb0e6a61fe3451f4752fe718d3649dd7039
Sami Tolvanen <samitolvanen@google.com>
2019-09-04 14:56:40 -0700

    ANDROID: arm64: bpf: validate bpf_func with CFI_CLANG
    Validate that the pointer to a jited function points to a correctly
    aligned address in the correct region before calling it.
    
    Change-Id: I10c448eda6a8b0bf4c16ee591fc65974696216b9
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -949,3 +949,22 @@ out:
 					   tmp : orig_prog);
 	return prog;
 }
+
+#ifdef CONFIG_CFI_CLANG
+bool arch_bpf_jit_check_func(const struct bpf_prog *prog)
+{
+	const uintptr_t func = (const uintptr_t)prog->bpf_func;
+
+	/*
+	 * bpf_func must be correctly aligned and within the correct region.
+	 * module_alloc places JIT code in the module region, unless
+	 * ARM64_MODULE_PLTS is enabled, in which case we might end up using
+	 * the vmalloc region too.
+	 */
+	return (((func >= MODULES_VADDR &&
+                  func <  MODULES_END)  ||
+		 (IS_ENABLED(CONFIG_ARM64_MODULE_PLTS) &&
+		  is_vmalloc_addr(prog->bpf_func))) &&
+                IS_ALIGNED(func, sizeof(u32)));
+}
+#endif
