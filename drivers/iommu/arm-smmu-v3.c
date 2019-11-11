be3ccc16e2f388e129d021a170428c9c15908117
Will Deacon <will@kernel.org>
2019-11-11 12:43:19 +0000

    FROMLIST: iommu/arm-smmu: Update my email address in MODULE_AUTHOR()
    I no longer work for Arm, so update the stale reference to my old email
    address.
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Bug: 140290589
    Link: https://lore.kernel.org/lkml/20191121114918.2293-15-will@kernel.org/
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 143513877
    Change-Id: I7676e5f736fcb31376444bbfee419bc49ef17287

--- drivers/iommu/arm-smmu-v3.c
+++ drivers/iommu/arm-smmu-v3.c
@@ -2940,5 +2940,5 @@ static struct platform_driver arm_smmu_driver = {
 module_platform_driver(arm_smmu_driver);
 
 MODULE_DESCRIPTION("IOMMU API for ARM architected SMMUv3 implementations");
-MODULE_AUTHOR("Will Deacon <will.deacon@arm.com>");
+MODULE_AUTHOR("Will Deacon <will@kernel.org>");
 MODULE_LICENSE("GPL v2");
