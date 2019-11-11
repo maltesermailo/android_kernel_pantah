434812e747a9b7225d00d2e72f0f3cfcb779d0a4
Will Deacon <will@kernel.org>
2019-11-11 12:16:40 +0000

    FROMLIST: drivers/iommu: Allow IOMMU bus ops to be unregistered
    'bus_set_iommu()' allows IOMMU drivers to register their ops for a given
    bus type. Unfortunately, it then doesn't allow them to be removed, which
    is necessary for modular drivers to shutdown cleanly so that they can be
    reloaded later on.
    
    Allow 'bus_set_iommu()' to take a NULL 'ops' argument, which clear the
    ops pointer for the selected bus_type.
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Bug: 140290589
    Link: https://lore.kernel.org/lkml/20191121114918.2293-7-will@kernel.org/
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: Ia89fa2df61d2a9ad04901d4c2e71e293c4c6171d

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1304,6 +1304,11 @@ int bus_set_iommu(struct bus_type *bus, const struct iommu_ops *ops)
 {
 	int err;
 
+	if (ops == NULL) {
+		bus->iommu_ops = NULL;
+		return 0;
+	}
+
 	if (bus->iommu_ops != NULL)
 		return -EBUSY;
 
