ad03c5541b200185ae9b18b8d034f652ec008f59
chenqiwu <chenqiwu@xiaomi.com>
2019-12-11 19:06:45 +0800

    kernel/exit: do panic earlier to get coredump if global init task exit
    If global init task get a chance to be killed, panic will happen in later
    call steps by do_exit()->exit_notify()->forget_original_parent()->
    find_child_reaper() when all threads have exited. However, we cannot extract
    the init task's coredump from a kernel crashdump since exit_mm() has
    released mm before panic. For getting the backtrace of init in userspace,
    it's better to do panic earlier at the beginning of init's exitting route.
    It's worth noting that we must take case of a multi-threaded init exitting issue.
    
    Signed-off-by: chenqiwu <chenqiwu@xiaomi.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -577,10 +577,6 @@ static struct task_struct *find_child_reaper(struct task_struct *father,
 	}
 
 	write_unlock_irq(&tasklist_lock);
-	if (unlikely(pid_ns == &init_pid_ns)) {
-		panic("Attempted to kill init! exitcode=0x%08x\n",
-			father->signal->group_exit_code ?: father->exit_code);
-	}
 
 	list_for_each_entry_safe(p, n, dead, ptrace_entry) {
 		list_del_init(&p->ptrace_entry);
@@ -783,6 +779,9 @@ void __noreturn do_exit(long code)
 		panic("Aiee, killing interrupt handler!");
 	if (unlikely(!tsk->pid))
 		panic("Attempted to kill the idle task!");
+	if (unlikely(is_global_init(tsk) &&
+		(signal_group_exit(tsk->signal) || thread_group_empty(tsk))))
+		panic("Attempted to kill init! exitcode=0x%08lx\n", code);
 
 	/*
 	 * If do_exit is called because this processes oopsed, it's possible
