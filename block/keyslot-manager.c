48b3f68fdaaf43ff51f0d8b416540f61b34eb5aa
Barani Muthukumaran <bmuthuku@codeaurora.org>
2020-01-02 12:01:34 -0800

    ANDROID: Add KSM op to get raw secret from wrapped keys
    Some Inline Crypto Engine  implementations support the
    ability to protect keys from software by generating keys
    in hardware and returning a wrapped key to software. This
    allows all file data to be encrypted with keys protected
    by hardware.
    
    A new API is added to KSM ops to allow Fscrypt / other
    use cases of KSM to get a raw secret to be used by software.
    The API uses a different pre-fixed context to derive the
    secret ensuring the key used to encrypt / decrypt data is
    derived using a different derivative.
    
    Bug: 147209885
    
    Signed-off-by: Gaurav Kashyap <gaurkash@codeaurora.org>
    Signed-off-by: Barani Muthukumaran <bmuthuku@codeaurora.org>
    Change-Id: I32f3aa4f25bcf6b9d6f7d8890260533fad00dd1d

--- block/keyslot-manager.c
+++ block/keyslot-manager.c
@@ -424,3 +424,22 @@ void keyslot_manager_destroy(struct keyslot_manager *ksm)
 	}
 }
 EXPORT_SYMBOL_GPL(keyslot_manager_destroy);
+
+int keyslot_manager_get_raw_secret(struct keyslot_manager *ksm,
+				   const u8 *key, unsigned int key_size,
+				   u8 *secret, unsigned int secret_size)
+{
+	int err = 0;
+
+	down_write(&ksm->lock);
+	if (ksm->ksm_ll_ops.get_raw_secret) {
+		err = ksm->ksm_ll_ops.get_raw_secret(ksm, key, key_size,
+						     secret, secret_size);
+	} else {
+		err = -ENOTSUPP;
+	}
+	up_write(&ksm->lock);
+
+	return err;
+}
+EXPORT_SYMBOL_GPL(keyslot_manager_get_raw_secret);
