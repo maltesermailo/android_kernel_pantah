192ecb6398e8bb6887ed369f7134a809a959a1b4
Badhri Jagan Sridharan <badhri@google.com>
2020-02-06 17:30:16 -0800

    ANDROID: usb: typec: tcpci: Add callback to notify of pd capable partner
    Chip drivers can use this callback to perform board specific
    operations, for instance, input current arbitration.
    
    Bug: 138685578
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    (cherry picked from commit 05a6c0e936ff3b56718ed99b223d3115e4e8fead)
    (cherry picked from commit b532bf8c7ad7a36897de54917d90bcbc1a3876d6)
    (cherry picked from commit f001d3b5a84bd2487ca65472f883db7c7388f696)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I3d49dc99fca1a26f8212f1a4df728b1debc6cd87

--- drivers/usb/typec/tcpm/tcpci.c
+++ drivers/usb/typec/tcpm/tcpci.c
@@ -471,6 +471,14 @@ static int tcpci_set_current_limit(struct tcpc_dev *tcpc, u32 max_ma, u32 mv)
 	return ret;
 }
 
+static void tcpci_set_pd_capable(struct tcpc_dev *tcpc, bool capable)
+{
+	struct tcpci *tcpci = tcpc_to_tcpci(tcpc);
+
+	if (tcpci->data->set_pd_capable)
+		tcpci->data->set_pd_capable(tcpci, tcpci->data, capable);
+}
+
 static int tcpci_set_vbus(struct tcpc_dev *tcpc, bool source, bool sink)
 {
 	struct tcpci *tcpci = tcpc_to_tcpci(tcpc);
@@ -778,6 +786,7 @@ struct tcpci *tcpci_register_port(struct device *dev, struct tcpci_data *data)
 	tcpci->tcpc.set_partner_usb_comm_capable = tcpci_set_partner_usb_comm_capable;
 	tcpci->tcpc.check_contaminant = tcpci_check_contaminant;
 	tcpci->tcpc.set_current_limit = tcpci_set_current_limit;
+	tcpci->tcpc.set_pd_capable = tcpci_set_pd_capable;
 
 	if (tcpci->data->auto_discharge_disconnect) {
 		tcpci->tcpc.enable_auto_vbus_discharge = tcpci_enable_auto_vbus_discharge;
