4fa98a507a042f5cbe8ec2b1489c6aff850f4052
Quentin Perret <qperret@google.com>
2020-02-12 20:21:39 +0000

    BACKPORT: FROMLIST: kbuild: split adjust_autoksyms.sh in two parts
    In order to prepare the ground for a build-time optimization, split
    adjust_autoksyms.sh into two scripts: one that generates autoksyms.h
    based on all currently available information (whitelist, and .mod
    files), and the other to inspect the diff between two versions of
    autoksyms.h and trigger appropriate rebuilds.
    
    [ qperret: 60ae1b194b45 not present, sed 3rd line of modules.order ]
    
    Bug: 148277666
    Link: https://lore.kernel.org/lkml/20200212202140.138092-3-qperret@google.com
    Change-Id: I7ed4f8f72c0ef9ac98419877efb5ef828d10b0d7
    Signed-off-by: Quentin Perret <qperret@google.com>

---
+++ scripts/gen_autoksyms.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+
+# Create an autoksyms.h header file from the list of all module's needed symbols
+# as recorded on the third line of *.mod files and the user-provided symbol
+# whitelist.
+
+set -e
+
+output_file="$1"
+
+# Use "make V=1" to debug this script.
+case "$KBUILD_VERBOSE" in
+*1*)
+	set -x
+	;;
+esac
+
+# We need access to CONFIG_ symbols
+. include/config/auto.conf
+
+# Use 'eval' to expand the whitelist path and check if it is relative
+eval ksym_wl="${CONFIG_UNUSED_KSYMS_WHITELIST:-/dev/null}"
+[ "${ksym_wl:0:1}" = "/" ] || ksym_wl="$abs_srctree/$ksym_wl"
+
+# Generate a new ksym list file with symbols needed by the current
+# set of modules.
+cat > "$output_file" << EOT
+/*
+ * Automatically generated file; DO NOT EDIT.
+ */
+
+EOT
+
+sed 's/ko$/mod/' modules.order |
+xargs -n1 sed -n -e '3{s/ /\n/g;/^$/!p;}' -- |
+cat - "$ksym_wl" |
+sort -u |
+sed -e 's/\(.*\)/#define __KSYM_\1 1/' >> "$output_file"
+
+# Special case for modversions (see modpost.c)
+if [ -n "$CONFIG_MODVERSIONS" ]; then
+	echo "#define __KSYM_module_layout 1" >> "$output_file"
+fi
