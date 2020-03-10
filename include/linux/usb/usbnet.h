4a2801028711ea34f7f73ee494db0521917d8d4a
Greg Kroah-Hartman <gregkh@google.com>
2020-03-10 18:16:26 +0100

    ANDROID: GKI: USB: add Android ABI padding to some structures
    To try to mitigate potential future USB api changes, add some padding to
    the following structures:
    	struct usb_interface
    	struct usb_host_bos
    	struct usb_bus
    	struct usb_device
    	struct usb_driver
    	struct urb
    	struct usb_hcd
    	struct hc_driver
    	struct usb_tt
    	struct usbnet
    	struct driver_info (for usbnet driver)
    
    Based on a patch from Oliver Neukum <oneukum@suse.de> from the SLES
    kernel.
    
    Bug: 151154716
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I382243c680cf946995de195bc445846c97c6b88d

--- include/linux/usb/usbnet.h
+++ include/linux/usb/usbnet.h
@@ -83,6 +83,11 @@ struct usbnet {
 #		define EVENT_LINK_CHANGE	11
 #		define EVENT_SET_RX_MODE	12
 #		define EVENT_NO_IP_ALIGN	13
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 };
 
 static inline struct usb_driver *driver_of(struct usb_interface *intf)
@@ -172,6 +177,9 @@ struct driver_info {
 	int		out;		/* tx endpoint */
 
 	unsigned long	data;		/* Misc driver specific data */
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
 };
 
 /* Minidrivers are just drivers using the "usbnet" core as a powerful
