b5d7d924a5b506533dbbc053fc457c9dda74bc5a
Richard Liu <xuegang.liu@nxp.com>
2020-03-20 15:42:01 +0000

    staging: android: ion: Flush cache after zero CMA allocated memory
    ION CMA memory default is cacheable, need flush cache after memset(),
    else cache and physical memory not sync may cause problem.
    
    Issue case:
    VPU Video playback or GPU render have dirty line issue.
    
    Root cause:
    ION CMA allocate cacheable buffer and do memset(), some data still in cache
    not in physical memory, VPU or GPU write the buffer with physical address,
    or user call ion_mmap() to map the buffer through pgprot_writecombine() as
    no-cache and write the buffer, later some CPU cache access trigger cache
    flush, previous memset() data go to physical memory as dirty data.
    
    Change-Id: I82b4cb61bbe6cffc687d452f9f81c1e35914d2f1
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- drivers/staging/android/ion/ion_cma_heap.c
+++ drivers/staging/android/ion/ion_cma_heap.c
@@ -13,6 +13,10 @@
 #include <linux/cma.h>
 #include <linux/scatterlist.h>
 #include <linux/highmem.h>
+#include <asm/cacheflush.h>
+#ifdef CONFIG_ARM
+#include <asm/outercache.h>
+#endif
 
 #include "ion.h"
 
@@ -51,12 +55,29 @@ static int ion_cma_allocate(struct ion_heap *heap, struct ion_buffer *buffer,
 			void *vaddr = kmap_atomic(page);
 
 			memset(vaddr, 0, PAGE_SIZE);
+#ifdef CONFIG_ARM
+			__cpuc_flush_dcache_area(vaddr,PAGE_SIZE);
+#else
+			__flush_dcache_area(vaddr,PAGE_SIZE);
+#endif
 			kunmap_atomic(vaddr);
 			page++;
 			nr_clear_pages--;
 		}
+#ifdef CONFIG_ARM
+		phys_addr_t base = __pfn_to_phys(page_to_pfn(pages));
+		phys_addr_t end = base + size;
+		outer_flush_range(base, end);
+#endif
 	} else {
 		memset(page_address(pages), 0, size);
+#ifdef CONFIG_ARM
+		void *ptr = page_address(pages);
+		__cpuc_flush_dcache_area(page_address(pages),size);
+		outer_flush_range(__pa(ptr), __pa(ptr) + size);
+#else
+		__flush_dcache_area(page_address(pages),size);
+#endif
 	}
 
 	table = kmalloc(sizeof(*table), GFP_KERNEL);
