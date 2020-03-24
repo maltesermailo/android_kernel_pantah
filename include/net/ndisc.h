65afa24a487018ffe71d1f86568d0858b504da4d
Maciej Żenczykowski <maze@google.com>
2020-03-23 18:10:19 -0700

    UPSTREAM: ipv6: ndisc: add support for 'PREF64' dns64 prefix identifier
    This is trivial since we already have support for the entirely
    identical (from the kernel's point of view) RDNSS, DNSSL, etc. that
    also contain opaque data that needs to be passed down to userspace
    for further processing.
    
    As specified in draft-ietf-6man-ra-pref64-09 (while it is still a draft,
    it is purely waiting on the RFC Editor for cleanups and publishing):
      PREF64 option contains lifetime and a (up to) 96-bit IPv6 prefix.
    
    The 8-bit identifier of the option type as assigned by the IANA is 38.
    
    Since we lack DNS64/NAT64/CLAT support in kernel at the moment,
    thus this option should also be passed on to userland.
    
    See:
      https://tools.ietf.org/html/draft-ietf-6man-ra-pref64-09
      https://www.iana.org/assignments/icmpv6-parameters/icmpv6-parameters.xhtml#icmpv6-parameters-5
    
    Cc: Erik Kline <ek@google.com>
    Cc: Jen Linkova <furry@google.com>
    Cc: Lorenzo Colitti <lorenzo@google.com>
    Cc: Michael Haro <mharo@google.com>
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Acked-By: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c24a77edc9a7ac9b5fea75407f197fe1469262f4)
    Bug: 150648313
    Change-Id: I02bff2103194a8171f907e82f811d7ab66962138

--- include/net/ndisc.h
+++ include/net/ndisc.h
@@ -41,9 +41,9 @@ enum {
 	ND_OPT_DNSSL = 31,		/* RFC6106 */
 	ND_OPT_6CO = 34,		/* RFC6775 */
 	ND_OPT_CAPTIVE_PORTAL = 37,	/* RFC7710 */
+	ND_OPT_PREF64 = 38,		/* RFC-ietf-6man-ra-pref64-09 */
 	__ND_OPT_MAX
 };
-
 #define MAX_RTR_SOLICITATION_DELAY	HZ
 
 #define ND_REACHABLE_TIME		(30*HZ)
