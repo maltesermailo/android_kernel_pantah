536264db83fea77c6105ced959f5ef1ae9808c4f
Satya Tangirala <satyat@google.com>
2020-03-31 18:36:39 -0700

    ANDROID: scsi: ufs: remove some unnecessary crypto variant ops
    Remove crypto_debug since ufshcd_vops_dbg_register_dump is another
    variant op that's called in ufshcd.c right before the call the
    ufshcd_crypto_debug anyway.
    
    Remove crypto_set_vops since it hasn't been exported yet, and no driver
    can be using yet, and drivers can simply set the crypto_vops directly.
    
    Remove setup_rq_keyslot_manager and destroy_keyslot_manager since
    they shouldn't need any non-standard behaviour.
    
    Remove ufshcd_complete_lrbp_crypto since nobody is known to be using it now/
    in the near future.
    
    Bug: 137270441
    Change-Id: I466db2ab75c134c2203036ff9d312bcccf9bf9cf
    Signed-off-by: Satya Tangirala <satyat@google.com>

--- drivers/scsi/ufs/ufshcd-crypto.h
+++ drivers/scsi/ufs/ufshcd-crypto.h
@@ -30,20 +30,15 @@ struct blk_ksm_ll_ops;
 int ufshcd_hba_init_crypto_spec(struct ufs_hba *hba,
 				const struct blk_ksm_ll_ops *ksm_ops);
 
-void ufshcd_crypto_setup_rq_keyslot_manager_spec(struct ufs_hba *hba,
-						 struct request_queue *q);
+void ufshcd_crypto_setup_rq_keyslot_manager(struct ufs_hba *hba,
+					    struct request_queue *q);
 
-void ufshcd_crypto_destroy_keyslot_manager_spec(struct ufs_hba *hba);
+void ufshcd_crypto_destroy_keyslot_manager(struct ufs_hba *hba);
 
 /* Crypto Variant Ops Support */
 bool ufshcd_crypto_enable(struct ufs_hba *hba);
 
 int ufshcd_hba_init_crypto(struct ufs_hba *hba);
-
-void ufshcd_crypto_setup_rq_keyslot_manager(struct ufs_hba *hba,
-					    struct request_queue *q);
-
-void ufshcd_crypto_destroy_keyslot_manager(struct ufs_hba *hba);
 
 void ufshcd_prepare_lrbp_crypto(struct ufs_hba *hba,
 				struct scsi_cmnd *cmd,
@@ -51,18 +46,9 @@ void ufshcd_prepare_lrbp_crypto(struct ufs_hba *hba,
 
 int ufshcd_map_sg_crypto(struct ufs_hba *hba, struct ufshcd_lrb *lrbp);
 
-int ufshcd_complete_lrbp_crypto(struct ufs_hba *hba,
-				struct scsi_cmnd *cmd,
-				struct ufshcd_lrb *lrbp);
-
-void ufshcd_crypto_debug(struct ufs_hba *hba);
-
 int ufshcd_crypto_suspend(struct ufs_hba *hba, enum ufs_pm_op pm_op);
 
 int ufshcd_crypto_resume(struct ufs_hba *hba, enum ufs_pm_op pm_op);
-
-void ufshcd_crypto_set_vops(struct ufs_hba *hba,
-			    struct ufs_hba_crypto_variant_ops *crypto_vops);
 
 #else /* CONFIG_SCSI_UFS_CRYPTO */
 
@@ -92,8 +78,6 @@ static inline int ufshcd_map_sg_crypto(struct ufs_hba *hba,
 	return 0;
 }
 
-static inline void ufshcd_crypto_debug(struct ufs_hba *hba) { }
-
 static inline int ufshcd_crypto_suspend(struct ufs_hba *hba,
 					enum ufs_pm_op pm_op)
 {
@@ -105,9 +89,6 @@ static inline int ufshcd_crypto_resume(struct ufs_hba *hba,
 {
 	return 0;
 }
-
-static inline void ufshcd_crypto_set_vops(struct ufs_hba *hba,
-			struct ufs_hba_crypto_variant_ops *crypto_vops) { }
 
 #endif /* CONFIG_SCSI_UFS_CRYPTO */
 
