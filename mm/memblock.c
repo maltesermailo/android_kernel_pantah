f0e686d804a025d5b48db51c1b18d47c7dbfe3bd
Suren Baghdasaryan <surenb@google.com>
2020-05-28 16:10:58 -0700

    WIP: mm/page_alloc: export memblock_{start|end}_of_DRAM
    Export memblock_start_of_DRAM and memblock_end_of_DRAM functions
    to allow drivers to query memory boundaries.
    
    Bug: 149906776
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I4086898d1b02925963248be9babfe70b87069be1

--- mm/memblock.c
+++ mm/memblock.c
@@ -1616,6 +1616,7 @@ phys_addr_t __init_memblock memblock_start_of_DRAM(void)
 {
 	return memblock.memory.regions[0].base;
 }
+EXPORT_SYMBOL_GPL(memblock_start_of_DRAM);
 
 phys_addr_t __init_memblock memblock_end_of_DRAM(void)
 {
@@ -1623,6 +1624,7 @@ phys_addr_t __init_memblock memblock_end_of_DRAM(void)
 
 	return (memblock.memory.regions[idx].base + memblock.memory.regions[idx].size);
 }
+EXPORT_SYMBOL_GPL(memblock_end_of_DRAM);
 
 static phys_addr_t __init_memblock __find_max_addr(phys_addr_t limit)
 {
