c9ac11f44278f5f6626c6a5a589503feebfc5130
Saravana Kannan <saravanak@google.com>
2020-05-28 09:44:45 -0700

    FROMLIST: driver core: Add dev_set_drv_sync_state()
    This can be used by frameworks to set the sync_state() helper functions
    for drivers that don't already have them set.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Bug: 171338861
    Link: https://lore.kernel.org/lkml/20200716042053.1927676-1-saravanak@google.com/
    Change-Id: I943d2532629aeb63c9650c74bb9dbdbac30994fb

--- include/linux/device.h
+++ include/linux/device.h
@@ -823,6 +823,18 @@ static inline bool dev_has_sync_state(struct device *dev)
 	return false;
 }
 
+static inline int dev_set_drv_sync_state(struct device *dev,
+					 void (*fn)(struct device *dev))
+{
+	if (!dev || !dev->driver)
+		return 0;
+	if (dev->driver->sync_state && dev->driver->sync_state != fn)
+		return -EBUSY;
+	if (!dev->driver->sync_state)
+		dev->driver->sync_state = fn;
+	return 0;
+}
+
 /*
  * High level routines for use by the bus drivers
  */
