14b7ab378cef3be9905b7cdc6bf169ca905e09fc
JianMin Liu <jian-min.liu@mediatek.com>
2020-07-21 12:13:35 +0800

    ANDROID: binder: Add vendor hook to set and restore vendor data
    To inherit some vendor data when binder_set_priority, and restore
    vendor data when binder_restore_priority. Before binder_set_priority,
    the orginal data will save into ANDROID_VENDOR_DATA field of
    binder_transaction.
    
    Bug: 319399115
    Bug: 461931955
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>
    Signed-off-by: Guanwun Chen <guan-wun.chen@mediatek.com>
    Change-Id: If60870623ce2669200238172737dd8455ac34b02
    [guochen: remove the unnecessary parameter 'current' in
    android_vh_binder_restore_priority and update the commit message]
    Signed-off-by: Guo Chen <guo.chen@oss.qualcomm.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -10,6 +10,7 @@
 #include <trace/hooks/vendor_hooks.h>
 #include <linux/tracepoint.h>
 
+#include <trace/hooks/binder.h>
 #include <trace/hooks/cpuidle.h>
 #include <trace/hooks/mpam.h>
 #include <trace/hooks/wqlockup.h>
@@ -47,6 +48,8 @@
  */
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_send_sig_info);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_arch_set_freq_scale);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_set_priority);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_restore_priority);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_idle_enter);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_idle_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mpam_set);
