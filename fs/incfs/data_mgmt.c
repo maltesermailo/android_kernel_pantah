9d9c6d47e1a84aacc6f5002972a9032d8656bcfa
Paul Lawrence <paullawrence@google.com>
2020-08-12 15:12:47 -0700

    ANDROID: Incremental fs: Store fs-verity state in backing file
    Now fsverity state is preserved across inode eviction.
    
    Added incfs.verity xattr to track when a file is fs-verity enabled.
    
    Bug: 160634504
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I41d90abd55527884d9eff642c9834ad837ff6918

--- fs/incfs/data_mgmt.c
+++ fs/incfs/data_mgmt.c
@@ -5,6 +5,7 @@
 #include <linux/crc32.h>
 #include <linux/delay.h>
 #include <linux/file.h>
+#include <linux/fsverity.h>
 #include <linux/gfp.h>
 #include <linux/ktime.h>
 #include <linux/lz4.h>
@@ -18,6 +19,7 @@
 #include "data_mgmt.h"
 #include "format.h"
 #include "integrity.h"
+#include "verity.h"
 
 static int incfs_scan_metadata_chain(struct data_file *df);
 
@@ -260,6 +262,8 @@ struct data_file *incfs_open_data_file(struct mount_info *mi, struct file *bf)
 		goto out;
 	}
 
+	mutex_init(&df->df_enable_verity);
+
 	df->df_backing_file_context = bfc;
 	df->df_mount_info = mi;
 	for (i = 0; i < ARRAY_SIZE(df->df_segments); i++)
@@ -330,6 +334,8 @@ void incfs_free_data_file(struct data_file *df)
 	incfs_free_bfc(df->df_backing_file_context);
 	kfree(df->df_signature);
 	kfree(df->df_verity_file_digest.data);
+	kfree(df->df_verity_signature);
+	mutex_destroy(&df->df_enable_verity);
 	kfree(df);
 }
 
@@ -1468,7 +1474,31 @@ static int process_status_md(struct incfs_status *is,
 		   df->df_initial_hash_blocks_written);
 
 	df->df_status_offset = handler->md_record_offset;
+	return 0;
+}
 
+static int process_file_verity_signature_md(
+		struct incfs_file_verity_signature *vs,
+		struct metadata_handler *handler)
+{
+	struct data_file *df = handler->context;
+	struct incfs_df_verity_signature *verity_signature;
+
+	if (!df)
+		return -EFAULT;
+
+	verity_signature = kzalloc(sizeof(*verity_signature), GFP_NOFS);
+	if (!verity_signature)
+		return -ENOMEM;
+
+	verity_signature->offset = le64_to_cpu(vs->vs_offset);
+	verity_signature->size = le32_to_cpu(vs->vs_size);
+	if (verity_signature->size > FS_VERITY_MAX_SIGNATURE_SIZE) {
+		kfree(verity_signature);
+		return -EFAULT;
+	}
+
+	df->df_verity_signature = verity_signature;
 	return 0;
 }
 
@@ -1494,6 +1524,7 @@ static int incfs_scan_metadata_chain(struct data_file *df)
 	handler->handle_blockmap = process_blockmap_md;
 	handler->handle_signature = process_file_signature_md;
 	handler->handle_status = process_status_md;
+	handler->handle_verity_signature = process_file_verity_signature_md;
 
 	while (handler->md_record_offset > 0) {
 		error = incfs_read_next_metadata_record(bfc, handler);
