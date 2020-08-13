0059a6d6028e8503c0d4de83eddb3034dc5156eb
Yun Hsiang <yun.hsiang@mediatek.com>
2020-08-13 14:41:46 +0800

    ANDROID: sched: add vendor hook for correcting cpu capacity
    Current CPU capacity is scaled by frequency
    but it will sometime overestimate.
    Add restricted vendor hook to adjust cpu capacity.
    
    android_rvh_arch_set_freq_scale
    	Adjust cpu freq scale for each opp.
    
    Bug: 163935827
    
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    Change-Id: I054740a7f60301d5a3b085bfa540d52853f5eec5

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -13,6 +13,7 @@
 #include <trace/hooks/binder.h>
 #include <trace/hooks/rwsem.h>
 #include <trace/hooks/futex.h>
+#include <trace/hooks/topology.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -31,6 +32,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_prepare_prio_fork);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_finish_prio_fork);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_user_nice);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_setscheduler);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_arch_set_freq_scale);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_transaction_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_set_priority);
