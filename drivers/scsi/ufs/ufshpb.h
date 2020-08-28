9e4274fa58de482bdf7cd91694125a0c6ecf7fdc
Daejun Park <daejun7.park@samsung.com>
2020-08-28 15:01:07 +0900

    ANDROID: scsi: ufs: L2P map management for HPB read
    This is a patch for managing L2P map in HPB module.
    
    The HPB divides logical addresses into several regions. A region consists
    of several sub-regions. The sub-region is a basic unit where L2P mapping is
    managed. The driver loads L2P mapping data of each sub-region. The loaded
    sub-region is called active-state. The HPB driver unloads L2P mapping data
    as region unit. The unloaded region is called inactive-state.
    
    Sub-region/region candidates to be loaded and unloaded are delivered from
    the UFS device. The UFS device delivers the recommended active sub-region
    and inactivate region to the driver using sensedata.
    The HPB module performs L2P mapping management on the host through the
    delivered information.
    
    A pinned region is a pre-set regions on the UFS device that is always
    activate-state.
    
    The data structure for map data request and L2P map uses mempool API,
    minimizing allocation overhead while avoiding static allocation.
    
    The mininum size of the memory pool used in the HPB is implemented
    as a module parameter, so that it can be configurable by the user.
    
    To gurantee a minimum memory pool size of 4MB: ufshpb_host_map_kbytes=4096
    
    The map_work manages active/inactive by 2 "to-do" lists.
    Each hpb lun maintains 2 "to-do" lists:
      hpb->lh_inact_rgn - regions to be inactivated, and
      hpb->lh_act_srgn - subregions to be activated
    Those lists are maintained on IO completion.
    
    Bug: 170940265
    
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Acked-by: Avri Altman <Avri.Altman@wdc.com>
    Tested-by: Bean Huo <beanhuo@micron.com>
    Signed-off-by: Daejun Park <daejun7.park@samsung.com>
    Change-Id: I2bf39ee39b00e56d19e1db4f3b51346ca20b7fbf

--- drivers/scsi/ufs/ufshpb.h
+++ drivers/scsi/ufs/ufshpb.h
@@ -83,10 +83,19 @@ struct ufshpb_lu_info {
 	int max_active_rgns;
 };
 
+struct ufshpb_map_ctx {
+	struct page **m_page;
+	unsigned long *ppn_dirty;
+};
+
 struct ufshpb_subregion {
+	struct ufshpb_map_ctx *mctx;
 	enum HPB_SRGN_STATE srgn_state;
 	int rgn_idx;
 	int srgn_idx;
+
+	/* below information is used by rsp_list */
+	struct list_head list_act_srgn;
 };
 
 struct ufshpb_region {
@@ -94,6 +103,43 @@ struct ufshpb_region {
 	enum HPB_RGN_STATE rgn_state;
 	int rgn_idx;
 	int srgn_cnt;
+
+	/* below information is used by rsp_list */
+	struct list_head list_inact_rgn;
+
+	/* below information is used by lru */
+	struct list_head list_lru_rgn;
+};
+
+#define for_each_sub_region(rgn, i, srgn)				\
+	for ((i) = 0;							\
+	     ((i) < (rgn)->srgn_cnt) && ((srgn) = &(rgn)->srgn_tbl[i]); \
+	     (i)++)
+
+/**
+ * struct ufshpb_req - UFSHPB READ BUFFER (for caching map) request structure
+ * @req: block layer request for READ BUFFER
+ * @bio: bio for holding map page
+ * @hpb: ufshpb_lu structure that related to the L2P map
+ * @mctx: L2P map information
+ * @rgn_idx: target region index
+ * @srgn_idx: target sub-region index
+ * @lun: target logical unit number
+ */
+struct ufshpb_req {
+	struct request *req;
+	struct bio *bio;
+	struct ufshpb_lu *hpb;
+	struct ufshpb_map_ctx *mctx;
+
+	unsigned int rgn_idx;
+	unsigned int srgn_idx;
+};
+
+struct victim_select_info {
+	struct list_head lh_lru_rgn;
+	int max_lru_active_cnt; /* supported hpb #region - pinned #region */
+	atomic_t active_cnt;
 };
 
 struct ufshpb_stats {
@@ -113,6 +159,16 @@ struct ufshpb_lu {
 	spinlock_t hpb_state_lock;
 	atomic_t hpb_state; /* hpb_state_lock */
 
+	spinlock_t rsp_list_lock;
+	struct list_head lh_act_srgn; /* rsp_list_lock */
+	struct list_head lh_inact_rgn; /* rsp_list_lock */
+
+	/* cached L2P map management worker */
+	struct work_struct map_work;
+
+	/* for selecting victim */
+	struct victim_select_info lru_info;
+
 	/* pinned region information */
 	u32 lu_pinned_start;
 	u32 lu_pinned_end;
@@ -131,6 +187,9 @@ struct ufshpb_lu {
 
 	struct ufshpb_stats stats;
 
+	struct kmem_cache *map_req_cache;
+	struct kmem_cache *m_page_cache;
+
 	struct list_head list_hpb_lu;
 };
 
@@ -138,6 +197,7 @@ struct ufs_hba;
 struct ufshcd_lrb;
 
 #ifndef CONFIG_SCSI_UFS_HPB
+static void ufshpb_rsp_upiu(struct ufs_hba *hba, struct ufshcd_lrb *lrbp) {}
 static void ufshpb_resume(struct ufs_hba *hba) {}
 static void ufshpb_suspend(struct ufs_hba *hba) {}
 static void ufshpb_reset(struct ufs_hba *hba) {}
@@ -150,6 +210,7 @@ static bool ufshpb_is_allowed(struct ufs_hba *hba) { return false; }
 static void ufshpb_get_geo_info(struct ufs_hba *hba, u8 *geo_buf) {}
 static void ufshpb_get_dev_info(struct ufs_hba *hba, u8 *desc_buf) {}
 #else
+void ufshpb_rsp_upiu(struct ufs_hba *hba, struct ufshcd_lrb *lrbp);
 void ufshpb_resume(struct ufs_hba *hba);
 void ufshpb_suspend(struct ufs_hba *hba);
 void ufshpb_reset(struct ufs_hba *hba);
