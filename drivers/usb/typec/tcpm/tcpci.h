415594855bf33502e63cc272154707b1f334dc6a
Badhri Jagan Sridharan <badhri@google.com>
2020-09-10 18:31:30 -0700

    ANDROID: usb: typec: tcpci: Allow chip level driver to update vbus_present
    From TCPCI spec:
    "The TCPC shall report VBUS present when TCPC detects VBUS rises above
    4V. The TCPC shall report VBUS is not present when TCPC detects VBUS
    falls below 3.5V. The TCPC may report VBUS is not present if VBUS is
    between 3.5V and 4V."
    
    Between one implementation of TCPC and another, 3.5V < VBUS < 4V might
    or might not be reported as PRESENT. When VBUS < 4V is reported as
    absent, the link might be disconnected to early before vSinkDisconnect
    max(3.67V) is reached. Hence provide a chip specific callback for the
    tcpc chip driver override if needed.
    
    Bug: 168555244
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    (cherry picked from commit 6163f5070e0c5f0cc725457a1da37d68d51b41d0)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    (cherry picked from commit e97799e011d097638b1259b7d1b48e468b0c9584)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I0f4a3a9c08f6f068c33f9f89f6eda547c78cb937

--- drivers/usb/typec/tcpm/tcpci.h
+++ drivers/usb/typec/tcpm/tcpci.h
@@ -162,6 +162,16 @@ struct tcpci;
  *		Optional; Enables TCPC to autonously discharge vbus on disconnect.
  * @vbus_vsafe0v:
  *		optional; Set when TCPC can detect whether vbus is at VSAFE0V.
+ * @get_vbus:
+ *		Optional; From the tcpci spec, "The TCPC shall report VBUS present
+ *		when TCPC detects VBUS rises above 4V. The TCPC shall report VBUS is
+ *		not present when TCPC detects VBUS falls below 3.5V. The TCPC may report
+ *		VBUS is not present if VBUS is between 3.5V and 4V." Between one
+ *		implementation of TCPC and another, 3.5V < VBUS < 4V might or
+ *		might not be reported as PRESENT. When VBUS < 4V is reported as
+ *		absent, the link might be disconnected to early before
+ *		vSinkDisconnect max(3.67V) is reached. Hence provide a chip
+ *		specific callback for the tcpc chip driver override if needed.
  */
 struct tcpci_data {
 	struct regmap *regmap;
@@ -177,6 +187,7 @@ struct tcpci_data {
 				  enum typec_cc_status cc);
 	int (*set_vbus)(struct tcpci *tcpci, struct tcpci_data *data, bool source, bool sink);
 	void (*frs_sourcing_vbus)(struct tcpci *tcpci, struct tcpci_data *data);
+	int (*get_vbus)(struct tcpci *tcpci, struct tcpci_data *data);
 	int (*set_roles)(struct tcpci *tcpci, struct tcpci_data *data, bool attached,
 			 enum typec_role role, enum typec_data_role data_role,
 			 bool usb_comm_capable);
