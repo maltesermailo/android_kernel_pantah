2f3f8a737353c6db026274b9e096130a3b6f01bb
Badhri Jagan Sridharan <badhri@google.com>
2020-09-14 16:36:24 -0700

    ANDROID: usb: typec: tcpci: Add chip level callbacks for contaminant detection
    Bug: 168544734
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    (cherry picked from commit 5ab0a8976ef272753d2b54d3862b05db1cb83b16)
    (cherry picked from commit 675a2e0dc8a03fac77e52e6e5270ce587d709e36)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    (cherry picked from commit f669edb6d752c698e953a8f69be645d6d5521b45)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I5d955121a9f5454e1e08f045adf5c496aab3da83

--- drivers/usb/typec/tcpm/tcpci.c
+++ drivers/usb/typec/tcpm/tcpci.c
@@ -488,6 +488,16 @@ static void tcpci_set_pd_capable(struct tcpc_dev *tcpc, bool capable)
 		tcpci->data->set_pd_capable(tcpci, tcpci->data, capable);
 }
 
+static int tcpci_check_contaminant(struct tcpc_dev *tcpc)
+{
+	struct tcpci *tcpci = tcpc_to_tcpci(tcpc);
+
+	if (tcpci->data->check_contaminant)
+		return tcpci->data->check_contaminant(tcpci, tcpci->data);
+
+	return 0;
+}
+
 static int tcpci_set_vbus(struct tcpc_dev *tcpc, bool source, bool sink)
 {
 	struct tcpci *tcpci = tcpc_to_tcpci(tcpc);
@@ -812,6 +822,7 @@ struct tcpci *tcpci_register_port(struct device *dev, struct tcpci_data *data)
 	tcpci->tcpc.set_partner_usb_comm_capable = tcpci_set_partner_usb_comm_capable;
 	tcpci->tcpc.set_current_limit = tcpci_set_current_limit;
 	tcpci->tcpc.set_pd_capable = tcpci_set_pd_capable;
+	tcpci->tcpc.check_contaminant = tcpci_check_contaminant;
 
 	if (tcpci->data->vbus_vsafe0v)
 		tcpci->tcpc.is_vbus_vsafe0v = tcpci_is_vbus_vsafe0v;
@@ -827,6 +838,15 @@ struct tcpci *tcpci_register_port(struct device *dev, struct tcpci_data *data)
 	return tcpci;
 }
 EXPORT_SYMBOL_GPL(tcpci_register_port);
+
+bool tcpci_is_debouncing(struct tcpci *tcpci)
+{
+	if (tcpci && tcpci->port)
+		return tcpm_is_debouncing(tcpci->port);
+
+	return false;
+}
+EXPORT_SYMBOL_GPL(tcpci_is_debouncing);
 
 void tcpci_unregister_port(struct tcpci *tcpci)
 {
