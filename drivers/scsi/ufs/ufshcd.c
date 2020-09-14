139e9d085253eeac156b46fd8a1c3d58274b0d67
Jaegeuk Kim <jaegeuk@google.com>
2020-09-13 23:21:59 -0700

    ANDROID: export bkops and query/flag query functions
    This allows to call outside of ufshcd core, so they should check error handler
    is in progress and power is on.
    
    Bug: 158050260
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: Ic989ab580303edaca03e7a47a243524c5e22c8de

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2806,11 +2806,14 @@ static inline void ufshcd_init_query(struct ufs_hba *hba,
 	(*request)->upiu_req.selector = selector;
 }
 
-static int ufshcd_query_flag_retry(struct ufs_hba *hba,
+int ufshcd_query_flag_retry(struct ufs_hba *hba,
 	enum query_opcode opcode, enum flag_idn idn, u8 index, bool *flag_res)
 {
 	int ret;
 	int retries;
+
+	if (ufshcd_eh_in_progress(hba))
+		return -EBUSY;
 
 	for (retries = 0; retries < QUERY_REQ_RETRIES; retries++) {
 		ret = ufshcd_query_flag(hba, opcode, idn, index, flag_res);
@@ -2828,6 +2831,7 @@ static int ufshcd_query_flag_retry(struct ufs_hba *hba,
 			__func__, opcode, idn, ret, retries);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(ufshcd_query_flag_retry);
 
 /**
  * ufshcd_query_flag() - API function for sending flag query requests
@@ -2976,12 +2980,15 @@ EXPORT_SYMBOL_GPL(ufshcd_query_attr);
  *
  * Returns 0 for success, non-zero in case of failure
 */
-static int ufshcd_query_attr_retry(struct ufs_hba *hba,
+int ufshcd_query_attr_retry(struct ufs_hba *hba,
 	enum query_opcode opcode, enum attr_idn idn, u8 index, u8 selector,
 	u32 *attr_val)
 {
 	int ret = 0;
 	u32 retries;
+
+	if (ufshcd_eh_in_progress(hba))
+		return -EBUSY;
 
 	for (retries = QUERY_REQ_RETRIES; retries > 0; retries--) {
 		ret = ufshcd_query_attr(hba, opcode, idn, index,
@@ -2999,6 +3006,7 @@ static int ufshcd_query_attr_retry(struct ufs_hba *hba,
 			__func__, idn, ret, QUERY_REQ_RETRIES);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(ufshcd_query_attr_retry);
 
 static int __ufshcd_query_descriptor(struct ufs_hba *hba,
 			enum query_opcode opcode, enum desc_idn idn, u8 index,
@@ -3087,6 +3095,9 @@ int ufshcd_query_descriptor_retry(struct ufs_hba *hba,
 	int err;
 	int retries;
 
+	pm_runtime_get_sync(hba->dev);
+	ufshcd_hold(hba, false);
+
 	for (retries = QUERY_REQ_RETRIES; retries > 0; retries--) {
 		err = -EAGAIN;
 		down_read(&hba->query_lock);
@@ -3097,6 +3108,9 @@ int ufshcd_query_descriptor_retry(struct ufs_hba *hba,
 		if (!err || err == -EINVAL)
 			break;
 	}
+
+	ufshcd_release(hba);
+	pm_runtime_put_noidle(hba->dev);
 
 	return err;
 }
@@ -3211,6 +3225,7 @@ out:
 		kfree(desc_buf);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(ufshcd_read_desc_param);
 
 /**
  * struct uc_string_id - unicode string
@@ -5181,11 +5196,14 @@ static inline int ufshcd_get_bkops_status(struct ufs_hba *hba, u32 *status)
  * to know whether auto bkops is enabled or disabled after this function
  * returns control to it.
  */
-static int ufshcd_bkops_ctrl(struct ufs_hba *hba,
+int ufshcd_bkops_ctrl(struct ufs_hba *hba,
 			     enum bkops_status status)
 {
 	int err;
 	u32 curr_status = 0;
+
+	if (ufshcd_eh_in_progress(hba))
+		return -EBUSY;
 
 	err = ufshcd_get_bkops_status(hba, &curr_status);
 	if (err) {
@@ -5206,6 +5224,7 @@ static int ufshcd_bkops_ctrl(struct ufs_hba *hba,
 out:
 	return err;
 }
+EXPORT_SYMBOL_GPL(ufshcd_bkops_ctrl);
 
 /**
  * ufshcd_urgent_bkops - handle urgent bkops exception event
