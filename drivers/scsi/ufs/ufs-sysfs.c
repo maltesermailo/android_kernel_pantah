63ab8c0876c0c720af7510b7df34da949925c606
Jaegeuk Kim <jaegeuk@google.com>
2020-09-13 22:17:05 -0700

    ANDROID: scsi: ufs: add two hooks to prepare command and sysfs entries
    This patch adds two variants in UFS driver:
    
    void    (*prepare_command)(struct ufs_hba *hba,
                            struct request *rq, struct ufshcd_lrb *lrbp);
     -> called in the beginning of queuecommand to update the request, if needed
    
    int     (*update_sysfs)(struct ufs_hba *hba);
     -> called after creating sysfs for vendor to add more sysfs entries
    
    Bug: 158050260
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: Id8c1197ec704e998e89dc25cc69b0f6a1b86e978

--- drivers/scsi/ufs/ufs-sysfs.c
+++ drivers/scsi/ufs/ufs-sysfs.c
@@ -854,15 +854,17 @@ const struct attribute_group ufs_sysfs_lun_attributes_group = {
 	.attrs = ufs_sysfs_lun_attributes,
 };
 
-void ufs_sysfs_add_nodes(struct device *dev)
+void ufs_sysfs_add_nodes(struct ufs_hba *hba)
 {
 	int ret;
 
-	ret = sysfs_create_groups(&dev->kobj, ufs_sysfs_groups);
+	ret = sysfs_create_groups(&hba->dev->kobj, ufs_sysfs_groups);
 	if (ret)
-		dev_err(dev,
+		dev_err(hba->dev,
 			"%s: sysfs groups creation failed (err = %d)\n",
 			__func__, ret);
+
+	ufshcd_vops_update_sysfs(hba);
 }
 
 void ufs_sysfs_remove_nodes(struct device *dev)
