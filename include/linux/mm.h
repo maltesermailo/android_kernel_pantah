a21d14a25dcf707b2db653ef3279ba171ef56167
Peter Xu <peterx@redhat.com>
2020-09-25 18:25:58 -0400

    BACKPORT: mm/fork: Pass new vma pointer into copy_page_range()
    This prepares for the future work to trigger early cow on pinned pages
    during fork().
    
    No functional change intended.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit 7a4830c380f3a8b3425f6383deff58e65b2557b5)
    
    Bug: 293549530
    Change-Id: I94ff9a6050c854c0919f3c96cf9eca7a3744272f
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1450,7 +1450,7 @@ int walk_page_vma(struct vm_area_struct *vma, struct mm_walk *walk);
 void free_pgd_range(struct mmu_gather *tlb, unsigned long addr,
 		unsigned long end, unsigned long floor, unsigned long ceiling);
 int copy_page_range(struct mm_struct *dst, struct mm_struct *src,
-			struct vm_area_struct *vma);
+		    struct vm_area_struct *vma, struct vm_area_struct *new);
 int follow_pte_pmd(struct mm_struct *mm, unsigned long address,
 			     unsigned long *start, unsigned long *end,
 			     pte_t **ptepp, pmd_t **pmdpp, spinlock_t **ptlp);
