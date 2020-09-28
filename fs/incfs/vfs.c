4587c30dd65e99c53d0fba4bcd71f8edb460ffbd
Paul Lawrence <paullawrence@google.com>
2020-09-28 13:40:56 -0700

    ANDROID: Incremental fs: Add FS_IOC_MEASURE_VERITY
    Bug: 160634504
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I1bc2dc975b9be122e1c831a25a1d44f27a360f3c

--- fs/incfs/vfs.c
+++ fs/incfs/vfs.c
@@ -775,6 +775,8 @@ static long dispatch_ioctl(struct file *f, unsigned int req, unsigned long arg)
 		return ioctl_enable_verity(f, (const void __user *)arg);
 	case FS_IOC_GETFLAGS:
 		return incfs_verity_get_flags(f, (void __user *) arg);
+	case FS_IOC_MEASURE_VERITY:
+		return fsverity_ioctl_measure(f, (void __user *)arg);
 	default:
 		return -ENOTTY;
 	}
--- tools/testing/selftests/filesystems/incfs/incfs_test.c
+++ tools/testing/selftests/filesystems/incfs/incfs_test.c
@@ -3613,11 +3613,17 @@ static int validate_verity(const char *mount_dir, struct test_file *file)
 	char *filename = concat_file_name(mount_dir, file->name);
 	int fd = -1;
 	uint64_t flags;
+	uint8_t buffer[sizeof(struct fsverity_digest) + INCFS_MAX_HASH_SIZE];
+	struct fsverity_digest *digest = (struct fsverity_digest *) buffer;
 
 	TEST(filename = concat_file_name(mount_dir, file->name), filename);
 	TEST(fd = open(filename, O_RDONLY | O_CLOEXEC), fd != -1);
 	TESTEQUAL(ioctl(fd, FS_IOC_GETFLAGS, &flags), 0);
 	TESTEQUAL(flags & FS_VERITY_FL, FS_VERITY_FL);
+	digest->digest_size = INCFS_MAX_HASH_SIZE;
+	TESTEQUAL(ioctl(fd, FS_IOC_MEASURE_VERITY, digest), 0);
+	TESTEQUAL(digest->digest_algorithm, FS_VERITY_HASH_ALG_SHA256);
+	TESTEQUAL(digest->digest_size, 32);
 
 	result = TEST_SUCCESS;
 out:
