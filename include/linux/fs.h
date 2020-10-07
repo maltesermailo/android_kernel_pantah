ae2506305c136527177a7f4f31217e2d93cda1b7
Egor Uleyskiy <e.uleyskiy@samsung.com>
2020-10-07 21:54:07 +0300

    ANDROID: vendor_hooks: Add a vendor data to file struct
    Add ANDROID_VENDOR_DATA(1) to struct file
    
    Bug: 169343953
    Signed-off-by: Egor Uleyskiy <e.uleyskiy@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I1afc259f2bd02fee76b20802dda74cbe79658534
    (cherry picked from commit 3b300ff4c5aaccd8668b891b5f9c5815696faa50)

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -42,6 +42,7 @@
 
 #include <asm/byteorder.h>
 #include <uapi/linux/fs.h>
+#include <linux/android_vendor.h>
 
 struct backing_dev_info;
 struct bdi_writeback;
@@ -952,6 +953,8 @@ struct file {
 	struct address_space	*f_mapping;
 	errseq_t		f_wb_err;
 	errseq_t		f_sb_err; /* for syncfs */
+
+	ANDROID_VENDOR_DATA(1);
 } __randomize_layout
   __attribute__((aligned(4)));	/* lest something weird decides that 2 is OK */
 
