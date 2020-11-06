BayLibre/android_kernel_common

ANDROID: Incremental fs: Add verity descriptor to backing file
Bug: 160634504
Test: incfs_test passes
Signed-off-by: Paul Lawrence <paullawrence@google.com>
Change-Id: I41d90abd55527884d9eff642c9834ad837ff6918

fs/incfs/format.h
@@ -120,6 +120,7 @@ enum incfs_metadata_type {
 	INCFS_MD_FILE_ATTR = 2,
 	INCFS_MD_SIGNATURE = 3,
 	INCFS_MD_STATUS = 4,
+	INCFS_MD_VERITY_DESCRIPTOR = 5,
 };
 
 enum incfs_file_header_flags {
@@ -255,6 +256,21 @@ struct incfs_status {
 	__le32 is_dummy[6]; /* Spare fields */
 } __packed;
 
+/* Metadata record for verity descriptor. Type = INCFS_MD_VERITY_DESCRIPTOR */
+struct incfs_file_verity_descriptor {
+	struct incfs_md_header vd_header;
+
+	__le32 vd_size; /* The size of the signature. */
+
+	__le64 vd_offset; /* Signature's offset in the backing file */
+} __packed;
+
+/* In memory version of above */
+struct incfs_df_verity_descriptor {
+	u32 size;
+	u64 offset;
+};
+
 /* State of the backing file. */
 struct backing_file_context {
 	/* Protects writes to bc_file */
@@ -280,6 +296,7 @@ struct metadata_handler {
 		struct incfs_blockmap blockmap;
 		struct incfs_file_signature signature;
 		struct incfs_status status;
+		struct incfs_file_verity_descriptor verity_descriptor;
 	} md_buffer;
 
 	int (*handle_blockmap)(struct incfs_blockmap *bm,
@@ -288,6 +305,8 @@ struct metadata_handler {
 				 struct metadata_handler *handler);
 	int (*handle_status)(struct incfs_status *sig,
 				 struct metadata_handler *handler);
+	int (*handle_verity_descriptor)(struct incfs_file_verity_descriptor *d,
+					struct metadata_handler *handler);
 };
 #define INCFS_MAX_METADATA_RECORD_SIZE \
 	sizeof_field(struct metadata_handler, md_buffer)
@@ -328,6 +347,9 @@ int incfs_write_status_to_backing_file(struct backing_file_context *bfc,
 				       loff_t status_offset,
 				       u32 data_blocks_written,
 				       u32 hash_blocks_written);
+int incfs_write_verity_descriptor_to_backing_file(
+		struct backing_file_context *bfc, struct mem_range descriptor,
+		loff_t *offset);
 
 int incfs_make_empty_backing_file(struct backing_file_context *bfc,
 				  incfs_uuid_t *uuid, u64 file_size);

fs/incfs/vfs.c
@@ -1273,7 +1273,12 @@ static int file_open(struct inode *inode, struct file *file)
 		file->private_data = fd;
 
 		err = make_inode_ready_for_data_ops(mi, inode, backing_file);
+		if (err)
+			goto out;
 
+		err = fsverity_file_open(inode, file);
+		if (err)
+			goto out;
 	} else if (S_ISDIR(inode->i_mode)) {
 		struct dir_file *dir = NULL;
 

maltesermailo/android_kernel_pantah

fs/incfs/format.h
@@ -120,6 +120,7 @@ enum incfs_metadata_type {
 	INCFS_MD_FILE_ATTR = 2,
 	INCFS_MD_SIGNATURE = 3,
 	INCFS_MD_STATUS = 4,
+	INCFS_MD_VERITY_SIGNATURE = 5,
 };
 
 enum incfs_file_header_flags {
@@ -259,6 +260,21 @@ struct incfs_status {
 	__le32 is_dummy[6]; /* Spare fields */
 } __packed;
 
+/* Metadata record for verity descriptor. Type = INCFS_MD_VERITY_DESCRIPTOR */
+struct incfs_file_verity_signature {
+	struct incfs_md_header vs_header;
+
+	__le32 vs_size; /* The size of the signature. */
+
+	__le64 vs_offset; /* Signature's offset in the backing file */
+} __packed;
+
+/* In memory version of above */
+struct incfs_df_verity_signature {
+	u32 size;
+	u64 offset;
+};
+
 /* State of the backing file. */
 struct backing_file_context {
 	/* Protects writes to bc_file */
@@ -284,6 +300,7 @@ struct metadata_handler {
 		struct incfs_blockmap blockmap;
 		struct incfs_file_signature signature;
 		struct incfs_status status;
+		struct incfs_file_verity_signature verity_signature;
 	} md_buffer;
 
 	int (*handle_blockmap)(struct incfs_blockmap *bm,
@@ -292,6 +309,8 @@ struct metadata_handler {
 				 struct metadata_handler *handler);
 	int (*handle_status)(struct incfs_status *sig,
 				 struct metadata_handler *handler);
+	int (*handle_verity_signature)(struct incfs_file_verity_signature *s,
+					struct metadata_handler *handler);
 };
 #define INCFS_MAX_METADATA_RECORD_SIZE \
 	sizeof_field(struct metadata_handler, md_buffer)
@@ -330,6 +349,9 @@ int incfs_write_status_to_backing_file(struct backing_file_context *bfc,
 				       loff_t status_offset,
 				       u32 data_blocks_written,
 				       u32 hash_blocks_written);
+int incfs_write_verity_signature_to_backing_file(
+		struct backing_file_context *bfc, struct mem_range signature,
+		loff_t *offset);
 
 /* Reading stuff */
 int incfs_read_file_header(struct backing_file_context *bfc,

fs/incfs/vfs.c
@@ -1290,7 +1290,12 @@ static int file_open(struct inode *inode, struct file *file)
 		file->private_data = fd;
 
 		err = make_inode_ready_for_data_ops(mi, inode, backing_file);
+		if (err)
+			goto out;
 
+		err = incfs_fsverity_file_open(inode, file);
+		if (err)
+			goto out;
 	} else if (S_ISDIR(inode->i_mode)) {
 		struct dir_file *dir = NULL;
 