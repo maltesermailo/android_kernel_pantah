1a8ba42aebdf4bc6add6a032671de636ccec492f
Ard Biesheuvel <ardb@kernel.org>
2020-11-20 09:21:03 +0100

    arm64: implement support for static call trampolines
    Implement arm64 support for the 'unoptimized' static call variety, which
    routes all calls through a single trampoline that is patched to perform a
    tail call to the selected function.
    
    Since static call targets may be located in modules loaded out of direct
    branching range, we need to use a ADRP/ADD pair to load the branch target
    into R16 and use a branch-to-register (BR) instruction to perform an
    indirect call. Unlike on x86, there is no pressing need on arm64 to avoid
    indirect calls at all cost, but hiding it from the compiler as is done
    here does have some benefits:
    - the literal is located in .rodata, which gives us the same robustness
      advantage that code patching does;
    - no performance hit on CFI enabled Clang builds that decorate compiler
      emitted indirect calls with branch target validity checks.
    
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Change-Id: I72351e75ba9a40859e23a637bcc29211a91f380e
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

---
+++ arch/arm64/include/asm/static_call.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_STATIC_CALL_H
+#define _ASM_STATIC_CALL_H
+
+#define __ARCH_DEFINE_STATIC_CALL_TRAMP(name, target)			    \
+	asm("	.pushsection	.static_call.text, \"ax\"		\n" \
+	    "	.align		3					\n" \
+	    "	.globl		" STATIC_CALL_TRAMP_STR(name) "		\n" \
+	    STATIC_CALL_TRAMP_STR(name) ":				\n" \
+	    "	hint 	34	/* BTI C */				\n" \
+	    "	adrp	x16, 1f						\n" \
+	    "	ldr	x16, [x16, :lo12:1f]				\n" \
+	    "	cbz	x16, 0f						\n" \
+	    "	br	x16						\n" \
+	    "0:	ret							\n" \
+	    "	.popsection						\n" \
+	    "	.pushsection	.rodata, \"a\"				\n" \
+	    "	.align		3					\n" \
+	    "1:	.quad		" target "				\n" \
+	    "	.popsection						\n")
+
+#define ARCH_DEFINE_STATIC_CALL_TRAMP(name, func)			\
+	__ARCH_DEFINE_STATIC_CALL_TRAMP(name, #func)
+
+#define ARCH_DEFINE_STATIC_CALL_NULL_TRAMP(name)			\
+	__ARCH_DEFINE_STATIC_CALL_TRAMP(name, "0x0")
+
+#define ARCH_DEFINE_STATIC_CALL_RET0_TRAMP(name)			\
+	ARCH_DEFINE_STATIC_CALL_TRAMP(name, __static_call_return0)
+
+#endif /* _ASM_STATIC_CALL_H */
