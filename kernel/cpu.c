b9542138a28e8abafb3f0fe7ef5fd1973cf26194
Stephen Dickey <dickey@codeaurora.org>
2020-11-23 19:01:12 -0800

    ANDROID: cpu/hotplug: lazy resume operation
    Current code for the pause operation does a lazy
    operation.  This enables fast time-to-idle while
    ultimately correcting the situation.  Resume
    operations have the same issues for performance.
    
    This change sets the active flag prior to the cpus
    write lock is taken, thereby activating the cpu early.
    
    Bug: 161210528
    Change-Id: I54a057bd1e34148634ffe887e212b7f1ec67a29d
    Signed-off-by: Vincent Donnefort <vincent.donnefort@arm.com>
    Signed-off-by: Stephen Dickey <dickey@codeaurora.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1253,6 +1253,10 @@ int resume_cpus(struct cpumask *cpus)
 	if (cpumask_empty(cpus))
 		goto err_cpu_maps_update;
 
+	for_each_cpu(cpu, cpus) {
+		set_cpu_active(cpu, true);
+	}
+
 	cpus_write_lock();
 
 	cpuhp_tasks_frozen = 0;
