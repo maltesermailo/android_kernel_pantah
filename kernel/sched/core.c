7876ff046d1aa6370aaa8cbf7951b27eb8bd7723
Quentin Perret <qperret@google.com>
2020-12-15 17:55:48 +0000

    Revert "ANDROID: sched/fair: update_max_interval() active_mask dependant"
    This reverts commit 75d6951cf8cb7aec6b7a8cc4f595ca2e11082fd7.
    
    CPU Pause causes major merge conflicts with the 5.11 scheduler changes
    (migrate-disable specifically), so lets revert Pause temporarily as it
    is not needed urgently in android-mainline.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I05b0288ae67b72f9a4151f41fad1546e4f7ba1f7

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6973,8 +6973,6 @@ int sched_cpu_activate(unsigned int cpu)
 	}
 	rq_unlock_irqrestore(rq, &rf);
 
-	update_max_interval();
-
 	return 0;
 }
 
@@ -7009,9 +7007,6 @@ int sched_cpu_deactivate(unsigned int cpu)
 		return ret;
 	}
 	sched_domains_numa_masks_clear(cpu);
-
-	update_max_interval();
-
 	return 0;
 }
 
@@ -7020,6 +7015,7 @@ static void sched_rq_cpu_starting(unsigned int cpu)
 	struct rq *rq = cpu_rq(cpu);
 
 	rq->calc_load_update = calc_load_update;
+	update_max_interval();
 }
 
 int sched_cpu_starting(unsigned int cpu)
@@ -7051,6 +7047,7 @@ int sched_cpu_dying(unsigned int cpu)
 	trace_android_rvh_sched_cpu_dying(cpu);
 
 	calc_load_migrate(rq);
+	update_max_interval();
 	nohz_balance_exit_idle(rq);
 	hrtick_clear(rq);
 	return 0;
