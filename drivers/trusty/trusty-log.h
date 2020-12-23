8151acd1659853cb89200de8853abcc6e96d1527
Rajesh Nyamagoud <nyamagoud@google.com>
2020-12-23 06:37:04 -0800

    ANDROID: trusty: Adding metadata to log entries
    Log entries are processed as chunks of data with metadata
    
    Bug: 172661714
    Change-Id: Id66fe1625fa43b0eecef3992f75302b86b8c13a4
    Signed-off-by: Rajesh Nyamagoud <nyamagoud@google.com>

--- drivers/trusty/trusty-log.h
+++ drivers/trusty/trusty-log.h
@@ -8,6 +8,32 @@
 #define _TRUSTY_LOG_H_
 
 /*
+ * Below is the log-data-entry in Ringbuffer
+ * {
+ *    ----- header part -----
+ *    total_size_of_entry +
+ *    log_string + app_name +
+ *    ------footer part -----
+ *    app-id +
+ *    timestamp +
+ *    app_name_size +
+ *    log_string_size
+ *    -----------------------
+ * }
+ */
+struct log_data_header {
+	uint32_t entry_size;
+	char data[192];
+} __attribute__((packed));
+
+struct log_data_footer {
+	int32_t app_id;
+	uint64_t timestamp;
+	uint32_t app_name_len;
+	uint32_t log_len;
+} __attribute__((packed));
+
+/*
  * Ring buffer that supports one secure producer thread and one
  * linux side consumer thread.
  */
@@ -22,7 +48,6 @@ struct log_rb {
 #define SMC_SC_SHARED_LOG_ADD		SMC_STDCALL_NR(SMC_ENTITY_LOGGING, 1)
 #define SMC_SC_SHARED_LOG_RM		SMC_STDCALL_NR(SMC_ENTITY_LOGGING, 2)
 
-#define TRUSTY_LOG_API_VERSION	1
+#define TRUSTY_LOG_API_VERSION	2
 
 #endif
-
