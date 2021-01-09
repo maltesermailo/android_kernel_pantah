9f2e2b3d827b87e3e1b2e56954204dc735c1dd16
Isaac J. Manjarres <isaacm@codeaurora.org>
2021-01-08 16:54:35 -0800

    FROMLIST: iommu/arm-smmu: Hook up map_sg()
    Now that everything is in place for iommu_map_sg() to defer
    mapping a scatter-gather list to the io-pgtable layer, implement
    the map_sg() callback in the SMMU driver, so that iommu_map_sg()
    can invoke it with the entire scatter-gather list that will be
    mapped.
    
    Bug: 176779203
    Link: https://lore.kernel.org/linux-iommu/1610376862-927-1-git-send-email-isaacm@codeaurora.org/T/#t
    Change-Id: I5c68d2643c7632d6298948624093f43f5675e7be
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1235,6 +1235,24 @@ static int arm_smmu_map(struct iommu_domain *domain, unsigned long iova,
 	return ret;
 }
 
+static int arm_smmu_map_sg(struct iommu_domain *domain, unsigned long iova,
+			   struct scatterlist *sg, unsigned int nents, int prot,
+			   gfp_t gfp, size_t *mapped)
+{
+	struct io_pgtable_ops *ops = to_smmu_domain(domain)->pgtbl_ops;
+	struct arm_smmu_device *smmu = to_smmu_domain(domain)->smmu;
+	int ret;
+
+	if (!ops)
+		return -ENODEV;
+
+	arm_smmu_rpm_get(smmu);
+	ret = ops->map_sg(ops, iova, sg, nents, prot, gfp, mapped);
+	arm_smmu_rpm_put(smmu);
+
+	return ret;
+}
+
 static size_t arm_smmu_unmap(struct iommu_domain *domain, unsigned long iova,
 			     size_t size, struct iommu_iotlb_gather *gather)
 {
@@ -1628,6 +1646,7 @@ static struct iommu_ops arm_smmu_ops = {
 	.domain_free		= arm_smmu_domain_free,
 	.attach_dev		= arm_smmu_attach_dev,
 	.map			= arm_smmu_map,
+	.map_sg			= arm_smmu_map_sg,
 	.unmap			= arm_smmu_unmap,
 	.flush_iotlb_all	= arm_smmu_flush_iotlb_all,
 	.iotlb_sync		= arm_smmu_iotlb_sync,
