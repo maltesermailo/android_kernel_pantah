e3124b3bd0667b32675bb466b0729ba3a45b3a62
Wesley Cheng <wcheng@codeaurora.org>
2021-01-21 01:33:06 -0800

    BACKPORT: FROMLIST: usb: dwc3: Resize TX FIFOs to meet EP bursting requirements
    Some devices have USB compositions which may require multiple endpoints
    that support EP bursting.  HW defined TX FIFO sizes may not always be
    sufficient for these compositions.  By utilizing flexible TX FIFO
    allocation, this allows for endpoints to request the required FIFO depth to
    achieve higher bandwidth.  With some higher bMaxBurst configurations, using
    a larger TX FIFO size results in better TX throughput.
    
    By introducing the check_config() callback, the resizing logic can fetch
    the maximum number of endpoints used in the USB composition (can contain
    multiple configurations), which helps ensure that the resizing logic can
    fulfill the configuration(s), or return an error to the gadget layer
    otherwise during bind time.
    
    Signed-off-by: Wesley Cheng <wcheng@codeaurora.org>
    
    Bug: 186724205
    Link: https://lore.kernel.org/linux-arm-msm/1611895604-4496-4-git-send-email-wcheng@codeaurora.org/
    Change-Id: Ie42b74ec0f35f61d36ecd564c04d511a0d403b22
    Signed-off-by: Wesley Cheng <wcheng@codeaurora.org>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1238,6 +1238,7 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 	u8			rx_max_burst_prd;
 	u8			tx_thr_num_pkt_prd;
 	u8			tx_max_burst_prd;
+	u8			tx_fifo_resize_max_num;
 	const char		*usb_psy_name;
 	int			ret;
 
@@ -1252,6 +1253,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 	 * threshold value of 0b1100
 	 */
 	hird_threshold = 12;
+
+	tx_fifo_resize_max_num = 6;
 
 	dwc->maximum_speed = usb_get_maximum_speed(dev);
 	dwc->max_ssp_rate = usb_get_maximum_ssp_rate(dev);
@@ -1296,6 +1299,10 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 				&tx_thr_num_pkt_prd);
 	device_property_read_u8(dev, "snps,tx-max-burst-prd",
 				&tx_max_burst_prd);
+	dwc->do_fifo_resize = device_property_read_bool(dev,
+							"tx-fifo-resize");
+	device_property_read_u8(dev, "tx-fifo-max-num",
+				&tx_fifo_resize_max_num);
 
 	dwc->disable_scramble_quirk = device_property_read_bool(dev,
 				"snps,disable_scramble_quirk");
@@ -1361,6 +1368,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 	dwc->tx_max_burst_prd = tx_max_burst_prd;
 
 	dwc->imod_interval = 0;
+
+	dwc->tx_fifo_resize_max_num = tx_fifo_resize_max_num;
 }
 
 /* check whether the core supports IMOD */
