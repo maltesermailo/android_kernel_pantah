6dee15fc3353bef53c5f3520eb45e53eda7c9031
Yuan Zhao <yzhao@codeaurora.org>
2021-01-26 16:13:20 +0800

    ANDROID: drm: add drm_panel_notify vendor hook
    When drm panel power mode and refresh rate were changed, could
    notify this event. Since other modules, like touch or scheduler
    might want to know the event.
    
    Bug: 178559642
    Signed-off-by: Yuan Zhao <yzhao@codeaurora.org>
    Change-Id: I47770c4e80c2752a3f0f7d7cdd32416b197db25a

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -28,6 +28,7 @@
 #include <trace/hooks/mm.h>
 #include <trace/hooks/preemptirq.h>
 #include <trace/hooks/ftrace_dump.h>
+#include <trace/hooks/drm_panel_notify.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -108,3 +109,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ftrace_oops_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ftrace_size_check);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ftrace_format_check);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ftrace_dump_buffer);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_drm_panel_notify);
