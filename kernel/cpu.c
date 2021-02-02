0458f71cc07377d761cecc775ea420e2111e4e3a
Stephen Dickey <dickey@codeaurora.org>
2021-02-01 17:25:10 -0800

    ANDROID: cpuhp/aarch32: keep last 32bit cpu active
    It is possible that all the 32 bit CPUs are paused in
    the system, which is not ideal for quickly launching
    32 bit apps.
    
    Detect if a pause operation is about to pause to the
    last 32 bit CPU, and prevent it.
    
    Bug: 175896474
    Change-Id: I21b4dad7ba9f3ef9be460137098e6fb2c0e336e6
    Signed-off-by: Stephen Dickey <dickey@codeaurora.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1134,6 +1134,26 @@ void __wait_drain_rq(struct cpumask *cpus)
 		sched_cpu_drain_rq_wait(cpu);
 }
 
+static int validate_32bit_paused_cpus(struct cpumask *cpus)
+{
+	cpumask_var_t active_32bit_cpus;
+	int cpu_reserved;
+
+	if (!zalloc_cpumask_var(&active_32bit_cpus, GFP_KERNEL))
+		return -ENOMEM;
+
+	cpumask_and(active_32bit_cpus, cpu_active_mask, system_32bit_el0_cpumask());
+	cpu_reserved = cpumask_any(active_32bit_cpus);
+
+	cpumask_andnot(active_32bit_cpus, active_32bit_cpus, cpus);
+	if (cpumask_weight(active_32bit_cpus) == 0)
+		cpumask_clear_cpu(cpu_reserved, cpus);
+
+	free_cpumask_var(active_32bit_cpus);
+
+	return 0;
+}
+
 int pause_cpus(struct cpumask *cpus)
 {
 	int err = 0;
@@ -1163,6 +1183,10 @@ int pause_cpus(struct cpumask *cpus)
 		err = -EBUSY;
 		goto err_cpu_maps_update;
 	}
+
+	err = validate_32bit_paused_cpus(cpus);
+	if (err)
+		goto err_cpu_maps_update;
 
 	if (cpumask_empty(cpus))
 		goto err_cpu_maps_update;
