82dbb82a9068e606b378b14e33981899c98c7300
Greg Kroah-Hartman <gregkh@google.com>
2021-02-02 11:31:33 +0100

    ANDROID: modpost.c: fix up incorrect merge resolution
    When merging changes done in scripts/mod/modpost.c I picked the wrong
    "side" of the merge branches.
    
    This fixes things up to match what is upstream identically at this point
    in time, as there should not be any need for any changes in this file
    right now.
    
    Reported-by: Matthias Maennich <maennich@google.com>
    Cc: Quentin Perret <qperret@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I014adb697866bc15a893f7a108bec361c525d7ad

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -430,9 +430,10 @@ static struct symbol *sym_add_exported(const char *name, struct module *mod,
 		s = new_symbol(name, mod, export);
 	} else if (!external_module || s->module->is_vmlinux ||
 		   s->module == mod) {
-		fatal("%s: '%s' exported twice. Previous export was in %s%s\n",
-		      mod->name, name, s->module->name,
-		      s->module->is_vmlinux ? "" : ".ko");
+		warn("%s: '%s' exported twice. Previous export was in %s%s\n",
+		     mod->name, name, s->module->name,
+		     s->module->is_vmlinux ? "" : ".ko");
+		return s;
 	}
 
 	s->module = mod;
@@ -2647,9 +2648,9 @@ int main(int argc, char **argv)
 
 		for (s = symbolhash[n]; s; s = s->next) {
 			if (s->is_static)
-				warn("\"%s\" [%s] is a static %s\n",
-				     s->name, s->module->name,
-				     export_str(s->export));
+				error("\"%s\" [%s] is a static %s\n",
+				      s->name, s->module->name,
+				      export_str(s->export));
 		}
 	}
 
