b6f40c127f116351252ddfd8a34db9de81340e49
Neeraj Upadhyay <neeraju@codeaurora.org>
2021-02-11 13:20:39 +0530

    ANDROID: gic-v3: Update vendor hook to set affinity in GIC v3
    GIC provides implementation specific registers, to configure
    affinity of a  SPI. Update the existing affinity hook to allow
    vendors to configure those implementation defined settings.
    
    Bug: 180471389
    Change-Id: I273035da65eaeb346c0d8b303a722f4d8d7918d6
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>
    (cherry picked from commit f52f34358744754df82678e6a0726370fcdbec31)
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>
    (cherry-picked from commit fdb01338797f7ff2e1fde30bab7827084d81eaaf)
    [quic_gurus@quicinc.com: Fixed trivial merge conflicts]
    Signed-off-by: Guru Das Srinagesh <quic_gurus@quicinc.com>

--- include/trace/hooks/gic_v3.h
+++ include/trace/hooks/gic_v3.h
@@ -18,8 +18,8 @@ DECLARE_HOOK(android_vh_gic_v3_affinity_init,
 	TP_ARGS(irq, offset, affinity));
 DECLARE_HOOK(android_vh_gic_v3_set_affinity,
 	TP_PROTO(struct irq_data *d, const struct cpumask *mask_val,
-		 u64 *affinity),
-	TP_ARGS(d, mask_val, affinity));
+		 u64 *affinity, bool force, void __iomem *base),
+	TP_ARGS(d, mask_val, affinity, force, base));
 #else
 #define trace_android_vh_gic_v3_affinity_init(irq, offset, affinity)
 #define trace_android_vh_gic_v3_set_affinity(d, mask_val, affinity)
