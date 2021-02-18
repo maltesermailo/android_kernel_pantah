ae92e6e00f3dd6e818e7cbd764d0d5de14bf15f4
Chiawei Wang <chiaweiwang@google.com>
2021-02-18 10:43:50 +0800

    ANDROID: mm: Add vendor hook in rmqueue()
    Add a vendor hook for costly order page counting
    and other vendor specific functions.
    
    Bug: 174521902
    Bug: 172987241
    Signed-off-by: Chiawei Wang <chiaweiwang@google.com>
    Change-Id: I89206727a462548cc3500b695d85c83ff003eec7

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -36,6 +36,7 @@
 #include <trace/hooks/power.h>
 #include <trace/hooks/fault.h>
 #include <trace/hooks/iommu.h>
+#include <trace/hooks/mm.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -152,3 +153,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sea);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_mem_abort);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sp_pc_abort);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iommu_setup_dma_ops);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rmqueue);
