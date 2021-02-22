f0ffcfd66d873fa5c293f455b505bc7d4077968f
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2021-02-22 16:25:20 +0800

    ANDROID: kernel: Add vendor hook in creds
    Add vendor hook for creds, so we get the cred information
    to monitor cred lifetime.
    
    Bug: 181639260
    
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    Change-Id: I8f254464e07f9c88336995152479ce91deb13c75

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -39,6 +39,7 @@
 #include <trace/hooks/thermal.h>
 #include <trace/hooks/ufshcd.h>
 #include <trace/hooks/avc.h>
+#include <trace/hooks/creds.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -172,3 +173,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_selinux_avc_insert);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_selinux_avc_node_delete);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_selinux_avc_node_replace);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_selinux_avc_lookup);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_commit_creds);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_creds);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_override_creds);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_revert_creds);
