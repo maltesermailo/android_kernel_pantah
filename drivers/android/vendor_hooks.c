43ebe2b875da6d5db50d34ebc84b869ceeb263d6
Zhuguangqing <zhuguangqing@xiaomi.com>
2021-03-09 15:47:43 +0800

    ANDROID: Add vendor hooks for binder perf tuning
    Add some hooks in the binder module so that we can do task dependency analysis and statistical work in OEM's module for further optimization.
    
    Bug: 182496370
    Bug: 235925535
    
    Signed-off-by: Zhuguangqing <zhuguangqing@xiaomi.com>
    Change-Id: Id47e59c4e3ccd07b26eef758ada147b98cd1964e
    Signed-off-by: heshuai1 <heshuai1@xiaomi.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    [ cmllamas: don't export complete private definition struct binder_alloc
      in vendor hooks, instead just pass member alloc->free_async_space as
      implemented by heshuai1 and squashed here ]
    (cherry picked from commit 254fb1f4034fde523378ee58a501d212a59047b7)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -216,3 +216,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_thread_release);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_has_work_ilocked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_read_done);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_alloc_new_buf_locked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_reply);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_trans);
