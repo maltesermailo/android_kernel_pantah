256f9463051e519076703cfed939294f3834dd7d
Zhuguangqing <zhuguangqing@xiaomi.com>
2021-03-10 14:56:02 +0800

    ANDROID: Add vendor hooks to signal.
    This hook will allow us to get signal messages so that we can set limitations for certain tasks and restore them when receiving important signals.
    
    Bug: 184898838
    
    Signed-off-by: Zhuguangqing <zhuguangqing@xiaomi.com>
    Change-Id: I83a28b0a6eb413976f4c57f2314d008ad792fa0d
    (cherry picked from commit 58e3f869fc3fe84fb7062496ccd049db47f3ed7f)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -58,6 +58,7 @@
 #include <trace/hooks/thermal.h>
 #include <trace/hooks/audio_usboffload.h>
 #include <trace/hooks/typec.h>
+#include <trace/hooks/signal.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -70,6 +71,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_transaction_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_set_priority);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_restore_priority);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_wakeup_ilocked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_send_sig_info);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rtmutex_wait_start);
