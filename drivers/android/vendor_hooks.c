032384dca0362fcd1ceaafad7b008baee9449ff4
Manish Varma <varmam@google.com>
2021-03-15 21:40:24 -0700

    ANDROID: fs: Add vendor hooks for ep_create_wakeup_source & timerfd_create
    timerfd doesn't create any wakelocks, but eventpoll can.  When it does,
    it names them after the underlying file descriptor, and since all
    timerfd file descriptors are named "[timerfd]" (which saves memory on
    systems like desktops with potentially many timerfd instances), all
    wakesources created as a result of using the eventpoll-on-timerfd idiom
    are called... "[timerfd]".
    
    However, it becomes impossible to tell which "[timerfd]" wakesource is
    affliated with which process and hence troubleshooting is difficult.
    
    Adding vendor hooks to allow vendor to assign appropriate names to
    timerfd descriptors and eventoll wakesource.
    
    Bug: 155142106
    
    Signed-off-by: Manish Varma <varmam@google.com>
    Change-Id: I330a42ab48bed4b26d5eb2f636925c66061165ec

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -26,6 +26,7 @@
 #include <trace/hooks/gic_v3.h>
 #include <trace/hooks/epoch.h>
 #include <trace/hooks/cpufreq.h>
+#include <trace/hooks/fs.h>
 #include <trace/hooks/mm.h>
 #include <trace/hooks/preemptirq.h>
 #include <trace/hooks/ftrace_dump.h>
@@ -201,3 +202,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_memory_ro);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_memory_rw);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_module_permit_before_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_module_permit_after_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ep_create_wakeup_source);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timerfd_create);
