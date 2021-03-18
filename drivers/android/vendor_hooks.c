0250493b3880508529dbd4065abfe897707892a9
Badhri Jagan Sridharan <badhri@google.com>
2021-03-18 16:15:41 -0700

    ANDROID: usb: typec: tcpci: Add vendor hook to mask vbus present
    This hook is for addressing hardware anomalies where
    TCPC_POWER_STATUS_VBUS_PRES bit can return 0 even before falling
    below sSinkDisconnect threshold.
    
    Handler has to set bypass to override the value that would otherwise
    be returned by this function.
    Handler can set vbus or clear vbus to indicate vbus present or absent
    
    Squashed <89f633a9f41259a168473917674dd5cb1bfb991d>
    "ANDROID: usb: typec: tcpci: Migrate restricted vendor hook"
    
    OOT_bug:
    Bug: 183149028
    Bug: 168245874
    Bug: 173252019
    Bug: 271294543
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    Change-Id: I2ca994d49b37bf8600c5913b892fde9acd0dc896
    (cherry picked from commit eeffe02c9672f931ba20f456c8e4fe5a1df10b94)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -299,3 +299,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_si_meminfo);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpci_override_toggling);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_typec_tcpci_get_vbus);
