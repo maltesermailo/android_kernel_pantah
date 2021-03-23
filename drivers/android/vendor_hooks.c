aef9b2852d37ab5475f3ff3c40f4bba74c9b926a
lijianzhong <lijianzhong@xiaomi.com>
2021-03-23 20:31:45 +0800

    ANDROID: cgroup: Add vendor hook for cpuset.
    This hook allows us to capture information when a process is forked so
    that we can stat and set some key task's CPU affinity in the ko module
    later. This patch, along with aosp/2548175, is necessary for our
    affinity settings.
    
    Bug: 429333853
    Bug: 425550656
    Bug: 339371220
    Bug: 183674818
    
    Signed-off-by: lijianzhong <lijianzhong@xiaomi.com>
    Change-Id: Ib93e05e5f6c338c5f7ada56bfebdd705f87f1f66
    (cherry picked from commit a188361628461c58a4dfc72869d9acb1dfa2542f)
    (cherry picked from commit 02992f5b8d68424ba34e1f905c3bde3b72aade88)
    (cherry picked from commit 5111c399168c03b0792226cf56b0176ff1c9a0ab)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -553,3 +553,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_charge);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_add_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_tsk_need_resched_lazy);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpuset_fork);
