779b82b125e6b0df3db63f4889b78efa12982532
lijianzhong <lijianzhong@xiaomi.com>
2021-03-23 20:31:45 +0800

    ANDROID: cgroup: Add vendor hook for cpuset.
    Add a vendor hook in cpuset_fork() to capture information when a process is forked, so that we can adjust the affinity according to the OEM's scheduling policy.
    
    Bug: 183674818
    Signed-off-by: lijianzhong <lijianzhong@xiaomi.com>
    Change-Id: Ib93e05e5f6c338c5f7ada56bfebdd705f87f1f66
    (cherry picked from commit a188361628461c58a4dfc72869d9acb1dfa2542f)

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -67,6 +67,8 @@
 #include <linux/cgroup.h>
 #include <linux/wait.h>
 
+#include <trace/hooks/cgroup.h>
+
 DEFINE_STATIC_KEY_FALSE(cpusets_pre_enable_key);
 DEFINE_STATIC_KEY_FALSE(cpusets_enabled_key);
 
@@ -3225,10 +3227,14 @@ static void cpuset_bind(struct cgroup_subsys_state *root_css)
  */
 static void cpuset_fork(struct task_struct *task)
 {
+	bool inherit_cpus = false;
+
 	if (task_css_is_root(task, cpuset_cgrp_id))
 		return;
 
-	set_cpus_allowed_ptr(task, current->cpus_ptr);
+	trace_android_rvh_cpuset_fork(task, &inherit_cpus);
+	if (!inherit_cpus)
+		set_cpus_allowed_ptr(task, current->cpus_ptr);
 	task->mems_allowed = current->mems_allowed;
 }
 
