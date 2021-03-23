b66ab6371c6cdf0dfc7804658e4f456bf8569d93
lijianzhong <lijianzhong@xiaomi.com>
2021-03-23 20:31:45 +0800

    ANDROID: cgroup: Add vendor hook for cpuset.
    add vendor hook for cpuset to support oem's feature.
    
    Bug: 183674818
    Signed-off-by: lijianzhong <lijianzhong@xiaomi.com>
    Change-Id: Ib93e05e5f6c338c5f7ada56bfebdd705f87f1f66

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -67,6 +67,7 @@
 #include <linux/wait.h>
 
 #include <trace/hooks/sched.h>
+#include <trace/hooks/cgroup.h>
 
 DEFINE_STATIC_KEY_FALSE(cpusets_pre_enable_key);
 DEFINE_STATIC_KEY_FALSE(cpusets_enabled_key);
@@ -2902,10 +2903,13 @@ static void cpuset_bind(struct cgroup_subsys_state *root_css)
  */
 static void cpuset_fork(struct task_struct *task)
 {
+	int should_forbidden = 0;
 	if (task_css_is_root(task, cpuset_cgrp_id))
 		return;
 
-	set_cpus_allowed_ptr(task, current->cpus_ptr);
+	trace_android_rvh_cpuset_fork(task, &should_forbidden);
+	if (!should_forbidden)
+		set_cpus_allowed_ptr(task, current->cpus_ptr);
 	task->mems_allowed = current->mems_allowed;
 }
 
