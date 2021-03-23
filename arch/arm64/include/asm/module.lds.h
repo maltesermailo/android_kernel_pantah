d3c0a9c7cd2048f0b25b2f0f17c1765f019b2a43
Ard Biesheuvel <ardb@kernel.org>
2021-03-23 10:54:38 +0100

    ANDROID: crypto: fips140 - perform load time integrity check
    In order to comply with FIPS 140-2 requirements, implement a fips140
    module that carries all AES, SHA-xxx and DRBG implementations with the
    associated chaining mode templates, and perform an integrity selfcheck
    at load time. The algorithms contained in the module will be registered
    with the crypto API, and will supersede any existing copies of the same
    algorithms that were already being provided by the core kernel.
    
    Bug: 153614920
    Change-Id: Ia893d9992fc12e2617d1ed2899c9794859c389d1
    Signed-off-by: Ard Biesheuvel <ardb@google.com>

--- arch/arm64/include/asm/module.lds.h
+++ arch/arm64/include/asm/module.lds.h
@@ -3,5 +3,25 @@ SECTIONS {
 	.plt 0 (NOLOAD) : { BYTE(0) }
 	.init.plt 0 (NOLOAD) : { BYTE(0) }
 	.text.ftrace_trampoline 0 (NOLOAD) : { BYTE(0) }
+
+#ifdef CONFIG_CRYPTO_FIPS140_INTEGRITY_CHECK
+#define INIT_CALLS_LEVEL(level)						\
+		KEEP(*(.initcall##level##.init*))			\
+		KEEP(*(.initcall##level##s.init*))
+
+	.initcalls : {
+		*(.initcalls._start)
+		INIT_CALLS_LEVEL(0)
+		INIT_CALLS_LEVEL(1)
+		INIT_CALLS_LEVEL(2)
+		INIT_CALLS_LEVEL(3)
+		INIT_CALLS_LEVEL(4)
+		INIT_CALLS_LEVEL(5)
+		INIT_CALLS_LEVEL(rootfs)
+		INIT_CALLS_LEVEL(6)
+		INIT_CALLS_LEVEL(7)
+		*(.initcalls._end)
+	}
+#endif
 }
 #endif
