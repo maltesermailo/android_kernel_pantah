60df764be9dca817ebcf35bd9fa6caabcc7a3f1c
Isaac J. Manjarres <isaacm@codeaurora.org>
2021-04-01 18:34:51 -0700

    FROMLIST: iommu/arm-smmu: Implement the map_pages() IOMMU driver callback
    Implement the unmap_pages() callback for the ARM SMMU driver
    to allow calls from iommu_unmap to unmap multiple pages of
    the same size in one call.
    
    Bug: 178537788
    Change-Id: Ie35bd1842da60e4b63b0acad37a6cd095dcfea9d
    Link: https://lore.kernel.org/linux-iommu/20210405191112.28192-1-isaacm@codeaurora.org/T/#t
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>
    Suggested-by: Will Deacon <will@kernel.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1216,6 +1216,24 @@ static int arm_smmu_map(struct iommu_domain *domain, unsigned long iova,
 	return ret;
 }
 
+static int arm_smmu_map_pages(struct iommu_domain *domain, unsigned long iova,
+			      phys_addr_t paddr, size_t pgsize, size_t pgcount,
+			      int prot, gfp_t gfp, size_t *mapped)
+{
+	struct io_pgtable_ops *ops = to_smmu_domain(domain)->pgtbl_ops;
+	struct arm_smmu_device *smmu = to_smmu_domain(domain)->smmu;
+	int ret;
+
+	if (!ops)
+		return -ENODEV;
+
+	arm_smmu_rpm_get(smmu);
+	ret = ops->map_pages(ops, iova, paddr, pgsize, pgcount, prot, gfp, mapped);
+	arm_smmu_rpm_put(smmu);
+
+	return ret;
+}
+
 static size_t arm_smmu_unmap(struct iommu_domain *domain, unsigned long iova,
 			     size_t size, struct iommu_iotlb_gather *gather)
 {
@@ -1627,6 +1645,7 @@ static struct iommu_ops arm_smmu_ops = {
 	.domain_free		= arm_smmu_domain_free,
 	.attach_dev		= arm_smmu_attach_dev,
 	.map			= arm_smmu_map,
+	.map_pages		= arm_smmu_map_pages,
 	.unmap			= arm_smmu_unmap,
 	.unmap_pages		= arm_smmu_unmap_pages,
 	.flush_iotlb_all	= arm_smmu_flush_iotlb_all,
