044bc6d3488a6cf07aa919ade9e901e7509adb25
Pavankumar Kondeti <quic_pkondeti@quicinc.com>
2021-04-06 06:34:36 +0530

    ANDROID: sched: Update android_rvh_check_preempt_wakeup hook
    android_rvh_check_preempt_wakeup hook is in place to allow vendor
    modules to force the running task preemption by the waking task.
    Update the tracepoint to accept another input to not preempting
    the current running task. The hook is moved further down so that
    it can be updated to pass the sched_entity structure corresponding
    to waking and running tasks in the next patch.
    
    Bug: 184575210
    Change-Id: Id4f45ba2819802636b6b86ed34c124771d0d69eb
    Signed-off-by: Pavankumar Kondeti <quic_pkondeti@quicinc.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -255,8 +255,8 @@ DECLARE_RESTRICTED_HOOK(android_rvh_pick_next_entity,
 	TP_ARGS(cfs_rq, curr, se), 1);
 
 DECLARE_RESTRICTED_HOOK(android_rvh_check_preempt_wakeup,
-	TP_PROTO(struct rq *rq, struct task_struct *p, bool *preempt),
-	TP_ARGS(rq, p, preempt), 1);
+	TP_PROTO(struct rq *rq, struct task_struct *p, bool *preempt, bool *nopreempt),
+	TP_ARGS(rq, p, preempt, nopreempt), 1);
 
 DECLARE_RESTRICTED_HOOK(android_rvh_force_compatible_pre,
 	TP_PROTO(void *unused),
