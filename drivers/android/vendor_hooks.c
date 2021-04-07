743835fad9a5a5827c4cf69fefe752f1914dba19
Huang Yiwei <hyiwei@codeaurora.org>
2021-04-07 17:08:00 +0800

    ANDROID: hung_task: Add vendor hook for hung task detect
    Add vendor hook for hung task detect, so we can decide which
    threads need to check, avoiding false alarms.
    
    Bug: 188684133
    Change-Id: I5d7dfeb071cbfda8121134c38a458202aaa3a8c6
    Signed-off-by: Huang Yiwei <hyiwei@codeaurora.org>
    [xuewen: minor conflicts fixups]
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    [guochen: Fix conflict in check_hung_uninterruptible_tasks]
    Signed-off-by: Guo Chen <guo.chen@oss.qualcomm.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -36,6 +36,7 @@
 #include <trace/hooks/timer.h>
 #include <trace/hooks/fpsimd.h>
 #include <trace/hooks/signal.h>
+#include <trace/hooks/hung_task.h>
 #include <trace/hooks/topology.h>
 #include <trace/hooks/ftrace_dump.h>
 
@@ -96,4 +97,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks_dn);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ftrace_format_check);
