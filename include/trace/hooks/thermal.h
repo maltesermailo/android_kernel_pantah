c42a3d59d3683005ddcc32834655c13b19d466a3
David Chao <davidchao@google.com>
2021-04-07 17:49:53 +0800

    ANDROID: thermal: Add vendor thermal_pm_notify_suspend function
    Currently, most of the thermal_zones are IRQ capable and they do not need
    to be updated while resuming. To improve the system performance and reduce
    the resume time. Add a vendor function to check if the thermal_zone is
    not IRQ capable and needs to be updated.
    
    Bug: 170905417
    Test: boot and vendor function worked properly.
    Change-Id: I9389985bba29b551a7a20b55e1ed26b6c4da9b3d
    Signed-off-by: David Chao <davidchao@google.com>

--- include/trace/hooks/thermal.h
+++ include/trace/hooks/thermal.h
@@ -7,12 +7,17 @@
 #if !defined(_TRACE_HOOK_THERMAL_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_HOOK_THERMAL_H
 
+#include <linux/thermal.h>
 #include <linux/tracepoint.h>
 #include <trace/hooks/vendor_hooks.h>
 
 DECLARE_HOOK(android_vh_enable_thermal_genl_check,
 	TP_PROTO(int event, int tz_id, int *enable_thermal_genl),
 	TP_ARGS(event, tz_id, enable_thermal_genl));
+
+DECLARE_HOOK(android_vh_thermal_pm_notify_suspend,
+	     TP_PROTO(struct thermal_zone_device *tz, int *irq_wakeable),
+	     TP_ARGS(tz, irq_wakeable));
 
 #endif /* _TRACE_HOOK_THERMAL_H */
 /* This part must be outside protection */
