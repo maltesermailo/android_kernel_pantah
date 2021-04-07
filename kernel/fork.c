BayLibre/android_kernel_common

ANDROID: sched: Add vendor hooks for sched.
Add vendor hooks in sched to support oem's feature.

Bug: 183543991

Signed-off-by: lijianzhong <lijianzhong@xiaomi.com>
Change-Id: I8415958749948b3702e411f835c227ad4f8d8e92

kernel/fork.c
@@ -109,6 +109,8 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/task.h>
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/sched.h>
 /*
  * Minimum number of threads to boot the kernel
  */
@@ -446,6 +448,7 @@ void free_task(struct task_struct *tsk)
 	cpufreq_task_times_exit(tsk);
 	scs_release(tsk);
 
+	trace_android_rvh_oem_free_task(tsk);
 #ifndef CONFIG_THREAD_INFO_IN_TASK
 	/*
 	 * The task is finally done with both the stack and thread_info,

maltesermailo/android_kernel_pantah

Bug: 183674818

kernel/fork.c
@@ -109,6 +109,8 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/task.h>
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/sched.h>
 /*
  * Minimum number of threads to boot the kernel
  */
@@ -446,6 +448,7 @@ void free_task(struct task_struct *tsk)
 	cpufreq_task_times_exit(tsk);
 	scs_release(tsk);
 
+	trace_android_vh_free_task(tsk);
 #ifndef CONFIG_THREAD_INFO_IN_TASK
 	/*
 	 * The task is finally done with both the stack and thread_info,