b7248ee1c21ceddd51d7d6f4289d5a88c9a8366f
Matthew Maurer <mmaurer@google.com>
2021-04-14 13:35:26 -0700

    ANDROID: trusty: Support setting FF-A Tag
    Allow optional specification of platform specific tags.
    
    Signed-off-by: Matthew Maurer <mmaurer@google.com>
    Bug: 181979088
    Change-Id: I94bacab620136f9322d9f80d7d50c44d8c8d8fcc

--- include/linux/trusty/trusty.h
+++ include/linux/trusty/trusty.h
@@ -68,8 +68,16 @@ int trusty_share_memory(struct device *dev, trusty_shared_mem_id_t *id,
 int trusty_share_memory_compat(struct device *dev, trusty_shared_mem_id_t *id,
 			       struct scatterlist *sglist, unsigned int nents,
 			       pgprot_t pgprot);
+int trusty_transfer_memory(struct device *dev, u64 *id,
+			   struct scatterlist *sglist, unsigned int nents,
+			   pgprot_t pgprot, u64 tag);
 int trusty_reclaim_memory(struct device *dev, trusty_shared_mem_id_t id,
 			  struct scatterlist *sglist, unsigned int nents);
+
+#ifdef CONFIG_TRUSTY_DMA_BUF_FFA_TAG
+struct dma_buf;
+u64 trusty_dma_buf_get_ffa_tag(struct dma_buf *dma_buf);
+#endif
 
 struct trusty_nop {
 	struct list_head node;
