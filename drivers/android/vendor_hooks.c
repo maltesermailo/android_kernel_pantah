7a1bb46f5032b9fcca1e554bfac8e663b68611f8
Georgi Djakov <quic_c_gdjako@quicinc.com>
2021-04-19 09:55:11 -0700

    ANDROID: psi: Add vendor hooks for PSI tracing
    Add hooks to capture various per-zone memory stats when
    a trigger threshold is hit.
    
    Bug: 268290366
    Change-Id: Ibe39263ddb05ffc3fa63b5225497a90c6480c8d7
    Signed-off-by: Georgi Djakov <quic_c_gdjako@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -61,6 +61,7 @@
 #include <trace/hooks/audio_usboffload.h>
 #include <trace/hooks/typec.h>
 #include <trace/hooks/user.h>
+#include <trace/hooks/psi.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -230,3 +231,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpuset_fork);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_event);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_group);
