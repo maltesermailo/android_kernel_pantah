ed4dcef7e399f4ce322c470605110f1e347338e2
Prakash Gupta <guptap@codeaurora.org>
2021-04-22 19:33:23 +0530

    ANDROID: mm, oom: add vendor hook
    Add vendor hook for oom_panic_timeout to allow vendor enhancement.
    
    Bug: 186875166
    Change-Id: I64e74b2c013d6f18d74504777c6559d9ae07e008
    Signed-off-by: Prakash Gupta <guptap@codeaurora.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -51,6 +51,9 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/oom.h>
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/mm.h>
+
 int sysctl_panic_on_oom;
 int sysctl_oom_kill_allocating_task;
 int sysctl_oom_dump_tasks = 1;
@@ -1092,6 +1095,7 @@ EXPORT_SYMBOL_GPL(unregister_oom_notifier);
 bool out_of_memory(struct oom_control *oc)
 {
 	unsigned long freed = 0;
+	int ret;
 
 	if (oom_killer_disabled)
 		return false;
@@ -1144,8 +1148,13 @@ bool out_of_memory(struct oom_control *oc)
 	}
 
 	select_bad_process(oc);
+
 	/* Found nothing?!?! */
 	if (!oc->chosen) {
+		trace_android_vh_oom_panic_timeout(oc, &ret);
+		if (ret)
+			return true;
+
 		dump_header(oc, NULL);
 		pr_warn("Out of memory and no killable processes...\n");
 		/*
