89c7feb95f47a1e80b28b9dca42489e18c9c5e31
zhengding chen <chenzhengding@oppo.com>
2021-04-28 19:24:57 +0800

    ANDROID: vendor_hooks: Add hooks for binder proc transaction
    When servicemanager process added service proxy from other process
    register the service, we want to know the matching relation between
    handle in the process and service name. When binder transaction
    happened, We want to know what process calls what method on what service.
    
    Bug: 186604985
    
    Signed-off-by: zhengding chen <chenzhengding@oppo.com>
    Change-Id: I813d1cde10294d8665f899f7fef0d444ec1f1f5e
    (cherry picked from commit e107ea9e4dd1808d71922fb489297b4235ac6031)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -244,4 +244,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_page_add_new_anon_rmap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_event);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_group);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpufreq_acct_update_power);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_proc_transaction);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
