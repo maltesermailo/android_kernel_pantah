936371fa7a0d11eb08462685b8c746a3af834fca
Daejun Park <daejun7.park@samsung.com>
2021-04-28 10:11:15 +0900

    FROMLIST: scsi: ufs: Add HPB 2.0 support
    This patch supports the HPB 2.0.
    
    The HPB 2.0 supports read of varying sizes from 4KB to 512KB.
    In the case of Read (<= 32KB) is supported as single HPB read.
    In the case of Read (36KB ~ 512KB) is supported by as a combination of
    write buffer command and HPB read command to deliver more PPN.
    The write buffer commands may not be issued immediately due to busy tags.
    To use HPB read more aggressively, the driver can requeue the write buffer
    command. The requeue threshold is implemented as timeout and can be
    modified with requeue_timeout_ms entry in sysfs.
    
    Bug: 170940265
    
    Link: https://lore.kernel.org/linux-scsi/20210428075810epcms2p6f5331ed56db7959371b93a654feb60fe@epcms2p6/
    
    Reviewed-by: Can Guo <cang@codeaurora.org>
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Tested-by: Can Guo <cang@codeaurora.org>
    Signed-off-by: Daejun Park <daejun7.park@samsung.com>
    Change-Id: I0a54f9ff2c84eed17f77da59331d2400b7edffdc

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -648,6 +648,8 @@ struct ufs_hba_variant_params {
  * @srgn_size: device reported HPB sub-region size
  * @slave_conf_cnt: counter to check all lu finished initialization
  * @hpb_disabled: flag to check if HPB is disabled
+ * @max_hpb_single_cmd: maximum size of single HPB command
+ * @is_legacy: flag to check HPB 1.0
  */
 struct ufshpb_dev_info {
 	int num_lu;
@@ -655,6 +657,8 @@ struct ufshpb_dev_info {
 	int srgn_size;
 	atomic_t slave_conf_cnt;
 	bool hpb_disabled;
+	int max_hpb_single_cmd;
+	bool is_legacy;
 };
 #endif
 
