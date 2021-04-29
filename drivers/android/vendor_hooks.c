6a6b61c6d8e9d186c6cc689aa14cf4dffb0ad136
zhouhuacai <zhouohuacai@oppo.com>
2021-04-29 17:23:05 +0800

    ANDROID: vendor_hooks: tune reclaim inactive ratio
    Add hooks to apply oem's memory reclaim optimization
    
    Bug: 186669330
    Change-Id: I9a1c713bf3eea2823b2758ee44284a244b1ca7a1
    Signed-off-by: zhouhuacai <zhouohuacai@oppo.com>
    Signed-off-by: Tangquan Zheng <zhengtangquan@oppo.com>
    (cherry picked from commit 577f73412f579ef493e8e67795b3fd93f1a47dfd)
    Signed-off-by: zhaorou <v-zhaorou@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -356,6 +356,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_logbuf);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpufreq_acct_update_power);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_swappiness);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_inactive_ratio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf_pr_cont);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_calc_alloc_flags);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_regmap_update);
