43a210ffacb91a06a338c634dcb3566adf2af6ab
heshuai1 <heshuai1@xiaomi.com>
2021-05-07 19:40:52 +0800

    ANDROID: freezer: Add vendor hook to freezer for GKI purpose.
    Add the vendor hook to freezer.c, because of some special cases related to our feature, we do not want the process to be frozen immediately, so we add the hook at __refrigerator to make sure we can go to our own freeze logic when the process is about to be frozen.
    
    Bug: 187458531
    
    Signed-off-by: heshuai1 <heshuai1@xiaomi.com>
    Change-Id: Iea42fd9604d6b33ccd6502425416f0dd28eecebb
    (cherry picked from commit a1580311c36ca28344b2f03b3c8a72d9f8db5bde)

--- include/trace/hooks/cgroup.h
+++ include/trace/hooks/cgroup.h
@@ -20,6 +20,18 @@ struct cgroup_taskset;
 DECLARE_HOOK(android_vh_cgroup_set_task,
 	TP_PROTO(int ret, struct task_struct *task),
 	TP_ARGS(ret, task));
+<<<<<<< HEAD   (2b0d64 UPSTREAM: wifi: cfg80211: Allow P2P client interface to indi)
+=======
+DECLARE_RESTRICTED_HOOK(android_rvh_cpuset_fork,
+	TP_PROTO(struct task_struct *p, int *inherit_cpus),
+	TP_ARGS(p, inherit_cpus), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_cgroup_force_kthread_migration,
+	TP_PROTO(struct task_struct *tsk, struct cgroup *dst_cgrp, bool *force_migration),
+	TP_ARGS(tsk, dst_cgrp, force_migration), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_refrigerator,
+	TP_PROTO(bool f),
+	TP_ARGS(f), 1);
+>>>>>>> CHANGE (a15803 ANDROID: freezer: Add vendor hook to freezer for GKI purpose)
 
 DECLARE_HOOK(android_vh_cgroup_attach,
 	TP_PROTO(struct cgroup_subsys *ss, struct cgroup_taskset *tset),
