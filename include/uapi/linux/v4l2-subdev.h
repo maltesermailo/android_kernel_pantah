188ee5c4b52e6d7d61b54d0a8f22504df80a6a03
Louis Kuo <louis.kuo@mediatek.com>
2021-05-07 17:45:11 +0800

    FROMLIST: media: subdev: support which in v4l2_subdev_frame_interval
    This patch is to support try or set of VIDIOC_SUBDEV_ENUM_FRAME_INTERVAL through which parameter.
    This behavior is already supported in some similar V4L2 IOCTLs such as VIDIOC_SUBDEV_ENUM_FRAME_SIZE for the same reason.
    
    Bug: 187481455
    Link: https://lore.kernel.org/lkml/?q=20210507074604.15079-3-louis.kuo%40mediatek.com
    
    Signed-off-by: Louis Kuo <louis.kuo@mediatek.com>
    Change-Id: Idadbd1e55b9178a2934837e70fa145156161bc18

--- include/uapi/linux/v4l2-subdev.h
+++ include/uapi/linux/v4l2-subdev.h
@@ -112,11 +112,13 @@ struct v4l2_subdev_frame_size_enum {
  * struct v4l2_subdev_frame_interval - Pad-level frame rate
  * @pad: pad number, as reported by the media API
  * @interval: frame interval in seconds
+ * @which: format type (from enum v4l2_subdev_format_whence)
  */
 struct v4l2_subdev_frame_interval {
 	__u32 pad;
 	struct v4l2_fract interval;
-	__u32 reserved[9];
+	__u32 which;
+	__u32 reserved[8];
 };
 
 /**
