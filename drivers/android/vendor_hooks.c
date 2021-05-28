ffefb594d40b0a42fbdbb1d072ddd2a48201c712
qupanwang <qupanwang@xiaomi.com>
2021-05-28 16:18:35 +0800

    ANDROID: GKI: Add timerfd poll vendor hooks
    add timerfd poll vendor hook, we need add some info to print
    
    Bug: 189518440
    
    Signed-off-by: panwang qu <qupanwang@xiaomi.com>
    Change-Id: I48bf5c5141011e21b6284f891ee8042d220c4098

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -298,3 +298,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_vmalloc_stack);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_stack_hash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_track_hash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_task_comm);
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timerfd_poll);
