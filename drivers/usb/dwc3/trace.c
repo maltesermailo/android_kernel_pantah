60dac1076a3e1cee3ca5559f050fd44b5fe7ddab
fengmingli <mingli.feng@vivo.com>
2021-05-31 20:45:48 +0800

    ANDROID: usb: add EXPORT_TRACE_SYMBOL to export tracepoint
    To use the tracepoint in kernel module, add EXPORT_TRACE_SYMBOL_GPL to
    export the dwc3_event tracepoint.
    
    Bug: 189130101
    
    Change-Id: I3a917af82c9d8c19a085c5fc1a30c1b9af4b6885
    Signed-off-by: fengmingli <mingli.feng@vivo.com>
    (cherry picked from commit fe2ecaf37f1debb97870f5449bab79c93559a387)

--- drivers/usb/dwc3/trace.c
+++ drivers/usb/dwc3/trace.c
@@ -9,3 +9,11 @@
 
 #define CREATE_TRACE_POINTS
 #include "trace.h"
+<<<<<<< HEAD   (48246c ANDROID: Update the ABI symbol list)
+=======
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_ep_queue);
+EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_readl);
+EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_writel);
+EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_event);
+>>>>>>> CHANGE (fe2eca ANDROID: usb: add EXPORT_TRACE_SYMBOL to export tracepoint)
