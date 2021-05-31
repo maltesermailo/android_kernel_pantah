8f2062320bc3ebe6a8a8ea215024f70390451b4f
liuwei.a <liuwei.a@oppo.com>
2021-05-31 16:34:54 +0800

    ANDROID: vendor hook:request add vendor hook for tcp_write_timeout
    Change reason:In our network monitor system, we need to trace the tcp write timeout event.
    
    Bug: 189706322
    Signed-off-by: Wei Liu <liuwei.a@oppo.com>
    Change-Id: Ic7779fd4a37921123faf9d221ec6f2a647545a48

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -57,6 +57,7 @@
 #include <trace/hooks/selinux.h>
 #include <trace/hooks/hung_task.h>
 #include <trace/hooks/mmc_core.h>
+#include <trace/hooks/tcp_timeout.h>
 #include <trace/hooks/v4l2core.h>
 #include <trace/hooks/v4l2mc.h>
 #include <trace/hooks/scmi.h>
@@ -307,6 +308,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_vmalloc_stack);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_stack_hash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_track_hash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_task_comm);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tcp_timeout);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpufreq_acct_update_power);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpm_log);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_media_device_setup_link);
