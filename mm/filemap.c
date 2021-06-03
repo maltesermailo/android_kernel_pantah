d145a5a403b34a92625746715f5830e18f97b4f7
Liangliang Li <liliangliang@vivo.com>
2021-06-03 22:35:56 +0800

    ANDROID: mm: Add hooks to filemap_fault for oem's optimization
    Add vendor_hooks to filemap_fault to cache page for oem's optimization.
    Save the page for next time retry when VM_FAULT_RETRY returned.
    
    Add ANDROID_OEM_DATA to vm_fault to save the page.
    
    Bug: 188891314
    Change-Id: Ibfc9ec950c3360e6f6ccb9546cab0acd89e5d316
    Signed-off-by: Liangliang Li <liliangliang@vivo.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2728,12 +2728,18 @@ vm_fault_t filemap_fault(struct vm_fault *vmf)
 	struct inode *inode = mapping->host;
 	pgoff_t offset = vmf->pgoff;
 	pgoff_t max_off;
-	struct page *page;
+	struct page *page = NULL;
 	vm_fault_t ret = 0;
 
 	max_off = DIV_ROUND_UP(i_size_read(inode), PAGE_SIZE);
 	if (unlikely(offset >= max_off))
 		return VM_FAULT_SIGBUS;
+
+	trace_android_vh_filemap_fault_get_page(vmf, &page, &ret);
+	if (unlikely(ret))
+		goto out_retry;
+	if (unlikely(page))
+		goto page_ok;
 
 	/*
 	 * Do we have something in the page cache already?
@@ -2790,6 +2796,7 @@ retry_find:
 		goto out_retry;
 	}
 
+page_ok:
 	/*
 	 * Found the page and have a reference on it.
 	 * We must recheck i_size under page lock.
@@ -2835,6 +2842,7 @@ out_retry:
 	 * re-find the vma and come back and find our hopefully still populated
 	 * page.
 	 */
+	trace_android_vh_filemap_fault_cache_page(vmf, &page);
 	if (page)
 		put_page(page);
 	if (fpin)
