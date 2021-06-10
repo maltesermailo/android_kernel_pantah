4ddd6b85f07a7f0afa3e91db5eae439004c545ff
Quentin Perret <qperret@google.com>
2021-06-10 15:13:06 +0000

    BACKPORT: FROMLIST: sched: Make uclamp changes depend on CAP_SYS_NICE
    There is currently nothing preventing tasks from changing their per-task
    clamp values in anyway that they like. The rationale is probably that
    system administrators are still able to limit those clamps thanks to the
    cgroup interface. However, this causes pain in a system where both
    per-task and per-cgroup clamp values are expected to be under the
    control of core system components (as is the case for Android).
    
    To fix this, let's require CAP_SYS_NICE to increase per-task clamp
    values. This allows unprivileged tasks to lower their requests, but not
    increase them, which is consistent with the existing behaviour for nice
    values.
    
    BACKPORT rationale:
      There are ongoing discussions upstream about more flexible approaches
      than this using the RLIMIT API -- see [1]. But the upstream discussion
      has not converged yet, and this is way too late for UAPI changes in
      android12-5.10 anyway, so let's apply this change which provides the
      behaviour we want without actually impacting UAPIs.
    
    [1] https://lore.kernel.org/lkml/20210623123441.592348-4-qperret@google.com/
    
    Bug: 187186685
    Link: https://lore.kernel.org/r/20210610151306.1789549-4-qperret@google.com
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I749312a77306460318ac5374cf243d00b78120dd

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1476,6 +1476,11 @@ static int uclamp_validate(struct task_struct *p,
 	if (util_min != -1 && util_max != -1 && util_min > util_max)
 		return -EINVAL;
 
+	return 0;
+}
+
+static void uclamp_enable(void)
+{
 	/*
 	 * We have valid uclamp attributes; make sure uclamp is enabled.
 	 *
@@ -1484,8 +1489,32 @@ static int uclamp_validate(struct task_struct *p,
 	 * scheduler locks.
 	 */
 	static_branch_enable(&sched_uclamp_used);
+}
 
-	return 0;
+static bool uclamp_reduce(struct task_struct *p, const struct sched_attr *attr)
+{
+	if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP_MIN) {
+		int util_min = p->uclamp_req[UCLAMP_MIN].value;
+
+		if (attr->sched_util_min + 1 > util_min + 1)
+			return false;
+
+		if (rt_task(p) && attr->sched_util_min == -1 &&
+		    util_min < sysctl_sched_uclamp_util_min_rt_default)
+			return false;
+	}
+
+	if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP_MAX) {
+		int util_max = p->uclamp_req[UCLAMP_MAX].value;
+
+		if (attr->sched_util_max + 1 > util_max + 1)
+			return false;
+
+		if (attr->sched_util_max == -1 && util_max < uclamp_none(UCLAMP_MAX))
+			return false;
+	}
+
+	return true;
 }
 
 static bool uclamp_reset(const struct sched_attr *attr,
@@ -1627,6 +1656,11 @@ static inline int uclamp_validate(struct task_struct *p,
 				  const struct sched_attr *attr)
 {
 	return -EOPNOTSUPP;
+}
+static inline void uclamp_enable(void) { }
+static bool uclamp_reduce(struct task_struct *p, const struct sched_attr *attr)
+{
+	return true;
 }
 static void __setscheduler_uclamp(struct task_struct *p,
 				  const struct sched_attr *attr) { }
@@ -5491,6 +5525,13 @@ recheck:
 	    (rt_policy(policy) != (attr->sched_priority != 0)))
 		return -EINVAL;
 
+	/* Update task specific "requested" clamps */
+	if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP) {
+		retval = uclamp_validate(p, attr);
+		if (retval)
+			return retval;
+	}
+
 	/*
 	 * Allow unprivileged RT tasks to decrease priority:
 	 */
@@ -5540,6 +5581,10 @@ recheck:
 		/* Normal users shall not reset the sched_reset_on_fork flag: */
 		if (p->sched_reset_on_fork && !reset_on_fork)
 			return -EPERM;
+
+		/* Can't increase util-clamps */
+		if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP && !uclamp_reduce(p, attr))
+			return -EPERM;
 	}
 
 	if (user) {
@@ -5551,12 +5596,8 @@ recheck:
 			return retval;
 	}
 
-	/* Update task specific "requested" clamps */
-	if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP) {
-		retval = uclamp_validate(p, attr);
-		if (retval)
-			return retval;
-	}
+	if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP)
+		uclamp_enable();
 
 	/*
 	 * Make sure no PI-waiters arrive (or leave) while we are
