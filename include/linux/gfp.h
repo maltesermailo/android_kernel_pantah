a4f48dcd88f19e7add3e03ee051088ae0be542fe
Minchan Kim <minchan@google.com>
2021-06-29 12:08:44 -0700

    ANDROID: mm: cma: skip problematic pageblock
    alloc_contig_range is supposed to work on max(MAX_ORDER_NR_PAGES,
    or pageblock_nr_pages) granularity aligned range. If it fails
    at a page and return error to user, user doesn't know what page
    makes the allocation failure and keep retrying another allocation
    with new range including the failed page and encountered error
    again and again until it could escape the out of the granularity
    block. Instead, let's make CMA aware of what pfn was troubled in
    previous trial and then continue to work new pageblock out of the
    failed page so it doesn't see the repeated error repeatedly.
    
    Currently, this option works for only __GFP_NORETRY case for
    safe for existing CMA users.
    
    Bug: 192475091
    Signed-off-by: Minchan Kim <minchan@google.com>
    Change-Id: I0959c9df3d4b36408a68920abbb4d52d31026079

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -642,6 +642,8 @@ static inline bool pm_suspended_storage(void)
 #endif /* CONFIG_PM_SLEEP */
 
 #ifdef CONFIG_CONTIG_ALLOC
+extern unsigned long pfn_max_align_up(unsigned long pfn);
+
 enum acr_failure {
 	ACR_FAIL_ISOLATE,
 	ACR_FAIL_MIGRATE,
@@ -653,6 +655,7 @@ struct acr_info {
 	unsigned long nr_migrated;
 	unsigned long nr_reclaimed;
 	enum acr_failure fail;
+	unsigned long failed_pfn;
 };
 
 /* The below functions must be run on a range from a single zone. */
--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -8541,13 +8541,13 @@ struct page *has_unmovable_pages(struct zone *zone, struct page *page,
 }
 
 #ifdef CONFIG_CONTIG_ALLOC
-static unsigned long pfn_max_align_down(unsigned long pfn)
+unsigned long pfn_max_align_down(unsigned long pfn)
 {
 	return pfn & ~(max_t(unsigned long, MAX_ORDER_NR_PAGES,
 			     pageblock_nr_pages) - 1);
 }
 
-static unsigned long pfn_max_align_up(unsigned long pfn)
+unsigned long pfn_max_align_up(unsigned long pfn)
 {
 	return ALIGN(pfn, max_t(unsigned long, MAX_ORDER_NR_PAGES,
 				pageblock_nr_pages));
@@ -8652,6 +8652,11 @@ static int __alloc_contig_migrate_range(struct compact_control *cc,
 			page_pinner_mark_migration_failed_pages(&cc->migratepages);
 		}
 
+		if (!list_empty(&cc->migratepages)) {
+			page = list_first_entry(&cc->migratepages, struct page , lru);
+			info->failed_pfn = page_to_pfn(page);
+		}
+
 		putback_movable_pages(&cc->migratepages);
 		info->fail = ACR_FAIL_MIGRATE;
 		return ret;
@@ -8725,7 +8730,8 @@ int alloc_contig_range(unsigned long start, unsigned long end,
 	 */
 
 	ret = start_isolate_page_range(pfn_max_align_down(start),
-				       pfn_max_align_up(end), migratetype, 0);
+				       pfn_max_align_up(end), migratetype, 0,
+				       &info->failed_pfn);
 	if (ret) {
 		info->fail = ACR_FAIL_ISOLATE;
 		return ret;
@@ -8789,7 +8795,7 @@ int alloc_contig_range(unsigned long start, unsigned long end,
 	}
 
 	/* Make sure the range is really isolated. */
-	if (test_pages_isolated(outer_start, end, 0)) {
+	if (test_pages_isolated(outer_start, end, 0, &info->failed_pfn)) {
 		pr_info_ratelimited("%s: [%lx, %lx) PFNs busy\n",
 			__func__, outer_start, end);
 		ret = -EBUSY;
