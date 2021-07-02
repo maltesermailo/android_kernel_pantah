a36f3a584d39ce67af50d730be04de3154617747
Ram Prakash Gupta <rampraka@codeaurora.org>
2021-07-02 15:39:21 +0530

    FROMLIST: mmc: core: Export core functions required for clk scaling
    Export core function required for clk scaling post design change.
    This would help make clk scaling part of vendor module.
    
    Bug: 192337957
    Link: https://lore.kernel.org/lkml/1571668177-3766-2-git-send-email-
    rampraka@codeaurora.org/
    Change-Id: I0c5eccb2052197b3a290f7322429cab679c5ade5
    Signed-off-by: Ram Prakash Gupta <rampraka@codeaurora.org>
    Signed-off-by: Nitin Rawat <nitirawa@codeaurora.org>

--- drivers/mmc/core/queue.c
+++ drivers/mmc/core/queue.c
@@ -71,6 +71,7 @@ enum mmc_issue_type mmc_issue_type(struct mmc_queue *mq, struct request *req)
 
 	return MMC_ISSUE_SYNC;
 }
+EXPORT_SYMBOL(mmc_issue_type);
 
 static void __mmc_cqe_recovery_notifier(struct mmc_queue *mq)
 {
