5205472dda120e83594b85f0bf9f2536c335cd28
Puma Hsu <pumahsu@google.com>
2021-07-10 16:11:10 +0800

    ANDROID: usb: Add vendor hook for usb suspend and resume
    Add the hook that vendor can design and bypass the suspend/resume.
    When the bypass is set, skip the orignal suspend/resume methods.
    
    In mobile, a co-processor can be used with USB audio, and ACPU may
    be able to sleep in such condition to improve power consumption.
    We will need vendor hook to support this.
    
    Bug: 192774581
    Signed-off-by: Puma Hsu <pumahsu@google.com>
    Change-Id: Ic62a8a1e662bbe3fb0aa17af7491daace0b9f18a

---
+++ include/trace/hooks/usb.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM usb
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH trace/hooks
+#if !defined(_TRACE_HOOK_USB_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_USB_H
+#include <linux/tracepoint.h>
+#include <linux/usb.h>
+#include <trace/hooks/vendor_hooks.h>
+/*
+ * Following tracepoints are not exported in tracefs and provide a
+ * mechanism for vendor modules to hook and extend functionality
+ */
+struct usb_device;
+
+DECLARE_HOOK(android_vh_usb_dev_suspend,
+	TP_PROTO(struct usb_device *udev, int *bypass),
+	TP_ARGS(udev, bypass));
+
+DECLARE_HOOK(android_vh_usb_dev_resume,
+	TP_PROTO(struct usb_device *udev, int *bypass),
+	TP_ARGS(udev, bypass));
+
+#endif /* _TRACE_HOOK_USB_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+
