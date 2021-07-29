34eb336f54e44e0d998998cde897d3c7ab136219
zhengding chen <chenzhengding@oppo.com>
2021-07-29 14:41:25 +0800

    ANDROID: GKI: Export symbols for cpufreq governor
    Export cpufreq_driver_test_flags and topology_scale_freq_invariant for cpufreq governor.
    
    Bug: 194967443
    
    Signed-off-by: zhengding chen <chenzhengding@oppo.com>
    Change-Id: I971bf82010c0925a87a8407d4236be2c5e3fd37d

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1931,6 +1931,7 @@ bool cpufreq_driver_test_flags(u16 flags)
 {
 	return !!(cpufreq_driver->flags & flags);
 }
+EXPORT_SYMBOL_GPL(cpufreq_driver_test_flags);
 
 /**
  *	cpufreq_get_current_driver - return current driver's name
