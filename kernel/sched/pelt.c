883a1f0c27c16881ea4c436140eacacdc2bf44a2
JianMin Liu <jian-min.liu@mediatek.com>
2021-08-04 11:52:40 +0800

    ANDROID: sched: export symbols to change PELT half-life
    Export PELT half-life symbols for vendor module to
    dynamically switch to an alternative half-life after booted
    , e.g. 8ms.
    
    The vendor module is expected to quiesce the scheduler
    while changing these symbols.
    
    Bug: 195474490
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>
    Change-Id: Ife41997a032fe3384cfa126cbf7aee929c5c11cf

--- kernel/sched/pelt.c
+++ kernel/sched/pelt.c
@@ -31,6 +31,9 @@
 int pelt_load_avg_period = PELT32_LOAD_AVG_PERIOD;
 int pelt_load_avg_max = PELT32_LOAD_AVG_MAX;
 const u32 *pelt_runnable_avg_yN_inv = pelt32_runnable_avg_yN_inv;
+EXPORT_SYMBOL_GPL(pelt_load_avg_period);
+EXPORT_SYMBOL_GPL(pelt_load_avg_max);
+EXPORT_SYMBOL_GPL(pelt_runnable_avg_yN_inv);
 
 static int __init set_pelt(char *str)
 {
