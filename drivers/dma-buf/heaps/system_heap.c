5c230571acf3e9ddc767ae642f9c8de3a4583bed
Guangming Cao <Guangming.Cao@mediatek.com>
2021-09-07 16:04:24 +0800

    ANDROID: dma_heap: remove mapping check for dmabuf cache sync
    as mentioned in bellow commit:
    https://lore.kernel.org/lkml/20201121235002.69945-5-john.stultz@linaro.org/
    Using dma_sync_sgtable_for_cpu(device) maybe dangerous because
    sg_table->scatterlist.dma_address is invalid(maybe equals to zero).
    
    However we found it's valid even sg_table is not mapped, so, we think it should
    be removed on kernel-5.10, and thus userspace user can use libdmabufheap to
    do cache sync without fall into kernel to check if this dmabuf has attachments.
    
    Bug: 199121480
    Change-Id: Iad232580c944109e62cf8c48f484a979f42a9494
    Signed-off-by: Guangming Cao <Guangming.Cao@mediatek.com>

--- drivers/dma-buf/heaps/system_heap.c
+++ drivers/dma-buf/heaps/system_heap.c
@@ -180,8 +180,6 @@ static int system_heap_dma_buf_begin_cpu_access(struct dma_buf *dmabuf,
 
 	if (!buffer->uncached) {
 		list_for_each_entry(a, &buffer->attachments, list) {
-			if (!a->mapped)
-				continue;
 			dma_sync_sgtable_for_cpu(a->dev, a->table, direction);
 		}
 	}
@@ -203,8 +201,6 @@ static int system_heap_dma_buf_end_cpu_access(struct dma_buf *dmabuf,
 
 	if (!buffer->uncached) {
 		list_for_each_entry(a, &buffer->attachments, list) {
-			if (!a->mapped)
-				continue;
 			dma_sync_sgtable_for_device(a->dev, a->table, direction);
 		}
 	}
@@ -470,10 +466,8 @@ static struct dma_buf *system_heap_do_allocate(struct dma_heap *heap,
 	 * cpu and thus it is likely cached. Map (and implicitly flush) and
 	 * unmap it now so we don't get corruption later on.
 	 */
-	if (buffer->uncached) {
-		dma_map_sgtable(dma_heap_get_dev(heap), table, DMA_BIDIRECTIONAL, 0);
-		dma_unmap_sgtable(dma_heap_get_dev(heap), table, DMA_BIDIRECTIONAL, 0);
-	}
+	if (buffer->uncached)
+		dma_sync_sgtable_for_cpu(dma_heap_get_dev(heap), table, DMA_BIDIRECTIONAL);
 
 	return dmabuf;
 
