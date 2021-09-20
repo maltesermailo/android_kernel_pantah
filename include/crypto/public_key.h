ee76d0e7337fe9e8edaf1186cf03d74d98832e70
Greg Kroah-Hartman <gregkh@google.com>
2021-09-20 12:31:50 +0200

    Revert "crypto: public_key: fix overflow during implicit conversion"
    This partially reverts commit 5df14bba0056fb7d075dd6dc8c26ff3da33e4fc1
    which is commit f985911b7bc75d5c98ed24d8aaa8b94c590f7c6a upstream and
    came in the 5.10.67 release.
    
    The reordering of the fields was reverted, but the size change of the
    digest_size variable was kept.  This keeps the original size of the
    overall structure and resolves the overflow issue that the original bug
    was resolving.
    
    Fixes: faf816b0f8d0 ("Linux 5.10.67")
    Cc: Will Deacon <willdeacon@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ia884f7d3104a3479a7d983f05a50ac3c1acac39d

--- include/crypto/public_key.h
+++ include/crypto/public_key.h
@@ -39,8 +39,8 @@ extern void public_key_free(struct public_key *key);
 struct public_key_signature {
 	struct asymmetric_key_id *auth_ids[2];
 	u8 *s;			/* Signature */
-	u8 *digest;
 	u32 s_size;		/* Number of bytes in signature */
+	u8 *digest;
 	u32 digest_size;	/* Number of bytes in digest */
 	const char *pkey_algo;
 	const char *hash_algo;
