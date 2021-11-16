5505723e453e8f144aa1cdcec3777b66248d6194
Shaleen Agrawal <shalagra@codeaurora.org>
2021-11-16 10:57:40 -0800

    ANDROID: Add vendor hook while registering energy model
    Vendor architectures may contain CPUs running on the same clock line
    which contain different capacities. Add a tracehook in this path to
    allow vendor modules to skip implicit check to prevent crashes.
    
    Bug: 206602617
    Change-Id: Ica01a214689607b8d79b370c20bc9a8c44ca2117
    Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>

--- kernel/power/energy_model.c
+++ kernel/power/energy_model.c
@@ -286,6 +286,7 @@ int em_dev_register_perf_domain(struct device *dev, unsigned int nr_states,
 {
 	unsigned long cap, prev_cap = 0;
 	int cpu, ret;
+	bool cond = false;
 
 	if (!dev || !nr_states || !cb)
 		return -EINVAL;
@@ -314,6 +315,10 @@ int em_dev_register_perf_domain(struct device *dev, unsigned int nr_states,
 				ret = -EEXIST;
 				goto unlock;
 			}
+
+			trace_android_vh_em_dev_register_pd(&cond);
+			if (cond)
+				continue;
 			/*
 			 * All CPUs of a domain must have the same
 			 * micro-architecture since they all share the same
