77022863bf4149cd5554093ce7f1609760a7857b
jeson.gao <jeson.gao@unisoc.com>
2021-11-29 17:01:50 +0800

    ANDROID: thermal: Add vendor hooks for thermal
    Need to get the request frequency and target frequency
    use it to do frequency check and modify it according to
    the platform thermal requirements.
    
    Signed-off-by: jeson gao <jeson.gao@unisoc.com>
    Change-Id: I776b43c8f559b8a072abd8d3abcb3528348b2c5d

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -72,6 +72,7 @@
 #include <trace/hooks/ipv6.h>
 #include <trace/hooks/sound.h>
 #include <trace/hooks/snd_compr.h>
+#include <trace/hooks/thermal.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -369,3 +370,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_usb_dev_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ipv6_gen_linklocal_addr);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sound_usb_support_cpu_suspend);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_snd_compr_use_pause_in_drain);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_modify_thermal_request_freq);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_modify_thermal_target_freq);
