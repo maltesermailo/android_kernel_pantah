37d10d63e4f1e989c3e19ef164f9426fc6b34461
Eric Biggers <ebiggers@google.com>
2021-12-14 10:52:39 -0800

    ANDROID: fips140: expand fips140_lab_test.c to include AF_ALG tests
    We've received two new testing requirements:
    
       - Must call some crypto algorithms with invalid parameters to show
         that they fail.  (The details aren't precisely specified.)
    
       - Must provide output from the Jitter RNG.  A userspace build of the
         Jitter RNG which closely matches the kernel version could be used,
         but it would be preferable to use the kernel version directly.
    
    The easiest way to meet these requirements is to use AF_ALG.  This will
    start requiring custom kernel builds (not just custom module builds),
    though we need that for ACVP testing anyway.
    
    Add tests for these two requirements to fips140_lab_test.  Also refactor
    the program a bit and add the ability to select the tests to run.
    
    Bug: 188620248
    Change-Id: I3225feb21ced8383471c44d2b3c9b8e3d8468748
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- samples/crypto/fips140_lab_test.c
+++ samples/crypto/fips140_lab_test.c
@@ -2,12 +2,20 @@
 /*
  * Copyright 2021 Google LLC
  *
- * This is a sample program which calls some ioctls on /dev/fips140 and prints
- * the results.  The purpose of this program is to allow the FIPS certification
- * lab to test some services of fips140.ko, which they are required to do.  This
- * is a sample program only, and it can be modified by the lab as needed.  This
- * program must be run as root, and it only works if the system has loaded a
- * build of fips140.ko with evaluation testing support enabled.
+ * This is a sample program which does some tests of the fips140 module:
+ *
+ *   - Call some ioctls on /dev/fips140 and print the results.
+ *   - Use AF_ALG to try to call some crypto algorithms with invalid parameters,
+ *     and print the results.
+ *   - Use AF_ALG to dump some output from jitterentropy_rng.
+ *
+ * The purpose of this program is to allow the FIPS certification lab to test
+ * some services of fips140.ko, which they are required to do.  This is a sample
+ * program only, and it can be modified by the lab as needed.  This program must
+ * be run as root.  The tests that use /dev/fips140 only work if the system has
+ * loaded a build of fips140.ko that has evaluation testing support enabled.
+ * The tests that use AF_ALG only work if the kernel has AF_ALG enabled, and
+ * they will only test the fips140 module if it is actually loaded.
  *
  * This program can be compiled and run on an Android device as follows:
  *
@@ -17,30 +25,36 @@
  *	adb push fips140_lab_test /data/local/tmp/
  *	adb root
  *	adb shell /data/local/tmp/fips140_lab_test
+ *
+ * For advanced usage, try 'adb shell /data/local/tmp/fips140_lab_test --help'.
  */
 
 #include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
+#include <limits.h>
+#include <linux/if_alg.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
 #include <unistd.h>
 
 #include "../../crypto/fips140-eval-testing-uapi.h"
 
-static int fips140_dev_fd = -1;
+/* ---------------------------------------------------------------------------
+ *			       Utility functions
+ * ---------------------------------------------------------------------------*/
 
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
-static const char *booltostr(bool b)
-{
-	return b ? "true" : "false";
-}
+#define ARRAY_SIZE(A)	(sizeof(A) / sizeof((A)[0]))
+#define MIN(a, b)	((a) < (b) ? (a) : (b))
+#define MAX(a, b)	((a) > (b) ? (a) : (b))
 
 static void __attribute__((noreturn))
 do_die(const char *format, va_list va, int err)
@@ -73,6 +87,42 @@ die(const char *format, ...)
 	va_end(va);
 }
 
+static void __attribute__((noreturn))
+assertion_failed(const char *expr, const char *file, int line)
+{
+	die("Assertion failed: %s at %s:%d", expr, file, line);
+}
+
+#define ASSERT(e) ({ if (!(e)) assertion_failed(#e, __FILE__, __LINE__); })
+
+static void rand_bytes(uint8_t *bytes, size_t count)
+{
+	size_t i;
+
+	for (i = 0; i < count; i++)
+		bytes[i] = rand();
+}
+
+static const char *booltostr(bool b)
+{
+	return b ? "true" : "false";
+}
+
+static const char *bytes_to_hex(const uint8_t *bytes, size_t count)
+{
+	static char hex[1025];
+	size_t i;
+
+	ASSERT(count <= 512);
+	for (i = 0; i < count; i++)
+		sprintf(&hex[2*i], "%02x", bytes[i]);
+	return hex;
+}
+
+/* ---------------------------------------------------------------------------
+ *			      /dev/fips140 ioctls
+ * ---------------------------------------------------------------------------*/
+
 static int get_fips140_device_number(void)
 {
 	FILE *f;
@@ -82,7 +132,7 @@ static int get_fips140_device_number(void)
 
 	f = fopen("/proc/devices", "r");
 	if (!f)
-		die_errno("failed to open /proc/devices");
+		die_errno("Failed to open /proc/devices");
 	while (fgets(line, sizeof(line), f)) {
 		if (sscanf(line, "%d %31s", &number, name) == 2 &&
 		    strcmp(name, "fips140") == 0)
@@ -104,12 +154,25 @@ static void create_fips140_node_if_needed(void)
 
 	major = get_fips140_device_number();
 	if (mknod("/dev/fips140", S_IFCHR | 0600, makedev(major, 1)) != 0)
-		die_errno("failed to create fips140 device node");
+		die_errno("Failed to create fips140 device node");
+}
+
+static int fips140_dev_fd = -1;
+
+static int fips140_ioctl(int cmd, const void *arg)
+{
+	if (fips140_dev_fd < 0) {
+		create_fips140_node_if_needed();
+		fips140_dev_fd = open("/dev/fips140", O_RDONLY);
+		if (fips140_dev_fd < 0)
+			die_errno("Failed to open /dev/fips140");
+	}
+	return ioctl(fips140_dev_fd, cmd, arg);
 }
 
 static bool fips140_is_approved_service(const char *name)
 {
-	int ret = ioctl(fips140_dev_fd, FIPS140_IOCTL_IS_APPROVED_SERVICE, name);
+	int ret = fips140_ioctl(FIPS140_IOCTL_IS_APPROVED_SERVICE, name);
 
 	if (ret < 0)
 		die_errno("FIPS140_IOCTL_IS_APPROVED_SERVICE unexpectedly failed");
@@ -123,19 +186,67 @@ static bool fips140_is_approved_service(const char *name)
 
 static const char *fips140_module_version(void)
 {
-	char buf[256];
-	char *str;
-	int ret = ioctl(fips140_dev_fd, FIPS140_IOCTL_MODULE_VERSION, buf);
+	static char buf[256];
+	int ret;
 
+	memset(buf, 0, sizeof(buf));
+	ret = fips140_ioctl(FIPS140_IOCTL_MODULE_VERSION, buf);
 	if (ret < 0)
 		die_errno("FIPS140_IOCTL_MODULE_VERSION unexpectedly failed");
 	if (ret != 0)
-		die("FIPS140_IOCTL_MODULE_VERSION returned unexpected value %d", ret);
-	str = strdup(buf);
-	if (!str)
-		die("out of memory");
-	return str;
+		die("FIPS140_IOCTL_MODULE_VERSION returned unexpected value %d",
+		    ret);
+	return buf;
 }
+
+/* ---------------------------------------------------------------------------
+ *				AF_ALG utilities
+ * ---------------------------------------------------------------------------*/
+
+#define AF_ALG_MAX_RNG_REQUEST_SIZE	128
+
+static int get_alg_fd(const char *alg_type, const char *alg_name)
+{
+	struct sockaddr_alg addr = {};
+	int alg_fd;
+
+	alg_fd = socket(AF_ALG, SOCK_SEQPACKET, 0);
+	if (alg_fd < 0)
+		die("Failed to create AF_ALG socket.\n"
+"AF_ALG is only available when it has been enabled in the kernel.\n");
+
+	strncpy((char *)addr.salg_type, alg_type, sizeof(addr.salg_type) - 1);
+	strncpy((char *)addr.salg_name, alg_name, sizeof(addr.salg_name) - 1);
+
+	if (bind(alg_fd, (void *)&addr, sizeof(addr)) != 0)
+		die_errno("Failed to bind AF_ALG socket to %s %s",
+			  alg_type, alg_name);
+	return alg_fd;
+}
+
+static int get_req_fd(int alg_fd, const char *alg_name)
+{
+	int req_fd = accept(alg_fd, NULL, NULL);
+
+	if (req_fd < 0)
+		die_errno("Failed to get request file descriptor for %s",
+			  alg_name);
+	return req_fd;
+}
+
+/* ---------------------------------------------------------------------------
+ *			      Version service test
+ * ---------------------------------------------------------------------------*/
+
+static void test_version_service(void)
+{
+	printf("fips140_module_version() => \"%s\"\n\n",
+	       fips140_module_version());
+}
+
+/* ---------------------------------------------------------------------------
+ *			    Service indicators test
+ * ---------------------------------------------------------------------------*/
 
 static const char * const services_to_check[] = {
 	"aes",
@@ -163,25 +274,341 @@ static const char * const services_to_check[] = {
 	"xts(aes)",
 };
 
-int main(void)
+static void test_service_indicators(void)
 {
-	size_t i;
+	int i;
 
-	if (getuid() != 0)
-		die("This program requires root.  Run 'adb root' first.");
-
-	create_fips140_node_if_needed();
-
-	fips140_dev_fd = open("/dev/fips140", O_RDONLY);
-	if (fips140_dev_fd < 0)
-		die_errno("failed to open /dev/fips140");
-
-	printf("fips140_module_version() => \"%s\"\n", fips140_module_version());
 	for (i = 0; i < ARRAY_SIZE(services_to_check); i++) {
 		const char *service = services_to_check[i];
 
 		printf("fips140_is_approved_service(\"%s\") => %s\n", service,
 		       booltostr(fips140_is_approved_service(service)));
+	}
+	printf("\n");
+}
+
+/* ---------------------------------------------------------------------------
+ *			      Invalid inputs test
+ * ---------------------------------------------------------------------------*/
+
+enum direction {
+	UNSPECIFIED,
+	DECRYPT,
+	ENCRYPT,
+};
+
+static const struct invalid_input_test {
+	const char *alg_type;
+	const char *alg_name;
+	const uint8_t *key;
+	size_t key_size;
+	const uint8_t *msg;
+	size_t msg_size;
+	const uint8_t *iv;
+	size_t iv_size;
+	enum direction direction;
+	int setkey_error;
+	int crypt_error;
+} invalid_input_tests[] = {
+	{
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 16,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 17,
+		.setkey_error = EINVAL,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 24,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 32,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 33,
+		.setkey_error = EINVAL,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 16,
+		.msg_size = 1,
+		.direction = DECRYPT,
+		.crypt_error = EINVAL,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 16,
+		.msg_size = 16,
+		.direction = ENCRYPT,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "cbc(aes)",
+		.key_size = 16,
+		.msg_size = 17,
+		.direction = ENCRYPT,
+		.crypt_error = EINVAL,
+	}, {
+		.alg_type = "hash",
+		.alg_name = "cmac(aes)",
+		.key_size = 29,
+		.setkey_error = EINVAL,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "xts(aes)",
+		.key_size = 32,
+	}, {
+		.alg_type = "skcipher",
+		.alg_name = "xts(aes)",
+		.key = (const uint8_t *)"0123456789ABCDEF0123456789ABCDEF",
+		.key_size = 32,
+		.setkey_error = EINVAL,
+	}
+};
+
+static const char *describe_crypt_op(const struct invalid_input_test *t)
+{
+	if (t->direction == ENCRYPT)
+		return "encryption";
+	if (t->direction == DECRYPT)
+		return "decryption";
+	if (strcmp(t->alg_type, "hash") == 0)
+		return "hashing";
+	ASSERT(0);
+}
+
+static bool af_alg_setkey(const struct invalid_input_test *t, int alg_fd)
+{
+	const uint8_t *key = t->key;
+	uint8_t _key[t->key_size];
+
+	if (t->key_size == 0)
+		return true;
+
+	if (t->key == NULL) {
+		rand_bytes(_key, t->key_size);
+		key = _key;
+	}
+	if (setsockopt(alg_fd, SOL_ALG, ALG_SET_KEY, key, t->key_size) != 0) {
+		printf("%s: setting %zu-byte key failed with error '%s'\n",
+		       t->alg_name, t->key_size, strerror(errno));
+		printf("\tkey was %s\n\n", bytes_to_hex(key, t->key_size));
+		ASSERT(t->setkey_error == errno);
+		return false;
+	}
+	printf("%s: setting %zu-byte key succeeded\n",
+	       t->alg_name, t->key_size);
+	printf("\tkey was %s\n\n", bytes_to_hex(key, t->key_size));
+	ASSERT(t->setkey_error == 0);
+	return true;
+}
+
+static void af_alg_process_msg(const struct invalid_input_test *t, int alg_fd)
+{
+	struct iovec iov;
+	struct msghdr hdr = {
+		.msg_iov = &iov,
+		.msg_iovlen = 1,
+	};
+	const uint8_t *msg = t->msg;
+	uint8_t *_msg = NULL;
+	uint8_t *output = NULL;
+	uint8_t *control = NULL;
+	size_t controllen = 0;
+	struct cmsghdr *cmsg;
+	int req_fd;
+
+	if (t->msg_size == 0)
+		return;
+
+	req_fd = get_req_fd(alg_fd, t->alg_name);
+
+	if (t->msg == NULL) {
+		_msg = malloc(t->msg_size);
+		rand_bytes(_msg, t->msg_size);
+		msg = _msg;
+	}
+	output = malloc(t->msg_size);
+	iov.iov_base = (void *)msg;
+	iov.iov_len = t->msg_size;
+
+	if (t->direction != UNSPECIFIED)
+		controllen += CMSG_SPACE(sizeof(uint32_t));
+	if (t->iv_size)
+		controllen += CMSG_SPACE(sizeof(struct af_alg_iv) + t->iv_size);
+	control = calloc(1, controllen);
+	hdr.msg_control = control;
+	hdr.msg_controllen = controllen;
+	cmsg = CMSG_FIRSTHDR(&hdr);
+	if (t->direction != UNSPECIFIED) {
+		cmsg->cmsg_level = SOL_ALG;
+		cmsg->cmsg_type = ALG_SET_OP;
+		cmsg->cmsg_len = CMSG_LEN(sizeof(uint32_t));
+		*(uint32_t *)CMSG_DATA(cmsg) = t->direction == DECRYPT ?
+				ALG_OP_DECRYPT : ALG_OP_ENCRYPT;
+		cmsg = CMSG_NXTHDR(&hdr, cmsg);
+	}
+	if (t->iv_size) {
+		struct af_alg_iv *alg_iv;
+
+		cmsg->cmsg_level = SOL_ALG;
+		cmsg->cmsg_type = ALG_SET_IV;
+		cmsg->cmsg_len = CMSG_LEN(sizeof(*alg_iv) + t->iv_size);
+		alg_iv = (struct af_alg_iv *)CMSG_DATA(cmsg);
+		alg_iv->ivlen = t->iv_size;
+		memcpy(alg_iv->iv, t->iv, t->iv_size);
+	}
+
+	if (sendmsg(req_fd, &hdr, 0) != t->msg_size)
+		die_errno("sendmsg failed");
+
+	if (read(req_fd, output, t->msg_size) != t->msg_size) {
+		printf("%s: %s of %zu-byte message failed with error '%s'\n",
+		       t->alg_name, describe_crypt_op(t), t->msg_size,
+		       strerror(errno));
+		printf("\tmessage was %s\n\n", bytes_to_hex(msg, t->msg_size));
+		ASSERT(t->crypt_error == errno);
+	} else {
+		printf("%s: %s of %zu-byte message succeeded\n",
+		       t->alg_name, describe_crypt_op(t), t->msg_size);
+		printf("\tmessage was %s\n\n", bytes_to_hex(msg, t->msg_size));
+		ASSERT(t->crypt_error == 0);
+	}
+	free(_msg);
+	free(output);
+	free(control);
+	close(req_fd);
+}
+
+static void test_invalid_input(const struct invalid_input_test *t)
+{
+	int alg_fd = get_alg_fd(t->alg_type, t->alg_name);
+
+	if (af_alg_setkey(t, alg_fd))
+		af_alg_process_msg(t, alg_fd);
+
+	close(alg_fd);
+}
+
+static void test_invalid_inputs(void)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(invalid_input_tests); i++)
+		test_invalid_input(&invalid_input_tests[i]);
+}
+
+/* ---------------------------------------------------------------------------
+ *			       Jitterentropy test
+ * ---------------------------------------------------------------------------*/
+
+static size_t jitter_dump_amount = 128;
+
+static void test_jitterentropy(void)
+{
+	int alg_fd = get_alg_fd("rng", "jitterentropy_rng");
+	int req_fd = get_req_fd(alg_fd, "jitterentropy_rng");
+	size_t count = jitter_dump_amount;
+	uint8_t buf[AF_ALG_MAX_RNG_REQUEST_SIZE];
+
+	printf("Jitter RNG output: ");
+	while (count) {
+		ssize_t ret;
+
+		memset(buf, 0, sizeof(buf));
+		ret = read(req_fd, buf, MIN(count, sizeof(buf)));
+		if (ret < 0)
+			die_errno("error reading from jitterentropy_rng");
+		printf("%s", bytes_to_hex(buf, ret));
+		count -= ret;
+	}
+	printf("\n");
+
+	close(req_fd);
+	close(alg_fd);
+}
+
+/* ---------------------------------------------------------------------------
+ *				     main()
+ * ---------------------------------------------------------------------------*/
+
+static const struct test {
+	const char *name;
+	void (*func)(void);
+} tests[] = {
+	{ "version_service", test_version_service },
+	{ "service_indicators", test_service_indicators },
+	{ "invalid_inputs", test_invalid_inputs },
+	{ "jitterentropy", test_jitterentropy },
+};
+
+static void usage(void)
+{
+	int i;
+
+	fprintf(stderr, "Usage: fips140_lab_test TEST_TO_RUN...\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "Available tests: ");
+	for (i = 0; i < ARRAY_SIZE(tests); i++) {
+		fprintf(stderr, "%s%s", tests[i].name,
+			(i < ARRAY_SIZE(tests) - 1 ? ", " : " "));
+	}
+	fprintf(stderr, "\n"
+"By default, all tests are run.\n"
+"\n"
+"Options:\n"
+"  --dump-amount=AMOUNT      For jitterentropy test, amount to dump in bytes.\n"
+	);
+}
+
+static bool is_test_enabled(const char *name, int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 0)
+		return true;
+	for (i = 0; i < argc; i++) {
+		if (strcmp(name, argv[i]) == 0)
+			return true;
+	}
+	return false;
+}
+
+int main(int argc, char *argv[])
+{
+	static const struct option longopts[] = {
+		{ "dump-amount", required_argument, NULL, 'j' },
+		{ NULL, 0, NULL, 0 },
+	};
+	int c;
+	int i;
+
+	while ((c = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+		switch (c) {
+		case 'j':
+			jitter_dump_amount = strtoul(optarg, NULL, 0);
+			if (jitter_dump_amount == 0 ||
+			    jitter_dump_amount == ULONG_MAX)
+				die("invalid --dump-amount");
+			break;
+		default:
+			usage();
+			return 1;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (getuid() != 0)
+		die("This program requires root.  Run 'adb root' first.");
+
+	for (i = 0; i < ARRAY_SIZE(tests); i++) {
+		if (is_test_enabled(tests[i].name, argc, argv))
+			tests[i].func();
 	}
 	return 0;
 }
