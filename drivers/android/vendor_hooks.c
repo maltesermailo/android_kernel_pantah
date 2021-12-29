62a1a774824a65cb277ae3006fd71dc257d7c758
Liujie Xie <xieliujie@oppo.com>
2021-12-29 10:16:50 +0800

    ANDROID: vendor_hooks: Add hooks for binder
    We want to add some hooks in the binder module so that we can reduce
    block time until binder thread is available
    Here are what new hooks do for:
    1、android_vh_binder_looper_state_registered: choose a binder thread(do proc work) as a low-level thread.Only this thread has power to excute background binder transaction.
    2、android_vh_binder_thread_read: let binder thread do works which come from
    our list.
    3、android_vh_binder_free_proc: free some pointers and variable.
    4、android_vh_binder_thread_release: free the list that we create before.
    5、android_vh_binder_has_work_ilocked: to check if our list has work.
    6、android_vh_binder_read_done: because of we add hook in binder_has_work_ilocked,
    7、android_vh_binder_preset: mark target proc's binder threads.
    binder_has_work_ilocked may return true, so we try to wake up low-level thread immediately.
    
    Bug: 212483521
    Change-Id: Ic40f452cc4dcf8fc85422e23e6f1a7ad77547309
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    (cherry picked from commit 8d6074509e4ec7e10acf6ee78277ff8cd19e49d0)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -274,6 +274,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_css_offline);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_css_online);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_free);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_alloc);
+<<<<<<< HEAD   (71cf9c BACKPORT: FROMGIT: rcu: Avoid freeing new kfree_rcu() memory)
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_adjust);
@@ -296,3 +297,12 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_amu_fie);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
+=======
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_looper_state_registered);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_thread_read);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_free_proc);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_thread_release);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_has_work_ilocked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_read_done);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
+>>>>>>> CHANGE (8d6074 ANDROID: vendor_hooks: Add hooks for binder)
