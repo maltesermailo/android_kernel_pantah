f9bb943816f6ba9b8dbf63bb16de5aced79ebecd
Eric.Cai <Eric.Cai@unisoc.com>
2022-01-13 14:32:08 +0800

    ANDROID: GKI: ion: add vendor hook for ion buffer release
    We need to add a vendor hook for ion buffer release.
    When a process that created ion buffer crashes,  we need to hook other modules
    that depend on it in the ion_buffer_release() function.
    
    Bug: 214330811
    Signed-off-by: Eric.Cai <Eric.Cai@unisoc.com>
    Change-Id: I1a26ef8fd05de45e6242bed596f9e8332a7bd721

---
+++ include/trace/hooks/ion.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM ion
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_ION_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_ION_H
+#include <linux/tracepoint.h>
+#include <trace/hooks/vendor_hooks.h>
+
+DECLARE_HOOK(android_vh_ion_buffer_release,
+		TP_PROTO(void *data),
+		TP_ARGS(data));
+#endif /* _TRACE_HOOK_ION_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
