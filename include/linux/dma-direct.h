f95189fcb0abfcafacb64472942378ad9de9cfbf
Dmitry Shmidt <dimitrysh@google.com>
2022-02-15 14:19:07 -0800

    ANDROID: Fix zone_dma32_is_empty() check
    In patch "Add command line option to make ZONE_DMA32 empty" new function
    zone_dma32_is_empty() was introduced. However, if node = -1 will be passed
    to it, NODE_DATA(node) will crash like:
    [    2.497228] Unable to handle kernel NULL pointer dereference at virtual address 0000000000000078
    [    2.497232] Mem abort info:
    [    2.497234]   ESR = 0x96000004
    [    2.497237]   EC = 0x25: DABT (current EL), IL = 32 bits
    [    2.497240]   SET = 0, FnV = 0
    [    2.497242]   EA = 0, S1PTW = 0
    [    2.497244]   FSC = 0x04: level 0 translation fault
    [    2.497247] Data abort info:
    [    2.497248]   ISV = 0, ISS = 0x00000004
    [    2.497250]   CM = 0, WnR = 0
    [    2.497252] [0000000000000078] user address but active_mm is swapper
    [    2.497257] Internal error: Oops: 96000004 [#1] PREEMPT SMP
    [    2.497262] Modules linked in:
    [    2.497269] CPU: 2 PID: 100 Comm: kworker/u8:1 Not tainted 5.15.23-05034-g5472403a6730-dirty #944
    [    2.497278] Workqueue: events_unbound async_run_entry_fn
    [    2.497292] pstate: 60000009 (nZCv daif -PAN -UAO -TCO -DIT -SSBS BTYPE=--)
    [    2.497297] pc : dma_direct_optimal_gfp_mask+0xd0/0x124
    [    2.497305] lr : dma_direct_optimal_gfp_mask+0xc4/0x124
    [    2.497310] sp : ffff80000a853b10
    
    Bug: 199917449
    Test: Boot Yukawa board
    
    Change-Id: Ic1c240ea5eb1361c0d822aa768ed9730a0ab570e
    Proposed-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/dma-direct.h
+++ include/linux/dma-direct.h
@@ -27,7 +27,11 @@ struct bus_dma_region {
 static inline bool zone_dma32_is_empty(int node)
 {
 #ifdef CONFIG_ZONE_DMA32
-	pg_data_t *pgdat = NODE_DATA(node);
+	pg_data_t *pgdat;
+
+	if (node == NUMA_NO_NODE)
+		node = numa_mem_id();
+	pgdat = NODE_DATA(node);
 
 	return zone_is_empty(&pgdat->node_zones[ZONE_DMA32]);
 #else
