713b276a9e6cbfde90b4ffa4690a296f64354afa
Liujie Xie <xieliujie@oppo.com>
2022-02-20 16:53:05 +0800

    ANDROID: vendor_hooks: Add hooks to select binder worklist
    trace_android_vh_binder_proc_transaction_entry:
    We need change binder thread so that this work can be added in
    proc->todo, if we found the binder thread, skip native logic.
    
    trace_android_vh_binder_select_worklist_ilocked:
    we need this because we can't change list point in ”trace_android_vh_binder_thread_read“,
    otherwise, If a work has beed added in our own defined list before,
    current may goto retry and loop again and again.
    
    Bug: 219898723
    Change-Id: Ifdb3429c9ddac521bc75c1d21740ee7cc4b8f143
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    (cherry picked from commit acefa91e517b9321295429d3a0e534908e2939c1)
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    (cherry picked from commit d26c0e1c409179ea74aaa89f748006bd76000cc3)

--- include/trace/hooks/binder.h
+++ include/trace/hooks/binder.h
@@ -42,6 +42,22 @@ DECLARE_RESTRICTED_HOOK(android_rvh_binder_transaction,
 	TP_PROTO(struct binder_proc *target_proc, struct binder_proc *proc,
 		struct binder_thread *thread, struct binder_transaction_data *tr),
 	TP_ARGS(target_proc, proc, thread, tr), 1);
+<<<<<<< HEAD   (71cf9c BACKPORT: FROMGIT: rcu: Avoid freeing new kfree_rcu() memory)
+=======
+DECLARE_HOOK(android_vh_binder_proc_transaction_entry,
+	TP_PROTO(struct binder_proc *proc, struct binder_transaction *t,
+	struct binder_thread **thread, int node_debug_id, bool pending_async,
+	bool sync, bool *skip),
+	TP_ARGS(proc, t, thread, node_debug_id, pending_async, sync, skip));
+DECLARE_HOOK(android_vh_binder_select_worklist_ilocked,
+	TP_PROTO(struct list_head **list, struct binder_thread *thread, struct binder_proc *proc,
+	int wait_for_proc_work),
+	TP_ARGS(list, thread, proc, wait_for_proc_work));
+DECLARE_HOOK(android_vh_binder_proc_transaction_finish,
+	TP_PROTO(struct binder_proc *proc, struct binder_transaction *t,
+		struct task_struct *binder_th_task, bool pending_async, bool sync),
+	TP_ARGS(proc, t, binder_th_task, pending_async, sync));
+>>>>>>> CHANGE (d26c0e ANDROID: vendor_hooks: Add hooks to select binder worklist)
 DECLARE_HOOK(android_vh_binder_print_transaction_info,
 	TP_PROTO(struct seq_file *m, struct binder_proc *proc,
 		 const char *prefix, struct binder_transaction *t),
