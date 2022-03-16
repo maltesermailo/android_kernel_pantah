c4603f31ce1ce4581d38e1ffb2010e436e4c3188
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2022-03-16 11:14:09 +0000

    ANDROID: trusty: Make transports configurable
    With TRUSTY_SMC_TRANSPORT set to 'y', SMC based message passing and
    memory sharing support will be compiled in to trusty core.
    
    With TRUSTY_FFA_TRANSPORT set to 'y', FFA based message passing and
    memory sharing support will be compiled in to trusty core. This
    depends on ARM FF-A driver (ARM_FFA_TRANSPORT).
    
    Enabling any of the transport sets config TRUSTY_HAVE_TRANSPORT to 'y'.
    Not enabling any of the transport causes the build to break.
    
    Bug: 283822781
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: Ib5bbf0d39202e6897700264d14371ae33101c1d1

--- drivers/trusty/trusty.c
+++ drivers/trusty/trusty.c
@@ -928,8 +928,12 @@ static int trusty_remove(struct platform_device *pdev)
  *
  */
 static const struct trusty_transport_desc *trusty_transports[] = {
+#ifdef CONFIG_TRUSTY_SMC_TRANSPORT
 	&trusty_smc_transport,
+#endif
+#ifdef CONFIG_TRUSTY_FFA_TRANSPORT
 	&trusty_ffa_transport,
+#endif
 	NULL,
 };
 
@@ -953,6 +957,9 @@ static struct platform_driver trusty_driver = {
 static int __init trusty_driver_init(void)
 {
 	int ret;
+
+	BUILD_BUG_ON_MSG(!IS_ENABLED(CONFIG_TRUSTY_HAVE_TRANSPORT),
+			"Trusty transport not configured");
 
 	/*
 	 * Initialize trusty_irq_driver first since trusty_probe makes an
