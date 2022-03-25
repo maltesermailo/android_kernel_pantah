58b6f1ef17d6cb646f03b26634eaf8ebc9be8d16
Xuewen Yan <xuewen.yan@unisoc.com>
2022-03-25 10:08:13 +0800

    ANDROID: Add vendor hook to the effective_cpu_util
    android_rvh_effective_cpu_util:
    	To perform vendor-specific cpu util, it is used in EAS/schedutil/thermal.
    
    The effective_cpu_util would be called when thermal calc the dynamic power,
    it's non-atomic context, so set the hook be restricted.
    
    Bug: 226686099
    Test: build pass
    
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Change-Id: I6fd77f44ca4328f5ef37d96989aa2e08d65e29bb

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -367,6 +367,11 @@ DECLARE_HOOK(android_vh_dup_task_struct,
 	TP_PROTO(struct task_struct *tsk, struct task_struct *orig),
 	TP_ARGS(tsk, orig));
 
+DECLARE_RESTRICTED_HOOK(android_rvh_effective_cpu_util,
+	TP_PROTO(int cpu, unsigned long util_cfs, unsigned long max, int type,
+		 struct task_struct *p, unsigned long *new_util),
+	TP_ARGS(cpu, util_cfs, max, type, p, new_util), 1);
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
