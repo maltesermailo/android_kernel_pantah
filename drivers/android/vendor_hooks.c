04b86906fef6fa3cf765f932469021fb3fb3c2b0
Gaofeng Sheng <gaofeng.sheng@unisoc.com>
2022-04-06 15:36:28 +0800

    ANDROID: GKI: drm: add vendor hook for atomic_remove_fb
    Deleted by SPRD for HWC performance. There is no need to
    disable old planes for sprd drm. Because the dpu driver
    will clean all the planes first before flip each time.
    
    Bug: 227721498
    Signed-off-by: Gaofeng Sheng <gaofeng.sheng@unisoc.com>
    Change-Id: I81275ae86064d0b30163be17bce266120c90b130

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -8,6 +8,7 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/hooks/vendor_hooks.h>
+#include <linux/tracepoint.h>
 #include <trace/hooks/sched.h>
 #include <trace/hooks/fpsimd.h>
 #include <trace/hooks/binder.h>
@@ -53,6 +54,8 @@
 #include <trace/hooks/fips140.h>
 #include <trace/hooks/thermal.h>
 #include <trace/hooks/rwsem.h>
+#include <trace/hooks/timekeeping.h>
+#include <trace/hooks/drm_framebuffer.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -263,3 +266,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_finished);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_rwsem_list_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_thermal_power_cap);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_tk_based_time_sync);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kswapd_per_node);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_atomic_remove_fb);
