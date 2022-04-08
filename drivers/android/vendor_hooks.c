3c8bef25816127d1ce5d29a68727dcc730e0e3ee
Yunxian He <yunxian.he@unisoc.com>
2022-04-08 17:57:01 +0800

    ANDROID: usb: Add vendor hook when core detect a new device
    Add vendor hook when core detect a new device. Vendor side
    can use this hook to do several things. Our Unisoc ASIC had
    some drawback. most scene, we use DWC3 controller and
    associated PHY, when digital headset plug in, we need end the
    enumeration and switch to another controller and phy. Using this
    hook, we can do the switch easily and efficiently.
    
    Bug: 229330749
    Change-Id: I1cba28b43ff1b1f5ac0138c0bb743f3d8e9f1e4b
    Signed-off-by: Yunxian He <yunxian.he@unisoc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -428,6 +428,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_read_lazy_flag);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_tsk_need_resched_lazy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_usb_dev_suspend);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_usb_dev_resume);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_usb_new_device_added);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bd_link_disk_holder);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_new_mount_fc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_fill_rwbs);
