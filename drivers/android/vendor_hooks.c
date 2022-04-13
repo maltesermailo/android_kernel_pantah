a986f96ad4602b8512f44b978b07fde0ce7c9002
Jian Gong <Jian.Gong@unisoc.com>
2022-04-13 19:52:22 +0800

    ANDROID: GKI: psci: add hook to check if cpu is allowed to power off
    While TOS is running alongside with linux, cpu power off operation by linux
    may need be denied by TOS in some scenarios.
    This patch added two hooks in psci_tos_resident_on and psci_cpu_suspend
    to hook cpu off operation.
    
    The function psci_tos_resident_on originally is used to check if TOS is resident on
    a specific cpu and that cpu is dedicated for running TOS exclusively. If so, that
    cpu can not be power off. Actually if TOS supports SMP, TOS may need deny any
    other cpu to power down in some cases, i.e. there are no-expired timers in TOS.
    Thus the first hook for psci_tos_resident_on is used to determine if
    the specific cpu is allowed to power off in the cpu hotplug path.
    
    Besides cpu hotplug, a cpu also can power off by cpu_suspend.
    The second hook for psci_cpu_suspend determines if cpu suspend should go through
    or not. When the same conditions described above meets, cpu suspend will break up.
    
    The hook is same as commit 88d88955ae0b8b1f1a555d7810beb6c8ca4ca0f1 from android12-5.4
    
    Bug: 229067021
    Change-Id: Ib329beeff20f0cfef263f6a7813280d33f6a5eaa
    Signed-off-by: Jian Gong <Jian.Gong@unisoc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -58,6 +58,7 @@
 #include <trace/hooks/audio_usboffload.h>
 #include <trace/hooks/drm_framebuffer.h>
 #include <trace/hooks/drm_atomic.h>
+#include <trace/hooks/psci.h>
 #include <trace/hooks/usb.h>
 
 /*
@@ -281,4 +282,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_audio_usb_offload_disconnect);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_atomic_remove_fb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_drm_atomic_check_modeset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_get_thermal_zone_device);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psci_tos_resident_on);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psci_cpu_suspend);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_usb_new_device_added);
