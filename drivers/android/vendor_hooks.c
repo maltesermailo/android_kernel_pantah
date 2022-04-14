50d9e91d34d01fa9b5ff9a29303170badacb8468
Xiaopeng Bai <xiaopeng.bai@unisoc.com>
2022-04-14 16:22:43 +0800

    ANDROID: GKI: regmap: Add regmap vendor hook for of_syscon_register
    For global registers access, UNISOC have one special method called set/clear
    mechanism that would avoid using hardware spinlock. But now regmap framework
    does not support our set/clear mechanism, so add vendor hook to support
    this feature.
    
    Bug: 228907258
    Signed-off-by: Xiaopeng Bai <xiaopeng.bai@unisoc.com>
    Change-Id: I9a6651f07a048ffebd5c2d8e369a4e7b374bc182
    (cherry picked from commit 53e342c183af58f03bd1445838a3f499e7fbd191)
    (cherry picked from commit 8726a2d930a815066b7a0ed8a4d4c560fbc4d114)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -35,6 +35,7 @@
 #include <trace/hooks/timer.h>
 #include <trace/hooks/fpsimd.h>
 #include <trace/hooks/mm.h>
+#include <trace/hooks/regmap.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -91,3 +92,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_slab_folio_alloced);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kmalloc_large_alloced);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_regmap_update);
