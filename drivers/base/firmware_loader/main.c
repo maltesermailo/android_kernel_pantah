e70205c8c8ce3bf43c52745ac7531beea817384c
Thiébaud Weksteen <tweek@google.com>
2022-05-02 10:49:52 +1000

    UPSTREAM: firmware_loader: use kernel credentials when reading firmware
    Device drivers may decide to not load firmware when probed to avoid
    slowing down the boot process should the firmware filesystem not be
    available yet. In this case, the firmware loading request may be done
    when a device file associated with the driver is first accessed. The
    credentials of the userspace process accessing the device file may be
    used to validate access to the firmware files requested by the driver.
    Ensure that the kernel assumes the responsibility of reading the
    firmware.
    
    This was observed on Android for a graphic driver loading their firmware
    when the device file (e.g. /dev/mali0) was first opened by userspace
    (i.e. surfaceflinger). The security context of surfaceflinger was used
    to validate the access to the firmware file (e.g.
    /vendor/firmware/mali.bin).
    
    Previously, Android configurations were not setting up the
    firmware_class.path command line argument and were relying on the
    userspace fallback mechanism. In this case, the security context of the
    userspace daemon (i.e. ueventd) was consistently used to read firmware
    files. More Android devices are now found to set firmware_class.path
    which gives the kernel the opportunity to read the firmware directly
    (via kernel_read_file_from_path_initns). In this scenario, the current
    process credentials were used, even if unrelated to the loading of the
    firmware file.
    
    Signed-off-by: Thiébaud Weksteen <tweek@google.com>
    Cc: <stable@vger.kernel.org> # 5.10
    Reviewed-by: Paul Moore <paul@paul-moore.com>
    Acked-by: Luis Chamberlain <mcgrof@kernel.org>
    Link: https://lore.kernel.org/r/20220502004952.3970800-1-tweek@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 581dd69830341d299b0c097fc366097ab497d679)
    [adelva: merged thru LTS, but LTS merges are paused on a13-5.10]
    Bug: 232963476
    Signed-off-by: Alistair Delva <adelva@google.com>
    Change-Id: Ie24b5ec86451e36e7f982f403446161c326d5fe4

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -761,6 +761,12 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 		  enum fw_opt opt_flags)
 {
 	struct firmware *fw = NULL;
+<<<<<<< HEAD   (7926bf BACKPORT: can: ems_usb: ems_usb_start_xmit(): fix double dev)
+=======
+	struct cred *kern_cred = NULL;
+	const struct cred *old_cred;
+	bool nondirect = false;
+>>>>>>> CHANGE (839628 UPSTREAM: firmware_loader: use kernel credentials when readi)
 	int ret;
 
 	if (!firmware_p)
@@ -775,6 +781,18 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 					opt_flags);
 	if (ret <= 0) /* error or already assigned */
 		goto out;
+
+	/*
+	 * We are about to try to access the firmware file. Because we may have been
+	 * called by a driver when serving an unrelated request from userland, we use
+	 * the kernel credentials to read the file.
+	 */
+	kern_cred = prepare_kernel_cred(NULL);
+	if (!kern_cred) {
+		ret = -ENOMEM;
+		goto out;
+	}
+	old_cred = override_creds(kern_cred);
 
 	ret = fw_get_filesystem_firmware(device, fw->priv, "", NULL);
 #ifdef CONFIG_FW_LOADER_COMPRESS
@@ -791,6 +809,9 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 		ret = firmware_fallback_sysfs(fw, name, device, opt_flags, ret);
 	} else
 		ret = assign_fw(fw, device, opt_flags);
+
+	revert_creds(old_cred);
+	put_cred(kern_cred);
 
  out:
 	if (ret < 0) {
