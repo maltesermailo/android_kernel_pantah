53172decc92135f443473dbd084ea448ac39aa3f
Minchan Kim <minchan@google.com>
2022-05-09 10:18:31 -0700

    FROMLIST: BACKPORT: mm: fix is_pinnable_page against on cma page
    Pages on CMA area could have MIGRATE_ISOLATE as well as MIGRATE_CMA
    so current is_pinnable_page could miss CMA pages which has MIGRATE_
    ISOLATE. It ends up putting CMA pages longterm pinning possible on
    pin_user_pages APIs so CMA allocation fails.
    
    The CMA allocation path protects the migration type change race
    using zone->lock but what GUP path need to know is just whether the
    page is on CMA area or not rather than exact type. Thus, we don't
    need zone->lock but just checks the migratype in either of
    (MIGRATE_ISOLATE and MIGRATE_CMA).
    
    Adding the MIGRATE_ISOLATE check in is_pinnable_page could cause
    rejecting of pinning the page on MIGRATE_ISOLATE pageblock even
    thouth it's neither CMA nor movable zone if the page is temporarily
    unmovable. However, the migration failure is general issue, not
    only come from MIGRATE_ISOLATE and the MIGRATE_ISOLATE is also
    transient state like other temporal refcount holding of pages.
    
    Cc: David Hildenbrand <david@redhat.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    
    Conflicts:
            include/linux/mm.h
    
    1. There is no is_pinnable_page in 5.10
    
    Link: https://lore.kernel.org/all/20220509153430.4125710-1-minchan@kernel.org/
    Bug: 231227007
    Signed-off-by: Minchan Kim <minchan@google.com>
    Change-Id: I5cdd2b8eefdd7e89658abd21c32aa84876ad7782

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -74,7 +74,14 @@ extern const char * const migratetype_names[MIGRATE_TYPES];
 
 #ifdef CONFIG_CMA
 #  define is_migrate_cma(migratetype) unlikely((migratetype) == MIGRATE_CMA)
-#  define is_migrate_cma_page(_page) (get_pageblock_migratetype(_page) == MIGRATE_CMA)
+#  define is_migrate_cma_page(_page) ({			\
+	int __mt = get_pageblock_migratetype(_page);	\
+	int mt = __READ_ONCE(__mt);			\
+	bool __ret = false;				\
+	if (mt == MIGRATE_CMA || mt == MIGRATE_ISOLATE)	\
+		__ret = true;				\
+	__ret;						\
+})
 #  define get_cma_migrate_type() MIGRATE_CMA
 #else
 #  define is_migrate_cma(migratetype) false
