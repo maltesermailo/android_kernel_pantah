b5f0ff9d6109a71e7e6341cc712dd25e3ef559f0
Lee Jones <lee.jones@linaro.org>
2022-05-12 10:36:55 +0100

    ANDROID: mm: backing-dev: Take a reference to the bdi in use to prevent UAF
    KASAN reports a reproducible issue in the BDI handling code due to a
    dereference of a previously freed pointer to 'struct backing_dev_info'
    in bdi_unregister().
    
    Because of a distinct lack of locking and/or reference taking,
    blk_cleanup_queue() puts the final taken reference to the bdi, which
    is then promptly freed by release_bdi().  However, del_gendisk() calls
    bdi_unregister() after the fact, which then attempts to dereference
    it causing the kernel to panic.
    
    Bug: 182815710
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Iaf3dadf3b983a4b7d74d4e273fc676350cfc387f

--- mm/backing-dev.c
+++ mm/backing-dev.c
@@ -873,13 +873,20 @@ EXPORT_SYMBOL(bdi_alloc_node);
 int bdi_register_va(struct backing_dev_info *bdi, const char *fmt, va_list args)
 {
 	struct device *dev;
+	int ret;
 
-	if (bdi->dev)	/* The driver needs to use separate queues per device */
-		return 0;
+	bdi_get(bdi);
+
+	if (bdi->dev) {	/* The driver needs to use separate queues per device */
+		ret = 0;
+		goto out;
+	}
 
 	dev = device_create_vargs(bdi_class, NULL, MKDEV(0, 0), bdi, fmt, args);
-	if (IS_ERR(dev))
-		return PTR_ERR(dev);
+	if (IS_ERR(dev)) {
+		ret = PTR_ERR(dev);
+		goto out;
+	}
 
 	cgwb_bdi_register(bdi);
 	bdi->dev = dev;
@@ -893,6 +900,10 @@ int bdi_register_va(struct backing_dev_info *bdi, const char *fmt, va_list args)
 
 	trace_writeback_bdi_register(bdi);
 	return 0;
+
+out:
+	bdi_put(bdi);
+	return ret;
 }
 EXPORT_SYMBOL(bdi_register_va);
 
@@ -959,6 +970,8 @@ void bdi_unregister(struct backing_dev_info *bdi)
 		put_device(bdi->owner);
 		bdi->owner = NULL;
 	}
+
+	bdi_put(bdi);
 }
 
 static void release_bdi(struct kref *ref)
