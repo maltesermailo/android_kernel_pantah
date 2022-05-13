2a968e51d87d447b133bbd98aa1efa9c3c957ca5
Todd Kjos <tkjos@google.com>
2022-05-13 10:59:14 -0700

    ANDROID: fixup vendor hook names to restricted
    Change names of hooks changed to restricted now that we
    can change the KMI.
    
    Bug: 232468327
    Fixes: 870bf56005f9 ("ANDROID: Fix suspicious RCU usage warning")
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: Id97ceb6e9a37841e78c5ff5dea32a8a7c9b0c062

--- include/trace/hooks/psci.h
+++ include/trace/hooks/psci.h
@@ -10,11 +10,11 @@
 
 #include <trace/hooks/vendor_hooks.h>
 
-DECLARE_RESTRICTED_HOOK(android_vh_psci_tos_resident_on,
+DECLARE_RESTRICTED_HOOK(android_rvh_psci_tos_resident_on,
 	TP_PROTO(int cpu, bool *resident),
 	TP_ARGS(cpu, resident), 1);
 
-DECLARE_RESTRICTED_HOOK(android_vh_psci_cpu_suspend,
+DECLARE_RESTRICTED_HOOK(android_rvh_psci_cpu_suspend,
 	TP_PROTO(u32 state, bool *deny),
 	TP_ARGS(state, deny), 1);
 
