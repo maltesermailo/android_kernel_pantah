7817be2917a0a623f7974eee476e274fff23ebdc
Veerendranath Jakkam <quic_vjakkam@quicinc.com>
2022-05-24 13:47:40 +0530

    BACKPORT: FROMLIST: cfg80211: Indicate MLO connection info in connect and roam callbacks
    The MLO links used for connection with an MLD AP are decided by the
    driver in case of SME offloaded to driver.
    
    Add support for the drivers to indicate the information of links used
    for MLO connection in connect and roam callbacks, update the connected
    links information in wdev from connect/roam result sent by driver.
    Also, send the connected links information to userspace.
    
    Add a netlink flag attribute to indicate that userspace supports
    handling of MLO connection. Drivers must not do MLO connection when this
    flag is not set. This is to maintain backwards compatibility with older
    supplicant versions which doesn't have support for MLO connection.
    
    Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>
    
    Bug: 233387627
    Change-Id: I4aabf721c0976b3410d81fa56fc72408271f9806
    Link: https://lore.kernel.org/linux-wireless/1654679797-7740-1-git-send-email-quic_vjakkam@quicinc.com/
    Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -2713,6 +2713,10 @@ enum nl80211_commands {
  * @NL80211_ATTR_MLO_LINKS: A nested array of links, each containing some
  *	per-link information and a link ID.
  *
+ * @NL80211_ATTR_MLO_SUPPORT: Flag attribute to indicate user space supports MLO
+ *	connection. Used with %NL80211_CMD_CONNECT. If this attribute is not
+ *	included in NL80211_CMD_CONNECT drivers must not perform MLO connection.
+ *
  * @NUM_NL80211_ATTR: total number of nl80211_attrs available
  * @NL80211_ATTR_MAX: highest attribute number currently defined
  * @__NL80211_ATTR_AFTER_LAST: internal use
@@ -3272,6 +3276,9 @@ enum nl80211_attrs {
 /* Link: https://lore.kernel.org/linux-wireless/20220520132938.d45ac277fe08.I023f35d382282691d7e2cbc607fb11691355cd63@changeid/ */
 #define NL80211_ATTR_MLO_LINKS NL80211_ATTR_RESERVED_DO_NOT_USE_2
 #define NL80211_ATTR_MLO_LINK_ID NL80211_ATTR_RESERVED_DO_NOT_USE_3
+
+/* Link: https://lore.kernel.org/linux-wireless/1654679797-7740-1-git-send-email-quic_vjakkam@quicinc.com/ */
+#define NL80211_ATTR_MLO_SUPPORT NL80211_ATTR_RESERVED_DO_NOT_USE_4
 
 /* source-level API compatibility */
 #define NL80211_ATTR_SCAN_GENERATION NL80211_ATTR_GENERATION
