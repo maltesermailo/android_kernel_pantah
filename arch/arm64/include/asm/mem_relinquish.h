fbb9325ed05ee3a94a048227f7585c5f654b10c2
Keir Fraser <keirf@google.com>
2022-05-26 14:22:34 +0000

    ANDROID: memory balloon: Call ARM SMCCC relinquish hypercall when available.
    Bug: 235577623
    Signed-off-by: Keir Fraser <keirf@google.com>
    Change-Id: I5c7f144827212d726ab3104e666357fe3e4cf10b

---
+++ arch/arm64/include/asm/mem_relinquish.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ASM_MEM_RELINQUISH_H
+#define __ASM_MEM_RELINQUISH_H
+
+void page_relinquish(struct page *page);
+
+#endif	/* __ASM_MEM_RELINQUISH_H */
