d9e1f655b1a76602bc19f2c14294e70b4270aec2
Maulik Shah <quic_mkshah@quicinc.com>
2022-05-27 13:45:04 +0530

    ANDROID: irqchip: irq-gic: Add vendor hook for gic-v2 resume
    Add vendor hook for gic-v2 resume to allow vendor GIC enhancement.
    
    BUG: 234108369
    Signed-off-by: Maulik Shah <quic_mkshah@quicinc.com>
    Change-Id: I4c857b40f25ad58a6daca491e89adb90e8f6ca93

--- include/trace/hooks/gic.h
+++ include/trace/hooks/gic.h
@@ -10,10 +10,15 @@
 #include <trace/hooks/vendor_hooks.h>
 struct gic_chip_data;
 struct irq_data;
+struct irq_domain;
 
 DECLARE_HOOK(android_vh_gic_resume,
        TP_PROTO(struct gic_chip_data *gd),
        TP_ARGS(gd));
+
+DECLARE_HOOK(android_vh_gic_v2_resume,
+	TP_PROTO(struct irq_domain *domain, void __iomem *dist_base),
+	TP_ARGS(domain, dist_base));
 
 DECLARE_HOOK(android_vh_gic_set_affinity,
 	TP_PROTO(struct irq_data *d, const struct cpumask *mask_val,
