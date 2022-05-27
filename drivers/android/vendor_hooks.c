e4e73c8427d0b76bb475111f6ca18fbc6d0895ec
Maulik Shah <quic_mkshah@quicinc.com>
2022-05-27 13:45:04 +0530

    ANDROID: irqchip: irq-gic: Add vendor hook for gic-v2 resume
    This change adds vendor hook for gic-v2 resume to allow vendor GIC
    enhancement. This is inline with hook present today in irq-gic-v3.c
    with one difference that vendor hooks in v2 driver do not pass entire
    gic_chip_data struct to vendor module. Instead it passes the required
    fields which are irq_domain and the gic dist base to know the wakeup
    IRQ reason.
    
    The newly added vendor hook can get invoked by two ways.
      1. By registering with syscore ops, .resume method can invoke vendor
         hook. This is applicable for cases like suspend-to-ram which
         invokes syscore ops.
      2. By exporting the gic_v2_resume(), the newly added vendor hook will
         get invoked during the first CPU waking up from suspend-to-idle
         case.
    
    While at this move android_vh_gic_resume() to gic_v3.h as its specific
    to v3 driver.
    
    Bug: 234108369
    Signed-off-by: Maulik Shah <quic_mkshah@quicinc.com>
    Change-Id: I4c857b40f25ad58a6daca491e89adb90e8f6ca93

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -379,3 +379,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_css_offline);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_css_online);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_free);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_alloc);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gic_v2_resume);
