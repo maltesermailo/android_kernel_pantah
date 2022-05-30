83d304332e7497b278e961b2298bde54e6a0acb8
Bing Han <bing.han@transsion.com>
2022-05-30 15:09:17 +0800

    ANDROID: vendor_hook: Add hook in page_referenced_one()
      Add android_vh_page_referenced_one_end at the end of function
      page_referenced_one to update the status that whether the page
      need to be reclaimed to a specified swap location
    
    Bug: 234214858
    Signed-off-by: Bing Han <bing.han@transsion.com>
    Change-Id: Ia06a229956328ef776da5d163708dcb011a327fb

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -151,6 +151,9 @@ DECLARE_HOOK(android_vh_waiting_for_page_migration,
 DECLARE_HOOK(android_vh_migrate_page_states,
 	TP_PROTO(struct page *page, struct page *newpage),
 	TP_ARGS(page, newpage));
+DECLARE_HOOK(android_vh_page_referenced_one_end,
+	TP_PROTO(struct vm_area_struct *vma, struct page *page),
+	TP_ARGS(vma, page));
 DECLARE_HOOK(android_vh_count_pswpin_pswpout,
 	TP_PROTO(struct swap_info_struct *sis, enum vm_event_item vm_event),
 	TP_ARGS(sis, vm_event));
