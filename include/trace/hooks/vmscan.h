BayLibre/android_kernel_common

ANDROID: vendor_hook: Add hook to vmscan
Add hook function to specify where to store the reclaimed page:
1) trace_android_rvh_shrink_page_list: Replace the process of adding
 swap, according to the reclaimed location information of the page,
 add the page to swap of the specified location;
2) trace_android_rvh_inactive_is_low: Replace the calculation process
 of inactive_ratio, the calculation algorithm takes into account the
 difference between file pages and anonymous pages;
3) trace_android_rvh_snapshot_refaults: To record the refault statistics
 of WORKINGSET_RESTORE_ANON;

Bug: 234214858

Signed-off-by: Bing Han <bing.han@transsion.com>
Change-Id: I6cf9c47fbc440852cc36e04f49d644146eb2c6af

include/trace/hooks/vmscan.h
@@ -34,6 +34,16 @@ DECLARE_HOOK(android_vh_shrink_node_memcgs,
 DECLARE_HOOK(android_vh_tune_memcg_scan_type,
 	TP_PROTO(struct mem_cgroup *memcg, char *scan_type),
 	TP_ARGS(memcg, scan_type));
+DECLARE_RESTRICTED_HOOK(android_rvh_inactive_is_low,
+	TP_PROTO(int *ret, unsigned long gb, unsigned long *inactive_ratio,
+		enum lru_list inactive_lru),
+	TP_ARGS(ret, gb, inactive_ratio, inactive_lru), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_snapshot_refaults,
+	TP_PROTO(struct lruvec *target_lruvec),
+	TP_ARGS(target_lruvec), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_shrink_page_list,
+	TP_PROTO(struct page *page, int *ret),
+	TP_ARGS(page, ret), 1);
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>

maltesermailo/android_kernel_pantah

include/trace/hooks/vmscan.h
@@ -34,6 +34,16 @@ DECLARE_HOOK(android_vh_shrink_node_memcgs,
 DECLARE_HOOK(android_vh_tune_memcg_scan_type,
 	TP_PROTO(struct mem_cgroup *memcg, char *scan_type),
 	TP_ARGS(memcg, scan_type));
+DECLARE_HOOK(android_rvh_inactive_is_low,
+	TP_PROTO(unsigned long gb, unsigned long *inactive_ratio,
+		enum lru_list inactive_lru, bool *skip),
+	TP_ARGS(gb, inactive_ratio, inactive_lru, skip), 1);
+DECLARE_HOOK(android_rvh_snapshot_refaults,
+	TP_PROTO(struct lruvec *target_lruvec),
+	TP_ARGS(target_lruvec), 1);
+DECLARE_HOOK(android_rvh_shrink_page_list,
+	TP_PROTO(struct page *page, bool *skip),
+	TP_ARGS(page, skip), 1);
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>