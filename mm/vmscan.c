BayLibre/android_kernel_common

ANDROID: export putback_lru_page, isolate_lru_page and reclaim_pages
Export putback_lru_page, isolate_lru_page and reclaim_pages for
 reclaiming the page to the swap of the specified location

Bug: 234214858

Signed-off-by: Bing Han <bing.han@transsion.com>
Change-Id: I2fd0a2209d8a2882828c9e3b3bc61572e68405b8

mm/vmscan.c
@@ -1007,6 +1007,7 @@ void putback_lru_page(struct page *page)
 	lru_cache_add(page);
 	put_page(page);		/* drop ref from isolate */
 }
+EXPORT_SYMBOL(putback_lru_page);
 
 enum page_references {
 	PAGEREF_RECLAIM,
@@ -1853,6 +1854,7 @@ int isolate_lru_page(struct page *page)
 	}
 	return ret;
 }
+EXPORT_SYMBOL(isolate_lru_page);
 
 /*
  * A direct reclaimer may isolate SWAP_CLUSTER_MAX pages from the LRU list and
@@ -2228,6 +2230,7 @@ unsigned long reclaim_pages(struct list_head *page_list)
 
 	return nr_reclaimed;
 }
+EXPORT_SYMBOL(reclaim_pages);
 
 static unsigned long shrink_list(enum lru_list lru, unsigned long nr_to_scan,
 				 struct lruvec *lruvec, struct scan_control *sc)

maltesermailo/android_kernel_pantah

mm/vmscan.c
@@ -1007,6 +1007,7 @@ void putback_lru_page(struct page *page)
 	lru_cache_add(page);
 	put_page(page);		/* drop ref from isolate */
 }
+EXPORT_SYMBOL_GPL(putback_lru_page);
 
 enum page_references {
 	PAGEREF_RECLAIM,
@@ -1850,6 +1851,7 @@ int isolate_lru_page(struct page *page)
 	}
 	return ret;
 }
+EXPORT_SYMBOL_GPL(isolate_lru_page);
 
 /*
  * A direct reclaimer may isolate SWAP_CLUSTER_MAX pages from the LRU list and
@@ -2225,6 +2227,7 @@ unsigned long reclaim_pages(struct list_head *page_list)
 
 	return nr_reclaimed;
 }
+EXPORT_SYMBOL_GPL(reclaim_pages);
 
 static unsigned long shrink_list(enum lru_list lru, unsigned long nr_to_scan,
 				 struct lruvec *lruvec, struct scan_control *sc)