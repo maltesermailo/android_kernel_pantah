e58b98cec9f4ede05900f86772aab6916ff34b6e
Weichao Guo <guoweichao@oppo.corp-partner.google.com>
2022-06-14 10:35:39 +0800

    ANDROID: GKI: Add ANDROID_OEM_DATA_ARRAY() in struct ufs_hba
    Add ANDROID_OEM_DATA_ARRAY(2, 2) in struct ufs_hba to
    support customized features.
    
    Bug: 235673196
    Signed-off-by: Weichao Guo <guoweichao@oppo.corp-partner.google.com>
    Change-Id: I30d479d090256447b8e93157e30fc9db92563e46

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -974,6 +974,8 @@ struct ufs_hba {
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
+
+	ANDROID_OEM_DATA_ARRAY(1, 3);
 };
 
 /* Returns true if clocks can be gated. Otherwise false */
