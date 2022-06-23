5541974aea259d283bed4439a1c73576740a2522
Peifeng Li <lipeifeng@oppo.com>
2022-06-23 14:15:35 +0800

    ANDROID: vendor_hooks: account page-mapcount
    Support five hooks as follows to account
    the amount of multi-mapped pages in kernel:
    
    - android_vh_show_mapcount_pages
    - android_vh_do_traversal_lruvec
    - android_vh_update_page_mapcount
    - android_vh_add_page_to_lrulist
    - android_vh_del_page_from_lrulist
    
    Bug: 236578020
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: Ia2c7015aab442be7dbb496b8b630b9dff59ab935

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -1,4 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+#ifdef PROTECT_TRACE_INCLUDE_PATH
+#undef PROTECT_TRACE_INCLUDE_PATH
+
+#include <trace/hooks/save_incpath.h>
+#include <trace/hooks/mm.h>
+#include <trace/hooks/restore_incpath.h>
+
+#else /* PROTECT_TRACE_INCLUDE_PATH */
+
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM mm
 
@@ -122,6 +131,22 @@ DECLARE_HOOK(android_vh_drain_all_pages_bypass,
 		int migratetype, unsigned long did_some_progress,
 		bool *bypass),
 	TP_ARGS(gfp_mask, order, alloc_flags, migratetype, did_some_progress, bypass));
+DECLARE_HOOK(android_vh_update_page_mapcount,
+	TP_PROTO(struct page *page, bool inc_size, bool compound,
+			bool *ret, bool *success),
+	TP_ARGS(page, inc_size, compound, ret, success));
+DECLARE_HOOK(android_vh_add_page_to_lrulist,
+	TP_PROTO(struct page *page, bool compound, enum lru_list lru),
+	TP_ARGS(page, compound, lru));
+DECLARE_HOOK(android_vh_del_page_from_lrulist,
+	TP_PROTO(struct page *page, bool compound, enum lru_list lru),
+	TP_ARGS(page, compound, lru));
+DECLARE_HOOK(android_vh_show_mapcount_pages,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_do_traversal_lruvec,
+	TP_PROTO(struct lruvec *lruvec),
+	TP_ARGS(lruvec));
 DECLARE_HOOK(android_vh_cma_drain_all_pages_bypass,
 	TP_PROTO(unsigned int migratetype, bool *bypass),
 	TP_ARGS(migratetype, bypass));
@@ -142,3 +167,4 @@ DECLARE_HOOK(android_vh_ra_tuning_max_page,
 
 /* This part must be outside protection */
 #include <trace/define_trace.h>
+#endif /* PROTECT_TRACE_INCLUDE_PATH */
