d440e1eccf4744440eacafa684572d91e7fdce87
Chetan C R <quic_cchinnad@quicinc.com>
2022-07-01 17:09:21 +0530

    ANDROID: GKI: scsi: ufs: get storage type from command line
    Get the device storage type by reading it from
    kernel command line arguments and pass it to ufs drivers.
    
    Leaf changes summary: 1 artifact changed
    Changed leaf types summary: 0 leaf type changed
    Removed/Changed/Added functions summary: 0 Removed, 0 Changed, 1 Added function
    Removed/Changed/Added variables summary: 0 Removed, 0 Changed, 0 Added variable
    
    1 Added function:
    
      [A] 'function const char* get_storage_boot_device()'
    
    Bug: 239386922
    Change-Id: I955920c4f4dbceb9f81e6959be55fc79dbec3d02
    Signed-off-by: Chetan C R <quic_cchinnad@quicinc.com>

---
+++ drivers/scsi/ufs/ufs-cmdline.c
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2022, The Linux Foundation. All rights reserved.
+ */
+
+#include <linux/init.h>
+#include <linux/printk.h>
+#include <linux/string.h>
+#include <linux/bootconfig.h>
+
+#define ANDROID_BOOT_DEV_MAX_V3    30
+
+static char android_boot_dev_v3[ANDROID_BOOT_DEV_MAX_V3];
+static const char *android_boot_dev_v4;
+
+const char *get_storage_boot_device(void)
+{
+	if (android_boot_dev_v4 && strlen(android_boot_dev_v4))
+		return android_boot_dev_v4;
+
+	else if (strlen(android_boot_dev_v3))
+		return android_boot_dev_v3;
+
+	pr_err("Not able to get Bootconfig or Kernel command line param\n");
+	return NULL;
+};
+EXPORT_SYMBOL(get_storage_boot_device);
+
+static int __init get_android_boot_dev_v3(char *str)
+{
+	strscpy(android_boot_dev_v3, str, ANDROID_BOOT_DEV_MAX_V3);
+	return 1;
+}
+__setup("androidboot.bootdevice=", get_android_boot_dev_v3);
+
+static int __init get_android_boot_dev_v4(void)
+{
+	struct xbc_node *vnode = NULL;
+
+	android_boot_dev_v4 = xbc_find_value("androidboot.bootdevice", &vnode);
+	if (vnode && xbc_node_is_array(vnode))
+		xbc_array_for_each_value(vnode, android_boot_dev_v4)
+
+	pr_info("androidboot bootdevice V4 %s\n", android_boot_dev_v4);
+	return 0;
+}
+fs_initcall(get_android_boot_dev_v4);
