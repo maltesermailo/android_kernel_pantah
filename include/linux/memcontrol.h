c83483a04da618a0bd511ad821d115c815dd4557
Peifeng Li <lipeifeng@oppo.com>
2022-07-15 16:38:06 +0800

    ANDROID: add func:do_traversal_all_lruvec in mm/memcontrol.c
    do_traversal_all_lruvec: traversal all lruvec and do hookss.
    
    Bug: 236578020
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Signed-off-by: 80389617 <wangyongqiang1@oppo.com>
    Change-Id: I3d4f5159faaca1ee71ffa65f2fc1341f51da637c

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -352,6 +352,8 @@ enum page_memcg_data_flags {
 
 static inline bool folio_memcg_kmem(struct folio *folio);
 
+void do_traversal_all_lruvec(void);
+
 /*
  * After the initialization objcg->memcg is always pointing at
  * a valid memcg, but can be atomically swapped to the parent memcg.
@@ -1181,6 +1183,10 @@ static inline bool folio_memcg_kmem(struct folio *folio)
 static inline bool PageMemcgKmem(struct page *page)
 {
 	return false;
+}
+
+static inline void do_traversal_all_lruvec(void)
+{
 }
 
 static inline bool mem_cgroup_is_root(struct mem_cgroup *memcg)
