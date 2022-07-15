7d465cb164732d2f43418703619f01ddc9fb0305
Peifeng Li <lipeifeng@oppo.com>
2022-07-15 16:38:06 +0800

    ANDROID: add two func in mm/memcontrol.c
    - page_to_lruvec: get lruvec from page and pgdat.
    - do_traversal_all_lruvec: traversal all lruvec and do hookss.
    
    Bug: 236578020
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: I3d4f5159faaca1ee71ffa65f2fc1341f51da637c

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -382,6 +382,9 @@ enum page_memcg_data_flags {
 
 static inline bool PageMemcgKmem(struct page *page);
 
+struct lruvec *page_to_lruvec(struct page *page, pg_data_t *pgdat);
+void do_traversal_all_lruvec(void);
+
 /*
  * After the initialization objcg->memcg is always pointing at
  * a valid memcg, but can be atomically swapped to the parent memcg.
@@ -1165,6 +1168,15 @@ static inline struct mem_cgroup *page_memcg_check(struct page *page)
 static inline bool PageMemcgKmem(struct page *page)
 {
 	return false;
+}
+
+static inline struct lruvec *page_to_lruvec(struct page *page, pg_data_t *pgdat)
+{
+	return NULL;
+}
+
+static inline void do_traversal_all_lruvec(void)
+{
 }
 
 static inline bool mem_cgroup_is_root(struct mem_cgroup *memcg)
