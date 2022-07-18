5a144709353594860af34343881de8d5ee875d31
Nathan Huckleberry <nhuck@google.com>
2022-07-18 19:56:48 +0000

    ANDROID: dm-bufio: Add DM_BUFIO_GET_CANT_SLEEP
    Add an optional flag that ensures dm_bufio_get does not sleep.  This
    allows the dm-bufio cache to be queried from interrupt context.
    
    To ensure that dm-bufio does not sleep, dm-bufio must use a spinlock
    instead of a mutex.  Additionally, to avoid deadlocks, special care must
    be taken so that dm-bufio does not sleep while holding the spinlock.
    
    DM_BUFIO_GET_CANT_SLEEP is useful in some contexts, such as dm-verity,
    so that we can query the dm-bufio cache in a tasklet.  If the required
    data is cached, processing can be handled immediately in the tasklet
    instead of waiting for a work-queue job to be scheduled.  This can
    reduce latency when there is high CPU load and memory pressure.
    
    Change-Id: Ia8bece7f55f4861a4f6f105a6c0842e524c85d68
    Signed-off-by: Nathan Huckleberry <nhuck@google.com>

--- drivers/md/dm-bufio.c
+++ drivers/md/dm-bufio.c
@@ -81,6 +81,8 @@
  */
 struct dm_bufio_client {
 	struct mutex lock;
+	spinlock_t spinlock;
+	unsigned long spinlock_flags;
 
 	struct list_head lru[LIST_SIZE];
 	unsigned long n_buffers[LIST_SIZE];
@@ -90,6 +92,7 @@ struct dm_bufio_client {
 	s8 sectors_per_block_bits;
 	void (*alloc_callback)(struct dm_buffer *);
 	void (*write_callback)(struct dm_buffer *);
+	bool may_sleep;
 
 	struct kmem_cache *slab_buffer;
 	struct kmem_cache *slab_cache;
@@ -167,17 +170,26 @@ struct dm_buffer {
 
 static void dm_bufio_lock(struct dm_bufio_client *c)
 {
-	mutex_lock_nested(&c->lock, dm_bufio_in_request());
+	if (c->may_sleep)
+		mutex_lock_nested(&c->lock, dm_bufio_in_request());
+	else
+		spin_lock_irqsave_nested(&c->spinlock, c->spinlock_flags, dm_bufio_in_request());
 }
 
 static int dm_bufio_trylock(struct dm_bufio_client *c)
 {
-	return mutex_trylock(&c->lock);
+	if (c->may_sleep)
+		return mutex_trylock(&c->lock);
+	else
+		return spin_trylock_irqsave(&c->spinlock, c->spinlock_flags);
 }
 
 static void dm_bufio_unlock(struct dm_bufio_client *c)
 {
-	mutex_unlock(&c->lock);
+	if (c->may_sleep)
+		mutex_unlock(&c->lock);
+	else
+		spin_unlock_irqrestore(&c->spinlock, c->spinlock_flags);
 }
 
 /*----------------------------------------------------------------*/
@@ -879,7 +891,7 @@ static struct dm_buffer *__alloc_buffer_wait_no_callback(struct dm_bufio_client 
 	 * be allocated.
 	 */
 	while (1) {
-		if (dm_bufio_cache_size_latch != 1) {
+		if (dm_bufio_cache_size_latch != 1 && c->may_sleep) {
 			b = alloc_buffer(c, GFP_NOWAIT | __GFP_NORETRY | __GFP_NOMEMALLOC | __GFP_NOWARN);
 			if (b)
 				return b;
@@ -1041,6 +1053,7 @@ found_buffer:
 	 */
 	if (nf == NF_GET && unlikely(test_bit(B_READING, &b->state)))
 		return NULL;
+
 
 	b->hold_count++;
 	__relink_lru(b, test_bit(B_DIRTY, &b->state) ||
@@ -1749,12 +1762,17 @@ struct dm_bufio_client *dm_bufio_client_create(struct block_device *bdev, unsign
 	c->alloc_callback = alloc_callback;
 	c->write_callback = write_callback;
 
+	c->may_sleep = true;
+	if (flags & DM_BUFIO_GET_CANT_SLEEP)
+		c->may_sleep = false;
+
 	for (i = 0; i < LIST_SIZE; i++) {
 		INIT_LIST_HEAD(&c->lru[i]);
 		c->n_buffers[i] = 0;
 	}
 
 	mutex_init(&c->lock);
+	spin_lock_init(&c->spinlock);
 	INIT_LIST_HEAD(&c->reserved_buffers);
 	c->need_reserved_buffers = reserved_buffers;
 
