dd7faeb074c93cba9dabaeb932314bb2fc0dae7f
Will McVicker <willmcvicker@google.com>
2022-07-21 22:01:00 +0000

    PCI: dwc: update PCI msi_msg mapping
    Use GFP_DMA instead of GFP_DMA32 so that we can still allocate
    a DMA'able page when ZONE_DMA32 is disabled.
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: I1a7d4ac1b8eda5dc643df573c2c2fce27e104753

--- drivers/pci/controller/dwc/pcie-designware-host.c
+++ drivers/pci/controller/dwc/pcie-designware-host.c
@@ -378,7 +378,7 @@ int dw_pcie_host_init(struct pcie_port *pp)
 			if (ret)
 				dev_warn(pci->dev, "Failed to set DMA mask to 32-bit. Devices with only 32-bit MSI support may not work properly\n");
 
-			pp->msi_page = alloc_page(GFP_DMA32);
+			pp->msi_page = alloc_page(GFP_DMA);
 			pp->msi_data = dma_map_page(pci->dev, pp->msi_page, 0, PAGE_SIZE,
 						    DMA_FROM_DEVICE);
 			if (dma_mapping_error(pci->dev, pp->msi_data)) {
