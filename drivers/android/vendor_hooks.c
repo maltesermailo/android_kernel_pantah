c7d54510a406a476ad390ff4f4812ec8d2e2d3d6
Chris Goldsworthy <quic_cgoldswo@quicinc.com>
2022-07-27 16:58:09 -0700

    ANDROID: dma-buf: Add vendorhook to allow mmaping more memory than a DMA-BUF holds
    Add vendorhook to allow mmaping more memory than a DMA-BUF holds. The
    implementor of the vmap callback for the DMA-BUF is responsible for
    ensuring that all pages are backed by memory. The hook takes as input
    a DMA-BUF to allow the VMA bounds check to be done on a case-by-case
    basis for DMA-BUFs.
    
    Note that if the override is allowed to go through for a given
    DMA-BUF, then it can be the case that the size of this mmaped DMA-BUF
    is reported incorrectly when looking at /proc/pid/maps for the owning
    process.
    
    Change-Id: Iba8cc8adfd2290e4dc7ef04fce5d6a80ac92e0b3
    Signed-off-by: Chris Goldsworthy <quic_cgoldswo@quicinc.com>
    Bug: 234753494

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -34,6 +34,7 @@
 #include <trace/hooks/creds.h>
 #include <trace/hooks/selinux.h>
 #include <trace/hooks/syscall_check.h>
+#include <trace/hooks/dmabuf.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -150,5 +151,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_bpf_syscall);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_cpus_allowed_by_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ignore_dmabuf_vmap_bounds);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
