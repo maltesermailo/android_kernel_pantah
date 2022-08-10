b0458d9c93252c2c8c9e9f9313810c98fbfd3b03
Charan Teja Kalla <quic_charante@quicinc.com>
2022-08-10 11:43:12 +0530

    ANDROID: mm/cma: add vendor_hook in cma_alloc for retries
    Add a vendor hook in the cma alloc for the allocation retries that can
    be used by clients to control the number of retries before returning.
    
    Bug: 242836407
    Change-Id: Iccff0ac2a926d449da5af64086463bd8d93340fc
    Signed-off-by: Charan Teja Kalla <quic_charante@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -441,3 +441,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mark_page_accessed);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_ffu_update_cid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_retry);
