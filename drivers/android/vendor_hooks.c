ad5da77b88559ec6fd6ad727b73222808a8f1124
Charan Teja Kalla <quic_charante@quicinc.com>
2022-08-10 11:43:12 +0530

    ANDROID: mm/cma: add vendor_hook in cma_alloc for retries
    Add a vendor hook in the cma alloc for the allocation retries that can
    be used by clients to control the number of retries before returning.
    
    Bug: 366369206
    Bug: 364813584
    Bug: 242836407
    Change-Id: Iccff0ac2a926d449da5af64086463bd8d93340fc
    Signed-off-by: Charan Teja Kalla <quic_charante@quicinc.com>
    (cherry picked from commit 6f0ba6ad994a694cec25ee493376acb7a7b55659)
    (cherry picked from commit e1f8da20f9be910540663cf8b3c3cd817b3cd532)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -498,7 +498,12 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ogki_udp_unicast_rcv_skb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ogki_udp6_unicast_rcv_skb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ogki_kmem_cache_create_usercopy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_init_adjust_zone_wmark);
+<<<<<<< HEAD   (0bc3f8 ANDROID: oplus: Update the ABI xml and symbol list)
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fsnotify_open);
+=======
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_retry);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_ksys_umount);
+>>>>>>> CHANGE (e1f8da ANDROID: mm/cma: add vendor_hook in cma_alloc for retries)
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_group_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swap_writepage);
