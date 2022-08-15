b68cc5896053fa0f210ed37209ff8665ef53efc1
qizhong cheng <qizhong.cheng@mediatek.com>
2022-08-15 17:56:11 +0800

    ANDROID: fix add vendor hooks for unusual abort cases
    Add hooks to check the do_serror can be recover or not.
    When PCIe encounters completion timeout, PCIe hardware will
    reply slave error to bus, which will causes the system to panic.
    
    Bug: 247846368
    
    Signed-off-by: qizhong cheng <qizhong.cheng@mediatek.com>
    Change-Id: Iaef6c9a6ac07e552939c0f8e386213cb930cfb6e
    (cherry picked from commit a89fbc0de097cf73f7203f60e4d3fdca1439afd7)

--- include/trace/hooks/traps.h
+++ include/trace/hooks/traps.h
@@ -1,3 +1,48 @@
+<<<<<<< PATCH SET (8c7121 ANDROID: fix add vendor hooks for unusual abort cases)
+<<<<<<< HEAD   (fcc49a ANDROID: ABI: Update QCOM symbol list)
+=======
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM traps
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_TRAPS_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_TRAPS_H
+#include <trace/hooks/vendor_hooks.h>
+
+#ifdef __GENKSYMS__
+#include <asm/ptrace.h>
+#endif
+
+struct pt_regs;
+
+DECLARE_RESTRICTED_HOOK(android_rvh_do_undefinstr,
+	TP_PROTO(struct pt_regs *regs),
+	TP_ARGS(regs),
+	TP_CONDITION(!user_mode(regs)));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_do_ptrauth_fault,
+	TP_PROTO(struct pt_regs *regs, unsigned int esr),
+	TP_ARGS(regs, esr),
+	TP_CONDITION(!user_mode(regs)));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_panic_unhandled,
+	TP_PROTO(struct pt_regs *regs, const char *vector, unsigned int esr),
+	TP_ARGS(regs, vector, esr), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_arm64_serror_panic,
+	TP_PROTO(struct pt_regs *regs, unsigned int esr),
+	TP_ARGS(regs, esr), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_do_serror,
+	TP_PROTO(struct pt_regs *regs, unsigned int esr, int *ret),
+	TP_ARGS(regs, esr, ret), 1);
+
+#endif /* _TRACE_HOOK_TRAPS_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+>>>>>>> CHANGE (a89fbc ANDROID: fix add vendor hooks for unusual abort cases)
+=======
 /* SPDX-License-Identifier: GPL-2.0 */
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM traps
@@ -33,3 +78,4 @@ DECLARE_RESTRICTED_HOOK(android_rvh_arm64_serror_panic,
 #endif /* _TRACE_HOOK_TRAPS_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
+>>>>>>> BASE      (e0923f ANDROID: gic: Add vendor hook for gic-v3 resume)
