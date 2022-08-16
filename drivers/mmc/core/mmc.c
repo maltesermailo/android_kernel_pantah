c349373aa73e77aa601491cd0b059e37be7cc296
Wenchao Chen <wenchao.chen@unisoc.com>
2022-08-16 13:32:10 +0800

    ANDROID: GKI: update vendor hook for mmc
    These three vendor hooks are implemented respectively:
    trace_android_rvh_mmc_update_partition_status implements the partition protection function, trace_android_rvh_mmc_sd_update_cmdline_timing and
    trace_android_rvh_mmc_sd_update_dataline_timing implements low-speed card tuning.Because the vendor hook here may sleep when used, it must be updated to a restricted vendor hook.
    
    Bug: 242645760
    Change-Id: I880436e3159bec30d7d92c620b268ec350e97235
    Signed-off-by: Wenchao Chen <wenchao.chen@unisoc.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1768,7 +1768,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 			goto free_card;
 	}
 
-	trace_android_vh_mmc_update_partition_status(card);
+	trace_android_rvh_mmc_update_partition_status(card);
 
 	/*
 	 * Enable power_off_notification byte in the ext_csd register
