0949ab2500ab87b9d05bfc8599b50f854f5238fa
Todd Kjos <tkjos@google.com>
2022-09-21 21:48:21 +0000

    ANDROID: make sure all types for hooks are defined in KMI
    There are 2 remaining types directly referenced by vendor
    hooks that were not fully-defined in the KMI:
    
    struct gic_chip_data : defined in include/linux/irqchip/arm-gic-v3.h
    struct swap_slots_cache : defined include/linux/swap_slots.h
    
    libabigail is not finding definitions based on the instantiation
    of the hooks, so force them to be defined by defining dummy exported
    symbols.
    
    Bug: 233047575
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: I521b2a596e1d7361d0f44a87ffe330186896b9f8

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -458,3 +458,16 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_page_look_around_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_look_around);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_look_around_migrate_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_test_clear_look_around_ref);
+
+/*
+ * For type visibility
+ */
+#ifdef CONFIG_ARM64
+#include <linux/irqchip/arm-gic-v3.h>
+#endif
+const struct gic_chip_data *GKI_struct_gic_chip_data;
+EXPORT_SYMBOL_GPL(GKI_struct_gic_chip_data);
+
+#include <linux/swap_slots.h>
+const struct swap_slots_cache *GKI_struct_swap_slots_cache;
+EXPORT_SYMBOL_GPL(GKI_struct_swap_slots_cache);
