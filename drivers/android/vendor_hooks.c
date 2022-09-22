3f061a233aa7905b85768a5ef909171fa021e7dc
Rick Yiu <rickyiu@google.com>
2022-09-22 07:18:33 +0000

    ANDROID: cgroup: Add vendor hook for rebuild_root_domains_bypass
    Vendor could decide to bypass this function.
    
    Bug: 238390134
    Change-Id: Ia6bc71c7569d21da9ae226e5d8739f97b9ca1a4f
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -405,6 +405,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_hib_resume_bdev);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dma_buf_stats_teardown);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_cold_or_pageout);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_retry);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rebuild_root_domains_bypass);
 
 /*
  * For type visibility
--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -956,6 +956,12 @@ static void rebuild_root_domains(void)
 {
 	struct cpuset *cs = NULL;
 	struct cgroup_subsys_state *pos_css;
+	bool bypass = false;
+
+	trace_android_vh_rebuild_root_domains_bypass(cpuhp_tasks_frozen, &bypass);
+
+	if (bypass)
+		return;
 
 	percpu_rwsem_assert_held(&cpuset_rwsem);
 	lockdep_assert_cpus_held();
