53d37384fcb4c461c3e4b41ac9881eab4fb1b31b
Paul Lawrence <paullawrence@google.com>
2022-10-03 09:56:21 -0700

    ANDROID: fuse-bpf: set error_in to ENOENT in negative lookup
    Bug: 250617797
    Test: fuse-test passes, app installs
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I423741b307c25b58984d856d54b61588f5219d9f

--- fs/fuse/backing.c
+++ fs/fuse/backing.c
@@ -1359,8 +1359,10 @@ int fuse_lookup_backing(struct bpf_fuse_args *fa, struct dentry **out, struct in
 		.mnt = mntget(dir_fuse_entry->backing_path.mnt),
 	};
 
-	if (d_is_negative(backing_entry))
+	if (d_is_negative(backing_entry)) {
+		fa->error_in = -ENOENT;
 		return 0;
+	}
 
 	err = vfs_getattr(&fuse_entry->backing_path, &stat,
 				  STATX_BASIC_STATS, 0);
