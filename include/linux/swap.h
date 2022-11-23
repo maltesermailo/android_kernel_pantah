7c721a24de07145381170fa563551138f06f0f96
Shreyas K K <quic_shrekk@quicinc.com>
2022-11-23 11:31:25 +0530

    ANDROID: vendor hooks: Add vendor hooks to encrypt hibernation snapshot
    Add support to encrypt the hibernation snapshot image before it is
    written to the swap partition.
    
    Bug: 260069271
    Change-Id: I07046ad7fb848fc62258871ab41b3e03246c40dc
    Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -494,6 +494,7 @@ static inline long get_nr_swap_pages(void)
 	return atomic_long_read(&nr_swap_pages);
 }
 
+struct hib_bio_batch;
 extern void si_swapinfo(struct sysinfo *);
 extern swp_entry_t get_swap_page(struct page *page);
 extern void put_swap_page(struct page *page, swp_entry_t entry);
@@ -523,6 +524,8 @@ extern int init_swap_address_space(unsigned int type, unsigned long nr_pages);
 extern void exit_swap_address_space(unsigned int type);
 extern struct swap_info_struct *get_swap_device(swp_entry_t entry);
 sector_t swap_page_sector(struct page *page);
+extern int write_page(void *buf, sector_t offset, struct hib_bio_batch *hb);
+extern sector_t alloc_swapdev_block(int swap);
 
 static inline void put_swap_device(struct swap_info_struct *si)
 {
