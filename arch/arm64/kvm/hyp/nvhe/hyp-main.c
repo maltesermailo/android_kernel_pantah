f7922d0cbbba5a428242ac1868e0bb0bcfb624b5
Vincent Donnefort <vdonnefort@google.com>
2022-12-09 15:08:52 +0000

    ANDROID: KVM: arm64: Extend memory sharing to allow wrprotected donations
    Sharing memory between the hypervisor and the host where the latter only
    retains read-only access is interesting for some features such as
    tracing to reduce attack surface.
    
    Reducing the permissions on the host own memory implies an ownership
    transfer. To avoid a costly host-hyp donation followed by a hyp-host
    sharing, brings a new donation-sharing mechanism. This is similar to the
    regular sharing but the ownership is also transfered and the initiator
    can set reduce permissions.
    
    For now this mechanism is limited to hyp/host sharing with wrprotect as
    this is what we're interested in. The wrprotect value is cached into the
    hyp vmemmap to avoid having to walk the table for later permission
    verifications.
    
    Bug: 229972309
    Change-Id: I77f1827090b65f60a3e4f5c2f97bc85c433db3fa
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1048,6 +1048,22 @@ static void handle___pkvm_host_unshare_hyp(struct kvm_cpu_context *host_ctxt)
 	cpu_reg(host_ctxt, 1) = __pkvm_host_unshare_hyp(pfn);
 }
 
+static void
+handle___pkvm_host_wrprotect_share_hyp(struct kvm_cpu_context *host_ctxt)
+{
+	DECLARE_REG(u64, pfn, host_ctxt, 1);
+
+	cpu_reg(host_ctxt, 1) = __pkvm_host_wrprotect_share_hyp(pfn);
+}
+
+static void
+handle___pkvm_host_wrprotect_unshare_hyp(struct kvm_cpu_context *host_ctxt)
+{
+	DECLARE_REG(u64, pfn, host_ctxt, 1);
+
+	cpu_reg(host_ctxt, 1) = __pkvm_host_wrprotect_unshare_hyp(pfn);
+}
+
 static void handle___pkvm_host_reclaim_page(struct kvm_cpu_context *host_ctxt)
 {
 	DECLARE_REG(u64, pfn, host_ctxt, 1);
@@ -1259,6 +1275,8 @@ static const hcall_t host_hcall[] = {
 
 	HANDLE_FUNC(__pkvm_host_share_hyp),
 	HANDLE_FUNC(__pkvm_host_unshare_hyp),
+	HANDLE_FUNC(__pkvm_host_wrprotect_share_hyp),
+	HANDLE_FUNC(__pkvm_host_wrprotect_unshare_hyp),
 	HANDLE_FUNC(__pkvm_host_reclaim_page),
 	HANDLE_FUNC(__pkvm_host_map_guest),
 	HANDLE_FUNC(__kvm_adjust_pc),
