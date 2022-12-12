005aff72f620d3a260b4f024db8b6c9e7e2e7d2f
Quentin Perret <qperret@google.com>
2022-12-12 18:09:47 +0000

    ANDROID: KVM: arm64: Introduce concept of pKVM moveable regions
    The pKVM memory pool is currently sized to allow mapping all of memory
    with page-granularity mappings in the host stage-2 page-table, as well
    as up to 1GiB of MMIO range with page-granularity. Indeed, pKVM
    currently assumes that MMIO regions are completely and solely owned by
    the host for the entire lifetime of the system. As such, the pages used
    to map MMIO regions can always be recycled to allow forward progress if
    the memory pool ran out of pages -- pKVM can unmap MMIO ranges at
    stage-2 without fearing to loose important information about the state
    of the underlying page, and those mappings can always be reconstructed
    later.
    
    In order to allow transitioning the ownership of non-memory regions,
    introduce a concept of pkvm 'moveable' regions, which represents regions
    of the physical address space which can be 'moved' from an ownership
    perspective. These moveable regions are used to size the hyp memory
    pool. In a first step, the list of moveable regions is equal to the
    memblock list, but it will be extended in subsequent changes.
    
    No functional changes intended.
    
    Bug: 244543039
    Bug: 244373730
    Change-Id: I7f451924b1eed9579868e6ff8c7adc7b4a5a0ae1
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -24,6 +24,7 @@ static struct reserved_mem *pkvm_firmware_mem;
 static phys_addr_t *pvmfw_base = &kvm_nvhe_sym(pvmfw_base);
 static phys_addr_t *pvmfw_size = &kvm_nvhe_sym(pvmfw_size);
 
+static struct pkvm_moveable_reg *moveable_regs = kvm_nvhe_sym(pkvm_moveable_regs);
 static struct memblock_region *hyp_memory = kvm_nvhe_sym(hyp_memory);
 static unsigned int *hyp_memblock_nr_ptr = &kvm_nvhe_sym(hyp_memblock_nr);
 
@@ -63,6 +64,50 @@ static int __init register_memblock_regions(void)
 	return 0;
 }
 
+static int cmp_moveable_reg(const void *p1, const void *p2)
+{
+	const struct pkvm_moveable_reg *r1 = p1;
+	const struct pkvm_moveable_reg *r2 = p2;
+
+	if (r1->start < r2->start)
+		return -1;
+	else if (r1->start > r2->start)
+		return 1;
+	else if (r1->size > r2->size)
+		return -1;
+	else if (r1->size < r2->size)
+		return 1;
+	return 0;
+}
+
+static void __init sort_moveable_regs(void)
+{
+	sort(moveable_regs,
+	     kvm_nvhe_sym(pkvm_moveable_regs_nr),
+	     sizeof(struct pkvm_moveable_reg),
+	     cmp_moveable_reg,
+	     NULL);
+}
+
+static int __init register_moveable_regions(void)
+{
+	struct memblock_region *reg;
+	int i = 0;
+
+	for_each_mem_region(reg) {
+		if (i >= PKVM_NR_MOVEABLE_REGS)
+			return -ENOMEM;
+		moveable_regs[i].start = reg->base;
+		moveable_regs[i].size = reg->size;
+		moveable_regs[i].type = PKVM_MREG_MEMORY;
+		i++;
+	}
+	kvm_nvhe_sym(pkvm_moveable_regs_nr) = i;
+	sort_moveable_regs();
+
+	return 0;
+}
+
 void __init kvm_hyp_reserve(void)
 {
 	u64 hyp_mem_pages = 0;
@@ -78,6 +123,13 @@ void __init kvm_hyp_reserve(void)
 	if (ret) {
 		*hyp_memblock_nr_ptr = 0;
 		kvm_err("Failed to register hyp memblocks: %d\n", ret);
+		return;
+	}
+
+	ret = register_moveable_regions();
+	if (ret) {
+		*hyp_memblock_nr_ptr = 0;
+		kvm_err("Failed to register pkvm moveable regions: %d\n", ret);
 		return;
 	}
 
