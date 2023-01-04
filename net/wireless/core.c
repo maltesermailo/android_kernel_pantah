d4e388c036811e1b8614f690caeb427ab6e8b32a
Isaac Chiou <isaacchiou@google.com>
2023-01-04 20:49:46 +0800

    Porting Cross AKM Roaming feature from kernel 5.15 to 5.10
    Support NL80211_ATTR_MAX_NUM_AKM_SUITES command in NL80211 for 5.10 kernel
    
    Test: Connection and Roaming test
    Bug:265234448
    Change-Id: Id85c75284744346d3be489f43dbfd7295acce786
    Signed-off-by: Isaac Chiou <isaacchiou@google.com>

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -905,6 +905,11 @@ int wiphy_register(struct wiphy *wiphy)
 				rdev->wiphy.wowlan->pattern_max_len)))
 		return -EINVAL;
 #endif
+	if (!wiphy->max_num_akm_suites)
+		wiphy->max_num_akm_suites = NL80211_MAX_NR_AKM_SUITES;
+	else if (wiphy->max_num_akm_suites < NL80211_MAX_NR_AKM_SUITES ||
+		 wiphy->max_num_akm_suites > CFG80211_MAX_NUM_AKM_SUITES)
+		return -EINVAL;
 
 	/* check and set up bitrates */
 	ieee80211_set_bitrate_flags(wiphy);
