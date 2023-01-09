955c523d7332dc2882b14fe8d5fc5997f9073d19
Marc Zyngier <maz@kernel.org>
2023-01-09 19:20:19 +0530

    FROMLIST: clocksource/drivers/arm_arch_timer: Update sched_clock when non-boot CPUs need counter workaround
    When booting on a CPU that has a countertum on the counter read,
    we use the arch_counter_get_cnt{v,p}ct_stable() backend which
    applies the workaround.
    
    However, we don't do the same thing when an affected CPU is
    a secondary CPU, and we're stuck with the standard sched_clock()
    backend that knows nothing about the workaround.
    
    Fix it by always indirecting sched_clock(), making arch_timer_read_counter
    a function instead of a function pointer. In turn, we update the
    pointer (now private to the driver code) when detecting a new
    workaround.
    
    Bug: 265374093
    Link: https://lore.kernel.org/linux-arm-kernel/Y8FSUOC7k3ChMazG@FVFF77S0Q05N/T/
    Change-Id: Ic85d89e1ac0dfad598a7ad3e0023aed7de61640d
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Daniel Lezcano <daniel.lezcano@kernel.org>
    Cc: Thomas Gleixner <tglx@linotronix.de>
    Cc: stable@vger.kernel.org
    Reported-by: Yogesh Lal <quic_ylal@quicinc.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Fixes: 0ea415390cd3 ("clocksource/arm_arch_timer: Use arch_timer_read_counter to access stable counters")
    Signed-off-by: Srinivasarao Pathipati <quic_spathi@quicinc.com>

--- include/clocksource/arm_arch_timer.h
+++ include/clocksource/arm_arch_timer.h
@@ -84,7 +84,7 @@ struct arch_timer_mem {
 #ifdef CONFIG_ARM_ARCH_TIMER
 
 extern u32 arch_timer_get_rate(void);
-extern u64 (*arch_timer_read_counter)(void);
+extern u64 arch_timer_read_counter(void);
 extern struct arch_timer_kvm_info *arch_timer_get_kvm_info(void);
 extern bool arch_timer_evtstrm_available(void);
 
