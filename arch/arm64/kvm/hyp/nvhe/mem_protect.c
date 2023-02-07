07b62795a31f4d399bec77546864596d71e80563
Quentin Perret <qperret@google.com>
2023-02-07 13:13:14 +0000

    ANDROID: KVM: arm64: Allow setting {P,U}XN in stage-2 PTEs
    FEAT_XNX allows to specify PXN and UXN attributes on stage-2 entries.
    Make this usable from pKVM by exposing two new kvm_pgtable_prot entries
    for each of them.
    
    No functional changes intended.
    
    Bug: 264070847
    Change-Id: I47d861fa64ba511370b182f4609fe1c27695a949
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -1964,6 +1964,10 @@ static int restrict_host_page_perms(u64 addr, kvm_pte_t pte, u32 level, enum kvm
 	return ret;
 }
 
+#define MODULE_PROT_ALLOWLIST (KVM_PGTABLE_PROT_RWX |	\
+			       KVM_PGTABLE_PROT_NC |	\
+			       KVM_PGTABLE_PROT_PXN |	\
+			       KVM_PGTABLE_PROT_UXN)
 int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot)
 {
 	u64 addr = hyp_pfn_to_phys(pfn);
@@ -1972,7 +1976,7 @@ int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot)
 	u32 level;
 	int ret;
 
-	if ((prot & KVM_PGTABLE_PROT_RWX) != prot || !addr_is_memory(addr))
+	if ((prot & MODULE_PROT_ALLOWLIST) != prot || !addr_is_memory(addr))
 		return -EINVAL;
 
 	host_lock_component();
