9a35e02e2637b2d719750fe7e6c82797105b1ffc
Vincent Donnefort <vdonnefort@google.com>
2023-02-10 14:03:01 +0000

    ANDROID: KVM: arm64: Decode pKVM module tracing section
    In preparation to support in-pKVM-module declared events, decode the hyp
    event ELF sections for later registration.
    
    The hyp_event table (defined by asm/kvm_define_hypevents.h) is only
    accessed by the host.
    
    The hyp_event_id table (defined by nvhe/define_events.h) is populated by
    the host before deprivilege and then is read-only by the hypervisor.
    
    It is then not easy for the later to declare the table as a separated
    ELF section. The granularity must be PAGE_SIZE (as the host will donate
    the page containing those events to the hypervisor) and also we want the
    end of the section indicating the last hyp_event_id, not the end of a
    page. As a solution, fold the table into hyp.rodata and declare linker
    symbols (__hyp_event_ids_start/end) the module loading can read from the
    module ELF headers.
    
    Bug: 278749606
    Bug: 268495982
    Change-Id: I7cd911144d58f8bbd173e23ce08d59bec10a6262
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kernel/module.c
+++ arch/arm64/kernel/module.c
@@ -20,6 +20,8 @@
 #include <asm/alternative.h>
 #include <asm/insn.h>
 #include <asm/scs.h>
+#include <asm/kvm_hyptrace.h>
+#include <asm/kvm_hypevents_defs.h>
 #include <asm/sections.h>
 
 void *module_alloc(unsigned long size)
@@ -511,6 +513,7 @@ static int module_init_hyp(const Elf_Ehdr *hdr, const Elf_Shdr *sechdrs,
 			   struct module *mod)
 {
 #ifdef CONFIG_KVM
+	struct pkvm_el2_module *hyp_mod = &mod->arch.hyp;
 	const Elf_Shdr *s;
 
 	/*
@@ -521,7 +524,7 @@ static int module_init_hyp(const Elf_Ehdr *hdr, const Elf_Shdr *sechdrs,
 	if (!s || !s->sh_size)
 		return 0;
 
-	mod->arch.hyp.text = (struct pkvm_module_section) {
+	hyp_mod->text = (struct pkvm_module_section) {
 		.start	= (void *)s->sh_addr,
 		.end	= (void *)s->sh_addr + s->sh_size,
 	};
@@ -555,6 +558,25 @@ static int module_init_hyp(const Elf_Ehdr *hdr, const Elf_Shdr *sechdrs,
 			.start	= (void *)s->sh_addr,
 			.end	= (void *)s->sh_addr + s->sh_size,
 		};
+	}
+
+	s = find_section(hdr, sechdrs, "_hyp_events");
+	if (s) {
+		hyp_mod->hyp_events = (void *)s->sh_addr;
+		hyp_mod->nr_hyp_events = s->sh_size /
+			sizeof(*hyp_mod->hyp_events);
+
+		s = find_section(hdr, sechdrs, ".hyp.event_ids");
+		if (s) {
+			hyp_mod->hyp_event_ids = (void *)s->sh_addr;
+			hyp_mod->nr_hyp_event_ids = s->sh_size /
+					sizeof(*hyp_mod->hyp_event_ids);
+		} else {
+			hyp_mod->hyp_events = NULL;
+			hyp_mod->nr_hyp_events = 0;
+			pr_warn("%s contains _hyp_events but no .hyp.event_ids",
+				mod->name);
+		}
 	}
 #endif
 	return 0;
