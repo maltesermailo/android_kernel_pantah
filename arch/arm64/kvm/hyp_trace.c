d01057a51f3852d3bbfd93b3703f8f6a838354d8
Vincent Donnefort <vdonnefort@google.com>
2023-02-14 19:21:22 +0000

    ANDROID: KVM: arm64: Allow to reset hyp trace buffers
    Writing anything to the "trace" file will delete the content of the
    buffer. When using the common "trace", the ring buffer will also be
    unloaded from the hypervisor and all the memory will be freed.
    
    At the same time, tracing_on will not reset the buffers anymore,
    bringing the hyp tracing interface a bit closer from the host behavior.
    
    Bug: 249050813
    Change-Id: I9d4ba7b18504440f3d03dbedf1186d384a53a990
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp_trace.c
+++ arch/arm64/kvm/hyp_trace.c
@@ -20,6 +20,8 @@
 #define RB_POLL_MS 1000
 
 #define TRACEFS_DIR "hyp"
+#define TRACEFS_MODE_WRITE 0640
+#define TRACEFS_MODE_READ 0440
 
 static bool hyp_trace_on;
 static int hyp_trace_readers;
@@ -198,25 +200,11 @@ end:
 	hyp_trace_buffer = NULL;
 }
 
-static void hyp_free_tracing(void)
-{
-	if (!hyp_trace_buffer)
-		return;
-
-	trace_buffer_teardown(NULL);
-	bpage_backing_teardown();
-}
-
-static int hyp_start_tracing(void)
+static int hyp_load_tracing(void)
 {
 	struct hyp_trace_pack *pack;
 	size_t pack_size;
-	int ret = 0;
-
-	if (hyp_trace_on || hyp_trace_readers)
-		return -EBUSY;
-
-	hyp_free_tracing();
+	int ret;
 
 	ret = trace_buffer_setup(&pack, &pack_size);
 	if (ret)
@@ -231,7 +219,6 @@ static int hyp_start_tracing(void)
 	/* Load also enable tracing */
 	ret = kvm_call_hyp_nvhe(__pkvm_load_tracing, (unsigned long)pack, pack_size);
 	if (!ret) {
-		hyp_trace_on = true;
 		goto end_free_pack;
 	}
 
@@ -244,6 +231,41 @@ end_free_pack:
 	return ret;
 }
 
+static void hyp_free_tracing(void)
+{
+	int ret;
+
+	if (!hyp_trace_buffer)
+		return;
+
+	ret = kvm_call_hyp_nvhe(__pkvm_teardown_tracing);
+	if (ret) {
+		WARN_ON(1);
+		return;
+	}
+
+	trace_buffer_teardown(NULL);
+	bpage_backing_teardown();
+}
+
+static int hyp_start_tracing(void)
+{
+	int ret = 0;
+
+	if (hyp_trace_on || hyp_trace_readers)
+		return -EBUSY;
+
+	if (hyp_trace_buffer)
+		ret = kvm_call_hyp_nvhe(__pkvm_enable_tracing, true);
+	else
+		ret = hyp_load_tracing();
+
+	if (!ret)
+		hyp_trace_on = true;
+
+	return ret;
+}
+
 static void hyp_stop_tracing(void)
 {
 	int ret;
@@ -251,7 +273,7 @@ static void hyp_stop_tracing(void)
 	if (!hyp_trace_buffer || !hyp_trace_on)
 		return;
 
-	ret = kvm_call_hyp_nvhe(__pkvm_teardown_tracing);
+	ret = kvm_call_hyp_nvhe(__pkvm_enable_tracing, false);
 	if (ret) {
 		WARN_ON(1);
 		return;
@@ -323,6 +345,8 @@ static ssize_t hyp_buffer_size(struct file *filp, const char __user *ubuf,
 
 	mutex_lock(&hyp_trace_lock);
 	hyp_trace_buffer_size = val << 10; /* KB to B */
+	if (hyp_trace_buffer)
+		pr_info("Clear the hyp trace buffer to take into account the new size\n");
 	mutex_unlock(&hyp_trace_lock);
 
 	return cnt;
@@ -561,6 +585,22 @@ static const struct seq_operations hyp_trace_ops = {
 	.show	= ht_show,
 };
 
+static int hyp_trace_reset(int cpu)
+{
+	if (!hyp_trace_buffer)
+		return 0;
+
+	if (hyp_trace_on)
+		return -EBUSY;
+
+	if (cpu == RING_BUFFER_ALL_CPUS)
+		hyp_free_tracing();
+	else
+		ring_buffer_reset_cpu(hyp_trace_buffer, cpu);
+
+	return 0;
+}
+
 static int hyp_trace_open(struct inode *inode, struct file *file)
 {
 	int cpu = (s64)inode->i_private;
@@ -569,14 +609,19 @@ static int hyp_trace_open(struct inode *inode, struct file *file)
 
 	mutex_lock(&hyp_trace_lock);
 
+	if (file->f_mode & FMODE_WRITE) {
+		ret = hyp_trace_reset(cpu);
+		goto unlock;
+	}
+
 	iter = __seq_open_private(file, &hyp_trace_ops, sizeof(*iter));
 	if (!iter) {
 		ret = -ENOMEM;
-		goto unlock_err;
+		goto unlock;
 	}
 
 	if (!hyp_trace_buffer)
-		goto unlock;
+		goto start_read;
 
 	iter->buf_iter = kcalloc(nr_cpu_ids, sizeof(*iter->buf_iter), GFP_KERNEL);
 	if (!iter->buf_iter) {
@@ -598,9 +643,9 @@ static int hyp_trace_open(struct inode *inode, struct file *file)
 							       cpu, GFP_KERNEL);
 		ring_buffer_read_start(iter->buf_iter[cpu]);
 	}
-unlock:
+start_read:
 	hyp_trace_readers++;
-unlock_err:
+unlock:
 	mutex_unlock(&hyp_trace_lock);
 
 	return ret;
@@ -610,6 +655,9 @@ int hyp_trace_release(struct inode *inode, struct file *file)
 {
 	struct seq_file *m = file->private_data;
 	struct ht_iterator *iter = m->private;
+
+	if (file->f_mode & FMODE_WRITE)
+		return 0;
 
 	if (!iter->buf_iter)
 		goto end;
@@ -631,11 +679,19 @@ end:
 	return seq_release_private(inode, file);
 }
 
+static ssize_t hyp_trace_write(struct file *filp, const char __user *ubuf,
+			       size_t count, loff_t *ppos)
+{
+	/* No matter the input, write reset the buffer ... */
+	return count;
+}
+
 static const struct file_operations hyp_trace_fops = {
-	.open  = hyp_trace_open,
-	.read  = seq_read,
-	.llseek = seq_lseek,
-	.release = hyp_trace_release,
+	.open		= hyp_trace_open,
+	.read		= seq_read,
+	.write		= hyp_trace_write,
+	.llseek		= seq_lseek,
+	.release	= hyp_trace_release,
 };
 
 /*
@@ -840,10 +896,11 @@ static const struct file_operations hyp_trace_raw_fops = {
 
 static void hyp_tracefs_create_cpu_file(const char *file_name,
 					int cpu,
+					umode_t mode,
 					const struct file_operations *fops,
 					struct dentry *parent)
 {
-	if (!tracefs_create_file(file_name, 0440, parent, (void *)(s64)cpu, fops))
+	if (!tracefs_create_file(file_name, mode, parent, (void *)(s64)cpu, fops))
 		pr_warn("Failed to create tracefs %pd/%s\n", parent, file_name);
 }
 
@@ -879,7 +936,8 @@ int init_hyp_tracefs(void)
 
 
 	hyp_tracefs_create_cpu_file("trace", RING_BUFFER_ALL_CPUS,
-				    &hyp_trace_fops, root_dir);
+				    TRACEFS_MODE_WRITE, &hyp_trace_fops,
+				    root_dir);
 
 	per_cpu_root_dir = tracefs_create_dir("per_cpu", root_dir);
 	if (!per_cpu_root_dir) {
@@ -898,10 +956,12 @@ int init_hyp_tracefs(void)
 			continue;
 		}
 
-		hyp_tracefs_create_cpu_file("trace", cpu, &hyp_trace_fops, dir);
-		hyp_tracefs_create_cpu_file("trace_pipe", cpu,
+		hyp_tracefs_create_cpu_file("trace", cpu, TRACEFS_MODE_WRITE,
+					    &hyp_trace_fops, dir);
+		hyp_tracefs_create_cpu_file("trace_pipe", cpu, TRACEFS_MODE_READ,
 					    &hyp_trace_pipe_fops, dir);
 		hyp_tracefs_create_cpu_file("trace_pipe_raw", cpu,
+					    TRACEFS_MODE_READ,
 					    &hyp_trace_raw_fops, dir);
 	}
 
