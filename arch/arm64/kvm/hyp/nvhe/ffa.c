3019be8617b9e3ec34090c9fcfdcc2c338ed9811
Sebastian Ene <sebastianene@google.com>
2023-02-14 16:43:59 +0000

    ANDROID: KVM: arm64: Use the hypervisor lock for FF-A transactions
    In preparation for adding FF-A support for guest VMs, make sure that we
    synchronize the access of the hypervisor buffers using the correct lock.
    This will introduce lock contention when there are multipple guests
    issuing FF-A transactions that make use of the hypervisor buffers.
    
    Bug: 269285339
    Change-Id: Id7f79ffc8d2bc20be591251991cd66e307746102
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -62,8 +62,7 @@ struct kvm_ffa_buffers {
 
 /*
  * Note that we don't currently lock these buffers explicitly, instead
- * relying on the locking of the host FFA buffers as we only have one
- * client.
+ * relying on the locking of the hyp FFA buffers.
  */
 static struct kvm_ffa_buffers hyp_buffers;
 static struct kvm_ffa_buffers host_buffers;
@@ -200,7 +199,7 @@ static void do_ffa_rxtx_map(struct arm_smccc_res *res,
 		goto out;
 	}
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&hyp_buffers.lock);
 	if (host_buffers.tx) {
 		ret = FFA_RET_DENIED;
 		goto out_unlock;
@@ -244,7 +243,7 @@ static void do_ffa_rxtx_map(struct arm_smccc_res *res,
 	host_buffers.rx = rx_virt;
 
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&hyp_buffers.lock);
 out:
 	ffa_to_smccc_res(res, ret);
 	return;
@@ -272,7 +271,7 @@ static void do_ffa_rxtx_unmap(struct arm_smccc_res *res,
 		goto out;
 	}
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&hyp_buffers.lock);
 	if (!host_buffers.tx) {
 		ret = FFA_RET_INVALID_PARAMETERS;
 		goto out_unlock;
@@ -289,7 +288,7 @@ static void do_ffa_rxtx_unmap(struct arm_smccc_res *res,
 	ffa_unmap_hyp_buffers();
 
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&hyp_buffers.lock);
 out:
 	ffa_to_smccc_res(res, ret);
 }
@@ -380,7 +379,7 @@ static void do_ffa_mem_frag_tx(struct arm_smccc_res *res,
 	if (fraglen % sizeof(*buf))
 		goto out;
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&hyp_buffers.lock);
 	if (!host_buffers.tx)
 		goto out_unlock;
 
@@ -405,7 +404,7 @@ static void do_ffa_mem_frag_tx(struct arm_smccc_res *res,
 		WARN_ON(ffa_host_unshare_ranges(buf, nr_ranges));
 
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&hyp_buffers.lock);
 out:
 	if (ret)
 		ffa_to_smccc_res(res, ret);
@@ -451,7 +450,7 @@ static __always_inline void do_ffa_mem_xfer(const u64 func_id,
 		goto out;
 	}
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&hyp_buffers.lock);
 	if (!host_buffers.tx) {
 		ret = FFA_RET_INVALID_PARAMETERS;
 		goto out_unlock;
@@ -497,7 +496,7 @@ static __always_inline void do_ffa_mem_xfer(const u64 func_id,
 	}
 
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&hyp_buffers.lock);
 out:
 	if (ret)
 		ffa_to_smccc_res(res, ret);
@@ -524,7 +523,7 @@ static void do_ffa_mem_reclaim(struct arm_smccc_res *res,
 
 	handle = PACK_HANDLE(handle_lo, handle_hi);
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&hyp_buffers.lock);
 
 	buf = hyp_buffers.tx;
 	*buf = (struct ffa_mem_region) {
@@ -586,7 +585,7 @@ static void do_ffa_mem_reclaim(struct arm_smccc_res *res,
 	WARN_ON(ffa_host_unshare_ranges(reg->constituents,
 					reg->addr_range_cnt));
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&hyp_buffers.lock);
 
 	if (ret)
 		ffa_to_smccc_res(res, ret);
@@ -789,7 +788,7 @@ static void do_ffa_part_get(struct arm_smccc_res *res,
 	DECLARE_REG(u32, flags, ctxt, 5);
 	u32 count, partition_sz, copy_sz;
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&hyp_buffers.lock);
 	if (!host_buffers.rx) {
 		ffa_to_smccc_res(res, FFA_RET_BUSY);
 		goto out_unlock;
@@ -825,7 +824,7 @@ static void do_ffa_part_get(struct arm_smccc_res *res,
 
 	memcpy(host_buffers.rx, hyp_buffers.rx, copy_sz);
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&hyp_buffers.lock);
 }
 
 bool kvm_host_ffa_handler(struct kvm_cpu_context *ctxt, u32 func_id)
