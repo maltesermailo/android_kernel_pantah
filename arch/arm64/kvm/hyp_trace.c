c34f012cf17302e7af8daf3aefb1f4e793353672
Nikita Ioffe <ioffe@google.com>
2023-02-15 12:45:43 +0000

    ANDROID: KVM: arm64: add trace_clock for nVHE tracing
    Added userspace file that describes what clock is used for hyp tracing.
    Unlike tracefs root instance, the hyp tracing only supports boot clock,
    hence the trace_clock file is read-only.
    
    Bug: 249050813
    Change-Id: Ib9cc1f582699245ed94cf745dae0888eb7556ced
    Signed-off-by: Nikita Ioffe <ioffe@google.com>

--- arch/arm64/kvm/hyp_trace.c
+++ arch/arm64/kvm/hyp_trace.c
@@ -720,6 +720,24 @@ static const struct file_operations hyp_trace_raw_fops = {
 	.llseek		= no_llseek,
 };
 
+static int hyp_trace_clock_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "[boot]\n");
+	return 0;
+}
+
+static int hyp_trace_clock_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, hyp_trace_clock_show, NULL);
+}
+
+static const struct file_operations hyp_trace_clock_fops = {
+	.open = hyp_trace_clock_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
 static void hyp_tracefs_create_cpu_file(const char *file_name,
 					unsigned long cpu,
 					const struct file_operations *fops,
@@ -759,6 +777,11 @@ int init_hyp_tracefs(void)
 				&hyp_buffer_size_fops);
 	if (!d)
 		pr_err("Failed to create tracefs "TRACEFS_DIR"/buffer_size_kb\n");
+
+	d = tracefs_create_file("trace_clock", 0440, root_dir, NULL,
+				&hyp_trace_clock_fops);
+	if (!d)
+		pr_err("Failed to create tracefs "TRACEFS_DIR"/trace_clock\n");
 
 
 	per_cpu_root_dir = tracefs_create_dir("per_cpu", root_dir);
