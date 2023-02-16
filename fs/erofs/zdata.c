4ec983f6b0aec8fa049a7656b59f6df7ad8f1063
Dan Carpenter <error27@gmail.com>
2023-02-16 15:13:04 +0300

    UPSTREAM: erofs: fix an error code in z_erofs_init_zip_subsystem()
    Return -ENOMEM if alloc_workqueue() fails.  Don't return success.
    
    Fixes: d8a650adf429 ("erofs: add per-cpu threads for decompression as an option")
    Bug: 271636421
    Bug: 278520205
    Bug: 278550752
    Tests: launch_cvd
    Change-Id: I49dcf8094655db47ac71ae8967b5402373fc7adc
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Link: https://lore.kernel.org/r/Y+4d0FRsUq8jPoOu@kili
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 8d1b80a79452630f157bf634ae9cfcd9f4eed161)
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>
    (cherry picked from commit 5004cc655730355d681b333ff5eafd62a4ee02f2)
    (cherry picked from commit b7f5039f59d8bee886f584c65ca30f14be4b642c)
    (cherry picked from commit adbcd1e3b530e4fa5b80c7a5ff04b5432562b24b)

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -150,8 +150,16 @@ int __init z_erofs_init_zip_subsystem(void)
 	int err = z_erofs_create_pcluster_pool();
 
 	if (err)
-		return err;
-	err = z_erofs_init_workqueue();
+		goto out_error_pcluster_pool;
+
+	z_erofs_workqueue = alloc_workqueue("erofs_worker",
+			WQ_UNBOUND | WQ_HIGHPRI, num_possible_cpus());
+	if (!z_erofs_workqueue) {
+		err = -ENOMEM;
+		goto out_error_workqueue_init;
+	}
+
+	err = erofs_init_percpu_workers();
 	if (err)
 		z_erofs_destroy_pcluster_pool();
 	return err;
