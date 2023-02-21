96b7a45c981f16777ca4b5143dd5a2e65c3864b3
Lee Jones <joneslee@google.com>
2023-02-21 11:13:05 +0000

    ANDROID: DO NOT MERGE: WERROR canary
    Bug: 154919360
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ie3aaa1e227e51b561e4f7163e667dfc9e0ec2db8

--- init/main.c
+++ init/main.c
@@ -1402,6 +1402,13 @@ void __weak free_initmem(void)
 static int __ref kernel_init(void *unused)
 {
 	int ret;
+	char blow_stack[4096];
+
+	printk("sizeof(*blow_stack): %lu\n", sizeof(*blow_stack));
+
+	memset(blow_stack, 0xff, sizeof(*blow_stack));
+
+	printk("blow_stack[%lu]: 0x%x\n", (long)&blow_stack % 4096, blow_stack[(long)&blow_stack % 4096]);
 
 	kernel_init_freeable();
 	/* need to finish all async __init code before freeing the memory */
