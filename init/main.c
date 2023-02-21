65eb60a6dea80d031e9dcdd91e19f34a7c12768d
Lee Jones <joneslee@google.com>
2023-02-21 11:13:05 +0000

    ANDROID: DO NOT MERGE: WERROR canary
    Bug: 154919360
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ie3aaa1e227e51b561e4f7163e667dfc9e0ec2db8
    (cherry picked from commit ae815dc2ed16f1392ef2de173184d58ae8fa445c)

--- init/main.c
+++ init/main.c
@@ -1379,9 +1379,16 @@ void __weak free_initmem(void)
 	free_initmem_default(POISON_FREE_INITMEM);
 }
 
+#define BLOW_STACK_SIZE 2048
 static int __ref kernel_init(void *unused)
 {
 	int ret;
+	char blow_stack[BLOW_STACK_SIZE];
+	long rand_index = (long)&blow_stack % BLOW_STACK_SIZE;
+
+	memset(blow_stack, 0xff, BLOW_STACK_SIZE);
+
+	printk("blow_stack[%lu]: 0x%x\n", rand_index, blow_stack[rand_index]);
 
 	kernel_init_freeable();
 	/* need to finish all async __init code before freeing the memory */
