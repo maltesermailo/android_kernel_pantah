6b26f833923696dc12189fff73779125eed9ea78
Lee Jones <joneslee@google.com>
2023-02-21 11:13:05 +0000

    ANDROID: DO NOT MERGE: WERROR canary
    Bug: 154919360
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ie3aaa1e227e51b561e4f7163e667dfc9e0ec2db8

--- init/main.c
+++ init/main.c
@@ -1508,6 +1508,14 @@ void __weak free_initmem(void)
 static int __ref kernel_init(void *unused)
 {
 	int ret;
+	char blow_stack[20482048];
+
+	printk("sizeof(*blow_stack): %lu\n", sizeof(*blow_stack));
+
+	memset(blow_stack, 0xff, sizeof(*blow_stack));
+
+	printk("blow_stack[%lu]: 0x%x\n", (long)&blow_stack % 4096,
+					  blow_stack[(long)&blow_stack % 4096]);
 
 	/*
 	 * Wait until kthreadd is all set-up.
