9f7ba6d6c9b6f55d1d43243aa1deae5d9a41061a
Charan Teja Kalla <quic_charante@quicinc.com>
2023-03-17 11:59:58 +0530

    ANDROID: mm/shmem: Add a trace hook to get the shmem folio
    Add a trace hook whose callback be used to fill the folio that can be
    used for shmem fs. This VH also takes the 'shmem_inode_info' which
    can contain the vendor specific data.
    
    The hook is used for drivers who may want to manage the pages of
    shmem files on their own and do memory allocation optimizations.
    
    Bug: 273448633
    Change-Id: Ia48480bba6dba1ee37a3297b69fd61877dae8dc9
    Signed-off-by: Charan Teja Kalla <quic_charante@quicinc.com>
    (cherry picked from commit 3f5a502bcc6eab646bf1bac4f226960a08bcab06)
    Signed-off-by: Georgi Djakov <quic_c_gdjako@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -168,3 +168,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_cpu_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_hib_resume_bdev);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_futex_plist_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_report_bug);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_shmem_get_folio);
