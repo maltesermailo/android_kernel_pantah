fd6508fa671529c9cacf0d38ef3bc37b70f080e5
Lucas Wei <lucaswei@google.com>
2023-04-12 07:45:45 +0000

    ANDROID: usb: Add EXPORT_SYMBOL_GPL for usb/dwc3
    Export dwc3_send_gadget_ep_cmd and dwc3_stop_active_transfer to
    help vendor to gadget customize operations.
    
    Bug: 206311442
    Change-Id: Ib487c2f9b0700c8cd71755e1c680fa0aefaebe03
    Signed-off-by: Lucas Wei <lucaswei@google.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -417,6 +417,7 @@ int dwc3_send_gadget_ep_cmd(struct dwc3_ep *dep, unsigned int cmd,
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(dwc3_send_gadget_ep_cmd);
 
 static int dwc3_send_clear_stall_ep_cmd(struct dwc3_ep *dep)
 {
@@ -3681,6 +3682,7 @@ void dwc3_stop_active_transfer(struct dwc3_ep *dep, bool force,
 
 	__dwc3_stop_active_transfer(dep, force, interrupt);
 }
+EXPORT_SYMBOL_GPL(dwc3_stop_active_transfer);
 
 static void dwc3_clear_stall_all_ep(struct dwc3 *dwc)
 {
