8ec8e43f26b17e3f301f7e76e8380aba127283f3
guoqingxin <guoqingxin@oppo.com>
2023-04-17 17:42:54 +0800

    ANDROID: vendor_hooks: Add hooks for binder
    Add hooks to support OPPO's customized freezer.
    when the process receives a sync binder,and so on,
    OPPO's freezer will unfreeze it.
    
    Bug: 278505222
    
    Change-Id: Icb9b5d42db9ce083840da8527878e2c802db402c
    Signed-off-by: guoqingxin <guoqingxin@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -298,3 +298,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_si_meminfo);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_alloc_new_buf_locked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_reply);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_trans);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
