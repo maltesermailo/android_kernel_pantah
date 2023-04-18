f8fe665f38e99903eae10ffadf6a563c550fd369
John Stultz <jstultz@google.com>
2023-04-18 14:13:48 +0000

    WIP: tick: Introduce a dyn_hz boot option
    Introduce dyn_hz= option, which allows the actual timer tick to
    be scaled down at boot time.
    
    This allows kerenls to be built with HZ=1000 but systems to
    effectively run as if HZ=100 if specified.
    
    The system will still run with the configured HZ value, but
    ticks will just arrive "late".
    
    The valid values are between 100 and the build time CONFIG_HZ
    value.
    
    Change-Id: Ie834508e1f64a4d39f13b426329e3c54ae9155a1
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/time/tick-common.c
+++ kernel/time/tick-common.c
@@ -81,6 +81,24 @@ int tick_is_oneshot_available(void)
 	return tick_broadcast_oneshot_available();
 }
 
+// XXX This is just for testing, dyn_tick_nsec should default to TICK_NSEC
+#define DEFAULT_DYN_HZ 100
+long long dyn_tick_nsec = TICK_NSEC * HZ / DEFAULT_DYN_HZ;
+
+static int __init set_dyn_hz(char *str)
+{
+	int ret, dyn_hz;
+
+	ret = kstrtoint(str, 0, &dyn_hz);
+	if (ret)
+		return ret;
+	if (dyn_hz > HZ || dyn_hz < 100)
+		dyn_hz = HZ;
+	dyn_tick_nsec = TICK_NSEC * HZ / dyn_hz;
+	return 1;
+}
+__setup("dyn_hz=", set_dyn_hz);
+
 /*
  * Periodic tick
  */
@@ -91,8 +109,7 @@ static void tick_periodic(int cpu)
 		write_seqcount_begin(&jiffies_seq);
 
 		/* Keep track of the next tick event */
-		tick_next_period = ktime_add_ns(tick_next_period, TICK_NSEC);
-
+		tick_next_period = ktime_add_ns(tick_next_period, DYN_TICK_NSEC);
 		do_timer(1);
 		write_seqcount_end(&jiffies_seq);
 		raw_spin_unlock(&jiffies_lock);
@@ -129,7 +146,7 @@ void tick_handle_periodic(struct clock_event_device *dev)
 		 * Setup the next period for devices, which do not have
 		 * periodic mode:
 		 */
-		next = ktime_add_ns(next, TICK_NSEC);
+		next = ktime_add_ns(next, DYN_TICK_NSEC);
 
 		if (!clockevents_program_event(dev, next, false))
 			return;
@@ -175,7 +192,7 @@ void tick_setup_periodic(struct clock_event_device *dev, int broadcast)
 		for (;;) {
 			if (!clockevents_program_event(dev, next, false))
 				return;
-			next = ktime_add_ns(next, TICK_NSEC);
+			next = ktime_add_ns(next, DYN_TICK_NSEC);
 		}
 	}
 }
