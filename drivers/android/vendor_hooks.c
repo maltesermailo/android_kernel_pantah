25ba4dee8350644441728b6e4f6949e8f4212fa0
yangyang yan <yanyangyang@zeku.com>
2023-04-24 17:19:40 +0800

    ANDROID: add hook android_vh_binder_preset, android_vh_binder_trans, android_vh_binder_proc_transaction_finish
    add hook android_vh_binder_preset, android_vh_binder_trans and android_vh_binder_proc_transaction_finish, which is for dlkm to get the the description of task. It is used to find related tasks for boost in dlkm.
    
    Bug: 279524581
    Change-Id: I07bf83f3b6bdef8265628d27d00bfbf930c3e30c
    Signed-off-by: yangyang yan <yanyangyang@zeku.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -298,3 +298,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_amu_fie);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_proc_transaction_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_trans);
