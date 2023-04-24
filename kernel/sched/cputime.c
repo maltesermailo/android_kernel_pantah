b6beae2619f12714b38d111a6b77a032cf4cfb87
Liujie Xie <xieliujie@oppo.com>
2023-04-24 11:20:20 +0800

    ANDROID: vendor_hooks: add hook account_process_tick_gran
    this hook will allow to account tick in every third of more ticks
    to save cpu time for accounting
    
    Bug: 279549765
    Change-Id: I5d18e0167fdce076d13aecc653dcf6387bcb25f2
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -499,16 +499,21 @@ EXPORT_SYMBOL_GPL(thread_group_cputime_adjusted);
 void account_process_tick(struct task_struct *p, int user_tick)
 {
 	u64 cputime, steal;
+	int ticks = 1;
+
+	trace_android_vh_account_process_tick_gran(user_tick, &ticks);
+	if (!ticks)
+		return;
 
 	if (vtime_accounting_enabled_this_cpu())
 		return;
 
 	if (sched_clock_irqtime) {
-		irqtime_account_process_tick(p, user_tick, 1);
+		irqtime_account_process_tick(p, user_tick, ticks);
 		return;
 	}
 
-	cputime = TICK_NSEC;
+	cputime = TICK_NSEC * ticks;
 	steal = steal_account_process_time(ULONG_MAX);
 
 	if (steal >= cputime)
