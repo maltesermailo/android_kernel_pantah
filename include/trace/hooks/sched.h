681fb19232e9bfc1d4fbe5b1a44955bc820ea5f1
xieliujie <xieliujie@oppo.com>
2023-04-24 11:20:20 +0800

    ANDROID: vendor_hooks: add hook account_process_tick_gran
    this hook will allow to account tick in every third of more ticks
    to save cpu time for accounting
    
    Bug: 279549765
    Change-Id: I5d18e0167fdce076d13aecc653dcf6387bcb25f2
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Signed-off-by: xieliujie <xieliujie@oppo.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -338,6 +338,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_find_new_ilb,
 	TP_PROTO(struct cpumask *nohz_idle_cpus_mask, int *ilb),
 	TP_ARGS(nohz_idle_cpus_mask, ilb), 1);
 
+DECLARE_HOOK(android_vh_account_process_tick_gran,
+	TP_PROTO(int user_tick, int *ticks),
+	TP_ARGS(user_tick, ticks));
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
