47b4e2743087048f4b04d5886427047114ebbe8a
zhouwenhao <zhouwenhao@xiaomi.com>
2023-04-25 11:06:46 +0800

    ANDROID: vendor_hooks:vendor hook for madvise_cold_or_pageout_pte_range.
    add vendor hook in madvise_cold_or_pageout_pte_range to control the
    pages to be reclaimed more fine-grained.
    
    Bug: 284808098
    Signed-off-by: zhouwenhao <zhouwenhao@xiaomi.com>
    Change-Id: I298fde436df192cea9b1541d857f3a46808e06f2

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -278,3 +278,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_thermal_register);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_thermal_unregister);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_get_thermal_zone_device);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_thermal_power_cap);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_cold_pageout_skip);
