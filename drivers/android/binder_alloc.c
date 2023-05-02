142a14b6ccd71e82387c8ad4ce85a2986d1339c7
Carlos Llamas <cmllamas@google.com>
2023-05-02 20:12:19 +0000

    BACKPORT: FROMGIT: binder: add lockless binder_alloc_(set|get)_vma()
    Bring back the original lockless design in binder_alloc to determine
    whether the buffer setup has been completed by the ->mmap() handler.
    However, this time use smp_load_acquire() and smp_store_release() to
    wrap all the ordering in a single macro call.
    
    Also, add comments to make it evident that binder uses alloc->vma to
    determine when the binder_alloc has been fully initialized. In these
    scenarios acquiring the mmap_lock is not required.
    
    Fixes: a43cfc87caaf ("android: binder: stop saving a pointer to the VMA")
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20230502201220.1756319-3-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 284244471
    Bug: 284081259
    Bug: 274561072
    (cherry picked from commit 0fa53349c3acba0239369ba4cd133740a408d246
     git: //git.kernel.org/pub/scm/linux/kernel/git/gregkh/char-misc.git
     char-misc-linus)
    [cmllamas: resolved conflicts with missing commit d6d04d71daae]
    Change-Id: I208daff07036a2a962f0fd45f30ec88df7e63d55
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    (cherry picked from commit 50745c470ca7386b21ad5c013679d4c579afaf2e)

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -314,6 +314,7 @@ err_no_vma:
 static inline void binder_alloc_set_vma(struct binder_alloc *alloc,
 		struct vm_area_struct *vma)
 {
+<<<<<<< HEAD   (db6d03 UPSTREAM: scsi: ufs: Reduce the START STOP UNIT timeout)
 	if (vma)
 		alloc->vma_vm_mm = vma->vm_mm;
 	/*
@@ -324,19 +325,17 @@ static inline void binder_alloc_set_vma(struct binder_alloc *alloc,
 	 */
 	smp_wmb();
 	alloc->vma = vma;
+=======
+	/* pairs with smp_load_acquire in binder_alloc_get_vma() */
+	smp_store_release(&alloc->vma, vma);
+>>>>>>> CHANGE (50745c BACKPORT: FROMGIT: binder: add lockless binder_alloc_(set|ge)
 }
 
 static inline struct vm_area_struct *binder_alloc_get_vma(
 		struct binder_alloc *alloc)
 {
-	struct vm_area_struct *vma = NULL;
-
-	if (alloc->vma) {
-		/* Look at description in binder_alloc_set_vma */
-		smp_rmb();
-		vma = alloc->vma;
-	}
-	return vma;
+	/* pairs with smp_store_release in binder_alloc_set_vma() */
+	return smp_load_acquire(&alloc->vma);
 }
 
 static bool debug_low_async_space_locked(struct binder_alloc *alloc, int pid)
@@ -399,7 +398,11 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 	size_t size, data_offsets_size;
 	int ret;
 
+<<<<<<< HEAD   (db6d03 UPSTREAM: scsi: ufs: Reduce the START STOP UNIT timeout)
 	mmap_read_lock(alloc->vma_vm_mm);
+=======
+	/* Check binder_alloc is fully initialized */
+>>>>>>> CHANGE (50745c BACKPORT: FROMGIT: binder: add lockless binder_alloc_(set|ge)
 	if (!binder_alloc_get_vma(alloc)) {
 		mmap_read_unlock(alloc->vma_vm_mm);
 		binder_alloc_debug(BINDER_DEBUG_USER_ERROR,
@@ -792,6 +795,8 @@ int binder_alloc_mmap_handler(struct binder_alloc *alloc,
 	buffer->free = 1;
 	binder_insert_free_buffer(alloc, buffer);
 	alloc->free_async_space = alloc->buffer_size / 2;
+
+	/* Signal binder_alloc is fully initialized */
 	binder_alloc_set_vma(alloc, vma);
 
 	return 0;
