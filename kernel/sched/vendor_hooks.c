d2c2d90c7a128456f01b7a8c9d6fbcd6be89372e
Liujie Xie <xieliujie@oppo.com>
2023-05-15 11:30:56 +0800

    ANDROID: vendor_hooks: Add hooks for account process tick
    Add a hook in account_process_tick, which help us to get information
    about the high load task and the cpu they running on.
    
    Bug: 183260319
    Change-Id: I54162ce3c65bd69e08d2d4747e4d4883efe4c442
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>

--- kernel/sched/vendor_hooks.c
+++ kernel/sched/vendor_hooks.c
@@ -89,3 +89,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dup_task_struct);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_task_comm);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_new_ilb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_account_process_tick_gran);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_account_task_time);
