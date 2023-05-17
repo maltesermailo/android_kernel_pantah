82a5d22ef2c05d4e97322140c098db183039dd9e
Darren Hsu <darrenhsu@google.com>
2023-05-17 11:34:58 +0800

    ANDROID: Add vendor hooks to improve eventpoll logging
    The timerfd() system call does not create any wakelocks, but the
    eventpoll() system call can. When eventpoll() creates a wakelock,
    it names it after the underlying file descriptor. Since all timerfd
    file descriptors are named "[timerfd]", all wakesources created as
    a result of using the eventpoll-on-timerfd idiom are also named
    "[timerfd]". This can make it difficult to troubleshoot problems,
    because it is impossible to tell which "[timerfd]" wakesource is
    associated with which process.
    
    To address this issue, vendors can add hooks to allow them to
    assign appropriate names to timerfd descriptors and eventpoll
    wakesources. This would make it easier to troubleshoot problems,
    because it would be possible to identify the wakesource that is
    associated with a particular process.
    
    Bug: 280678693
    Change-Id: I7cf17eb49e43837d62ebad87747e98ed92165755
    Signed-off-by: Darren Hsu <darrenhsu@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -53,6 +53,7 @@
 #include <trace/hooks/bug.h>
 #include <trace/hooks/softlockup.h>
 #include <trace/hooks/power.h>
+#include <trace/hooks/fs.h>
 #include <trace/hooks/fault.h>
 #include <trace/hooks/traps.h>
 #include <trace/hooks/fips140.h>
@@ -226,6 +227,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_report_bug);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_watchdog_timer_softlockup);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_freeze_todo);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_freeze_todo_unfrozen);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ep_create_wakeup_source);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timerfd_create);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_die_kernel_fault);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sea);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_mem_abort);
