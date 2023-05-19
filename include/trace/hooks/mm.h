1ab76ebc4127aafbdfe88330c74ca8a12017785a
Dezhi Huang <huangdezhi@hihonor.com>
2023-05-19 09:58:01 +0800

    ANDROID: mm: create vendor hooks for page alloc
    Add vendor hook inside of get_page_from_freelist() to check
    and modify the watermark in some special situations.
    Additional page flag bit will be set for future identification.
    
    Separately, a vendor hook inside of page_add_new_anon_rmap()
    is added to set the referenced bit in some situations, e.g.
    if the special bit in the page flag mentioned before is set,
    we will give this page one more chance before it gets reclaimed.
    
    Bug: 279793368
    Change-Id: I363853a050a87201f6f368ccc580485dddd6c6b6
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -74,6 +74,14 @@ DECLARE_HOOK(android_vh_save_track_hash,
 DECLARE_HOOK(android_vh_kmalloc_slab,
 	TP_PROTO(unsigned int index, gfp_t flags, struct kmem_cache **s),
 	TP_ARGS(index, flags, s));
+DECLARE_HOOK(android_vh_get_page_wmark,
+	TP_PROTO(unsigned int alloc_flags, unsigned long *page_wmark),
+	TP_ARGS(alloc_flags, page_wmark));
+DECLARE_HOOK(android_vh_page_add_new_anon_rmap,
+	TP_PROTO(struct page *page, struct vm_area_struct *vma,
+		unsigned long address),
+	TP_ARGS(page, vma, address));
+
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
