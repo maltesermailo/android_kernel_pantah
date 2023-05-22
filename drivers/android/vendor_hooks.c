78839929dc86201215e6c38a2247e2e0068759d2
Dezhi Huang <huangdezhi@hihonor.com>
2023-05-22 12:15:37 +0800

    ANDROID:mm: create vendor hooks for memory reclaim
    we try to adjust page reclaim operations based on the running task
    and kernel memory pressure. Thus, we want to create some vendor hooks
    into kernel6.1.
    
    Firstly, we add ADNRROID_VENDOR_DATA into the struct scan_control,
    special operations would be performed based on this special scan option.
    We measure the importance of the current process in the system and
    obtain its weight, which is recorded in ANDROID_VENDOR_DATA.
    
    The hook function: trace_android_vh_modify_scan_control is added inside
    of the function modify_scan_control() to adjust reclaim operations based on
    memory pressure.
    
    The hook function: trace_android_vh_should_continue_reclaim is added inside
    of the function shrink_node() to decide if page_reclaim would continue
    or not based on memory pressure.
    
    trace_android_vh_file_is_tiny_bypass is added into the function
    prepare_scan_count() to decide if the file pages should be skipped
    in condition to file refualts and memory pressure.
    
    Bug: 279793370
    Change-Id: I1efe9d3e866f37b0295c7cd94ec8ca0117a9bd4a
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -241,4 +241,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmap_region);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_event);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_group);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_modify_scan_control);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_should_continue_reclaim);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_file_is_tiny_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
