39a5b5d61afd1da9d967127cbceda522767f230f
Sooyong Suk <s.suk@samsung.com>
2023-05-23 10:27:13 +0900

    ANDROID: mm: add vendor hooks in madvise for swap entry
    Add vendor hooks in madvise for swap entry
    - android_vh_madvise_pageout
    - android_vh_madvise_pageout_swap_entry
    - android_vh_madvise_swapin_walk_pmd_entry
    - android_vh_process_madvise_end
    
    Bug: 284059805
    
    Change-Id: Ic389244e343737a583286c20cadb6774efd8890c
    Signed-off-by: Sooyong Suk <s.suk@samsung.com>

--- mm/madvise.c
+++ mm/madvise.c
@@ -219,6 +219,7 @@ static int swapin_walk_pmd_entry(pmd_t *pmd, unsigned long start,
 		entry = pte_to_swp_entry(pte);
 		if (unlikely(non_swap_entry(entry)))
 			continue;
+		trace_android_vh_madvise_swapin_walk_pmd_entry(entry);
 
 		page = read_swap_cache_async(entry, GFP_HIGHUSER_MOVABLE,
 					     vma, index, false, &splug);
@@ -350,12 +351,17 @@ static int madvise_cold_or_pageout_pte_range(pmd_t *pmd,
 	struct page *page = NULL;
 	LIST_HEAD(page_list);
 	bool pageout_anon_only_filter;
+	bool allow_writeback = false;
+	swp_entry_t entry;
 
 	if (fatal_signal_pending(current))
 		return -EINTR;
 
 	pageout_anon_only_filter = pageout && !vma_is_anonymous(vma) &&
 					!can_do_file_pageout(vma);
+
+	if (pageout)
+		trace_android_vh_madvise_pageout(&allow_writeback);
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	if (pmd_trans_huge(*pmd)) {
@@ -440,8 +446,12 @@ regular_page:
 		if (pte_none(ptent))
 			continue;
 
-		if (!pte_present(ptent))
+		if (!pte_present(ptent)) {
+			entry = pte_to_swp_entry(ptent);
+			trace_android_vh_madvise_pageout_swap_entry(entry,
+					swp_swapcount(entry), allow_writeback);
 			continue;
+		}
 
 		page = vm_normal_page(vma, addr, ptent);
 		if (!page || is_zone_device_page(page))
@@ -1518,6 +1528,7 @@ SYSCALL_DEFINE5(process_madvise, int, pidfd, const struct iovec __user *, vec,
 		iov_iter_advance(&iter, iovec.iov_len);
 	}
 
+	trace_android_vh_process_madvise_end(behavior, &ret);
 	ret = (total_len - iov_iter_count(&iter)) ? : ret;
 
 release_mm:
