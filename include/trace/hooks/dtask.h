85301964501dd3f3c7abadf3e8c21ccbeec6469a
xieliujie <xieliujie@oppo.com>
2023-07-10 20:05:46 +0800

    ANDROID: Inherit "user-aware property" across rtmutex.
    Since upstream commit 715f7f9ece46 ("locking/rtmutex: Squash !RT
    tasks to DEFAULT_PRIO"), non-rt tasks do not inherit the
    nice-priority values across rt_mutexes. This removes the minor
    (and indirect) priority-inheritance that rt-mutexes provided for
    CFS tasks.
    
    Though without priority inheritance, time-bounded priority
    inversion can occur between CFS tasks of different nice
    priorities / cgroup limitations.  The proxy-execution efforts
    are a work-in-progress to resolve this upstream, but in the
    meantime it is left to vendor hooks to provide a near term
    solution to avoid priority inversion between CFS tasks.
    
    In our oem scheduler, if a  CFS thread has an "user-aware
    property", we will always pick it even if it's vruntime is
    bigger than the smallest one in runqueue. That's why the
    trace_android_rvh_replace_next_task_fair vendorhook was added
    previously in commit 53e809978443 ("ANDROID: vendor_hooks: Add
    hooks for scheduler").
    
    Thus for our oem scheduler, important CFS tasks(like
    RenderThread) are marked with the "user-aware property" in their
    struct task_struct. If those tasks are blocked on an rtmutex, we
    want to allow the "user-aware property" to be inherited to lock
    owner, so it will be selected to run immediately to release the
    lock.
    
    To support this, we need new hooks to map "user-aware property"
    into different rtmutex_waiter prio and update the owner's
    "user-aware property" if needed. Thus these additional vendor
    hooks are needed.
    
    In the future, once an generalized upstream solution for CFS
    priority inheritance is in place, this will no longer be needed.
    
    Bug: 290585456
    Change-Id: I6521ed2086b147400a54da6b84a324baf16bc649
    Signed-off-by: xieliujie <xieliujie@oppo.com>

--- include/trace/hooks/dtask.h
+++ include/trace/hooks/dtask.h
@@ -48,7 +48,16 @@ DECLARE_HOOK(android_vh_rwsem_write_wait_finish,
 DECLARE_HOOK(android_vh_sched_show_task,
 	TP_PROTO(struct task_struct *task),
 	TP_ARGS(task));
-
+struct rt_mutex_waiter;
+struct ww_acquire_ctx;
+DECLARE_HOOK(android_vh_task_blocks_on_rtmutex,
+        TP_PROTO(struct rt_mutex_base *lock, struct rt_mutex_waiter *waiter,
+                struct task_struct *task, struct ww_acquire_ctx *ww_ctx,
+                unsigned int *chwalk),
+        TP_ARGS(lock, waiter, task, ww_ctx, chwalk));
+DECLARE_HOOK(android_vh_rtmutex_waiter_prio,
+        TP_PROTO(struct task_struct *task, int *waiter_prio),
+        TP_ARGS(task, waiter_prio));
 #endif /* _TRACE_HOOK_DTASK_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
