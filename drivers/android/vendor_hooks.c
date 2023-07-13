946bc23c8d1c07ab1d93a6315017279cef6afb48
Kalesh Singh <kaleshsingh@google.com>
2023-07-13 12:56:21 -0700

    ANDROID: vendor_hook: Add vendor hook to decide scan abort policy
    Allow vendor hook to enable checking of the high water marks to
    decide if reclaim should continue scanning.
    
    Bug: 224956008
    Change-Id: I63fe1fd386e7599451c2df0a04c8440b4fc142fc
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -326,3 +326,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_swappiness);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal_whether_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_check);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_freeze_whether_wake);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_scan_abort_check_wmarks);
