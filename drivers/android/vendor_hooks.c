BayLibre/android_kernel_common

ANDROID: GKI: drm: add vendor hook for drm_atomic_helper_check_modeset
Just mode changed no need to display previous planes.Deleted by SPRD for HWC SR function.
There is no need to add all old planes for sprd drm according to plane mask.
Because the dpu driver will display extra planes which reduce display abnormal

Bug: 227721498
Change-Id: I47048e430d2b2605b9111b6c9ee4958443dc7239
Signed-off-by: Gaofeng Sheng <gaofeng.sheng@unisoc.com>

drivers/android/vendor_hooks.c
@@ -54,6 +54,7 @@
 #include <trace/hooks/thermal.h>
 #include <trace/hooks/rwsem.h>
 #include <trace/hooks/drm_framebuffer.h>
+#include <trace/hooks/drm_atomic.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -265,3 +266,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_finished);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_rwsem_list_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_thermal_power_cap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_atomic_remove_fb);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_drm_atomic_helper_check_modeset);

drivers/gpu/drm/drm_atomic_helper.c
@@ -42,6 +42,8 @@
 #include <drm/drm_vblank.h>
 #include <drm/drm_writeback.h>
 
+#include <trace/hooks/drm_atomic.h>
+
 #include "drm_crtc_helper_internal.h"
 #include "drm_crtc_internal.h"
 
@@ -611,6 +613,7 @@ drm_atomic_helper_check_modeset(struct drm_device *dev,
 	struct drm_connector_state *old_connector_state, *new_connector_state;
 	int i, ret;
 	unsigned int connectors_mask = 0;
+	bool allow = false;
 
 	for_each_oldnew_crtc_in_state(state, crtc, old_crtc_state, new_crtc_state, i) {
 		bool has_connectors =
@@ -716,6 +719,10 @@ drm_atomic_helper_check_modeset(struct drm_device *dev,
 		ret = drm_atomic_add_affected_connectors(state, crtc);
 		if (ret != 0)
 			return ret;
+
+		trace_android_vh_drm_atomic_helper_check_modeset(state, crtc, &allow);
+		if (allow)
+			continue;
 
 		ret = drm_atomic_add_affected_planes(state, crtc);
 		if (ret != 0)

include/trace/hooks/drm_atomic.h
@@ -1,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM drm_atomic
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_DRM_ATOMIC_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_DRM_ATOMIC_H
+
+#include <linux/tracepoint.h>
+
+struct drm_atomic_state;
+struct drm_crtc;
+DECLARE_HOOK(android_vh_drm_atomic_helper_check_modeset,
+	TP_PROTO(struct drm_atomic_state *state, struct drm_crtc *crtc, bool *allow),
+	TP_ARGS(state, crtc, allow))
+
+#endif /* _TRACE_HOOK_DRM_ATOMIC_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+

maltesermailo/android_kernel_pantah

Just mode changed no need to display previous planes. Deleted by SPRD
for HWC SR function. There is no need to add all old planes for sprd drm
according to plane mask. Because the dpu driver will display extra
planes which reduce display abnormal.
Signed-off-by: Carlos Llamas <cmllamas@google.com>
(cherry picked from commit 7b084e61359400af1f93807377c7c6232e3e4445)

drivers/android/vendor_hooks.c
@@ -68,6 +68,7 @@
 #include <trace/hooks/drm_framebuffer.h>
 #include <trace/hooks/bl_hib.h>
 #include <trace/hooks/regmap.h>
+#include <trace/hooks/drm_atomic.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -315,3 +316,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_one_page_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_atomic_remove_fb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_regmap_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_enable_thermal_genl_check);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_drm_atomic_check_modeset);

drivers/gpu/drm/drm_atomic_helper.c
@@ -43,6 +43,8 @@
 #include <drm/drm_vblank.h>
 #include <drm/drm_writeback.h>
 
+#include <trace/hooks/drm_atomic.h>
+
 #include "drm_crtc_helper_internal.h"
 #include "drm_crtc_internal.h"
 
@@ -621,6 +623,7 @@ drm_atomic_helper_check_modeset(struct drm_device *dev,
 	struct drm_connector_state *old_connector_state, *new_connector_state;
 	int i, ret;
 	unsigned int connectors_mask = 0;
+	bool allow = false;
 
 	for_each_oldnew_crtc_in_state(state, crtc, old_crtc_state, new_crtc_state, i) {
 		bool has_connectors =
@@ -731,6 +734,10 @@ drm_atomic_helper_check_modeset(struct drm_device *dev,
 		ret = drm_atomic_add_affected_connectors(state, crtc);
 		if (ret != 0)
 			return ret;
+
+		trace_android_vh_drm_atomic_check_modeset(state, crtc, &allow);
+		if (allow)
+			continue;
 
 		ret = drm_atomic_add_affected_planes(state, crtc);
 		if (ret != 0)

include/trace/hooks/drm_atomic.h
@@ -1,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM drm_atomic
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_DRM_ATOMIC_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_DRM_ATOMIC_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+struct drm_atomic_state;
+struct drm_crtc;
+DECLARE_HOOK(android_vh_drm_atomic_check_modeset,
+	TP_PROTO(struct drm_atomic_state *state, struct drm_crtc *crtc, bool *allow),
+	TP_ARGS(state, crtc, allow))
+
+#endif /* _TRACE_HOOK_DRM_ATOMIC_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+