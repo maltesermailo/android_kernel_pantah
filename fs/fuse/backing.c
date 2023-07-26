0a8650224dbdcf7a1fc710ad8d08c52152436605
Paul Lawrence <paullawrence@google.com>
2023-07-26 15:03:41 -0700

    ANDROID: fuse-bpf: Check inode not null
    fuse_iget_backing returns an inode or null, not a ERR_PTR. So check it's
    not NULL
    
    Also make sure we put the inode if d_splice_alias fails
    
    Bug: 293349757
    Test: fuse_test runs
    Signed_off_by: Paul Lawrence <paullawrence@google.com>
    
    Change-Id: I1eadad32f80bab6730e461412b4b7ab4d6c56bf2

--- fs/fuse/backing.c
+++ fs/fuse/backing.c
@@ -253,8 +253,8 @@ int fuse_create_open_backing(
 
 	inode = fuse_iget_backing(dir->i_sb, target_nodeid,
 			fuse_entry->backing_path.dentry->d_inode);
-	if (IS_ERR(inode)) {
-		err = PTR_ERR(inode);
+	if (!inode) {
+		err = -EIO;
 		goto out;
 	}
 
@@ -269,10 +269,12 @@ int fuse_create_open_backing(
 		goto out;
 	}
 
+	inode = NULL;
 	entry = newent ? newent : entry;
 	err = finish_open(file, entry, fuse_open_file_backing);
 
 out:
+	iput(inode);
 	dput(backing_dentry);
 	return err;
 }
