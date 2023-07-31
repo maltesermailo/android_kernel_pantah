d13d11386272bb8704962c0be1b2fcc69c9234be
Alice Ryhl <aliceryhl@google.com>
2023-07-31 11:25:20 +0000

    FROMLIST: rust_binder: add binder_logs/state
    The binderfs directory has four files intended for debugging the driver.
    This patch implements the state file so that you can use it to view the
    current state of the driver.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Change-Id: I81af0fe17f9a5da5b51a50a707e3c7517ade32b8
    Link: https://lore.kernel.org/rust-for-linux/20231101-rust-binder-v1-18-08ba9197f637@google.com/
    (cherry picked from commit ae9a3a8093ac96135d5168f99b447320320d85a3
     https://github.com/darksonn/linux rust-binder-rfc)
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Bug: 278052745

--- drivers/android/node.rs
+++ drivers/android/node.rs
@@ -6,6 +6,8 @@ use kernel::{
         TryNewListArc,
     },
     prelude::*,
+    seq_file::SeqFile,
+    seq_print,
     sync::lock::{spinlock::SpinLockBackend, Guard},
     sync::{Arc, LockedBy, SpinLock},
     user_ptr::UserSlicePtrWriter,
@@ -121,6 +123,41 @@ impl Node {
             owner,
             links_track <- AtomicListArcTracker::new(),
         })
+    }
+
+    #[inline(never)]
+    pub(crate) fn debug_print(&self, m: &mut SeqFile) -> Result<()> {
+        let weak;
+        let strong;
+        let has_weak;
+        let has_strong;
+        let active_inc_refs;
+        {
+            let mut guard = self.owner.inner.lock();
+            let inner = self.inner.access_mut(&mut guard);
+            weak = inner.weak.count;
+            has_weak = inner.weak.has_count;
+            strong = inner.strong.count;
+            has_strong = inner.strong.has_count;
+            active_inc_refs = inner.active_inc_refs;
+        }
+
+        let has_weak = if has_weak { "Y" } else { "N" };
+        let has_strong = if has_strong { "Y" } else { "N" };
+
+        seq_print!(
+            m,
+            "node gid:{},ptr:{:#x},cookie:{:#x}: strong{}{} weak{}{} active{}\n",
+            self.global_id(),
+            self.ptr,
+            self.cookie,
+            strong,
+            has_strong,
+            weak,
+            has_weak,
+            active_inc_refs
+        );
+        Ok(())
     }
 
     /// An id that is unique across all binder nodes on the system. Used as the key in the
