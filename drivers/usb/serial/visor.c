668832d902accab89561e1521aad1a298632398e
Greg Kroah-Hartman <gregkh@google.com>
2023-08-22 09:25:33 +0200

    ANDROID: android11-5.4-lts build canary test.
    Nonsense patch to kick TH
    
    Do not merge.
    
    Bug: 210277953
    
    Change-Id: I1c1e244d9c99a7742cf3cbe334c6ef534388641d
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/serial/visor.c
+++ drivers/usb/serial/visor.c
@@ -25,6 +25,7 @@
 #include <linux/usb/serial.h>
 #include <linux/usb/cdc.h>
 #include "visor.h"
+// Blank line added for Android Canary build test of branch android11-5.4-lts
 
 /*
  * Version Information
