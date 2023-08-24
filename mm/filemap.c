BayLibre/android_kernel_common

ANDROID: vendor_hooks: Add hooks in wait_on_page_bit_common.
- trace_android_vh_sort_wait_q

Bug: 295096321
Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
Change-Id: I4f2707ab809e3add82a31d301068815ea5ed2230

mm/filemap.c
@@ -1272,8 +1272,13 @@ repeat:
 	 */
 	spin_lock_irq(&q->lock);
 	folio_set_waiters(folio);
-	if (!folio_trylock_flag(folio, bit_nr, wait))
-		__add_wait_queue_entry_tail(q, wait);
+	if (!folio_trylock_flag(folio, bit_nr, wait)) {
+		bool queued = false;
+
+		trace_android_vh_sort_wait_q(q, wait, &queued);
+		if (!queued)
+			__add_wait_queue_entry_tail(q, wait);
+	}
 	spin_unlock_irq(&q->lock);
 
 	/*

maltesermailo/android_kernel_pantah

mm/filemap.c
@@ -1321,8 +1321,13 @@ repeat:
 	 */
 	spin_lock_irq(&q->lock);
 	SetPageWaiters(page);
-	if (!trylock_page_bit_common(page, bit_nr, wait))
-		__add_wait_queue_entry_tail(q, wait);
+	if (!trylock_page_bit_common(page, bit_nr, wait)) {
+		bool queued = false;
+
+		trace_android_vh_sort_wait_q(q, wait, &queued);
+		if (!queued)
+			__add_wait_queue_entry_tail(q, wait);
+	}
 	spin_unlock_irq(&q->lock);
 
 	/*