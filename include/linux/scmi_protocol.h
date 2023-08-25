28e03171424dd13582b8199d565dddcaee331eff
Ulf Hansson <ulf.hansson@linaro.org>
2023-08-25 13:26:22 +0200

    BACKPORT: firmware: arm_scmi: Extend perf protocol ops to get information of a domain
    Similar to other protocol ops, it's useful for an scmi module driver to get
    some generic information of a performance domain. Therefore, let's add a
    new callback to provide this information. The information is currently
    limited to the name of the performance domain and whether the set-level
    operation is supported, although this can easily be extended if we find the
    need for it.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Link: https://lore.kernel.org/r/20230825112633.236607-3-ulf.hansson@linaro.org
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    
    Bug: 323966425
    Change-Id: Ica11495c1ba40c646cf835418788795e6671e7db
    (cherry picked from commit 3d99ed60721bf2e108c8fc660775766057689a92)
    [nikunj: Resolved minor conflict in drivers/firmware/arm_scmi/perf.c ]
    [anantg: Resolved minor conflict in drivers/firmware/arm_scmi/perf.c ]
    Signed-off-by: Nikunj Kela <quic_nkela@quicinc.com>
    Signed-off-by: Anant Goel <quic_anantg@quicinc.com>
    (cherry picked from commit 742d32f206d82e0acf6b5bbea04f0db2ad984a8c)

--- include/linux/scmi_protocol.h
+++ include/linux/scmi_protocol.h
@@ -100,10 +100,20 @@ struct scmi_clk_proto_ops {
 	ANDROID_KABI_RESERVE(1);
 };
 
+struct scmi_perf_domain_info {
+	char name[SCMI_MAX_STR_SIZE];
+	bool set_perf;
+};
+
 /**
  * struct scmi_perf_proto_ops - represents the various operations provided
  *	by SCMI Performance Protocol
  *
+<<<<<<< HEAD   (1e97d7 ANDROID: GKI: Add initial sunxi symbol list)
+=======
+ * @num_domains_get: gets the number of supported performance domains
+ * @info_get: get the information of a performance domain
+>>>>>>> CHANGE (742d32 BACKPORT: firmware: arm_scmi: Extend perf protocol ops to ge)
  * @limits_set: sets limits on the performance level of a domain
  * @limits_get: gets limits on the performance level of a domain
  * @level_set: sets the performance level of a domain
@@ -123,6 +133,12 @@ struct scmi_clk_proto_ops {
  *	or in some other (abstract) scale
  */
 struct scmi_perf_proto_ops {
+<<<<<<< HEAD   (1e97d7 ANDROID: GKI: Add initial sunxi symbol list)
+=======
+	int (*num_domains_get)(const struct scmi_protocol_handle *ph);
+	const struct scmi_perf_domain_info __must_check *(*info_get)
+		(const struct scmi_protocol_handle *ph, u32 domain);
+>>>>>>> CHANGE (742d32 BACKPORT: firmware: arm_scmi: Extend perf protocol ops to ge)
 	int (*limits_set)(const struct scmi_protocol_handle *ph, u32 domain,
 			  u32 max_perf, u32 min_perf);
 	int (*limits_get)(const struct scmi_protocol_handle *ph, u32 domain,
