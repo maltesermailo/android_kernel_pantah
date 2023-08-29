b5f8eb15164b1d4db33abd59b79d7b0442e18991
Alice Ryhl <aliceryhl@google.com>
2023-08-29 13:28:13 +0000

    ANDROID: rust_binder: add priority inheritance
    This implements priority inhertiance in the same way as how it is
    implemented in Android Linux.
    
    Change-Id: I7b571c836b089492abb1b4a8f300e4f72aab655a
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Bug: 278052745

--- drivers/android/node.rs
+++ drivers/android/node.rs
@@ -16,6 +16,7 @@ use kernel::{
 use crate::{
     defs::*,
     error::BinderError,
+    prio::{self, BinderPriority},
     process::{Process, ProcessInner},
     thread::Thread,
     transaction::Transaction,
@@ -123,6 +124,22 @@ impl Node {
             owner,
             links_track <- AtomicListArcTracker::new(),
         })
+    }
+
+    pub(crate) fn node_prio(&self) -> prio::BinderPriority {
+        let flags = self.flags;
+        let priority = (flags & FLAT_BINDER_FLAG_PRIORITY_MASK) as prio::Nice;
+        let sched_policy =
+            (flags & FLAT_BINDER_FLAG_SCHED_POLICY_MASK) >> FLAT_BINDER_FLAG_SCHED_POLICY_SHIFT;
+
+        BinderPriority {
+            sched_policy,
+            prio: prio::to_kernel_prio(sched_policy, priority),
+        }
+    }
+
+    pub(crate) fn inherit_rt(&self) -> bool {
+        (self.flags & FLAT_BINDER_FLAG_INHERIT_RT) != 0
     }
 
     #[inline(never)]
