0f51d07ade4e2d0883e6cbc702279e8dabee837e
Alice Ryhl <aliceryhl@google.com>
2023-09-05 12:11:05 +0000

    FROMLIST: rust_binder: add vma shrinker
    When the system is under memory pressure, we want the driver to release
    unused pages. We do this by registering a memory shrinker with the
    kernel.
    
    The data for incoming transactions is stored in an mmap'ed region.
    Previously in this patch series, we just allocated all of the pages in
    that region immediately. With this patch series, we do not allocate the
    pages until we need them.
    
    Furthermore, when we no longer need a page, we mark it as "available"
    using an lru list. If the system is under memory pressure, this allows
    the shrinker to free that page by removing it from the lru list. If we
    need to use the page again before the shrinker frees it, then we just
    remove it from the lru list, and we don't need to reallocate the page.
    
    The page range abstraction is split into a fast path and slow path. The
    slow path is only used when a page is not allocated, which should only
    happen on first use, and when the system is under memory pressure.
    
    I'm not yet completely happy with this implementation. Specifically, I
    would like to improve the robustness of the unsafe code found in
    `allocation.rs`.
    
    The slow-path/fast-path implementation in `page_range.rs` is different
    from C Binder's current implementation, and was suggested to me by
    Carlos.
    
    Suggested-by: Carlos Llamas <cmllamas@google.com>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Change-Id: I13590839ad31a266016f61747f03587ffad45843
    Link: https://lore.kernel.org/rust-for-linux/20231101-rust-binder-v1-19-08ba9197f637@google.com/
    (cherry picked from commit 09ac0083f0f07edda4fe70c4256a1dbfaec9a45e
     https://github.com/darksonn/linux rust-binder-rfc)
    [ Resolved conflicts due to priority inheritance patch. ]
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Bug: 278052745

--- drivers/android/rust_binder.rs
+++ drivers/android/rust_binder.rs
@@ -9,6 +9,7 @@ use kernel::{
     list::{
         HasListLinks, ListArc, ListArcSafe, ListItem, ListLinks, ListLinksSelfPtr, TryNewListArc,
     },
+    page_range::Shrinker,
     prelude::*,
     seq_file::SeqFile,
     seq_print,
@@ -179,6 +180,9 @@ const fn ptr_align(value: usize) -> usize {
     (value + size) & !size
 }
 
+// SAFETY: We call register in `init`.
+static BINDER_SHRINKER: Shrinker = unsafe { Shrinker::new() };
+
 struct BinderModule {}
 
 impl kernel::Module for BinderModule {
@@ -187,6 +191,8 @@ impl kernel::Module for BinderModule {
         // called `init` yet. Furthermore, we cannot move a value in a global, so the `Contexts`
         // will not be moved after this call.
         unsafe { crate::context::CONTEXTS.init() };
+
+        BINDER_SHRINKER.register(kernel::c_str!("android-binder"))?;
 
         // SAFETY: The module is being loaded, so we can initialize binderfs.
         #[cfg(CONFIG_ANDROID_BINDERFS_RUST)]
--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -43,6 +43,7 @@ pub mod kunit;
 pub mod list;
 pub mod mm;
 pub mod page;
+pub mod page_range;
 pub mod prelude;
 pub mod print;
 pub mod rbtree;
