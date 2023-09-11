15d8c95b36d4d66d57311d3e9e2f5ce431a03319
William Wu <william.wu@rock-chips.com>
2023-09-11 11:31:12 +0800

    FROMLIST: usb: dwc3: core: Avoid resume dwc3 if already suspended in pm resume
    If we enable PM runtime auto suspend for dwc3 on rockchip
    platforms (e.g. RK3562), it allows the dwc3 controller to
    enter runtime suspend if usb cable detached and power off
    the power domain of the controller. When system resume, if
    the dwc3 already in runtime suspended, it Shouldn't access
    the dwc3 registers in dwc3_resume() because its power domain
    maybe power off.
    
    Test on RK3562 tablet, this patch can help to avoid kernel
    panic when accessing the dwc3 registers in dwc3_resume() if
    the dwc3 is in runtime suspended and it's power domain is
    power off.
    
    Kernel panic - not syncing: Asynchronous SError Interrupt
    Hardware name: Rockchip RK3562 RK817 TABLET LP4 Board (DT)
    Call trace:
    dump_backtrace.cfi_jt+0x0/0x8
      dump_stack_lvl+0xc0/0x13c
      panic+0x174/0x468
      arm64_serror_panic+0x1b0/0x200
      do_serror+0x184/0x1e4
      el1_error+0x94/0x118
      el1_abort+0x40/0x68
      el1_sync_handler+0x58/0x88
      el1_sync+0x8c/0x140
      dwc3_readl+0x30/0x1a0
      dwc3_phy_setup+0x38/0x510
      dwc3_core_init+0x68/0xcd4
      dwc3_core_init_for_resume+0x10c/0x25c
      dwc3_resume_common+0x44/0x3d0
      dwc3_resume+0x5c/0xb8
      dpm_run_callback+0x70/0x488
      device_resume+0x250/0x2f8
      dpm_resume+0x258/0x9dc
    
    Bug: 300024866
    Link: https://lore.kernel.org/linux-usb/20230911033112.3321-1-william.wu@rock-chips.com/
    Change-Id: I4e00bb3184567641881d44387a71d9a995f64257
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -2235,9 +2235,11 @@ static int dwc3_resume(struct device *dev)
 
 	pinctrl_pm_select_default_state(dev);
 
-	ret = dwc3_resume_common(dwc, PMSG_RESUME);
-	if (ret)
-		return ret;
+	if (!pm_runtime_suspended(dwc->dev)) {
+		ret = dwc3_resume_common(dwc, PMSG_RESUME);
+		if (ret)
+			return ret;
+	}
 
 	pm_runtime_disable(dev);
 	pm_runtime_set_active(dev);
