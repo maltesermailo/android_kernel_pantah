04233eb5beaf9da069637deac51ba2bec986ff68
qinglin.li <qinglin.li@amlogic.com>
2023-09-13 14:24:03 +0800

    ANDROID: vendor_hooks: add vendor hook in xhci_urb_suitable_for_idt()
    By this vh, so that we can reimplement the function xhci_urb_suitable_for_idt()
    
    Bug: 301055890
    Change-Id: I5e4eabf1155eceb650eee6a42b0e00e8d55281dc
    Signed-off-by: Wanwei Jiang <wanwei.jiang@amlogic.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -2233,6 +2233,8 @@ static inline struct xhci_ring *xhci_urb_to_transfer_ring(struct xhci_hcd *xhci,
 					urb->stream_id);
 }
 
+void _trace_android_vh_xhci_urb_suitable_bypass(struct urb *urb, bool *ret, bool* bypass);
+
 /*
  * TODO: As per spec Isochronous IDT transmissions are supported. We bypass
  * them anyways as we where unable to find a device that matches the
@@ -2240,6 +2242,13 @@ static inline struct xhci_ring *xhci_urb_to_transfer_ring(struct xhci_hcd *xhci,
  */
 static inline bool xhci_urb_suitable_for_idt(struct urb *urb)
 {
+	bool ret;
+	bool bypass = false;
+
+	_trace_android_vh_xhci_urb_suitable_bypass(urb, &ret, &bypass);
+	if (bypass)
+		return ret;
+
 	if (!usb_endpoint_xfer_isoc(&urb->ep->desc) && usb_urb_dir_out(urb) &&
 	    usb_endpoint_maxp(&urb->ep->desc) >= TRB_IDT_MAX_SIZE &&
 	    urb->transfer_buffer_length <= TRB_IDT_MAX_SIZE &&
