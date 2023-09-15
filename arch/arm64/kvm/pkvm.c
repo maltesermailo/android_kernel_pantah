ddf151db9eacfc66ab45070f1fb42cab4362f9b7
Saravana Kannan <saravanak@google.com>
2023-09-14 18:10:14 -0700

    TEST ONLY: Flush deferred probe list before dropping host priv
    Change-Id: I5fb3af9b0f7d1b4dbf57078707112dfdb8a3dc23
    Signed-off-by: Saravana Kannan <saravanak@google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -516,6 +516,8 @@ static int __init finalize_pkvm(void)
 	kmemleak_free_part(__hyp_data_start, __hyp_data_end - __hyp_data_start);
 	kmemleak_free_part_phys(hyp_mem_base, hyp_mem_size);
 
+	flush_deferred_probe_now();
+
 	ret = pkvm_drop_host_privileges();
 	if (ret) {
 		pr_err("Failed to de-privilege the host kernel: %d\n", ret);
