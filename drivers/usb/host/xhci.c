0bc1e6a59ef171d76b9799651db68826d7c08b76
Udipto Goswami <quic_ugoswami@quicinc.com>
2023-09-19 14:28:47 +0530

    FROMLIST: usb: XHCI: Implement xhci_handshake_check_state() API
    In some situations where xhci removal happens parallel to
    xhci_handshake, we enoughter a scenario where the
    xhci_handshake will fails because the status does not change
    the entire duration of polling. This causes the xhci_handshake
    to timeout resulting in long wait which might lead to watchdog
    timeout.
    
    The API  handles command timeout which may happen upon XHCI
    stack removal. Check for xhci state and exit the handshake if
    xhci is removed.
    
    Signed-off-by: Udipto Goswami <quic_ugoswami@quicinc.com>
    
    Bug:
    Link: https://lore.kernel.org/all/20230919085847.8210-1-quic_ugoswami@quicinc.com/
    Change-Id: I1da2606da035083ca6e6525c492b33f74f049259
    Signed-off-by: Udipto Goswami <quic_ugoswami@quicinc.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -86,6 +86,24 @@ int xhci_handshake(void __iomem *ptr, u32 mask, u32 done, u64 timeout_us)
 /*
  * Disable interrupts and begin the xHCI halting process.
  */
+int xhci_handshake_check_state(struct xhci_hcd *xhci, void __iomem *ptr,
+		u32 mask, u32 done, int usec, unsigned int exit_state)
+{
+	u32	result;
+	int	ret;
+
+	ret = readl_poll_timeout_atomic(ptr, result,
+				(result & mask) == done ||
+				result == U32_MAX ||
+				xhci->xhc_state & exit_state,
+				1, usec);
+
+	if (result == U32_MAX || xhci->xhc_state & exit_state)
+		return -ENODEV;
+
+	return ret;
+}
+
 void xhci_quiesce(struct xhci_hcd *xhci)
 {
 	u32 halted;
@@ -203,7 +221,8 @@ int xhci_reset(struct xhci_hcd *xhci, u64 timeout_us)
 	if (xhci->quirks & XHCI_INTEL_HOST)
 		udelay(1000);
 
-	ret = xhci_handshake(&xhci->op_regs->command, CMD_RESET, 0, timeout_us);
+	ret = xhci_handshake_check_state(xhci, &xhci->op_regs->command,
+				CMD_RESET, 0, timeout_us, XHCI_STATE_REMOVING);
 	if (ret)
 		return ret;
 
