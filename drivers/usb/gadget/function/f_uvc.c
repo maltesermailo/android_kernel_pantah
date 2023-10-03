b129c89a7736de7f09eac006253fb458183822f4
Jayant Chowdhary <jchowdhary@google.com>
2023-10-03 17:10:04 +0000

    Make number of uvc requests allocated configurable using configfs.
    Bug: 301915972
    
    Test: manual
    
    Change-Id: If99cff4797168765622a0b76ca99d3dc51d729e7
    Signed-off-by: Jayant Chowdhary <jchowdhary@google.com>

--- drivers/usb/gadget/function/f_uvc.c
+++ drivers/usb/gadget/function/f_uvc.c
@@ -608,7 +608,8 @@ uvc_function_bind(struct usb_configuration *c, struct usb_function *f)
 	opts->streaming_interval = clamp(opts->streaming_interval, 1U, 16U);
 	opts->streaming_maxpacket = clamp(opts->streaming_maxpacket, 1U, 3072U);
 	opts->streaming_maxburst = min(opts->streaming_maxburst, 15U);
-
+	opts->streaming_max_uvcrequests = clamp(opts->streaming_max_uvcrequests, 64U, 256U);
+        uvc->streaming_max_uvcrequests = opts->streaming_max_uvcrequests;
 	/* For SS, wMaxPacketSize has to be 1024 if bMaxBurst is not 0 */
 	if (opts->streaming_maxburst &&
 	    (opts->streaming_maxpacket % 1024) != 0) {
@@ -873,6 +874,7 @@ static struct usb_function_instance *uvc_alloc_inst(void)
 
 	opts->streaming_interval = 1;
 	opts->streaming_maxpacket = 1024;
+	opts->streaming_max_uvcrequests = 64;
 	snprintf(opts->function_name, sizeof(opts->function_name), "UVC Camera");
 
 	ret = uvcg_attach_configfs(opts);
--- drivers/usb/gadget/function/uvc_configfs.c
+++ drivers/usb/gadget/function/uvc_configfs.c
@@ -2407,6 +2407,7 @@ UVC_ATTR(f_uvc_opts_, cname, cname)
 UVCG_OPTS_ATTR(streaming_interval, streaming_interval, 16);
 UVCG_OPTS_ATTR(streaming_maxpacket, streaming_maxpacket, 3072);
 UVCG_OPTS_ATTR(streaming_maxburst, streaming_maxburst, 15);
+UVCG_OPTS_ATTR(streaming_max_uvcrequests, streaming_max_uvcrequests, 256);
 
 #undef UVCG_OPTS_ATTR
 
@@ -2456,6 +2457,7 @@ static struct configfs_attribute *uvc_attrs[] = {
 	&f_uvc_opts_attr_streaming_interval,
 	&f_uvc_opts_attr_streaming_maxpacket,
 	&f_uvc_opts_attr_streaming_maxburst,
+	&f_uvc_opts_attr_streaming_max_uvcrequests,
 	&f_uvc_opts_string_attr_function_name,
 	NULL,
 };
