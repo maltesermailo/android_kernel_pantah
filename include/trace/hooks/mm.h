e67d4663e9f2443a438228a0ec2dab7b07407c8a
80389617 <wangyongqiang1@oppo.com>
2023-10-09 18:07:09 +0800

    NDROID: vendor_hooks: protect multi-mapcount pages in kernel
    Support two hooks as follows to protect multi-mapcount pages in kernel:
    
    - trace_android_vh_page_should_be_protect
    - trace_android_vh_mapped_page_try_sorthead
    
    Bug: 236578020
    Signed-off-by: wangyongqiang1@oppo.com
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: I688aceabf17d9de2feac7c3ad7144d307de6ef29

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -156,6 +156,12 @@ DECLARE_HOOK(android_vh_show_mapcount_pages,
 DECLARE_HOOK(android_vh_do_traversal_lruvec,
 	TP_PROTO(struct lruvec *lruvec),
 	TP_ARGS(lruvec));
+DECLARE_HOOK(android_vh_page_should_be_protected,
+	TP_PROTO(struct page *page, bool *should_protect),
+	TP_ARGS(page, should_protect));
+DECLARE_HOOK(android_vh_mark_page_accessed,
+	TP_PROTO(struct page *page),
+	TP_ARGS(page));
 
 DECLARE_HOOK(android_vh_mm_alloc_pages_direct_reclaim_enter,
 	TP_PROTO(unsigned int order),
