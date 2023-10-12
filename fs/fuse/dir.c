7e5c87b78598802ac091dff38ce0f1873d5f595f
liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
2023-10-12 12:28:06 +0800

    ANDROID: fuse-bpf: Add NULL pointer check in fuse_entry_revalidate
    If userspace tried to add a backing file in a fuse_dentry_revalidate
    where there wasn't one originally, this would trigger a crash. Disallow
    this operation for now.
    
    Bug: 296013218
    Bug: 296347888
    Test: fuse_test passes, following script no longer crashes:
    
    adb shell su root setenforce 0
    adb shell su root chmod ug+w /data/media
    adb shell su root rm /data/media/Android -rf
    adb shell su root mkdir -p /storage/emulated/Android/data/test
    adb shell su root ls -l /storage/emulated/Android/data/test
    
    Change-Id: Id8a67c43d1edfa010403d5f17e31109b796998cf
    Signed-off-by: liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -199,7 +199,9 @@ static bool backing_data_changed(struct fuse_inode *fi, struct dentry *entry,
 
 	get_fuse_backing_path(entry, &new_backing_path);
 	new_backing_inode = fi->backing_inode;
-	ihold(new_backing_inode);
+	if(new_backing_inode) {
+		ihold(new_backing_inode);
+	}
 
 	err = fuse_handle_backing(bpf_arg, &new_backing_inode, &new_backing_path);
 
