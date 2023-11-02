32074846000f095008443e59e42ce96bbd45daa9
Keir Fraser <keirf@google.com>
2023-11-02 16:26:11 +0000

    ANDROID: KVM: arm64: Introduce module_change_host_prot_range
    This allows protection attributes to be changed for a range of
    pages via a single module API call.
    
    The original API call modifying a single page is now implemented
    as a shim on top of the new range-based call.
    
    Bug: 308373293
    Change-Id: I6fbb2e0b325aa972148f48746565dcc10d74edaf
    Signed-off-by: Keir Fraser <keirf@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -2008,25 +2008,80 @@ int __pkvm_hyp_donate_host(u64 pfn, u64 nr_pages)
 	return ret;
 }
 
+/* Find the highest memblock region lower than, or containing, addr. */
+static struct memblock_region *find_leq_mem_region(phys_addr_t addr)
+{
+	int cur, left = 0, right = hyp_memblock_nr-1;
+	struct memblock_region *reg;
+
+	/* The list of memblock regions is sorted, binary search it */
+	while (left <= right) {
+		cur = (left + right + 1) >> 1;
+		reg = &hyp_memory[cur];
+		if (addr < reg->base) {
+			right = cur-1;
+		} else {
+			left = cur;
+			if (left == right)
+				return reg;
+		}
+	}
+
+	/* Specified address is below the lowest memblock region. */
+	return NULL;
+}
+
+/* Checks the range [start,end) for consistency.
+ * Returns 0 iff the entire range is mmio.
+ * Returns 1 iff the entire range is memory.
+ * Returns -EPERM iff the range is a mix of both mmio and memory.
+ */
+static int whole_range_is_memory(u64 start, u64 end)
+{
+	struct memblock_region *reg = find_leq_mem_region(start);
+
+	if (!reg || (start >= reg->base + reg->size)) {
+		/*
+		 * Start address is mmio. Check whether the specified range
+		 * extends into a memblock region.
+		 */
+		reg = reg ? reg+1 : hyp_memory;
+		WARN_ON(reg - hyp_memory > hyp_memblock_nr);
+		if ((reg - hyp_memory < hyp_memblock_nr) && (end > reg->base))
+			goto fail;
+		/* Whole range is mmio */
+		return 0;
+	} else if (end <= reg->base + reg->size) {
+		/* Whole range is memory */
+		return 1;
+	}
+
+fail:
+	/* Error: Mix of memory and mmio */
+	return -EPERM;
+}
+
 #define MODULE_PROT_ALLOWLIST (KVM_PGTABLE_PROT_RWX |	\
 			       KVM_PGTABLE_PROT_DEVICE |\
 			       KVM_PGTABLE_PROT_NC |	\
 			       KVM_PGTABLE_PROT_PXN |	\
 			       KVM_PGTABLE_PROT_UXN)
-int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot)
+
+int module_change_host_page_prot_range(u64 pfn, enum kvm_pgtable_prot prot, u64 nr_pages)
 {
-	u64 addr = hyp_pfn_to_phys(pfn);
+	u64 i, j, addr = hyp_pfn_to_phys(pfn);
 	struct hyp_page *page = NULL;
-	kvm_pte_t pte;
-	u32 level;
 	int ret;
 
 	if ((prot & MODULE_PROT_ALLOWLIST) != prot)
 		return -EINVAL;
 
 	host_lock_component();
-	ret = kvm_pgtable_get_leaf(&host_mmu.pgt, addr, &pte, &level);
-	if (ret)
+
+	ret = whole_range_is_memory(addr, addr + nr_pages*PAGE_SIZE);
+
+	/* It is invalid to specify a range containing both memory and mmio. */
+	if (ret < 0)
 		goto unlock;
 
 	/*
@@ -2034,40 +2089,65 @@ int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot)
 	 * of module-owned MMIO regions hard, so we trust the modules not to
 	 * mess things up.
 	 */
-	if (!addr_is_memory(addr))
+	if (ret == 0)
 		goto update;
 
-	ret = -EPERM;
+	/* Range is memory: we can track module ownership. */
+	WARN_ON(ret != 1);
 	page = hyp_phys_to_page(addr);
 
 	/*
 	 * Modules can only modify pages they already own, and pristine host
 	 * pages.
 	 */
-	if (!(page->flags & MODULE_OWNED_PAGE) &&
-	    (host_get_page_state(pte, addr) != PKVM_PAGE_OWNED))
-		goto unlock;
+	j = 0;
+	for (;;) {
+		/* Module-owned pages are permitted to be modified. */
+		for (i = j; i < nr_pages; i++)
+			if (!(page[i].flags & MODULE_OWNED_PAGE))
+				break;
+		if (i >= nr_pages)
+			break;
+		/* Find a range [i,j) of non-module pages.  */
+		for (j = i+1; j < nr_pages; j++)
+			if (page[j].flags & MODULE_OWNED_PAGE)
+				break;
+		/* The non-module range must be pristine. */
+		ret = __host_check_page_state_range(
+			hyp_pfn_to_phys(pfn+i), (j-i) << PAGE_SHIFT,
+			PKVM_PAGE_OWNED);
+		if (ret)
+			goto unlock;
+	}
 
 update:
 	if (!prot) {
-		ret = host_stage2_set_owner_locked(addr, PAGE_SIZE,
-						   PKVM_ID_PROTECTED);
+		ret = host_stage2_set_owner_locked(
+			addr, nr_pages << PAGE_SHIFT, PKVM_ID_PROTECTED);
 	} else {
-		ret = host_stage2_idmap_locked(addr, PAGE_SIZE, prot, false);
+		ret = host_stage2_idmap_locked(
+			addr, nr_pages << PAGE_SHIFT, prot, false);
 	}
 
 	if (ret || !page)
 		goto unlock;
 
-	if (prot != KVM_PGTABLE_PROT_RWX)
-		hyp_phys_to_page(addr)->flags |= MODULE_OWNED_PAGE;
-	else
-		hyp_phys_to_page(addr)->flags &= ~MODULE_OWNED_PAGE;
+	for (i = 0; i < nr_pages; i++) {
+		if (prot != KVM_PGTABLE_PROT_RWX)
+			page[i].flags |= MODULE_OWNED_PAGE;
+		else
+			page[i].flags &= ~MODULE_OWNED_PAGE;
+	}
 
 unlock:
 	host_unlock_component();
 
 	return ret;
+}
+
+int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot)
+{
+	return module_change_host_page_prot_range(pfn, prot, 1);
 }
 
 int hyp_pin_shared_mem(void *from, void *to)
