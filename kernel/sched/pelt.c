fbe5ce79f5481f7fb5fa6ac212d5c907e90148fd
Chungkai Mei <chungkai@google.com>
2023-11-02 05:31:05 +0000

    ANDROID: sched: Add vendor hook for update_load_sum
    vendor may have the need to modify update_load_sum function
    
    Bug: 181105055
    Change-Id: I35964977a4b8917c62773d48a37340f880774e38
    Signed-off-by: Chungkai Mei <chungkai@google.com>
    (cherry picked from commit 984523c368423410bf53ca4c68402897d160c5dd)
    (cherry picked from commit 0d97bca80a8f680f1fbf4b2a441c6ac581b8956a)

--- kernel/sched/pelt.c
+++ kernel/sched/pelt.c
@@ -24,6 +24,7 @@
  *  Author: Vincent Guittot <vincent.guittot@linaro.org>
  */
 
+#include <trace/hooks/sched.h>
 #include <linux/sched.h>
 #include "sched.h"
 #include "pelt.h"
@@ -241,6 +242,8 @@ int ___update_load_sum(u64 now, struct sched_avg *sa,
 		return 0;
 
 	sa->last_update_time += delta << 10;
+
+	trace_android_rvh_update_load_sum(sa, &delta, &sched_pelt_lshift);
 
 	/*
 	 * running is a subset of runnable (weight) so running can't be set if
