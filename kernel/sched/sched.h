c6ea4ffd151bb657b1f359688e25d66790088a65
Connor O'Brien <connoro@google.com>
2023-11-08 07:00:23 +0000

    sched: Add find_exec_ctx helper
    Add a helper to find the runnable owner down a chain of blocked waiters
    
    This patch was broken out from a larger chain migration
    patch originally by Connor O'Brien.
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: kernel-team@android.com
    Change-Id: Ib909ae5a5de87fab5dd1943caffb3a5d87392c03
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: split out from larger chain migration patch]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v8
    * Minor formatting changes suggested by Metin Kaya
    * Fix for null owner case, uncovered by enabling optimistic
      spinning
    * Add null exec_ctx check to this patch from an earlier patch,
      as suggested by Metin
    v10:
    * Cleanup suggested by Metin to go directly to mutex owner
    * Simplify logic to task_queued_on_rq suggested by Metin

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3648,7 +3648,17 @@ bool task_is_pushable(struct rq *rq, struct task_struct *p, int cpu)
 
 	return false;
 }
-#endif
+
+#ifdef CONFIG_SCHED_PROXY_EXEC
+struct task_struct *find_exec_ctx(struct rq *rq, struct task_struct *p);
+#else /* !CONFIG_SCHED_PROXY_EXEC */
+static inline
+struct task_struct *find_exec_ctx(struct rq *rq, struct task_struct *p)
+{
+	return p;
+}
+#endif /* CONFIG_SCHED_PROXY_EXEC */
+#endif /* CONFIG_SMP */
 
 #ifdef CONFIG_RT_SOFTIRQ_AWARE_SCHED
 extern bool cpu_busy_with_softirqs(int cpu);
