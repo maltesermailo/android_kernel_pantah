4456e0a2f90d526643a5c545e453ae3652693123
Eric Seckler <eseckler@google.com>
2023-11-10 14:05:29 +0000

    TEST-ONLY: drivers: hwtracing: coresight: set ccctlr to ccitmin
    Change-Id: If701ef64158a0bf5225f6f2ddfd64da864e11a02
    Signed-off-by: Eric Seckler <eseckler@google.com>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -637,7 +637,7 @@ static int etm4_parse_event_config(struct coresight_device *csdev,
 	if (attr->config & BIT(ETM_OPT_CYCACC)) {
 		config->cfg |= TRCCONFIGR_CCI;
 		/* TRM: Must program this for cycacc to work */
-		config->ccctlr = ETM_CYC_THRESHOLD_DEFAULT;
+		config->ccctlr = drvdata->ccitmin;
 	}
 	if (attr->config & BIT(ETM_OPT_TS)) {
 		/*
