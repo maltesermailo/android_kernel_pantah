2363efbc1c57fb689dfc0675c4205bcbc903a8fc
Kalesh Singh <kaleshsingh@google.com>
2023-11-20 11:51:28 -0800

    ANDROID: 16K: mlock: __PAGE_ALIGN addr and len
    In page-compat mode, mlock() syscalls must be at __PAGE_ALIGN-ed
    addresses and of __PAGE_SIZE-multiple length.
    
    Bug: 315325080
    Bug: 302403436
    Change-Id: I91f13086cb4cea8a6cdd92114993def22dd377d4
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/mlock.c
+++ mm/mlock.c
@@ -25,6 +25,7 @@
 #include <linux/memcontrol.h>
 #include <linux/mm_inline.h>
 #include <linux/secretmem.h>
+#include <linux/page_size_compat.h>
 
 #include "internal.h"
 
@@ -473,8 +474,8 @@ static int apply_vma_lock_flags(unsigned long start, size_t len,
 	int error;
 	MA_STATE(mas, &current->mm->mm_mt, start, start);
 
-	VM_BUG_ON(offset_in_page(start));
-	VM_BUG_ON(len != PAGE_ALIGN(len));
+	VM_BUG_ON(__offset_in_page(start));
+	VM_BUG_ON(len != __PAGE_ALIGN(len));
 	end = start + len;
 	if (end < start)
 		return -EINVAL;
@@ -575,8 +576,8 @@ static __must_check int do_mlock(unsigned long start, size_t len, vm_flags_t fla
 	if (!can_do_mlock())
 		return -EPERM;
 
-	len = PAGE_ALIGN(len + (offset_in_page(start)));
-	start &= PAGE_MASK;
+	len = __PAGE_ALIGN(len + (__offset_in_page(start)));
+	start &= __PAGE_MASK;
 
 	lock_limit = rlimit(RLIMIT_MEMLOCK);
 	lock_limit >>= PAGE_SHIFT;
@@ -635,8 +636,8 @@ SYSCALL_DEFINE2(munlock, unsigned long, start, size_t, len)
 
 	start = untagged_addr(start);
 
-	len = PAGE_ALIGN(len + (offset_in_page(start)));
-	start &= PAGE_MASK;
+	len = __PAGE_ALIGN(len + (__offset_in_page(start)));
+	start &= __PAGE_MASK;
 
 	if (mmap_write_lock_killable(current->mm))
 		return -EINTR;
