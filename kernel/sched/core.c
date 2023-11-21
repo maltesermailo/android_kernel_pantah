a923cded28877a4b7a2dae339b6f65eeaac85517
Rick Yiu <rickyiu@google.com>
2023-11-21 07:18:24 +0000

    ANDROID: sched: Add trace_android_rvh_set_user_nice_locked
    We will need vendor hook of set_user_nice with lock to avoid race
    condition.
    
    Bug: 300872872
    Change-Id: I24fc1e13cc6578dcc418d956a5146ad29ff76a56
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7187,15 +7187,16 @@ void set_user_nice(struct task_struct *p, long nice)
 	struct rq_flags rf;
 	struct rq *rq;
 
-	trace_android_rvh_set_user_nice(p, &nice, &allowed);
-	if ((task_nice(p) == nice || nice < MIN_NICE || nice > MAX_NICE) && !allowed)
-		return;
 	/*
 	 * We have to be careful, if called from sys_setpriority(),
 	 * the task might be in the middle of scheduling on another CPU.
 	 */
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
+
+	trace_android_rvh_set_user_nice_locked(p, &nice, &allowed);
+	if ((task_nice(p) == nice || nice < MIN_NICE || nice > MAX_NICE) && !allowed)
+		goto out_unlock;
 
 	/*
 	 * The RT priorities are set via sched_setscheduler(), but we still
