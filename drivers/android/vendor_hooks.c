43861f49668175e4f250803a398a1676cfa63bb8
Martin Liu <liumartin@google.com>
2023-11-30 11:00:56 +0800

    ANDROID: GKI: add a vendor hook in ptep_clear_flush_young()
    Add a vendor hook to be able to skip ptep tlb flush.
    
    Bug: 312692863
    Test: boot
    Change-Id: Ifbc7a52111616d0542b866532a9bf82368e6c8f1
    Signed-off-by: Martin Liu <liumartin@google.com>
    (cherry picked from commit 46f8b2ca58846be1052412fd90b4da12222c37fe)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -371,3 +371,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_oem_binder_struct);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_special_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_free_buf);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_isolate_freepages);
+<<<<<<< HEAD   (36dcb2 ANDROID: GKI: update mtktv symbol)
+=======
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_thermal_stats);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ptep_clear_flush_young);
+>>>>>>> CHANGE (46f8b2 ANDROID: GKI: add a vendor hook in ptep_clear_flush_young())
