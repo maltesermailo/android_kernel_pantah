62e5e2c06bf3574496f6e94d8ed4b1bf4a4641f7
Suren Baghdasaryan <surenb@google.com>
2024-01-17 17:27:43 -0800

    WIP: prevent address randomization loss when using compat syscall
    Testing fix from https://lore.kernel.org/all/CAHbLzkoL6sCDciHqVMJga288853CHdOTa5thOPQ9SHKSqjGGPQ@mail.gmail.com/
    
    Bug: 254683164
    Change-Id: I3b0617e1b8b51dcca42591a089a3616ae33a7839
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -634,6 +634,9 @@ static unsigned long __thp_get_unmapped_area(struct file *filp,
 	loff_t off_align = round_up(off, size);
 	unsigned long len_pad, ret;
 
+	if (IS_ENABLED(CONFIG_32BIT) || in_compat_syscall())
+		return 0;
+
 	if (off_end <= off_align || (off_end - off_align) < size)
 		return 0;
 
