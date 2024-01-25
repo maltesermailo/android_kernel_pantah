7711be7a7c664ffe338813605be10bc66a5d4817
Betty Zhou <bettyzhou@google.com>
2024-01-24 17:06:37 -0800

    Android: Update the kselftest.sh with option to special device serial.
    Add device serial number parsing from acloud command output.
    
    Update the TESTSDIR.
    
    Test: local test
    Bug: 309421656
    Change-Id: I6523f1a2135fee7d10dee0d1a8e08236ea49f535

--- tools/testing/android/bin/kselftest.sh
+++ tools/testing/android/bin/kselftest.sh
@@ -5,7 +5,7 @@ BAZEL=tools/bazel
 BIN_DIR=common/tools/testing/android/bin
 ACLOUD=$BIN_DIR/acloudb.sh
 TRADEFED=prebuilts/tradefed/filegroups/tradefed/tradefed.sh
-TESTSDIR=bazel-bin/common/testcases
+TESTSDIR=bazel-bin/common/
 
 print_help() {
     echo "Usage: $0 [OPTIONS]"
@@ -15,8 +15,9 @@ print_help() {
     echo "  --skip-kernel-build    Skip the kernel building step"
     echo "  --skip-cvd-launch      Skip the CVD launch step"
     echo "  --skip-cvd-kill        Do not kill CVD launched by running this script"
-    echo "  --dist-dir             The kernel dist dir (default is /tmp/kernel_dist)"
-    echo "  --help                 Display this help message and exit"
+    echo "  -d, --dist-dir=DIR     The kernel dist dir (default is /tmp/kernel_dist)"
+    echo "  -s, --serial=SERIAL    The device serial number with -s. If specified, cuttlefish device launch will be skipped"
+    echo "  -h, --help             Display this help message and exit"
     echo ""
     exit 0
 }
@@ -25,9 +26,17 @@ BUILD_KERNEL=true
 LAUNCH_CVD=true
 KILL_CVD=true
 DIST_DIR=/tmp/kernel_dist
+SERIAL_NUMBER=
 
-for arg in "$@"; do
-    case $arg in
+#ADB=$(which adb 2>/dev/null)
+#if [ -z "$ADB" ]; then
+#    panic "Can't find 'adb' tool in your path. Please install it."
+
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            print_help
+            ;;
         --skip-kernel-build)
             BUILD_KERNEL=false
             shift
@@ -40,12 +49,39 @@ for arg in "$@"; do
             KILL_CVD=false
             shift
             ;;
-        --dist-dir)
-            DIST_DIR="${arg#*=}"
+        -d)
+            shift
+            if test $# -gt 0; then
+                DIST_DIR=$1
+            else
+                echo "kernel distribution directory is not specified"
+                exit 1
+            fi
             shift
             ;;
-        --help)
-            print_help
+        --dist-dir*)
+            DIST_DIR=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+        -s)
+            shift
+            if test $# -gt 0; then
+                SERIAL_NUMBER=$1
+                BUILD_KERNEL=false
+                LAUNCH_CVD=false
+                KILL_CVD=false
+            else
+                echo "device serial is not specified"
+                exit 1
+            fi
+            shift
+            ;;
+        --serial*)
+            BUILD_KERNEL=false
+            LAUNCH_CVD=false
+            KILL_CVD=false
+            SERIAL_NUMBER=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
             ;;
         *)
             ;;
@@ -61,11 +97,21 @@ if $LAUNCH_CVD; then
     echo "Launching cvd..."
     CVD_OUT=$($ACLOUD create --local-kernel-image $DIST_DIR)
     echo $CVD_OUT
-    INSTANCE_NAME=$(echo $CVD_OUT|grep -o "ins-[^\[]*")
+    INSTANCE_NAME=$(echo "$CVD_OUT" | grep -o "ins-[^\[]*")
+    SERIAL_NUMBER=$(echo "$CVD_OUT" | grep -oE 'device serial: ([0-9]+\.){3}[0-9]+:[0-9]+' | sed 's/device serial: //')
+    echo "acloud launched device $SERIAL_NUMBER with instance $INSTANCE_NAME"
 fi
 
-echo "Building selftests..."
-ABI=$(adb shell getprop ro.product.cpu.abi)
+if [ -z "$SERIAL_NUMBER" ]; then
+    echo "Device serial number is not provided by acloud or by -s|--serial flag"
+    exit 1
+else
+    echo "Test with device: $SERIAL_NUMBER"
+fi
+
+echo "Get abi from device $SERIAL_NUMBER"
+ABI=$(adb -s $SERIAL_NUMBER shell getprop ro.product.cpu.abi)
+echo "Building kselftests according to device $SERIAL_NUMBER ro.product.cpu.abi $ABI ..."
 case $ABI in
 	arm64*)
 		$BAZEL build //common:kselftest_tests_arm64
@@ -80,7 +126,7 @@ case $ABI in
 esac
 
 $TRADEFED run commandAndExit template/local_min --template:map test=suite/test_mapping_suite \
---include-filter selftests --tests-dir=$TESTSDIR --primary-abi-only
+--include-filter selftests --tests-dir=$TESTSDIR -s $SERIAL_NUMBER
 
 if $LAUNCH_CVD && $KILL_CVD; then
     echo "Test finished. Deleting cvd..."
