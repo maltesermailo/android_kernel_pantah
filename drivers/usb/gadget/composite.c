24dbd6015c2b6674adfdbcfd515bb21a424307bc
Neill Kapron <nkapron@google.com>
2024-02-01 08:43:46 +0000

    ANDROID: usb: gadget: configfs: Add Uevent to notify userspace DO NOT SUBMIT: Work in progress.
    Android userspace UsbDeviceManager relies on the uevents generated
    by the composition driver to generate user notifications. Therefore,
    uevents will be generated whenever USB changes its state. This patch
    also add "state" attribute to android_device to show USB state. In
    addition, this patch adds create_function_device to create child
    function devices for USB gadget functions. Android UsbDeviceManager
    relies on communicating to the devices created by the gadget functions
    to implement functions such as midi.
    
    Bug: 317149848
    Change-Id: I84421a27a9720b42feb6e9f9613178937c56506a
    Signed-off-by: Neill Kapron <nkapron@google.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -20,6 +20,7 @@
 #include <asm/unaligned.h>
 
 #include "u_os_desc.h"
+#include "configfs_uevent.h"
 
 /**
  * struct usb_os_string - represents OS String to be reported by a gadget
@@ -856,6 +857,7 @@ static void reset_config(struct usb_composite_dev *cdev)
 		bitmap_zero(f->endpoints, 32);
 	}
 	cdev->config = NULL;
+	android_set_unconfigured(&cdev->android_opts);
 	cdev->delayed_status = 0;
 }
 
@@ -1698,6 +1700,8 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 	struct usb_function		*iter;
 	u8				endp;
 
+	android_set_connected(&cdev->android_opts);
+
 	if (w_length > USB_COMP_EP0_BUFSIZ) {
 		if (ctrl->bRequestType & USB_DIR_IN) {
 			/* Cast away the const, we are going to overwrite on purpose. */
@@ -1831,6 +1835,7 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 		}
 		spin_lock(&cdev->lock);
 		value = set_config(cdev, ctrl, w_value);
+		android_set_configured(&cdev->android_opts);
 		spin_unlock(&cdev->lock);
 		break;
 	case USB_REQ_GET_CONFIGURATION:
@@ -2120,6 +2125,7 @@ static void __composite_disconnect(struct usb_gadget *gadget)
 	 * disconnect callbacks?
 	 */
 	spin_lock_irqsave(&cdev->lock, flags);
+	android_set_disconnected(&cdev->android_opts);
 	cdev->suspended = 0;
 	if (cdev->config)
 		reset_config(cdev);
--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -7,6 +7,7 @@
 #include <linux/usb/composite.h>
 #include <linux/usb/gadget_configfs.h>
 #include "configfs.h"
+#include "configfs_uevent.h"
 #include "u_f.h"
 #include "u_os_desc.h"
 
@@ -273,7 +274,11 @@ static ssize_t gadget_dev_desc_UDC_store(struct config_item *item,
 
 	mutex_lock(&gi->lock);
 
-	if (!strlen(name)) {
+	/*
+	 * ANDROID: Not exactly sure why we need this "none", but worried it
+	 * would break something if removed.
+	 */
+	if (!strlen(name) || strcmp(name, "none") == 0) {
 		ret = unregister_gadget(gi);
 		if (ret)
 			goto err;
@@ -1639,6 +1644,9 @@ static struct config_group *gadgets_make(
 	if (!gi->composite.gadget_driver.function)
 		goto out_free_driver_name;
 
+	if (android_device_create(&gi->cdev.android_opts))
+		goto out_free_driver_name;
+
 	return &gi->group;
 
 out_free_driver_name:
@@ -1650,6 +1658,10 @@ err:
 
 static void gadgets_drop(struct config_group *group, struct config_item *item)
 {
+	struct gadget_info *gi;
+
+	gi = container_of(to_config_group(item), struct gadget_info, group);
+	android_device_destroy(&gi->cdev.android_opts);
 	config_item_put(item);
 }
 
@@ -1689,13 +1701,17 @@ static int __init gadget_cfs_init(void)
 
 	config_group_init(&gadget_subsys.su_group);
 
-	ret = configfs_register_subsystem(&gadget_subsys);
-	return ret;
+	ret = android_class_create();
+	if (ret)
+		return ret;
+
+	return configfs_register_subsystem(&gadget_subsys);
 }
 module_init(gadget_cfs_init);
 
 static void __exit gadget_cfs_exit(void)
 {
+	android_class_destroy();
 	configfs_unregister_subsystem(&gadget_subsys);
 }
 module_exit(gadget_cfs_exit);
