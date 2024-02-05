53965a6c2767c0a9a4007eb83fc531b80c0600cc
Giuliano Procida <gprocida@google.com>
2024-02-05 17:44:02 +0000

    ANDROID: testing the ABI compatibility monitoring
    Bug: 298933183
    Change-Id: Ib23eff1b4f96790826851617454dd0e25b6340ec
    Signed-off-by: Giuliano Procida <gprocida@google.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1065,6 +1065,7 @@ struct sk_buff {
 	/* only useable after checking ->active_extensions != 0 */
 	struct skb_ext		*extensions;
 #endif
+	unsigned char		giblet;
 };
 
 /* if you move pkt_type around you also must adapt those constants */
