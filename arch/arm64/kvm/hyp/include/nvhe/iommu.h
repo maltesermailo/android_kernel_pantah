0505e11da11f8fc03c75b1675678499f1ca000f6
Mostafa Saleh <smostafa@google.com>
2024-02-14 13:14:11 +0000

    ANDROID: drivers/arm-smmu-v3-kvm: Relax probe through register_device
    Currently the driver initialised of the SMMUs is roughly as follows
    
    - Kernel driver is registered by the kernel
    - Kernel driver registers itself to KVM
    - KVM calls the init function of the driver
    - The driver synchronously probes all devices and populated them in an array
    - The driver initialises the EL2 driver with the SMMU list.
    
    In theory this works, in practice that means this has multiple drawbacks:
    1) we block all SMMU registration  with the kernel until everything probes.
    2) All SMMU dependencies have to be present before the driver is loaded we
       don’t support deferred probing.
    
    We relax these with:
    1) Introduced a register_device hypercall that initialises each device separately
    2)Allow devices to be deferred probe, as each one is independent.
    
    This still assumes that all probes happen while the kernel is still trusted as
    the kernel driver will access the SMMU list which is donated to EL2, so if this
    happens later the kernel would panic. And because of that we do little checks
    on arguments passed from the kernel at the moment.
    
    This is relaxed even further in the next patches, where the IOMMUs would have a
    separate de-privilege.
    
    Bug: 277989609
    Bug: 278749606
    
    Change-Id: I12571c4074e1bc821d4a38181e68cb222907df99
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/iommu.h
+++ arch/arm64/kvm/hyp/include/nvhe/iommu.h
@@ -55,6 +55,7 @@ void kvm_iommu_host_stage2_idmap(phys_addr_t start, phys_addr_t end,
 
 struct kvm_iommu_ops {
 	int (*init)(unsigned long arg);
+	int (*register_device)(unsigned long id, void *data);
 	struct kvm_hyp_iommu *(*get_iommu_by_id)(pkvm_handle_t smmu_id);
 	int (*alloc_domain)(struct kvm_hyp_iommu_domain *domain, u32 type);
 	void (*free_domain)(struct kvm_hyp_iommu_domain *domain);
