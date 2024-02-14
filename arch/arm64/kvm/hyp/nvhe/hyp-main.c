0109a40ff198965e465e0ddc69e95cf750dd6f9f
Mostafa Saleh <smostafa@google.com>
2024-02-14 17:51:02 +0000

    ANDROID: KVM: arm64: iommu: Add pkvm_iommu_finalise
    Add a function to prevent any more registration of IOMMUs, for now
    this is called after all the nodes probe, However this can be hardened
    to have a hard timeout in case that this was not called for any HW
    failure or misconfiguration.
    
    Bug: 277989609
    Bug: 278749606
    
    Change-Id: I51cb9342fdc5ae2b302be9cfeb623289f79c8312
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1574,6 +1574,11 @@ static void handle___pkvm_iommu_register(struct kvm_cpu_context *host_ctxt)
 	cpu_reg(host_ctxt, 1) = kvm_iommu_register_device(id, data);
 }
 
+static void handle___pkvm_iommu_finalise(struct kvm_cpu_context *host_ctxt)
+{
+	cpu_reg(host_ctxt, 1) = kvm_iommu_finalise();
+}
+
 typedef void (*hcall_t)(struct kvm_cpu_context *);
 
 #define HANDLE_FUNC(x)	[__KVM_HOST_SMCCC_FUNC_##x] = (hcall_t)handle_##x
@@ -1637,6 +1642,7 @@ static const hcall_t host_hcall[] = {
 	HANDLE_FUNC(__pkvm_host_iommu_unmap_pages),
 	HANDLE_FUNC(__pkvm_host_iommu_iova_to_phys),
 	HANDLE_FUNC(__pkvm_iommu_register),
+	HANDLE_FUNC(__pkvm_iommu_finalise),
 	HANDLE_FUNC(__pkvm_host_hvc_pd),
 };
 
