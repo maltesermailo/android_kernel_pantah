df5fd656d6407dd319fe2d6de015506a738242c5
Sajid Dalvi <sdalvi@google.com>
2024-02-20 13:20:17 -0600

    ANDROID: PCI: dwc: Wait for the link only if it has been started
    The patch submitted to the upstream kernel (see link below) was not
    accepted due to no upstream user.
    
    The change here is a simplified version of that patch, which will wait
    for a link only if start_link ops has been defined.
    
    Bug: 315052790
    Link: https://lore.kernel.org/all/20240111152517.1881382-1-ajayagarwal@google.com/
    Change-Id: I4e8d00f6195062728417e41ddd51072880676920
    Signed-off-by: Sajid Dalvi <sdalvi@google.com>

--- drivers/pci/controller/dwc/pcie-designware-host.c
+++ drivers/pci/controller/dwc/pcie-designware-host.c
@@ -485,8 +485,10 @@ int dw_pcie_host_init(struct dw_pcie_rp *pp)
 			goto err_free_msi;
 	}
 
-	/* Ignore errors, the link may come up later */
-	dw_pcie_wait_for_link(pci);
+	if (pci->ops && pci->ops->start_link) {
+		/* Ignore errors, the link may come up later */
+		dw_pcie_wait_for_link(pci);
+	}
 
 	bridge->sysdata = pp;
 
