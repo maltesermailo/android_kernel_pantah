4c74f51529463d6a479de21e3cf3f0fb2bc7f4cc
Lokesh Gidra <lokeshgidra@google.com>
2024-03-08 10:59:56 -0800

    ANDROID: userfaultfd: check if mmap_lock is contended during uffdio ops
    Check if the mmap_lock is contended when looping over the pages that
    are requested to be filled. When it is observed, we rely on the already
    existing mechanism to return bytes copied/filled and -EAGAIN as error.
    
    This helps by avoiding contention of mmap_lock for long running
    userfaultfd operations. The userspace can perform other tasks before
    retrying the operation for the remaining pages.
    
    Bug: 320478828
    Change-Id: I6d485fd03c96a826956ee3962e58058be3cf81c1
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>

--- mm/userfaultfd.c
+++ mm/userfaultfd.c
@@ -664,6 +664,14 @@ retry:
 		if (unlikely(err == -ENOENT)) {
 			void *page_kaddr;
 
+			/*
+			 * Return early due to mmap_lock contention only if some
+			 * progress is made. This ensures that jank sensitive
+			 * threads don't get blocked for too long.
+			 */
+			if (copied && mmap_lock_is_contended(dst_mm))
+				break;
+
 			mmap_read_unlock(dst_mm);
 			BUG_ON(!page);
 
@@ -688,6 +696,9 @@ retry:
 
 			if (fatal_signal_pending(current))
 				err = -EINTR;
+
+			if (mmap_lock_is_contended(dst_mm))
+				err = -EAGAIN;
 		}
 		if (err)
 			break;
