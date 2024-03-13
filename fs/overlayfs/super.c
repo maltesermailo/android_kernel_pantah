dc0c26ff7da2655db4ac9133fbe0418965177349
Lee Jones <joneslee@google.com>
2024-03-13 16:25:41 +0000

    Revert "fs: factor out backing_file_{read,write}_iter() helpers"
    This commit reverses the Android specific override creds logic.
    
    This reverts commit a6293b3e285cd0d7692141d7981a5f144f0e2f0b.
    
    Change-Id: Ic4b12673afdd8fe1560671301d1a9525931fe82e
    Signed-off-by: Lee Jones <joneslee@google.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -1501,10 +1501,14 @@ static int __init ovl_init(void)
 	if (ovl_inode_cachep == NULL)
 		return -ENOMEM;
 
-	err = register_filesystem(&ovl_fs_type);
-	if (!err)
-		return 0;
+	err = ovl_aio_request_cache_init();
+	if (!err) {
+		err = register_filesystem(&ovl_fs_type);
+		if (!err)
+			return 0;
 
+		ovl_aio_request_cache_destroy();
+	}
 	kmem_cache_destroy(ovl_inode_cachep);
 
 	return err;
@@ -1520,6 +1524,7 @@ static void __exit ovl_exit(void)
 	 */
 	rcu_barrier();
 	kmem_cache_destroy(ovl_inode_cachep);
+	ovl_aio_request_cache_destroy();
 }
 
 module_init(ovl_init);
