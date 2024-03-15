ec4160f192693c8e73e185caf92e8e4cdf1dc705
Sebastian Ene <sebastianene@google.com>
2024-03-15 14:07:38 +0000

    ANDROID: KVM: arm64: Expose the host stage-2 start level and ipa range
    Register two new debugfs files which expose the host stage-2 start level
    and ipa range when they are accessed.
    
    Bug: 260238318
    Change-Id: I24fed9914daf2da38c7fd57fbf876804ba8a22f6
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/ptdump.c
+++ arch/arm64/kvm/ptdump.c
@@ -408,6 +408,14 @@ static int kvm_pgtable_debugfs_show(struct seq_file *m, void *unused)
 	return 0;
 }
 
+static pkvm_handle_t kvm_ptdump_get_handle(struct kvm *kvm)
+{
+	if (!kvm)
+		return 0;
+
+	return kvm->arch.pkvm.handle;
+}
+
 static int kvm_pgtable_debugfs_open(struct inode *m, struct file *file)
 {
 	struct kvm *kvm = m->i_private;
@@ -416,11 +424,11 @@ static int kvm_pgtable_debugfs_open(struct inode *m, struct file *file)
 	struct kvm_pgtable_snapshot *snap;
 	int ret = -EINVAL;
 
-	if (!kvm_get_kvm_safe(kvm))
+	if (kvm && !kvm_get_kvm_safe(kvm))
 		return -ENOENT;
 
 	if (is_protected_kvm_enabled()) {
-		snap = kvm_ptdump_get_snapshot(kvm->arch.pkvm.handle, 0, 0, NULL);
+		snap = kvm_ptdump_get_snapshot(kvm_ptdump_get_handle(kvm), 0, 0, NULL);
 		if (!snap)
 			goto free_with_kvm_ref;
 		pgtable = &snap->pgtable;
@@ -438,7 +446,8 @@ free_with_snapshot:
 	if (is_protected_kvm_enabled())
 		kvm_ptdump_put_snapshot(snap);
 free_with_kvm_ref:
-	kvm_put_kvm(kvm);
+	if (kvm)
+		kvm_put_kvm(kvm);
 	return ret;
 }
 
@@ -457,7 +466,8 @@ static int kvm_pgtable_debugfs_close(struct inode *m, struct file *file)
 		kvm_ptdump_put_snapshot(snap);
 	}
 
-	kvm_put_kvm(kvm);
+	if (kvm)
+		kvm_put_kvm(kvm);
 	return single_release(m, file);
 }
 
@@ -517,4 +527,8 @@ void kvm_ptdump_host_register(void)
 {
 	debugfs_create_file("host_stage2_page_tables", 0400, kvm_debugfs_dir,
 			    (void *)host_s2_pgtable_pages(), &kvm_ptdump_host_fops);
+	debugfs_create_file("ipa_range", 0400, kvm_debugfs_dir, NULL,
+			    &kvm_pgtable_debugfs_fops);
+	debugfs_create_file("stage2_levels", 0400, kvm_debugfs_dir,
+			    NULL, &kvm_pgtable_debugfs_fops);
 }
