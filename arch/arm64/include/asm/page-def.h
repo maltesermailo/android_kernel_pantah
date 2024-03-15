f22329e63abaf128cb258ee33f41af1a4ec4d05a
Kalesh Singh <kaleshsingh@google.com>
2024-03-15 11:38:56 -0700

    ANDROID: Introduce MAX_PAGE_SIZE macros
    Change-Id: I1a00387893dd0409e61c9604be684fd904d53839

--- arch/arm64/include/asm/page-def.h
+++ arch/arm64/include/asm/page-def.h
@@ -15,4 +15,9 @@
 #define PAGE_SIZE		(_AC(1, UL) << PAGE_SHIFT)
 #define PAGE_MASK		(~(PAGE_SIZE-1))
 
+/* Max page size */
+#define MAX_PAGE_SHIFT		16
+#define MAX_PAGE_SIZE		(_AC(1, UL) << MAX_PAGE_SHIFT)
+#define MAXPAGE_MASK		(~(MAX_PAGE_SIZE-1))
+
 #endif /* __ASM_PAGE_DEF_H */
