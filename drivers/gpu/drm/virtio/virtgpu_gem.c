b681fec406d2a60e9b89067a2394466d16b46ecd
Jason Macnak <natsu@google.com>
2024-03-27 12:59:31 -0700

    ANDROID: 16K:  __PAGE_ALIGN() virtio gpu dumb buffers
    Bug: 330556068
    Test: cvd start \
          --gpu_mode=guest_swiftshader \
          --kernel_path=<path>out/virtual_device_x86_64/dist/bzImage \
          --initramfs_path=<path>/out/virtual_device_x86_64/dist/initramfs.img \
          --extra_kernel_cmdline="androidboot.page_shift=14"
    Test: then open lens w/ camera
    Change-Id: I616b249a2fe2385262d75901b352b1fda0abc5e4
    Signed-off-by: Jason Macnak <natsu@google.com>

--- drivers/gpu/drm/virtio/virtgpu_gem.c
+++ drivers/gpu/drm/virtio/virtgpu_gem.c
@@ -73,7 +73,7 @@ int virtio_gpu_mode_dumb_create(struct drm_file *file_priv,
 
 	pitch = args->width * 4;
 	args->size = pitch * args->height;
-	args->size = ALIGN(args->size, PAGE_SIZE);
+	args->size = ALIGN(args->size, __PAGE_SIZE);
 
 	params.format = virtio_gpu_translate_format(DRM_FORMAT_HOST_XRGB8888);
 	params.width = args->width;
