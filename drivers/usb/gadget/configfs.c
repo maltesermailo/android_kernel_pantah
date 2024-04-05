a398e076d772a1bab8f81a98d1b2a4364ccfe170
Terence Tritton <ttritton@google.com>
2024-04-05 07:29:57 +0000

    ANDROID: common-android15-6.1 canary test.
    Nothing patch to trigger presubmit builds.
    
    Do not merge.
    
    Bug: 332443283
    Change-Id: I87faad06a15a645e9ebbd24cc445034f61202a3a
    Signed-off-by: Terence Tritton <ttritton@google.com>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1721,3 +1721,4 @@ static void __exit gadget_cfs_exit(void)
 	android_class_destroy();
 }
 module_exit(gadget_cfs_exit);
+//Blank line added for Android Canary build test of branch common-android15-6.1
