6d7e5c06bb77a45ca485b443e5f83960418dc559
liliangliang <liliangliang@vivo.com>
2024-04-07 15:19:02 +0800

    ANDROID: vendor_hooks: add vendor hooks for fuse request
    Add hooks to fuse queue request and request end so we can do boost
    to those background tasks which block the UX related task.
    
    Bug: 333220630
    Change-Id: I9be59ed88675c5102c57ba9cbd26cf4df3d2fd7f
    Signed-off-by: liliangliang <liliangliang@vivo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -63,6 +63,7 @@
 #include <trace/hooks/dmabuf.h>
 #include <trace/hooks/compaction.h>
 #include <trace/hooks/sd.h>
+#include <trace/hooks/fuse.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -325,3 +326,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_trylock_failed);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sd_init_unmap_multi_segment);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sd_setup_unmap_multi_segment);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_scan_type);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_queue_request_and_unlock);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fuse_request_end);
