d9aafa8149b9ca7e76928629016b75fd8085d32b
maoshenshen <maoshenshen@oppo.com>
2024-04-09 20:34:38 +0800

    ANDROID: vendor_hooks: add hooks in cpu_cgroup subsystem
    Add hooks to tune cpu policy based on cpu_cgroup.
    
    Bug:
    
    Change-Id: If7ca0f2ae774f433563abfbe4e761f3fb9645633
    Signed-off-by: shenshen mao <maoshenshen@oppo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10671,6 +10671,7 @@ void sched_move_task(struct task_struct *tsk)
 	struct rq_flags rf;
 	struct rq *rq;
 
+	trace_android_vh_sched_move_task(tsk);
 	rq = task_rq_lock(tsk, &rf);
 	/*
 	 * Esp. with SCHED_AUTOGROUP enabled it is possible to get superfluous
@@ -10720,6 +10721,7 @@ cpu_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 	struct task_group *tg;
 
 	if (!parent) {
+		trace_android_vh_cpu_cgroup_css_alloc_early(parent);
 		/* This is early initialization for the top cgroup */
 		return &root_task_group.css;
 	}
@@ -10727,6 +10729,8 @@ cpu_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 	tg = sched_create_group(parent);
 	if (IS_ERR(tg))
 		return ERR_PTR(-ENOMEM);
+
+	trace_android_vh_cpu_cgroup_css_alloc(tg, parent_css);
 
 	return &tg->css;
 }
@@ -10768,6 +10772,8 @@ static void cpu_cgroup_css_free(struct cgroup_subsys_state *css)
 	 * Relies on the RCU grace period between css_released() and this.
 	 */
 	sched_unregister_group(tg);
+
+	trace_android_vh_cpu_cgroup_css_free(css);
 }
 
 #ifdef CONFIG_RT_GROUP_SCHED
@@ -11711,7 +11717,7 @@ struct cgroup_subsys cpu_cgrp_subsys = {
 	.early_init	= true,
 	.threaded	= true,
 };
-
+EXPORT_SYMBOL(cpu_cgrp_subsys);
 #endif	/* CONFIG_CGROUP_SCHED */
 
 void dump_cpu_task(int cpu)
