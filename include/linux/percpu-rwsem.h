af4c2dbb123c11e141387290035193ece5de34ee
zhujingpeng <zhujingpeng@vivo.com>
2024-04-10 20:37:42 +0800

    ANDROID: GKI: add OEM data to struct percpu_rw_semaphore
    Add an OEM data field to percpu_rw_semaphore to support OEM customized
    modification.
    
    Bug: 333679049
    Bug: 503208507
    
    Change-Id: I73ef65e228b427d51fd1b1e622c0f5210ff8eb9e
    Signed-off-by: zhujingpeng <zhujingpeng@vivo.com>
    (cherry picked from commit 397759af069550305c6b29c999226d4947e2ee9a)

--- include/linux/percpu-rwsem.h
+++ include/linux/percpu-rwsem.h
@@ -21,6 +21,7 @@ struct percpu_rw_semaphore {
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map	dep_map;
 #endif
+	ANDROID_OEM_DATA(1);
 };
 
 void _trace_android_vh_record_pcpu_rwsem_time_early(
