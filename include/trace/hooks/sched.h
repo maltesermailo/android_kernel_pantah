497f9e6e3ee6184b82a372c77526bd6a117bc672
maoshenshen <maoshenshen@oppo.com>
2024-04-12 10:56:56 +0800

    ANDROID: sched: add trace_android_vh_map_util_freq parameter
    Add "cpufreq_policy" and "need_freq_update" parameters to the vendor
    hook to enable frequency calculation in certain special cases related to
    OEM's frequency tuning policy.
    
    Bug: 183674818
    
    Signed-off-by: lijianzhong <lijianzhong@xiaomi.com>
    Change-Id: I232d2e1ae885d6736eca9e4709870f4272b4873d
    Signed-off-by: maoshenshen <maoshenshen@oppo.com>
    Signed-off-by: Qixia Yang <yangqixia2@oppo.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -405,10 +405,12 @@ DECLARE_RESTRICTED_HOOK(android_rvh_update_rt_rq_load_avg_internal,
 	TP_PROTO(u64 now, struct rq *rq, int running, int *ret),
 	TP_ARGS(now, rq, running, ret), 1);
 
+struct cpufreq_policy;
 DECLARE_HOOK(android_vh_map_util_freq,
 	TP_PROTO(unsigned long util, unsigned long freq,
-		unsigned long cap, unsigned long *next_freq),
-	TP_ARGS(util, freq, cap, next_freq));
+		unsigned long cap, unsigned long *next_freq, struct cpufreq_policy *policy,
+		bool *need_freq_update),
+	TP_ARGS(util, freq, cap, next_freq, policy, need_freq_update));
 
 /* macro versions of hooks are no longer required */
 
