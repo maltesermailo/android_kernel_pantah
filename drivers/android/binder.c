6869eaede4601f9cfba47cf13ffd7c8b11d199d2
Fuchun Liao <lfc@oppo.com>
2024-04-23 14:57:42 +0800

    ANDROID: binder: Add vendor hooks to implement the feature of binder_proc->fg_todo list
    1. The implementation logic of this feature
    1) In trace_android_vh_binder_list_add_work, record the sequence of work, work->android_oem_data1 = atomic64_inc_return(&binder_work_seq).
    2) In the binder_proc_transaction() function, for special synchronized
    binders, if a thread is not found, t->work will be added to proc->fg_todo in trace_android_vh_binder_special_task().
    3) binder_thread_read() -> trace_android_vh_binder_select_special_worklist()
    will select work to execute from proc->fg_todo according to a specific
    algorithm, such as selecting the thread->todo first, then selecting two
    proc->fg_todo works, and then one proc->todo work, while taking into account the sequence of work (work->android_oem_data1).
    proc->ANDROID_OEM_DATA(1) is used to generate proc->fg_todo list, and it
    was committed in the id 8a3740893d9957783493364b118282daa71695cc by other colleagues.
    
    2. trace_android_vh_binder_list_add_work: record the sequence of work.
    
    3. trace_android_vh_binder_has_proc_work_ilocked: Check if there is work
    in proc->fg_todo list, and if so, then binder_wakeup_proc_ilocked.
    
    4. trace_android_vh_binder_release_special_work: release the work on
    proc->fg_todo list.
    
    Bug: 334746237
    
    Change-Id: I855618ee129fe1bb5925b169d24eb2301ad90664
    Signed-off-by: Fuchun Liao <lfc@oppo.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -443,6 +443,7 @@ binder_enqueue_work_ilocked(struct binder_work *work,
 {
 	BUG_ON(target_list == NULL);
 	BUG_ON(work->entry.next && !list_empty(&work->entry));
+	trace_android_vh_binder_list_add_work(work, target_list);
 	list_add_tail(&work->entry, target_list);
 }
 
@@ -5310,6 +5311,7 @@ static int binder_ioctl_write_read(struct file *filp, unsigned long arg,
 	struct binder_proc *proc = filp->private_data;
 	void __user *ubuf = (void __user *)arg;
 	struct binder_write_read bwr;
+	bool has_special_work = false;
 
 	if (copy_from_user(&bwr, ubuf, sizeof(bwr))) {
 		ret = -EFAULT;
@@ -5341,7 +5343,9 @@ static int binder_ioctl_write_read(struct file *filp, unsigned long arg,
 					 filp->f_flags & O_NONBLOCK);
 		trace_binder_read_done(ret);
 		binder_inner_proc_lock(proc);
-		if (!binder_worklist_empty_ilocked(&proc->todo))
+		trace_android_vh_binder_has_proc_work_ilocked(
+			thread, true, &has_special_work);
+		if (!binder_worklist_empty_ilocked(&proc->todo) || has_special_work)
 			binder_wakeup_proc_ilocked(proc);
 		binder_inner_proc_unlock(proc);
 		if (ret < 0) {
@@ -6085,6 +6089,7 @@ static void binder_deferred_release(struct binder_proc *proc)
 	struct binder_context *context = proc->context;
 	struct rb_node *n;
 	int threads, nodes, incoming_refs, outgoing_refs, active_transactions;
+	struct list_head *special_list = NULL;
 
 	mutex_lock(&binder_procs_lock);
 	hlist_del(&proc->proc_node);
@@ -6157,6 +6162,9 @@ static void binder_deferred_release(struct binder_proc *proc)
 	binder_proc_unlock(proc);
 
 	binder_release_work(proc, &proc->todo);
+	trace_android_vh_binder_release_special_work(proc, &special_list);
+	if (special_list)
+		binder_release_work(proc, special_list);
 	binder_release_work(proc, &proc->delivered_death);
 
 	binder_debug(BINDER_DEBUG_OPEN_CLOSE,
