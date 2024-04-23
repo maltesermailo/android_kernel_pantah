c3cde98920695548b0264fdaea2decb7e72b2bc3
jujiang <jyu.jiang@vivo.corp-partner.google.com>
2024-04-23 09:55:43 +0800

    ANDROID: GKI: net: add vendor hooks net qos for gki purpose
    Add vendor hooks to support net qos policy feature:
    
    1.android_rvh_tcp_select_window
    We want to modify the tcp_select_window return value
    
    2.android_rvh_inet_sock_create; android_rvh_inet_sock_release
    We want to add a field when a inet sock is created
    
    3.android_vh_tcp_rtt_estimator
    To record the rtt of tcp connections for specified uids
    
    4.android_vh_build_skb_around
    To initialize the oem data field in the skb_shared_info structure
    
    Bug: 335081123
    
    Change-Id: Ibb22813c5004464416346d2c4c526d6cc5531fcc
    Signed-off-by: jujiang <jyu.jiang@vivo.corp-partner.google.com>

--- include/trace/hooks/net.h
+++ include/trace/hooks/net.h
@@ -17,6 +17,7 @@ DECLARE_HOOK(android_vh_ptype_head,
 struct nf_conn;
 struct sock;
 struct msghdr;
+struct sk_buff;
 DECLARE_RESTRICTED_HOOK(android_rvh_nf_conn_alloc,
 	TP_PROTO(struct nf_conn *nf_conn), TP_ARGS(nf_conn), 1);
 DECLARE_RESTRICTED_HOOK(android_rvh_nf_conn_free,
@@ -37,7 +38,18 @@ DECLARE_RESTRICTED_HOOK(android_rvh_udpv6_sendmsg,
 	TP_PROTO(struct sock *sk, struct msghdr *msg, size_t len), TP_ARGS(sk, msg, len), 1);
 DECLARE_RESTRICTED_HOOK(android_rvh_udpv6_recvmsg,
 	TP_PROTO(struct sock *sk, struct msghdr *msg, size_t len, int flags, int *addr_len), TP_ARGS(sk, msg, len, flags, addr_len), 1);
-
+DECLARE_RESTRICTED_HOOK(android_rvh_tcp_select_window,
+	TP_PROTO(struct sock *sk, u32 *new_win), TP_ARGS(sk, new_win), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_inet_sock_create,
+	TP_PROTO(struct sock *sk), TP_ARGS(sk), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_inet_sock_release,
+	TP_PROTO(struct sock *sk), TP_ARGS(sk), 1);
+DECLARE_HOOK(android_vh_tcp_rtt_estimator,
+	TP_PROTO(struct sock *sk, long mrtt_us), TP_ARGS(sk, mrtt_us));
+DECLARE_HOOK(android_vh_udp_enqueue_schedule_skb,
+	TP_PROTO(struct sock *sk, struct sk_buff *skb), TP_ARGS(sk, skb));
+DECLARE_HOOK(android_vh_build_skb_around,
+	TP_PROTO(struct sk_buff *skb), TP_ARGS(skb));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_NET_VH_H */
