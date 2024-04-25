e3038c9cdb4f80b5a819d1c84bc391121ee0b584
maoshenshen <maoshenshen@oppo.com>
2024-04-25 17:06:32 +0800

    ANDROID: vendor_hooks: Add hooks in reweight_entity
    Add hooks in reweight_entity to track the load information of task group.
    
    Bug: 336959329
    
    Change-Id: I0928d412facbc226998fe46f9529b7bcd0d25bf3
    Signed-off-by: maoshenshen <maoshenshen@oppo.com>
    Signed-off-by: Qixia Yang <yangqixia2@oppo.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -412,6 +412,9 @@ DECLARE_HOOK(android_vh_map_util_freq,
 		bool *need_freq_update),
 	TP_ARGS(util, freq, cap, next_freq, policy, need_freq_update));
 
+DECLARE_HOOK(android_vh_reweight_entity,
+	TP_PROTO(struct sched_entity *se),
+	TP_ARGS(se));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
