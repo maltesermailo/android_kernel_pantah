87e933aa8a2b9d4a24293f43f8aaa1759f658b8d
Terence Tritton <ttritton@google.com>
2024-04-26 08:24:24 +0000

    ANDROID: common-android-mainline canary test.
    Nothing patch to trigger presubmit builds.
    
    Do not merge.
    
    Bug: 332443283
    Change-Id: Ibec39aff380feb5de82d345666b79d8178e29486
    Signed-off-by: Terence Tritton <ttritton@google.com>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -2094,3 +2094,4 @@ static void __exit gadget_cfs_exit(void)
 	configfs_unregister_subsystem(&gadget_subsys);
 }
 module_exit(gadget_cfs_exit);
+//Blank line added for Android Canary build test of branch common-android-mainline
