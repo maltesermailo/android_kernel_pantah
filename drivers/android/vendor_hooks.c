37d8d0f9b56814ec1d4306469ca3f2e5a0aa64e5
Sooyong Suk <s.suk@samsung.com>
2024-04-26 15:00:40 +0900

    ANDROID: mm: add vendor hooks in madvise for swap entry
    Add vendor hooks in madvise for swap entry
    - android_vh_madvise_pageout_swap_entry
    - android_vh_madvise_swapin_walk_pmd_entry
    - android_vh_process_madvise_end
    
    removed android_vh_madvise_pageout_swap_entry()
    renamed android_vh_process_madvise()
    
    Bug: 284059805
    
    Change-Id: Ic389244e343737a583286c20cadb6774efd8890c
    Signed-off-by: Sooyong Suk <s.suk@samsung.com>
    (cherry picked from commit 7adccac4c2778efee2a6683f360bf7e685931cf4)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -285,3 +285,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_retry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_smaps_pte_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_smap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rebalance_anon_lru_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_swapin_walk_pmd_entry);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_process_madvise);
