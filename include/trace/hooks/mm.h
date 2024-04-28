118b000f022a5188de0e40c993f31c1c37693950
Dezhi Huang <huangdezhi@hihonor.com>
2024-04-28 16:12:21 +0800

    ANDROID: vendor_hooks: add hook to perform targeted memory management
    Add vendor_hook trace_android_vh_should_fault_around, allow vendor modules
    to skip the fault_around processing for less important processes.
    
    Bug: 337547131
    Change-Id: I792dca2038f5ad7cba1d212ef95407244958609d
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>
    (cherry picked from commit 65ebb00fe7977348d5fcfa58985c29181f3ec173)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -74,12 +74,14 @@ DECLARE_HOOK(android_vh_look_around,
 	TP_PROTO(struct page_vma_mapped_walk *pvmw, struct folio *folio,
 		struct vm_area_struct *vma, int *referenced),
 	TP_ARGS(pvmw, folio, vma, referenced));
-
 DECLARE_HOOK(android_vh_calc_alloc_flags,
 	TP_PROTO(gfp_t gfp_mask, unsigned int *alloc_flags,
 		bool *bypass),
 	TP_ARGS(gfp_mask, alloc_flags, bypass));
 
+DECLARE_HOOK(android_vh_should_fault_around,
+	TP_PROTO(struct vm_fault *vmf, bool *should_around),
+	TP_ARGS(vmf, should_around));
 DECLARE_HOOK(android_vh_slab_folio_alloced,
 	TP_PROTO(unsigned int order, gfp_t flags),
 	TP_ARGS(order, flags));
