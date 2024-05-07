d4e7802f55495ed35b9c62a1f29363cc93919f33
Dezhi Huang <huangdezhi@hihonor.com>
2024-05-07 19:57:53 +0800

    ANDROID: GKI: net: add vendor hook for TCP parameter adjustment
    When a socket is in established state and retransmit timeout occurs
    on the corresponding TCP connection, we will adjust some parameters
    of this TCP connection.
    
    Bug: 339166433
    Change-Id: I0a987c729ae6cdee045a8ca1af5ea879b290cbba
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>
    (cherry picked from commit b442d5b3ad2aea6f55d651ba793976ade5cc9d35)
    (cherry picked from commit 887cd274a15e4f39e71a03bb727671094abfe47b)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -97,3 +97,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_shmem_get_folio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tcp_write_timeout_estab_retrans);
