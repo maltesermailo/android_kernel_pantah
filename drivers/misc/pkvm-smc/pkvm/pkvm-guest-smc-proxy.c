7315403b44c4754063b2f96a14d3bde30b5ec28a
Sebastian Ene <sebastianene@google.com>
2024-05-14 10:13:02 +0000

    ANDROID: KVM: arm64: Add a hyp vendor module for the guest SMC proxy
    Introduce a new hypervisor vendor module which will allow us to trap and
    forward the guest SMCs.
    
    Bug: 333300537
    Change-Id: I8ce555bc9c1132a6b6b89c77ccaba10a558c22b1
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

---
+++ drivers/misc/pkvm-smc/pkvm/pkvm-guest-smc-proxy.c
@@ -0,0 +1,29 @@
+#include <asm/kvm_pkvm_module.h>
+#include <linux/arm-smccc.h>
+
+static const struct pkvm_module_ops *pkvm_ops;
+
+static bool guest_proxy_smc(struct arm_smccc_1_2_regs *r)
+{
+	struct arm_smccc_res res;
+
+	arm_smccc_1_1_smc(r->a0, r->a1, r->a2, r->a3,
+			  r->a4, r->a5, r->a6, r->a7,
+			  &res);
+	return res.a0 == 0;
+}
+
+int pkvm_guest_smc_proxy_hyp_init(const struct pkvm_module_ops *ops)
+{
+	pkvm_ops = ops;
+
+	return 0;
+}
+
+void pkvm_set_guest_smc_trapping_hyp_hvc(struct user_pt_regs *r)
+{
+	if (!pkvm_ops)
+		return;
+
+	pkvm_ops->register_guest_smc_handler(guest_proxy_smc, r->regs[0]);
+}
