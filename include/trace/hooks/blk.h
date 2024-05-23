32ef8f1b7c953bab7d7c758a0563c07e71c45c0f
Wang Jianzheng <11134417@vivo.corp-partner.google.com>
2024-05-23 20:44:35 +0800

    ANDROID: vendor_hooks: add vendor hooks for readahead bio
    Different scenarios may require different readahead parameters and readahead mechanisms, such as application start-up or game loading. Add vendor hook to f2fs_read_single_page, so we can modify some bio flags according to the struct readahead_control.
    
    Bug: 342340767
    
    Change-Id: I0665c4bdec945808860b73217cc0d128f49c2eb7
    Signed-off-by: Wang Jianzheng <11134417@vivo.corp-partner.google.com>

--- include/trace/hooks/blk.h
+++ include/trace/hooks/blk.h
@@ -27,6 +27,12 @@ DECLARE_HOOK(android_vh_do_new_mount_fc,
 	TP_PROTO(struct path *mountpoint, struct vfsmount *mnt),
 	TP_ARGS(mountpoint, mnt));
 
+struct readahead_control;
+
+DECLARE_HOOK(android_vh_f2fs_read_single_page,
+	TP_PROTO(unsigned int *op_flag, struct readahead_control *rac),
+	TP_ARGS(op_flag, rac));
+
 #endif /* _TRACE_HOOK_BLK_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
