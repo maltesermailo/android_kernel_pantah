50be128bab7b6c1bb9e3fae7bc6fa0486d82fc87
xiaofeng <xiaofeng5@xiaomi.com>
2024-05-30 11:10:47 +0800

    ANDROID: vendor_hooks:vendor hook for madvise_cold_or_pageout_pte_range.
    add vendor hook in madvise_cold_or_pageout_pte_range to control the
    pages to be reclaimed more fine-grained.
    
    Bug: 284808098
    Signed-off-by: zhouwenhao <zhouwenhao@xiaomi.com>
    Change-Id: I298fde436df192cea9b1541d857f3a46808e06f2
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -376,3 +376,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_vmalloc_stack);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_stack_hash);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_cold_pageout_skip);
