a4a12129d665f012a5a8100485baf862ad2c3bea
Alice Ryhl <aliceryhl@google.com>
2024-06-07 13:15:02 +0000

    ANDROID: rust_binder: clean up C headers
    We are going to be defining a bunch of tracepoints and vendorhooks that
    will be written in C, but need to access the Rust Binder driver. To
    prepare for that, we clean up the relevant header files a bit.
    
    This also removes the reliance on binder_internal.h in rust_binderfs.c,
    which is not desirable as binder_internal.h declares a bunch of types
    from the C driver.
    
    Change-Id: Ic451898885d891ecbb1c90666eaaa51f55dc64c9
    Bug: 344571420
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- include/linux/rust_binder.h
+++ include/linux/rust_binder.h
@@ -2,15 +2,36 @@
 #ifndef _LINUX_RUST_BINDER_H
 #define _LINUX_RUST_BINDER_H
 
+#include <linux/seq_file.h>
+#include <uapi/linux/android/binder.h>
 #include <uapi/linux/android/binderfs.h>
 
 /*
- * This typedef is used for Rust binder driver instances. The driver object is
- * completely opaque from C and can only be accessed via calls into Rust, so we
- * use a typedef.
+ * This symbol is exposed by `rust_binderfs.c` and exists here so that Rust
+ * Binder can call it. All other symbols are exported by the Rust Binder driver.
+ */
+int init_rust_binderfs(void);
+
+/*
+ * The internal data types in the Rust Binder driver are opaque to C, so we use
+ * void pointer typedefs for these types.
  */
 typedef void *rust_binder_device;
 
-int init_rust_binderfs(void);
+int rust_binder_stats_show(struct seq_file *m, void *unused);
+DEFINE_SHOW_ATTRIBUTE(rust_binder_stats);
+
+int rust_binder_state_show(struct seq_file *m, void *unused);
+DEFINE_SHOW_ATTRIBUTE(rust_binder_state);
+
+int rust_binder_transactions_show(struct seq_file *m, void *unused);
+DEFINE_SHOW_ATTRIBUTE(rust_binder_transactions);
+
+int rust_binder_transaction_log_show(struct seq_file *m, void *unused);
+DEFINE_SHOW_ATTRIBUTE(rust_binder_transaction_log);
+
+extern const struct file_operations rust_binder_fops;
+rust_binder_device rust_binder_new_device(char *name);
+void rust_binder_remove_device(rust_binder_device device);
 
 #endif
