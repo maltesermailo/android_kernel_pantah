f6f88a64ed0c937710785aa22c4f8594a701fad5
Wang Jianzheng <11134417@vivo.corp-partner.google.com>
2024-06-07 11:18:44 +0800

    ANDROID: vendor hooks: add vendor hooks for do_new_mount
    Add a vendor hook for do_new_mount_fc is aimed at categorizing the
    properties according to the path. And making these properties more
    easily accessible.This will help us to customize readahead operations, improving the speed of App startup and game loading.
    
    Bug: 345590666
    
    Change-Id: I913bbb9bd4c3efd5f4c7d101056d301855881cc2
    Signed-off-by: Wang Jianzheng <11134417@vivo.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -420,3 +420,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_amu_fie);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_new_mount_fc);
