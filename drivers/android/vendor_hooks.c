73be7b7005d3bb3f45c3436bc64ac5c4a60cd87b
Wang Jianzheng <11134417@vivo.corp-partner.google.com>
2024-06-07 11:18:44 +0800

    ANDROID: vendor hooks: add vendor hooks for do_new_mount
    Add a vendor hook for do_new_mount_fc is aimed at categorizing the
    properties according to the path. And making these properties more
    easily accessible.This will help us to customize readahead operations, improving the speed of App startup and game loading.
    
    Bug: 345590666
    
    Change-Id: I913bbb9bd4c3efd5f4c7d101056d301855881cc2
    Signed-off-by: Wang Jianzheng <11134417@vivo.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -70,6 +70,7 @@
 #include <trace/hooks/user.h>
 #include <trace/hooks/fuse.h>
 #include <trace/hooks/psi.h>
+#include <trace/hooks/blk.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -400,3 +401,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_cold_pageout_skip);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_event);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_group);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_sdio_pm_flag_set);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_new_mount_fc);
