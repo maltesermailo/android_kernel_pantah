7e754a441cda03d1521180177852e0e920a0b4e2
Alice Ryhl <aliceryhl@google.com>
2024-06-12 12:16:36 +0000

    ANDROID: rust_binder: add set_priority tracepoint
    Bug: 348589263
    Change-Id: Ib1778312bc0f73c471430981a5b0202ea6b5653c
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/trace.rs
+++ drivers/android/binder/trace.rs
@@ -4,10 +4,14 @@
 
 use crate::{thread::Thread, transaction::Transaction};
 
-use kernel::bindings::{rust_binder_thread, rust_binder_transaction};
+use kernel::bindings::{rust_binder_thread, rust_binder_transaction, task_struct};
+use kernel::task::Task;
 use kernel::tracepoint::declare_trace;
 
+use core::ffi::c_int;
+
 declare_trace! {
+    fn rust_binder_set_priority(thread: *mut task_struct, desired_prio: c_int, new_prio: c_int);
     fn rust_binder_wait_for_work(proc_work: bool, transaction_stack: bool, thread_todo: bool);
     fn rust_binder_transaction(reply: bool, t: rust_binder_transaction);
     fn rust_binder_transaction_received(t: rust_binder_transaction);
@@ -22,6 +26,12 @@ fn raw_transaction(t: &Transaction) -> rust_binder_transaction {
 #[inline]
 fn raw_thread(t: &Thread) -> rust_binder_thread {
     t as *const Thread as rust_binder_thread
+}
+
+#[inline]
+pub(crate) fn trace_set_priority(thread: &Task, desired_prio: c_int, new_prio: c_int) {
+    // SAFETY: The pointer to the task is valid for the duration of this call.
+    unsafe { rust_binder_set_priority(thread.as_raw(), desired_prio, new_prio) }
 }
 
 #[inline]
