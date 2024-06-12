e9117e821fc76828114c943aae87fe6f8bd0643b
Alice Ryhl <aliceryhl@google.com>
2024-06-12 13:32:31 +0000

    ANDROID: rust_binder: add tracepoints for exiting from functions
    Also adjusts the error handling slightly to make it more convenient to
    introduce the tracepoints.
    
    Bug: 344571420
    Change-Id: I86dd41d03668356207df787fb21ca4183f83f416
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/trace.rs
+++ drivers/android/binder/trace.rs
@@ -5,12 +5,16 @@
 use crate::{thread::Thread, transaction::Transaction};
 
 use kernel::bindings::{rust_binder_thread, rust_binder_transaction, task_struct};
+use kernel::error::Result;
 use kernel::tracepoint::declare_trace;
 
-use core::ffi::{c_uint, c_ulong};
+use core::ffi::{c_int, c_uint, c_ulong};
 
 declare_trace! {
     fn rust_binder_ioctl(cmd: c_uint, arg: c_ulong);
+    fn rust_binder_ioctl_done(ret: c_int);
+    fn rust_binder_write_done(ret: c_int);
+    fn rust_binder_read_done(ret: c_int);
     fn rust_binder_set_priority(thread: *mut task_struct, desired_prio: c_uint, new_prio: c_uint);
     fn rust_binder_wait_for_work(proc_work: bool, transaction_stack: bool, thread_todo: bool);
     fn rust_binder_transaction(reply: bool, t: rust_binder_transaction);
@@ -32,6 +36,24 @@ fn raw_thread(t: &Thread) -> rust_binder_thread {
 pub(crate) fn trace_ioctl(cmd: u32, arg: u64) {
     // SAFETY: Always safe to call.
     unsafe { rust_binder_ioctl(cmd, arg) }
+}
+
+#[inline]
+pub(crate) fn trace_ioctl_done(ret: Result) {
+    // SAFETY: Always safe to call.
+    unsafe { rust_binder_ioctl_done(ret.map(|err| err.to_errno()).unwrap_or(0)) }
+}
+
+#[inline]
+pub(crate) fn trace_read_done(ret: Result) {
+    // SAFETY: Always safe to call.
+    unsafe { rust_binder_read_done(ret.map(|err| err.to_errno()).unwrap_or(0)) }
+}
+
+#[inline]
+pub(crate) fn trace_write_done(ret: Result) {
+    // SAFETY: Always safe to call.
+    unsafe { rust_binder_write_done(ret.map(|err| err.to_errno()).unwrap_or(0)) }
 }
 
 #[inline]
