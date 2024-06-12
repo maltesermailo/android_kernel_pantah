8f7ad3f8aaf6b2970ddf17fcb4246970c30b23cc
Alice Ryhl <aliceryhl@google.com>
2024-06-12 13:11:17 +0000

    ANDROID: rust_binder: add ioctl tracepoint
    Emit a trace whenever a binder ioctl is called.
    
    Bug: 348589263
    Change-Id: I624b7fe0e873bdeec942eef2e249f9ae536e7f7e
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- include/trace/events/rust_binder.h
+++ include/trace/events/rust_binder.h
@@ -11,6 +11,21 @@
 
 #include <linux/tracepoint.h>
 
+TRACE_EVENT(rust_binder_ioctl,
+	TP_PROTO(unsigned int cmd, unsigned long arg),
+	TP_ARGS(cmd, arg),
+
+	TP_STRUCT__entry(
+		__field(unsigned int, cmd)
+		__field(unsigned long, arg)
+	),
+	TP_fast_assign(
+		__entry->cmd = cmd;
+		__entry->arg = arg;
+	),
+	TP_printk("cmd=0x%x arg=0x%lx", __entry->cmd, __entry->arg)
+);
+
 TRACE_EVENT(rust_binder_set_priority,
 	TP_PROTO(struct task_struct *thread, int desired_prio, int new_prio),
 	TP_ARGS(thread, desired_prio, new_prio),
