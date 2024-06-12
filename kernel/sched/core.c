d3d6c351046d23c0fc779885b258bc861fad79bb
John Stultz <jstultz@google.com>
2024-06-12 16:19:40 -0700

    RFC: sched: Rework task_sched_runtime to avoid calling update_rq_clock
    I recently got a bug report that
    clock_gettime(CLOCK_THREAD_CPUTIME_ID,...) had regressed between
    5.10 and 6.1. Its not a huge regression in absolute time
    (~30-40ns), but is >10% change.
    
    I narrowed the cause down to the addition of
    psi_account_irqtime() in update_rq_clock_task(), in commit
    52b1364ba0b1 ("sched/psi: Add PSI_IRQ to track IRQ/SOFTIRQ
    pressure")
    
    So that explains the behavior change, but it also seems odd that
    we're doing psi irq accounting from a syscall that is just
    trying to read the thread's cputime.
    
    Thinking about it more, it seems the re-use of update_rq_clock()
    to handle accounting for any in-progress time for the current
    task has the potential for side effects and unnecessary work.
    
    So instead rework the logic so we calculate the current cpu
    runtime in a read-only fashion.
    
    This has the side benefit of improving
    clock_gettime(CLOCK_THREAD_CPUTIME_ID,...) performance by ~12%
    over the behavior in 5.10, and ~21% over the 6.1 behavior.
    
    NOTE: I'm not 100% sure this is correct yet. There may be some
    edge cases I've overlooked, so I'd greatly appreciate any
    review or feedback.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: kernel-team@android.com
    Signed-off-by: John Stultz <jstultz@google.com>
    Bug: 343748421
    Change-Id: I599adaf1f66bab679788df61863198668801ed46

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -690,16 +690,11 @@ EXPORT_SYMBOL_GPL(task_rq_lock);
  * RQ-clock updating methods:
  */
 
-static void update_rq_clock_task(struct rq *rq, s64 delta)
-{
-/*
- * In theory, the compile should just see 0 here, and optimize out the call
- * to sched_rt_avg_update. But I don't trust it...
- */
-	s64 __maybe_unused steal = 0, irq_delta = 0;
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
-	irq_delta = irq_time_read(cpu_of(rq)) - rq->prev_irq_time;
+static inline s64 get_irq_delta(struct rq *rq, s64 delta)
+{
+	s64 irq_delta = irq_time_read(cpu_of(rq)) - rq->prev_irq_time;
 
 	/*
 	 * Since irq_time is only updated on {soft,}irq_exit, we might run into
@@ -719,18 +714,55 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 	if (irq_delta > delta)
 		irq_delta = delta;
 
+	return irq_delta;
+}
+#else
+static inline s64 get_irq_delta(struct rq *rq, s64 delta)
+{
+	return 0;
+}
+#endif
+
+#ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
+static inline s64 get_steal_time(struct rq *rq, s64 delta)
+{
+	s64 steal;
+
+	if (!static_key_false(&paravirt_steal_rq_enabled))
+		return 0;
+
+	steal = paravirt_steal_clock(cpu_of(rq));
+	steal -= rq->prev_steal_time_rq;
+
+	if (unlikely(steal > delta))
+		steal = delta;
+
+	return steal;
+}
+#else
+static inline s64 get_steal_time(struct rq *rq, s64 delta)
+{
+	return 0;
+}
+#endif
+
+static void update_rq_clock_task(struct rq *rq, s64 delta)
+{
+/*
+ * In theory, the compile should just see 0 here, and optimize out the call
+ * to sched_rt_avg_update. But I don't trust it...
+ */
+	s64 __maybe_unused steal = 0, irq_delta = 0;
+
+#ifdef CONFIG_IRQ_TIME_ACCOUNTING
+	irq_delta = get_irq_delta(rq, delta);
 	rq->prev_irq_time += irq_delta;
 	delta -= irq_delta;
 	psi_account_irqtime(rq->curr, irq_delta);
 #endif
 #ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
 	if (static_key_false((&paravirt_steal_rq_enabled))) {
-		steal = paravirt_steal_clock(cpu_of(rq));
-		steal -= rq->prev_steal_time_rq;
-
-		if (unlikely(steal > delta))
-			steal = delta;
-
+		steal = get_steal_time(rq, delta);
 		rq->prev_steal_time_rq += steal;
 		delta -= steal;
 	}
@@ -5587,6 +5619,7 @@ unsigned long long task_sched_runtime(struct task_struct *p)
 {
 	struct rq_flags rf;
 	struct rq *rq;
+	s64 delta_exec = 0;
 	u64 ns;
 
 #if defined(CONFIG_64BIT) && defined(CONFIG_SMP)
@@ -5612,11 +5645,11 @@ unsigned long long task_sched_runtime(struct task_struct *p)
 	 * thread, breaking clock_gettime().
 	 */
 	if (task_current(rq, p) && task_on_rq_queued(p)) {
-		prefetch_curr_exec_start(p);
-		update_rq_clock(rq);
-		p->sched_class->update_curr(rq);
+		delta_exec = sched_clock_cpu(cpu_of(rq)) - p->se.exec_start;
+		delta_exec -= get_irq_delta(rq, delta_exec);
+		delta_exec -= get_steal_time(rq, delta_exec);
 	}
-	ns = p->se.sum_exec_runtime;
+	ns = p->se.sum_exec_runtime + delta_exec;
 	task_rq_unlock(rq, p, &rf);
 
 	return ns;
