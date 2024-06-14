3a709c333f08f4e1b4383190844d96d09ce31176
Erik Staats <estaats@google.com>
2024-06-14 19:24:01 +0000

    ANDROID: Add test struct
    Change-Id: I8518ff093dbce58de78ea2d4fccb1337d1170712
    Signed-off-by: Erik Staats <estaats@google.com>

--- kernel/acct.c
+++ kernel/acct.c
@@ -75,6 +75,10 @@ static int acct_parm[3] = {4, 2, 30};
 #define SUSPEND		(acct_parm[1])	/* <foo% free space - suspend */
 #define ACCT_TIMEOUT	(acct_parm[2])	/* foo second timeout between checks */
 
+struct test {
+       __u8 data[0];
+};
+
 #ifdef CONFIG_SYSCTL
 static struct ctl_table kern_acct_table[] = {
 	{
