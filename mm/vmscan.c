8f65bfbf9f7089e20b8a0ab36634ad15dcc7a2c2
Yuxuan Yan <yanyuxuan3@xiaomi.corp-partner.google.com>
2024-06-20 14:44:17 +0800

    ANDROID: GKI: add vendor hooks to android14-6.1
    follow android15-6.6, add two vendor hooks:
        android_vh_page_should_be_protected():protect pages from memory reclaim.
        android_vh_page_referenced_check_bypass():bypass rmap in active list shrink.
    
    The new vendor data field in scan_control are used to track how many pages are protected in current reclaim and the "protected / scanned" rate. These parameters are useful for understanding the impact of page protection operations on LRU and reclaim, helping us make better decisions.Since ANDROID_VENDOR_DATA(1) is already used by android_vh_should_continue_reclaim() and android_vh_modify_scan_control(), we need a new vendor data field. So,create a ANDROID_VENDOR_DATA_ARRAY here.
    
    Bug: 348285765
    
    Change-Id: I49567a4b1f978821a94da0a8339b2b8fdfd52daf
    Signed-off-by: Yuxuan Yan <yanyuxuan3@xiaomi.corp-partner.google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -177,6 +177,7 @@ struct scan_control {
 	/* for recording the reclaimed slab by now */
 	struct reclaim_state reclaim_state;
 	ANDROID_VENDOR_DATA(1);
+	ANDROID_VENDOR_DATA(2);
 };
 
 #ifdef ARCH_HAS_PREFETCHW
@@ -1477,6 +1478,10 @@ static enum folio_references folio_check_references(struct folio *folio,
 	unsigned long vm_flags;
 	int ret = 0;
 
+#ifdef CONFIG_ANDROID_VENDOR_OEM_DATA
+	trace_android_vh_page_should_be_protected(folio, sc->nr_scanned,
+		sc->priority, &sc->android_vendor_data2, &ret);
+#endif
 	trace_android_vh_check_folio_look_around_ref(folio, &ret);
 	if (ret)
 		return ret;
@@ -2637,6 +2642,8 @@ static void shrink_active_list(unsigned long nr_to_scan,
 	unsigned nr_rotated = 0;
 	int file = is_file_lru(lru);
 	struct pglist_data *pgdat = lruvec_pgdat(lruvec);
+	int should_protect = 0;
+	bool bypass = false;
 
 	lru_add_drain();
 
@@ -2673,6 +2680,19 @@ static void shrink_active_list(unsigned long nr_to_scan,
 			}
 		}
 
+#ifdef CONFIG_ANDROID_VENDOR_OEM_DATA
+		trace_android_vh_page_should_be_protected(folio, sc->nr_scanned,
+			sc->priority, &sc->android_vendor_data2, &should_protect);
+#endif
+		if (unlikely(should_protect)) {
+			nr_rotated += folio_nr_pages(folio);
+			list_add(&folio->lru, &l_active);
+			continue;
+		}
+
+		trace_android_vh_page_referenced_check_bypass(folio, nr_to_scan, lru, &bypass);
+		if (bypass)
+			goto skip_folio_referenced;
 		/* Referenced or rmap lock contention: rotate */
 		if (folio_referenced(folio, 0, sc->target_mem_cgroup,
 				     &vm_flags) != 0) {
@@ -2691,7 +2711,7 @@ static void shrink_active_list(unsigned long nr_to_scan,
 				continue;
 			}
 		}
-
+skip_folio_referenced:
 		folio_clear_active(folio);	/* we are de-activating */
 		folio_set_workingset(folio);
 		list_add(&folio->lru, &l_inactive);
