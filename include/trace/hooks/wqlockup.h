446d32960050a02aac068d27b1670b768f735aa6
Yang Yang <yang.yang@vivo.com>
2024-06-20 17:35:32 +0800

    ANDROID: vendor_hooks: add hook in alloc_and_link_pwqs()
    This hook is used to apply specific flags and attrs to workqueue_struct.
    
    Bug: 348321531
    Bug: 511306194
    Bug: 512272502
    Change-Id: I877eae65a8d02719e216ab2df6e627e360e11078
    Signed-off-by: Yang Yang <yang.yang@vivo.com>
    (cherry picked from commit f437bda7c408ba80a09dccd030abcff21641207c)
    Signed-off-by: ying zuxin <11154159@vivo.com>

--- include/trace/hooks/wqlockup.h
+++ include/trace/hooks/wqlockup.h
@@ -10,6 +10,11 @@
  * Following tracepoints are not exported in tracefs and provide a
  * mechanism for vendor modules to hook and extend functionality
  */
+
+DECLARE_RESTRICTED_HOOK(android_rvh_alloc_and_link_pwqs,
+	TP_PROTO(struct workqueue_struct *wq, int *ret, bool *skip),
+	TP_ARGS(wq, ret, skip), 1);
+
 DECLARE_HOOK(android_vh_wq_lockup_pool,
 	TP_PROTO(int cpu, unsigned long pool_ts),
 	TP_ARGS(cpu, pool_ts));
