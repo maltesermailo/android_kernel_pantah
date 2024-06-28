cb4bae940db639597348b7df1bf3cb215d4c8d78
Alice Ryhl <aliceryhl@google.com>
2024-06-28 13:26:03 +0000

    ANDROID: rust_binder: add page_range tracepoints
    This tracepoint can be used to debug allocation and deallocation of
    pages in the binder mmap.
    
    Bug: 348589263
    Change-Id: I8dd2c84240d28d681c6b634c6427a8d2eed1606b
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/trace.rs
+++ drivers/android/binder/trace.rs
@@ -9,7 +9,7 @@ use kernel::bindings::{
     rust_binder_transaction, task_struct,
 };
 use kernel::error::Result;
-use kernel::task::Task;
+use kernel::task::{Pid, Task};
 use kernel::tracepoint::declare_trace;
 
 use core::ffi::{c_int, c_uint, c_ulong};
@@ -33,6 +33,17 @@ declare_trace! {
     fn rust_binder_transaction_buffer_release(debug_id: c_int);
     fn rust_binder_transaction_failed_buffer_release(debug_id: c_int);
     fn rust_binder_transaction_update_buffer_release(debug_id: c_int);
+    fn rust_binder_update_page_range(pid: c_int, allocate: bool, start: usize, end: usize);
+    fn rust_binder_alloc_lru_start(pid: c_int, page_index: usize);
+    fn rust_binder_alloc_lru_end(pid: c_int, page_index: usize);
+    fn rust_binder_free_lru_start(pid: c_int, page_index: usize);
+    fn rust_binder_free_lru_end(pid: c_int, page_index: usize);
+    fn rust_binder_alloc_page_start(pid: c_int, page_index: usize);
+    fn rust_binder_alloc_page_end(pid: c_int, page_index: usize);
+    fn rust_binder_unmap_user_start(pid: c_int, page_index: usize);
+    fn rust_binder_unmap_user_end(pid: c_int, page_index: usize);
+    fn rust_binder_unmap_kernel_start(pid: c_int, page_index: usize);
+    fn rust_binder_unmap_kernel_end(pid: c_int, page_index: usize);
 
     fn android_vh_rust_binder_set_priority(t: rust_binder_transaction, task: *mut task_struct);
     fn android_vh_rust_binder_restore_priority(task: *mut task_struct);
@@ -173,4 +184,35 @@ pub(crate) fn trace_transaction_failed_buffer_release(debug_id: usize) {
 pub(crate) fn trace_transaction_update_buffer_release(debug_id: usize) {
     // SAFETY: Always safe to call.
     unsafe { rust_binder_transaction_update_buffer_release(debug_id as c_int) }
+}
+
+#[inline]
+pub(crate) fn trace_update_page_range(pid: Pid, allocate: bool, start: usize, end: usize) {
+    // SAFETY: Always safe to call.
+    unsafe { rust_binder_update_page_range(pid as c_int, allocate, start, end) }
+}
+
+macro_rules! define_wrapper_lru_page_class {
+    ($(fn $name:ident;)*) => {$(
+        kernel::macros::paste! {
+            #[inline]
+            pub(crate) fn [< trace_ $name >](pid: Pid, page_index: usize) {
+                // SAFETY: Always safe to call.
+                unsafe { [< rust_binder_ $name >](pid as c_int, page_index) }
+            }
+        }
+    )*}
+}
+
+define_wrapper_lru_page_class! {
+    fn alloc_lru_start;
+    fn alloc_lru_end;
+    fn free_lru_start;
+    fn free_lru_end;
+    fn alloc_page_start;
+    fn alloc_page_end;
+    fn unmap_user_start;
+    fn unmap_user_end;
+    fn unmap_kernel_start;
+    fn unmap_kernel_end;
 }
