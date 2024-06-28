33e854ad4d76612748a26ac900d83bce03935e9b
Christian Loehle <christian.loehle@arm.com>
2024-06-28 10:59:53 +0100

    BACKPORT: (partial) Revert: "cpuidle: teo: Introduce util-awareness"
    This (partially) reverts commit 9ce0f7c4bc64d820b02a1c53f7e8dba9539f942b.
    
    Util-awareness was reported to be too aggressive in selecting shallower
    states. Additionally a single threshold was found to not be suitable
    for reasoning about sleep length as, for all practical purposes,
    almost arbitrary sleep lengths are still possible for any load value.
    
    Fixes: 9ce0f7c4bc64 ("cpuidle: teo: Introduce util-awareness")
    Link: https://patch.msgid.link/20240628095955.34096-2-christian.loehle@arm.com
    Reported-by: Qais Yousef <qyousef@layalina.io>
    Reported-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Qais Yousef <qyousef@layalina.io>
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Change-Id: Id0965b5d855d5c656656987222a3e39c22774af5
    Signed-off-by: Christian Loehle <christian.loehle@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 0a2998fa48f0b00c20628b02b80bd7fa3582626d)
    [jstultz: Needed some tweaks to continue to work with commit
    59ae90d93ed5 ("ANDROID: cpuidle: teo: Export a function that allows
    modifying util_threshold"). Preserved KABI, but semantics of function
    changes]
    Signed-off-by: John Stultz <jstultz@google.com>

--- drivers/cpuidle/governors/teo.c
+++ drivers/cpuidle/governors/teo.c
@@ -2,13 +2,8 @@
 /*
  * Timer events oriented CPU idle governor
  *
- * TEO governor:
  * Copyright (C) 2018 - 2021 Intel Corporation
  * Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
- *
- * Util-awareness mechanism:
- * Copyright (C) 2022 Arm Ltd.
- * Author: Kajetan Puchalski <kajetan.puchalski@arm.com>
  */
 
 /**
@@ -95,38 +90,11 @@
  *      select the given idle state instead of the candidate one.
  *
  * 3. By default, select the candidate state.
- *
- * Util-awareness mechanism:
- *
- * The idea behind the util-awareness extension is that there are two distinct
- * scenarios for the CPU which should result in two different approaches to idle
- * state selection - utilized and not utilized.
- *
- * In this case, 'utilized' means that the average runqueue util of the CPU is
- * above a certain threshold.
- *
- * When the CPU is utilized while going into idle, more likely than not it will
- * be woken up to do more work soon and so a shallower idle state should be
- * selected to minimise latency and maximise performance. When the CPU is not
- * being utilized, the usual metrics-based approach to selecting the deepest
- * available idle state should be preferred to take advantage of the power
- * saving.
- *
- * In order to achieve this, the governor uses a utilization threshold.
- * The threshold is computed per-CPU as a percentage of the CPU's capacity
- * by bit shifting the capacity value. Based on testing, the shift of 6 (~1.56%)
- * seems to be getting the best results.
- *
- * Before selecting the next idle state, the governor compares the current CPU
- * util to the precomputed util threshold. If it's below, it defaults to the
- * TEO metrics mechanism. If it's above, the closest shallower idle state will
- * be selected instead, as long as is not a polling state.
  */
 
 #include <linux/cpuidle.h>
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>
 #include <linux/sched/clock.h>
 #include <linux/sched/topology.h>
 #include <linux/tick.h>
@@ -194,23 +162,6 @@ void teo_cpu_set_util_threshold(int cpu, unsigned long util)
 	cpu_data->util_threshold = util;
 }
 EXPORT_SYMBOL_GPL(teo_cpu_set_util_threshold);
-
-/**
- * teo_cpu_is_utilized - Check if the CPU's util is above the threshold
- * @cpu: Target CPU
- * @cpu_data: Governor CPU data for the target CPU
- */
-#ifdef CONFIG_SMP
-static bool teo_cpu_is_utilized(int cpu, struct teo_cpu *cpu_data)
-{
-	return sched_cpu_util(cpu) > cpu_data->util_threshold;
-}
-#else
-static bool teo_cpu_is_utilized(int cpu, struct teo_cpu *cpu_data)
-{
-	return false;
-}
-#endif
 
 /**
  * teo_update - Update CPU metrics after wakeup.
@@ -364,7 +315,6 @@ static int teo_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 	unsigned int hit_sum = 0;
 	int constraint_idx = 0;
 	int idx0 = 0, idx = -1;
-	bool cpu_utilized;
 	int prev_intercept_idx;
 	s64 duration_ns;
 	int i;
@@ -389,32 +339,6 @@ static int teo_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 
 	if (!dev->states_usage[0].disable)
 		idx = 0;
-
-	cpu_utilized = teo_cpu_is_utilized(dev->cpu, cpu_data);
-	/*
-	 * If the CPU is being utilized over the threshold and there are only 2
-	 * states to choose from, the metrics need not be considered, so choose
-	 * the shallowest non-polling state and exit.
-	 */
-	if (drv->state_count < 3 && cpu_utilized) {
-		/*
-		 * If state 0 is enabled and it is not a polling one, select it
-		 * right away unless the scheduler tick has been stopped, in
-		 * which case care needs to be taken to leave the CPU in a deep
-		 * enough state in case it is not woken up any time soon after
-		 * all.  If state 1 is disabled, though, state 0 must be used
-		 * anyway.
-		 */
-		if ((!idx && !(drv->states[0].flags & CPUIDLE_FLAG_POLLING) &&
-		    teo_state_ok(0, drv)) || dev->states_usage[1].disable) {
-			idx = 0;
-			goto out_tick;
-		}
-		/* Assume that state 1 is not a polling one and use it. */
-		idx = 1;
-		duration_ns = drv->states[1].target_residency_ns;
-		goto end;
-	}
 
 	/* Compute the sums of metrics for early wakeup pattern detection. */
 	for (i = 1; i < drv->state_count; i++) {
@@ -537,18 +461,6 @@ static int teo_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 	 */
 	if (idx > constraint_idx)
 		idx = constraint_idx;
-
-	/*
-	 * If the CPU is being utilized over the threshold, choose a shallower
-	 * non-polling state to improve latency, unless the scheduler tick has
-	 * been stopped already and the shallower state's target residency is
-	 * not sufficiently large.
-	 */
-	if (cpu_utilized) {
-		i = teo_find_shallower_state(drv, dev, idx, KTIME_MAX, true);
-		if (teo_state_ok(i, drv))
-			idx = i;
-	}
 
 	/*
 	 * Skip the timers check if state 0 is the current candidate one,
