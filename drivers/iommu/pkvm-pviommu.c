f3c9ba1969ee0c6e749ea3e498ec4204a416d90c
Mostafa Saleh <smostafa@google.com>
2024-07-08 10:03:06 +0000

    [DONOTSUBMIT] ANDROID: drivers: iommu: pviommu: request device from pKVM
    Ideally, pvmfw will request all device before booting a pvm,
    boot for development, it might no be there, so request the
    device from probe_device, this is harmless if done twice.
    
    Change-Id: I881b2c68a2c591ddff726b5ed5664db357d3705c
    Signed-off-by: Mostafa Saleh <smostafa@google.com>
    Signed-off-by: Pierre-Clément Tosi <ptosi@google.com>

--- drivers/iommu/pkvm-pviommu.c
+++ drivers/iommu/pkvm-pviommu.c
@@ -323,11 +323,34 @@ static struct pviommu *pviommu_get_by_fwnode(struct fwnode_handle *fwnode)
 
 static struct iommu_ops pviommu_ops;
 
+static int pviommu_request_device(struct device *dev)
+{
+	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
+	struct pviommu *pv = pviommu_get_by_fwnode(fwspec->iommu_fwnode);
+	int i;
+	u32 sid;
+	struct arm_smccc_res res;
+	int ret = 0;
+
+	for (i = 0; i < fwspec->num_ids; i++) {
+		sid = fwspec->ids[i];
+		arm_smccc_1_1_hvc(ARM_SMCCC_VENDOR_HYP_KVM_DEV_REQ_DMA_FUNC_ID,
+				  pv->id, sid, 0, 0, 0, 0, &res);
+		if (res.a0) {
+			dev_err(dev, "Failed to request device (sid = 0x%x) from the hypervisor\n", sid);
+			break;
+		}
+	}
+
+	return ret;
+}
+
 static struct iommu_device *pviommu_probe_device(struct device *dev)
 {
 	struct pviommu_master *master;
 	struct pviommu *pv = NULL;
 	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
+	int ret;
 
 	if (!fwspec)
 		return ERR_PTR(-ENODEV);
@@ -335,6 +358,14 @@ static struct iommu_device *pviommu_probe_device(struct device *dev)
 	pv = pviommu_get_by_fwnode(fwspec->iommu_fwnode);
 	if (!pv)
 		return ERR_PTR(-ENODEV);
+
+	/*
+	 * Request device from hypervisor, ideally done from firmware before booting,
+	 * but just incase, re-request it.
+	 */
+	ret = pviommu_request_device(dev);
+	if (ret)
+		return ERR_PTR(ret);
 
 	master = kzalloc(sizeof(*master), GFP_KERNEL);
 	if (!master)
