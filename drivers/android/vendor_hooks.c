bcc231011f312e5029836d1426784f2032317bfb
linjunjie1 <linjunjie1@xiaomi.corp-partner.google.com>
2024-07-18 17:41:58 +0800

    ANDROID: vendor_hooks:vendor hook for perf
    Using the performance metric tracker, we can analyze detailed performance metrics during program execution,
    providing data analysis reference for energy efficiency optimization.
    This functionality requires the addition of perf hooks.
    
    1. why need hook trace_android_rvh_armv8pmu_counter_overflowed?
    The performance metric tracker will create multiple perf counters via perf_event_create_kernel_counter and
    select a counter setting sample period. We will take the sample period as the statistical period. When the
    sample period expires, it need to notify the performance metric tracker to collect pmu counter statistics
    in this period. How to notify the Performance metric tracker? It rely on this hook, which is notified when
    the arm pmu counter overflow.
    
    2. why need hook trace_android_rvh_perf_rotate_context?
    When the pmu context is rotated, it needs this hook to notify the performance metric tracker to end the
    pmu counter statistics for this period.
    
    Bug: 353856690
    Change-Id: Ie08ba86339159caeebf5252056e55cfee53d3bc9
    Signed-off-by: linjunjie1 <linjunjie1@xiaomi.corp-partner.google.com>
    [jstultz: Fix up erronious exec bit on perf.h file]
    Signed-off-by: John Stultz <jstultz@google.com>
    (cherry picked from commit eec127b5484d09fef414b9914710cd1faa7be1bc)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -75,6 +75,7 @@
 #include <trace/hooks/blk.h>
 #include <trace/hooks/suspend.h>
 #include <trace/hooks/fsnotify.h>
+#include <trace/hooks/perf.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -445,3 +446,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_warn_alloc_tune_ratelimit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_warn_alloc_show_mem_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_vm_swappiness);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fsnotify_open);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_armv8pmu_counter_overflowed);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_perf_rotate_context);
