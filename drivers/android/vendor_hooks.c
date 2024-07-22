acb9c0b19cf6bc47b2b60a87a0d3c1ea63da281d
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-22 12:35:09 +0900

    ANDROID: page_alloc: add vendor hook to try alloc pages
    Add vendor hook to get pages from vendor implemented pool
    
    Bug: 351175506
    Change-Id: I9a1656d6d1658ff8166a94fa6d13de01adb12100
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -453,3 +453,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_pageout_return_error);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_process_madvise_return_error);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_pageout_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_set_max_retries);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_try_alloc_pages);
