1ed9d3d6ac0795f610b4862ff54c326e26628ef9
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-23 13:39:01 +0900

    ANDROID: fs: add vendor hook in ksys_umount
    Add a vendor hook to notify ksys_unmount to vendor module
    
    Bug: 351175506
    Change-Id: I08ec02dee00459bc99a59976ea61e945a6879c2f
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -469,3 +469,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dmabuf_page_pool_free_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_read_fault);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_read);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_map_pages);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_ksys_umount);
