9a084d261d56c0c785cfb8b911f8b23545e8352e
Bart Van Assche <bvanassche@google.com>
2024-07-31 11:36:35 -0700

    ANDROID: scsi: core: Fix system resume
    During system resume, sd_start_stop_device() submits a START STOP UNIT
    command to the SCSI device that is being resumed. That command is not
    retried in case of a unit attention and hence may fail. An example:
    
    [16575.983359] sd 0:0:0:3: [sdd] Starting disk
    [16575.983693] sd 0:0:0:3: [sdd] Start/Stop Unit failed: Result: hostbyte=0x00 driverbyte=DRIVER_OK
    [16575.983712] sd 0:0:0:3: [sdd] Sense Key : 0x6
    [16575.983730] sd 0:0:0:3: [sdd] ASC=0x29 ASCQ=0x0
    [16575.983738] sd 0:0:0:3: PM: dpm_run_callback(): scsi_bus_resume+0x0/0xa0 returns -5
    [16575.983783] sd 0:0:0:3: PM: failed to resume async: error -5
    
    Make the SCSI core retry the START STOP UNIT command if the device
    reports a unit attention. This patch has the "ANDROID" tag because the
    upstream discussion about what is the best way to solve this issue is
    still ongoing.
    
    This patch affects the SCSI parallel driver (spi), the SCSI core and the
    UFS driver:
    
    $ git grep -nHw BLK_MQ_REQ_PM drivers
    drivers/scsi/scsi_transport_spi.c:116:		.req_flags = BLK_MQ_REQ_PM,
    drivers/scsi/sd.c:670:		.req_flags = BLK_MQ_REQ_PM,
    drivers/scsi/sd.c:1587:		.req_flags = BLK_MQ_REQ_PM,
    drivers/scsi/sd.c:3628:		.req_flags = BLK_MQ_REQ_PM,
    drivers/scsi/sd.c:3797:			.req_flags = BLK_MQ_REQ_PM,
    drivers/ufs/core/ufshcd.c:9380:		.req_flags = BLK_MQ_REQ_PM,
    
    See also
    https://lore.kernel.org/linux-scsi/b0f92045-d10f-4038-a746-e3d87e5830e8@acm.org/.
    
    Bug: 348341595
    Bug: 356474203
    Change-Id: I44092a0d8853fd61bf619e5bae6d65eaaddad780
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- drivers/scsi/scsi_error.c
+++ drivers/scsi/scsi_error.c
@@ -1854,6 +1854,8 @@ check_type:
 	 * assume caller has checked sense and determined
 	 * the check condition was retryable.
 	 */
+	if (req->rq_flags & RQF_PM)
+		return false;
 	if (req->cmd_flags & REQ_FAILFAST_DEV || blk_rq_is_passthrough(req))
 		return true;
 
