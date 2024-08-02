5ef3f3300d0dd635afa5aeb57ab854b623b4b45f
hao lv <hao.lv5@transsion.com>
2024-08-02 14:14:43 +0800

    ANDROID: add hook into __scsi_execute
    android_rvh__scsi_execute: We have registered a new SCSI driver module,
    and this hook will route SCSI requests to the new SCSI driver for processing.
    
    Bug: 356977219
    
    Change-Id: I4e2d9768d5c00123cbb9b409d32614f204f87064
    Signed-off-by: hao lv <hao.lv5@transsion.com>

---
+++ include/trace/hooks/scsi.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM scsi
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_SCSI_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_SCSI_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+struct scsi_sense_hdr;
+struct scsi_device;
+
+DECLARE_RESTRICTED_HOOK(
+	android_rvh__scsi_execute,
+	TP_PROTO(bool *skip, int *ret, struct scsi_device *sdev,
+		 const unsigned char *cmd, int data_direction, void *buffer,
+		 unsigned bufflen, unsigned char *sense,
+		 struct scsi_sense_hdr *sshdr, int timeout, int retries,
+		 u64 flags, req_flags_t rq_flags, int *resid),
+	TP_ARGS(skip, ret, sdev, cmd, data_direction, buffer, bufflen, sense,
+		sshdr, timeout, retries, flags, rq_flags, resid), 1);
+
+#endif /* _TRACE_HOOK_SCSI_H */
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>
