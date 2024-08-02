6952e251e2a6494f97de600b62a43a67f6a49fc5
chunpeng li <chunpng.lee@gmail.com>
2024-08-02 15:22:36 +0800

    ANDROID: vendor_hook: Add hook to abort reclaim and compaction
    We need to abort the reclaim/compaction by sending
    signal(such as SIGUSR2) to the reclaim thread, or
    just abort when cpu-usage is too-high or free-mem is enough.
    
    Bug: 289987875
    Bug: 356961335
    Change-Id: I4b637cbd2b37235eec27a985a9b5b95598247c59
    Signed-off-by: shenjiangjiang <shenjiangjiang@oppo.com>
    Signed-off-by: chunpeng li <chunpng.lee@gmail.com>
    (cherry picked from commit 024628cc9203cbd4f8471d98435b3a3d6f85764d)

--- mm/madvise.c
+++ mm/madvise.c
@@ -374,8 +374,10 @@ static int madvise_cold_or_pageout_pte_range(pmd_t *pmd,
 	bool pageout_anon_only_filter;
 	int nr;
 	swp_entry_t entry;
+	bool abort_madvise = false;
 
-	if (fatal_signal_pending(current))
+	trace_android_vh_madvise_cold_or_pageout_abort(vma, &abort_madvise);
+	if (fatal_signal_pending(current) || abort_madvise)
 		return -EINTR;
 
 	pageout_anon_only_filter = pageout && !vma_is_anonymous(vma) &&
