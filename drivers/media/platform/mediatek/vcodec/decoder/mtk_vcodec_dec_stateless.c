41d663f4416829e6c0cc7273da61ee2ab4cf8f5c
Yunfei Dong <yunfei.dong@mediatek.com>
2024-08-07 16:24:36 +0800

    FROMLIST: media: mediatek: vcodec: replace v4l2_m2m_next_src_buf with v4l2_m2m_src_buf_remove
    There isn't lock to protect source buffer when get next src buffer,
    if the source buffer is removed for some unknown reason before lat
    work queue execute done, will lead to remove source buffer or buffer
    done error.
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    (am from https://patchwork.kernel.org/patch/13755951/)
    (also found at https://lore.kernel.org/r/20240807082444.21280-7-yunfei.dong@mediatek.com)
    
    BUG=b:336924470
    TEST=emerge-rauru sys-kernel/chromeos-kernel-6_6
    UPSTREAM-TASK=b:387448890
    
    Signed-off-by: Fei Shao <fshao@chromium.org>
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Change-Id: Ib829263c5b1cf56219c1e66a8965036f415a1a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6073942
    Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- drivers/media/platform/mediatek/vcodec/decoder/mtk_vcodec_dec_stateless.c
+++ drivers/media/platform/mediatek/vcodec/decoder/mtk_vcodec_dec_stateless.c
@@ -321,6 +321,7 @@ static void mtk_vdec_worker(struct work_struct *work)
 		container_of(work, struct mtk_vcodec_dec_ctx, decode_work);
 	struct mtk_vcodec_dec_dev *dev = ctx->dev;
 	struct vb2_v4l2_buffer *vb2_v4l2_src = ctx->last_vb2_v4l2_src;
+	struct vb2_v4l2_buffer *vb2_v4l2_dst;
 	struct vb2_buffer *vb2_src;
 	struct mtk_vcodec_mem *bs_src;
 	struct mtk_video_dec_buf *dec_buf_src;
@@ -329,7 +330,7 @@ static void mtk_vdec_worker(struct work_struct *work)
 	bool res_chg = false;
 	int ret;
 
-	vb2_v4l2_src = vb2_v4l2_src ? vb2_v4l2_src : v4l2_m2m_next_src_buf(ctx->m2m_ctx);
+	vb2_v4l2_src = vb2_v4l2_src ? vb2_v4l2_src : v4l2_m2m_src_buf_remove(ctx->m2m_ctx);
 	if (!vb2_v4l2_src) {
 		v4l2_m2m_job_finish(dev->m2m_dev_dec, ctx->m2m_ctx);
 		mtk_v4l2_vdec_dbg(1, ctx, "[%d] no available source buffer", ctx->id);
@@ -383,17 +384,28 @@ static void mtk_vdec_worker(struct work_struct *work)
 	    ctx->current_codec == V4L2_PIX_FMT_VP8_FRAME) {
 		if (src_buf_req)
 			v4l2_ctrl_request_complete(src_buf_req, &ctx->ctrl_hdl);
-		v4l2_m2m_buf_done_and_job_finish(dev->m2m_dev_dec, ctx->m2m_ctx, state);
-	} else {
-		if (ret != -EAGAIN) {
-			v4l2_m2m_src_buf_remove(ctx->m2m_ctx);
-			ctx->last_vb2_v4l2_src = NULL;
-		} else {
-			ctx->last_vb2_v4l2_src = vb2_v4l2_src;
-		}
+		vb2_v4l2_dst = v4l2_m2m_dst_buf_remove(ctx->m2m_ctx);
+		v4l2_m2m_buf_done(vb2_v4l2_dst, state);
+		v4l2_m2m_buf_done(vb2_v4l2_src, state);
 
 		v4l2_m2m_job_finish(dev->m2m_dev_dec, ctx->m2m_ctx);
+		return;
 	}
+
+	/* If each codec return -EAGAIN to decode again, need to backup current source
+	 * buffer, then the driver will get this buffer next time.
+	 *
+	 * If each codec decode error, must to set buffer done with error status for
+	 * this buffer have been removed from ready list.
+	 */
+	ctx->last_vb2_v4l2_src = (ret != -EAGAIN) ? NULL : vb2_v4l2_src;
+	if (ret && ret != -EAGAIN) {
+		if (src_buf_req)
+			v4l2_ctrl_request_complete(src_buf_req, &ctx->ctrl_hdl);
+		v4l2_m2m_buf_done(vb2_v4l2_src, state);
+	}
+
+	v4l2_m2m_job_finish(dev->m2m_dev_dec, ctx->m2m_ctx);
 }
 
 static void vb2ops_vdec_stateless_buf_queue(struct vb2_buffer *vb)
