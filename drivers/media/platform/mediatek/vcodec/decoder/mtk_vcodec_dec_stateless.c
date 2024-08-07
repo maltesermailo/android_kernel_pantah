1bcbc89faac8cefa22e4ed19a1bbba7c0bdee959
Yunfei Dong <yunfei.dong@mediatek.com>
2024-08-07 16:24:37 +0800

    FROMLIST: media: mediatek: vcodec: remove media request checking
    Need to set the buffer status to error if the media request of
    each source buffer is NULL, then schedule the work to process
    again in case of access NULL pointer.
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    (am from https://patchwork.kernel.org/patch/13755948/)
    (also found at https://lore.kernel.org/r/20240807082444.21280-8-yunfei.dong@mediatek.com)
    
    BUG=b:336924470
    TEST=emerge-rauru sys-kernel/chromeos-kernel-6_6
    UPSTREAM-TASK=b:387448890
    
    Signed-off-by: Fei Shao <fshao@chromium.org>
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Change-Id: I47cb63fe1890483c1a4c8e35735c4cd05746ac40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6073943
    Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- drivers/media/platform/mediatek/vcodec/decoder/mtk_vcodec_dec_stateless.c
+++ drivers/media/platform/mediatek/vcodec/decoder/mtk_vcodec_dec_stateless.c
@@ -361,10 +361,14 @@ static void mtk_vdec_worker(struct work_struct *work)
 			  ctx->id, bs_src->va, &bs_src->dma_addr, bs_src->size, vb2_src);
 	/* Apply request controls. */
 	src_buf_req = vb2_src->req_obj.req;
-	if (src_buf_req)
+	if (src_buf_req) {
 		v4l2_ctrl_request_setup(src_buf_req, &ctx->ctrl_hdl);
-	else
+	} else {
 		mtk_v4l2_vdec_err(ctx, "vb2 buffer media request is NULL");
+		v4l2_m2m_buf_done(vb2_v4l2_src, VB2_BUF_STATE_ERROR);
+		v4l2_m2m_job_finish(dev->m2m_dev_dec, ctx->m2m_ctx);
+		return;
+	}
 
 	ret = vdec_if_decode(ctx, bs_src, NULL, &res_chg);
 	if (ret && ret != -EAGAIN) {
@@ -382,8 +386,7 @@ static void mtk_vdec_worker(struct work_struct *work)
 	state = ret ? VB2_BUF_STATE_ERROR : VB2_BUF_STATE_DONE;
 	if (!IS_VDEC_LAT_ARCH(dev->vdec_pdata->hw_arch) ||
 	    ctx->current_codec == V4L2_PIX_FMT_VP8_FRAME) {
-		if (src_buf_req)
-			v4l2_ctrl_request_complete(src_buf_req, &ctx->ctrl_hdl);
+		v4l2_ctrl_request_complete(src_buf_req, &ctx->ctrl_hdl);
 		vb2_v4l2_dst = v4l2_m2m_dst_buf_remove(ctx->m2m_ctx);
 		v4l2_m2m_buf_done(vb2_v4l2_dst, state);
 		v4l2_m2m_buf_done(vb2_v4l2_src, state);
@@ -400,8 +403,7 @@ static void mtk_vdec_worker(struct work_struct *work)
 	 */
 	ctx->last_vb2_v4l2_src = (ret != -EAGAIN) ? NULL : vb2_v4l2_src;
 	if (ret && ret != -EAGAIN) {
-		if (src_buf_req)
-			v4l2_ctrl_request_complete(src_buf_req, &ctx->ctrl_hdl);
+		v4l2_ctrl_request_complete(src_buf_req, &ctx->ctrl_hdl);
 		v4l2_m2m_buf_done(vb2_v4l2_src, state);
 	}
 
