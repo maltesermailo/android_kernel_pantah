0aaeab3e355d2ae75293c2bddee9729b532cfdf7
Will Deacon <will@kernel.org>
2024-08-12 18:16:06 +0100

    FROMGIT: mm: vmalloc: ensure vmap_block is initialised before adding to queue
    Commit 8c61291fd850 ("mm: fix incorrect vbq reference in
    purge_fragmented_block") extended the 'vmap_block' structure to contain a
    'cpu' field which is set at allocation time to the id of the initialising
    CPU.
    
    When a new 'vmap_block' is being instantiated by new_vmap_block(), the
    partially initialised structure is added to the local 'vmap_block_queue'
    xarray before the 'cpu' field has been initialised.  If another CPU is
    concurrently walking the xarray (e.g.  via vm_unmap_aliases()), then it
    may perform an out-of-bounds access to the remote queue thanks to an
    uninitialised index.
    
    This has been observed as UBSAN errors in Android:
    
     | Internal error: UBSAN: array index out of bounds: 00000000f2005512 [#1] PREEMPT SMP
     |
     | Call trace:
     |  purge_fragmented_block+0x204/0x21c
     |  _vm_unmap_aliases+0x170/0x378
     |  vm_unmap_aliases+0x1c/0x28
     |  change_memory_common+0x1dc/0x26c
     |  set_memory_ro+0x18/0x24
     |  module_enable_ro+0x98/0x238
     |  do_init_module+0x1b0/0x310
    
    Move the initialisation of 'vb->cpu' in new_vmap_block() ahead of the
    addition to the xarray.
    
    Link: https://lkml.kernel.org/r/20240812171606.17486-1-will@kernel.org
    Fixes: 8c61291fd850 ("mm: fix incorrect vbq reference in purge_fragmented_block")
    Signed-off-by: Will Deacon <will@kernel.org>
    Reviewed-by: Baoquan He <bhe@redhat.com>
    Reviewed-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Cc: Zhaoyang Huang <zhaoyang.huang@unisoc.com>
    Cc: Hailong.Liu <hailong.liu@oppo.com>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Lorenzo Stoakes <lstoakes@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 3e3de7947c75 akpm/mm.git mm-hotfixes-stable)
    Bug: 364799305
    Bug: 357792929
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: Ie0bd3b10ac947f8e00ecf906008e5848fc5e5afc
    (cherry picked from commit 1a751af48eb9f39de6c89fa222284402c905b376)

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2068,6 +2068,7 @@ static void *new_vmap_block(unsigned int order, gfp_t gfp_mask)
 	vb->dirty_max = 0;
 	bitmap_set(vb->used_map, 0, (1UL << order));
 	INIT_LIST_HEAD(&vb->free_list);
+	vb->cpu = raw_smp_processor_id();
 
 	xa = addr_to_vb_xa(va->va_start);
 	vb_idx = addr_to_vb_idx(va->va_start);
@@ -2079,6 +2080,7 @@ static void *new_vmap_block(unsigned int order, gfp_t gfp_mask)
 	}
 
 	/*
+<<<<<<< HEAD   (94048e ANDROID: GKI: Update symbol list for vivo)
 	* list_add_tail_rcu could happened in another core
 	* rather than vb->cpu due to task migration, which
 	* is safe as list_add_tail_rcu will ensure the list's
@@ -2086,6 +2088,14 @@ static void *new_vmap_block(unsigned int order, gfp_t gfp_mask)
 	* side.
 	*/
 	vb->cpu = raw_smp_processor_id();
+=======
+	 * list_add_tail_rcu could happened in another core
+	 * rather than vb->cpu due to task migration, which
+	 * is safe as list_add_tail_rcu will ensure the list's
+	 * integrity together with list_for_each_rcu from read
+	 * side.
+	 */
+>>>>>>> CHANGE (1a751a FROMGIT: mm: vmalloc: ensure vmap_block is initialised befor)
 	vbq = per_cpu_ptr(&vmap_block_queue, vb->cpu);
 	spin_lock(&vbq->lock);
 	list_add_tail_rcu(&vb->free_list, &vbq->free);
