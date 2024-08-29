7ccdf3089aa802fc22ef167c1eb995db143e00c2
JohnnLee <johnnlee@google.com>
2024-08-29 16:39:19 +0800

    [test] Add init log for test
    Change-Id: I386d78d721db13a4d091c9ab70b04279a1f43b5e
    Signed-off-by: JohnnLee <johnnlee@google.com>

--- init/main.c
+++ init/main.c
@@ -970,6 +970,7 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 	build_all_zonelists(NULL);
 	page_alloc_init();
 
+	pr_notice("THIS LOG IS FOR TEST ONLY---------");
 	pr_notice("Kernel command line: %s\n", saved_command_line);
 	/* parameters may set static keys */
 	jump_label_init();
