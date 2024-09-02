005f1afd84b743cd998d899c2924a6f5cead107e
Jiewen Wang <jiewen.wang@vivo.com>
2024-09-02 14:36:56 +0800

    ANDROID: vendor_hooks: Add vendor hooks in __swap_writepage
    Add hook in __swap_writepage for reclaim synchronous or asynchronous in different scenarios.
    
    Bug: 363882594
    Change-Id: I06604454fa3019850a499912f6def97525960c0f
    Signed-off-by: Jiewen Wang <jiewen.wang@vivo.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -387,6 +387,9 @@ DECLARE_HOOK(android_vh_customize_thp_pcp_order,
 DECLARE_HOOK(android_vh_customize_thp_gfp_orders,
 	TP_PROTO(gfp_t *gfp_mask, unsigned long *orders, int *order),
 	TP_ARGS(gfp_mask, orders, order));
+DECLARE_HOOK(android_vh_swap_writepage,
+	TP_PROTO(unsigned long *sis_flags, struct page *page),
+	TP_ARGS(sis_flags, page));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
