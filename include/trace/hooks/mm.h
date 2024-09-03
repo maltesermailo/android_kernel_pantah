8ced9b1f52208802811308fedc4d449c42fe0ea8
Justin Jiang <justinjiang@vivo.corp-partner.google.com>
2024-09-03 21:06:53 +0800

    ANDROID: vendor_hooks: add hook for adjusting a more suitable watermark
    During zone initialization adjust a more suitable watermark for
    Customize, which can optimize kswapd load and system memory status.
    
    Bug: 364431560
    Bug: 364225965
    Change-Id: Ib6d94ec244f504a1c474657833a5f7558ea5d00b
    Signed-off-by: Justin Jiang <justinjiang@vivo.corp-partner.google.com>
    (cherry picked from commit fdf9c08933a6c12ab52486354be0cc38f208a198)
    (cherry picked from commit 99a599871125ecde84a493f423feedc300917548)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -332,6 +332,9 @@ DECLARE_HOOK(android_vh_split_large_folio_bypass,
 DECLARE_HOOK(android_vh_cma_alloc_set_max_retries,
 	TP_PROTO(int *max_retries),
 	TP_ARGS(max_retries));
+DECLARE_HOOK(android_vh_init_adjust_zone_wmark,
+	TP_PROTO(struct zone *zone, u64 interval),
+	TP_ARGS(zone, interval));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
