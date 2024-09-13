dd7524ce8b1fe55f868d366fa9e6d6fafaa0bc41
Alice Ryhl <aliceryhl@google.com>
2024-09-13 13:40:45 +0000

    ANDROID: ashmem_rust: add llseek
    Similarly to reading, we also forward seeking to the underlying file.
    
    Bug: 370906207
    Change-Id: Icba9b7416276211c71ca04b266d12523389ad733
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/staging/android/ashmem_rust.rs
+++ drivers/staging/android/ashmem_rust.rs
@@ -4,13 +4,16 @@
 
 //! Anonymous Shared Memory Subsystem for Android.
 
-use core::{ffi::c_long, pin::Pin};
+use core::{
+    ffi::{c_int, c_long},
+    pin::Pin,
+};
 use kernel::{
     bindings, c_str,
     error::Result,
-    fs::File,
+    fs::{File, LocalFile},
     ioctl::_IOC_SIZE,
-    miscdevice::{IovIter, Kiocb, MiscDevice, MiscDeviceOptions, MiscDeviceRegistration},
+    miscdevice::{loff_t, IovIter, Kiocb, MiscDevice, MiscDeviceOptions, MiscDeviceRegistration},
     mm::virt::{flags as vma_flags, VmAreaNew},
     page::page_align,
     prelude::*,
@@ -169,6 +172,27 @@ impl MiscDevice for Ashmem {
 
         vma.set_file(file.file());
         Ok(())
+    }
+
+    fn llseek(me: Pin<&Ashmem>, file: &LocalFile, offset: loff_t, whence: c_int) -> Result<loff_t> {
+        let asma_file = {
+            let asma = me.inner.lock();
+            if asma.size == 0 {
+                return Err(EINVAL);
+            }
+            match asma.file.as_ref() {
+                Some(asma_file) => asma_file.clone(),
+                None => return Err(EBADF),
+            }
+        };
+
+        let ret = asma_file.vfs_llseek(offset, whence)?;
+
+        // SAFETY: We protect the shmem file with the same mechanism as the ashmem file. We are in
+        // llseek, so our caller ensures that accessing f_pos is okay.
+        unsafe { shmem::file_set_fpos(file, shmem::file_get_fpos(asma_file.file())) };
+
+        Ok(ret)
     }
 
     fn read_iter(mut kiocb: Kiocb<'_, Self::Ptr>, iov: &mut IovIter) -> Result<usize> {
