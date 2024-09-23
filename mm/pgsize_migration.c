58314fc1071de826bc1b7bea3068b0493eabf011
Kalesh Singh <kaleshsingh@google.com>
2024-09-23 22:26:26 +0000

    ANDROID: 16K: Fixup padding vm_flags bits on VMA splits
    In some cases VMAs are split without the mmap write lock held;
    later the lock is taken to fixup vm_flags of the original VMA.
    Since some uppper bits of vm_flags are used to encode the ELF
    padding ranges, they need to be modified on splits. This is
    usually handled correctly by __split_vma(). However in the above
    case, the flags get over witten later under the write lock.
    
    Preserve vm_flag bits on reset to correctly represent padding.
    
    Also take opportunity to do some small noop refactor in
    vma_pad_pages() and split_pad_vma() for more robust code.
    
    Bug: 357901498
    Change-Id: I1cb75419e614791a47cbdb0341373f619daf0bf2
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/pgsize_migration.c
+++ mm/pgsize_migration.c
@@ -126,7 +126,7 @@ unsigned long vma_pad_pages(struct vm_area_struct *vma)
 	if (!is_pgsize_migration_enabled())
 		return 0;
 
-	return vma->vm_flags >> VM_PAD_SHIFT;
+	return (vma->vm_flags & VM_PAD_MASK) >> VM_PAD_SHIFT;
 }
 
 static __always_inline bool str_has_suffix(const char *str, const char *suffix)
@@ -397,7 +397,7 @@ void split_pad_vma(struct vm_area_struct *vma, struct vm_area_struct *new,
 	nr_vma2_pages = vma_pages(second);
 
 	if (nr_vma2_pages >= nr_pad_pages) { 			/* Case 1 & 3 */
-		first->vm_flags &= ~VM_PAD_MASK;
+		vma_set_pad_pages(first, 0);
 		vma_set_pad_pages(second, nr_pad_pages);
 	} else {						/* Case 2 */
 		vma_set_pad_pages(first, nr_pad_pages - nr_vma2_pages);
