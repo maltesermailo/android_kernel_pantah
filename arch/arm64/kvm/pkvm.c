d0848ceb8ebca07bcb9a6a15b6c64f0067bb7bb6
Sebastian Ene <sebastianene@google.com>
2024-09-30 14:51:38 +0000

    ANDROID: KVM: Send VM availability direct messages to Trustzone
    Verify if there are secure partitions which are interested in receiving
    VM availability messages and store them in an array. Notify the secure
    partitions which are registered for receiving these direct messages
    about VM creation and teardown.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: Ic812ad8b6fb3f30c821085141900c4e22555af57
    Signed-off-by: Sebastian Ene <sebastianene@google.com>
    Signed-off-by: Andrei Homescu <ahomescu@google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -376,6 +376,14 @@ static void __pkvm_destroy_hyp_vm(struct kvm *host_kvm)
 
 	WARN_ON(kvm_call_hyp_nvhe(__pkvm_start_teardown_vm, host_kvm->arch.pkvm.handle));
 
+	do {
+		ret = kvm_call_hyp_nvhe(__pkvm_notify_dying_guest_vm_avail,
+					host_kvm->arch.pkvm.handle);
+		WARN_ON(ret && ret != -EAGAIN && ret != -EINTR);
+		cond_resched();
+	} while (ret == -EAGAIN || ret == -EINTR);
+	WARN_ON(ret);
+
 retry:
 	pages = 0;
 	nr_busy = 0;
