269eff051327128c6ba28cedf9ee146fc9940b70
Vincent Donnefort <vdonnefort@google.com>
2024-10-01 16:10:08 +0100

    ANDROID: KVM: arm64: HVCs to filter Ftrace for pKVM hyp
    Introducing two new HVCs to configure which functions to trace.
    
      __pkvm_disable_ftrace() is disabling all functions.
    
      __pkvm_sync_ftrace() takes a host page containing the list of
      functions to be traced.
    
    The host populated page must have the same order as the original ELF
    .hyp.patchable_function_entries. The first bit of each address tells if
    a function must be enabled or not.
    
    Bug: 357781595
    Change-Id: I81b8e1733153936b0bd2850e189224e82cd94f5a
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/trace/trace.h
+++ arch/arm64/kvm/hyp/include/nvhe/trace/trace.h
@@ -80,12 +80,20 @@ do {							\
 
 #ifdef CONFIG_PROTECTED_NVHE_FTRACE
 void hyp_ftrace_setup_core(void);
+void *hyp_ftrace_sync(unsigned long *func_pg, unsigned long *funcs,
+		      unsigned long *funcs_end, unsigned long offset_idx,
+		      void *tramp);
 int hyp_ftrace_setup(unsigned long *funcs, unsigned long *funcs_end,
 		     unsigned long hyp_kern_offset, void *tramp);
+void hyp_ftrace_disable(unsigned long *funcs, unsigned long *funcs_end);
+int __pkvm_sync_ftrace(unsigned long host_func_pg);
+int __pkvm_disable_ftrace(void);
 #else
 static inline void hyp_ftrace_setup_core(void) { }
 static inline int hyp_ftrace_setup(unsigned long *funcs, unsigned long *funcs_end,
 				   unsigned long hyp_kern_offset, void *tramp) { return 0; }
+static inline int __pkvm_sync_ftrace(unsigned long host_func_pg) { return -EOPNOTSUPP; }
+static inline int __pkvm_disable_ftrace(void) { return -EOPNOTSUPP; }
 #endif /* CONFIG_PROTECTED_NVHE_FTRACE */
 #else /* CONFIG_TRACING */
 static inline void *tracing_reserve_entry(unsigned long length) { return NULL; }
