8d87b851e3515e3d12ea10977dfc5f9ecb9b6ce9
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2024-10-03 14:02:50 -0700

    ANDROID: GKI: Enable the option to alter task_struct size dynamically
    Enable the config option that allows for dynamic sizing of the
    task_struct. Once config option is enabled, ensure that
    arch_task_struct_size is initialized to hold a valid task_struct size.
    The motivation behind this change is to reduce the static memory added to
    the task_struct to accommodate vendor data.
    
    Bug: 233921394
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    Change-Id: Iebc422ba4c650864f470078220b99546e56302f3
    (cherry picked from db56ca3b78284f0916eba24043d655464cf8cc9e)
    [linjihan: Resolved minor conflict in init/main.c]
    Bug: 489588860
    Bug: 503208507
    Signed-off-by: Jihan LIN <linjihan@xiaomi.com>
    (cherry picked from commit 9718459e2c57dbb5846464720be983c96e51bb0d)

--- init/main.c
+++ init/main.c
@@ -1042,6 +1042,13 @@ static void __init print_kernel_cmdline(const char *cmdline)
 		pr_notice("%s%s\n", KERNEL_CMDLINE_PREFIX, cmdline);
 }
 
+#ifdef CONFIG_ARCH_WANTS_DYNAMIC_TASK_STRUCT
+static void __init setup_arch_task_struct_size(void)
+{
+	arch_task_struct_size = sizeof(struct task_struct);
+}
+#endif
+
 asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 {
 	char *command_line;
@@ -1064,6 +1071,9 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 	boot_cpu_init();
 	page_address_init();
 	pr_notice("%s", linux_banner);
+#ifdef CONFIG_ARCH_WANTS_DYNAMIC_TASK_STRUCT
+	setup_arch_task_struct_size();
+#endif
 	early_security_init();
 	setup_arch(&command_line);
 	setup_boot_config();
