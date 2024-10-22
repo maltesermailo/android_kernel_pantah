6f41f6135634ddfab8bc593029283343cb82880e
Rui Chen <chenrui9@honor.com>
2024-10-22 15:02:03 +0800

    ANDROID: GKI: Add patch that uses vendor hook define for Honor Add this trace point for IO statistics of direct/buffer x read/write, so as to analyse patterns of IO workloads in various performance-sensitive user scenarios.
    Bug: 362663044
    Change-Id: Ic7ac7430890851d1b69ea29f33f65722c1b644f1
    Signed-off-by: Rui Chen <chenrui9@honor.com>

--- fs/iomap/direct-io.c
+++ fs/iomap/direct-io.c
@@ -13,7 +13,7 @@
 #include <linux/task_io_accounting_ops.h>
 #include "trace.h"
 #include <trace/hooks/direct_io.h>
-
+#include <trace/hooks/mm.h>
 #include "../internal.h"
 
 /*
@@ -314,6 +314,8 @@ static loff_t iomap_dio_bio_iter(const struct iomap_iter *iter,
 			goto out;
 		}
 
+		trace_android_vh_io_statistics(inode->i_mapping, pos >> inode->i_blkbits,
+					nr_pages, !(dio->flags & IOMAP_DIO_WRITE), true);
 		bio = bio_alloc(GFP_KERNEL, nr_pages);
 		fscrypt_set_bio_crypt_ctx(bio, inode, pos >> inode->i_blkbits,
 					  GFP_KERNEL);
