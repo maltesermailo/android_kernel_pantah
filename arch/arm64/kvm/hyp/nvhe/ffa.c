31e33678b8617db0b3cf41f8a7e0d1849571ac13
Sebastian Ene <sebastianene@google.com>
2024-11-07 15:52:09 +0000

    ANDROID: KVM: arm64: Introduce KVM_CAP_ARM_FFA_SUPPORT uapi
    Restrict protected VMs from being able to talk to Trustzone unless
    userspace specifies the KVM_CAP_ARM_FFA_SUPPORT capability.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: I9f8adb47b03f076f17b32fdb280d5531b4578e25
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -47,6 +47,8 @@
 /* FF-A VM handle - 0 is reserved for the host */
 #define VM_FFA_HANDLE_FROM_VCPU(vcpu)	(((vcpu)->kvm->arch.pkvm.handle) - HANDLE_OFFSET + 1)
 
+#define VM_FFA_SUPPORTED(vcpu)		((vcpu)->kvm->arch.pkvm.ffa_support)
+
 /* The maximum number of secure partitions that can register for VM availability */
 #define FFA_MAX_REGISTERED_SP_IDS	(8)
 
@@ -1261,6 +1263,12 @@ bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 		return true;
 	}
 
+	if (!VM_FFA_SUPPORTED(vcpu)) {
+		ffa_to_smccc_error(&res, FFA_RET_NOT_SUPPORTED);
+		ffa_set_retval(ctxt, &res);
+		return true;
+	}
+
 	vm_handle = VM_FFA_HANDLE_FROM_VCPU(vcpu);
 	WARN_ON(vm_handle >= KVM_MAX_PVMS);
 
@@ -1331,6 +1339,9 @@ int kvm_reclaim_ffa_guest_pages(struct pkvm_hyp_vm *vm, pkvm_handle_t handle)
 	struct ffa_mem_transfer *transfer, *tmp;
 	struct arm_smccc_res res;
 	struct pkvm_hyp_vcpu *hyp_vcpu = vm->vcpus[0];
+
+	if (!VM_FFA_SUPPORTED(&hyp_vcpu->vcpu))
+		return 0;
 
 	vm_handle = VM_FFA_HANDLE_FROM_VCPU(&hyp_vcpu->vcpu);
 	WARN_ON(vm_handle >= KVM_MAX_PVMS);
