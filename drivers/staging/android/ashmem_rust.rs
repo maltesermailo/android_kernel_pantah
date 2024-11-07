e5c39eead0a8ef4cbad17ce44908865e55b9f2e2
Alice Ryhl <aliceryhl@google.com>
2024-11-07 11:50:50 +0000

    ANDROID: ashmem_rust: move shrinker to its own global
    To support changing whether the shrinker is registered at runtime, move
    it to a new global variable that will keep track of the current state.
    The actual support for changing the state at runtime happens in a later
    commit.
    
    Bug: 370906207
    Change-Id: Ic635c2cdfbe466f9a03583de881cbf63e39957df
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/staging/android/ashmem_rust.rs
+++ drivers/staging/android/ashmem_rust.rs
@@ -26,7 +26,6 @@ use kernel::{
     page::{page_align, PAGE_MASK, PAGE_SIZE},
     prelude::*,
     seq_file::{seq_print, SeqFile},
-    shrinker::{self, ShrinkerBuilder, ShrinkerRegistration},
     sync::{new_mutex, Mutex, UniqueArc},
     task::Task,
     uaccess::{UserSlice, UserSliceReader, UserSliceWriter},
@@ -77,7 +76,6 @@ module! {
 
 struct AshmemModule {
     _misc: Pin<Box<MiscDeviceRegistration<Ashmem>>>,
-    _shrinker: ShrinkerRegistration<Self>,
 }
 
 impl kernel::Module for AshmemModule {
@@ -86,11 +84,12 @@ impl kernel::Module for AshmemModule {
         unsafe { shmem::SHMEM_FOPS_ONCE.init() };
         // SAFETY: Called once since this is the module initializer.
         unsafe { ASHMEM_MUTEX.init() };
+        // SAFETY: Called once since this is the module initializer.
+        unsafe { ashmem_range::ASHMEM_SHRINKER.init() };
 
         pr_info!("Using Rust implementation.");
 
-        let mut shrinker = ShrinkerBuilder::new(c_str!("android-ashmem"))?;
-        shrinker.set_seeks(4 * shrinker::DEFAULT_SEEKS);
+        ashmem_range::register_shrinker()?;
 
         Ok(Self {
             _misc: Box::pin_init(
@@ -99,7 +98,6 @@ impl kernel::Module for AshmemModule {
                 }),
                 GFP_KERNEL,
             )?,
-            _shrinker: shrinker.register(()),
         })
     }
 }
