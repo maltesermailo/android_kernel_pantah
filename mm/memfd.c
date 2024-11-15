d92454cd44ecd22f67880577853eec06335aab04
Isaac J. Manjarres <isaacmanjarres@google.com>
2024-11-15 12:32:37 -0800

    ANDROID: mm/memfd: Add support for ashmem compatible memfds
    Install the ashmem compatible file operations on all memfds, so that
    applications that only work with ashmem fds can work with memfds
    seamlessly  (i.e. they can invoke ashmem ioctl commands on memfds
    without experiencing errors).
    
    Also, expose memfd_filp_create() so that the ashmem driver can invoke it
    to create a memfd file, to implement ashmem on top of memfd. All memfd
    files that are created through calls to memfd_filp_create().
    
    Bug: 111903542
    Change-Id: Idc55832eda7d851d70d0702ee1ada6a9961c8ec5
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- mm/memfd.c
+++ mm/memfd.c
@@ -21,6 +21,8 @@
 #include <linux/pid_namespace.h>
 #include <uapi/linux/memfd.h>
 
+#include "ashmem_compat.h"
+
 /*
  * We need a tag: a new tag would expand every xa_node by 8 bytes,
  * so reuse a tag which we firmly believe is never set or cleared on tmpfs
@@ -327,7 +329,7 @@ static int check_sysctl_memfd_noexec(unsigned int *flags)
 	return 0;
 }
 
-static struct file *memfd_filp_create(const char *name, unsigned int flags)
+static struct file *__memfd_filp_create(const char *name, unsigned int flags)
 {
 	unsigned int *file_seals;
 	struct file *file;
@@ -360,6 +362,40 @@ static struct file *memfd_filp_create(const char *name, unsigned int flags)
 			*file_seals &= ~F_SEAL_SEAL;
 	}
 
+	install_ashmem_compat_fops(file);
+	return file;
+}
+
+struct file *memfd_filp_create(const char *name, unsigned int flags)
+{
+	struct file *file;
+	char *file_name;
+	size_t len, buf_len;
+
+	if (!name)
+		return ERR_PTR(-EINVAL);
+
+	if (!(flags &MFD_HUGETLB)) {
+		if (flags & ~(unsigned int)MFD_ALL_FLAGS)
+			return ERR_PTR(-EINVAL);
+	} else {
+		if (flags & ~(unsigned int)(MFD_ALL_FLAGS |
+				(MFD_HUGE_MASK << MFD_HUGE_SHIFT)))
+			return ERR_PTR(-EINVAL);
+	}
+
+	len = strnlen(name, MFD_NAME_MAX_LEN + 1);
+	if (len > MFD_NAME_MAX_LEN)
+		return ERR_PTR(-EINVAL);
+
+	buf_len = len + MFD_NAME_PREFIX_LEN + 1;
+	file_name = kmalloc(buf_len, GFP_KERNEL);
+	if (!file_name)
+		return ERR_PTR(-ENOMEM);
+
+	scnprintf(file_name, buf_len, "%s%s", MFD_NAME_PREFIX, name);
+	file = __memfd_filp_create(file_name, flags);
+	kfree(file_name);
 	return file;
 }
 
@@ -419,7 +455,7 @@ SYSCALL_DEFINE2(memfd_create,
 		goto err_name;
 	}
 
-	file = memfd_filp_create(name, flags);
+	file = __memfd_filp_create(name, flags);
 	if (IS_ERR(file)) {
 		error = PTR_ERR(file);
 		goto err_fd;
