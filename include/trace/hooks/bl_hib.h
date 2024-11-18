MotorolaMobilityLLC/kernel-sprd

ANDROID: vendor hooks: Add hooks to support bootloader based hibernation
Add vendor hooks to disable randomization of swap slot allocation for
swap partition used for saving hibernation image. Another level of
randomization of swap slots takes place at the firmware level as well
in order to address the wear leveling for UFS/MMC devices, so this
vendor hook checks if a block device represents the swap partition being
used for saving hibernation image, if yes, the swap slot allocation for
such partition is serialized at kernel level.

There is a performance advantage of reading contiguous pages of hibernation
image, it makes the restore logic of hibernation image simpler and faster
as there are no seeks involved in the secondary storage to read multiple
contiguous pages of the image.

Bug: 228815136
Change-Id: Id41a25fd15c5b71f8064f1c7d9cac9560fcdde85
Signed-off-by: Vivek Kumar <quic_vivekuma@quicinc.com>
Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>

include/trace/hooks/bl_hib.h
@@ -1,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM bl_hib
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_S2D_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_S2D_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+struct block_device;
+
+DECLARE_HOOK(android_vh_check_hibernation_swap,
+	TP_PROTO(struct block_device *resume_block, bool *hib_swap),
+	TP_ARGS(resume_block, hib_swap));
+
+DECLARE_HOOK(android_vh_save_cpu_resume,
+	TP_PROTO(u64 *addr, u64 phys_addr),
+	TP_ARGS(addr, phys_addr));
+
+DECLARE_HOOK(android_vh_save_hib_resume_bdev,
+	TP_PROTO(struct block_device *hib_resume_bdev),
+	TP_ARGS(hib_resume_bdev));
+
+#endif /* _TRACE_HOOK_S2D_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>

kernel/power/swap.c
@@ -31,6 +31,7 @@
 #include <linux/kthread.h>
 #include <linux/crc32.h>
 #include <linux/ktime.h>
+#include <trace/hooks/bl_hib.h>
 
 #include "power.h"
 
@@ -1527,6 +1528,7 @@ int swsusp_check(void)
 					    FMODE_READ | FMODE_EXCL, &holder);
 	if (!IS_ERR(hib_resume_bdev)) {
 		set_blocksize(hib_resume_bdev, PAGE_SIZE);
+		trace_android_vh_save_hib_resume_bdev(hib_resume_bdev);
 		clear_page(swsusp_header);
 		error = hib_submit_io(REQ_OP_READ, 0,
 					swsusp_resume_block,

maltesermailo/android_kernel_pantah

Bug: 335384668
Signed-off-by: Auditya Bhattaram <quic_audityab@quicinc.com>

include/trace/hooks/bl_hib.h
@@ -1,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM bl_hib
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_S2D_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_S2D_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+struct file;
+
+DECLARE_HOOK(android_vh_check_hibernation_swap,
+	TP_PROTO(struct file *resume_block_file, bool *hib_swap),
+	TP_ARGS(resume_block_file, hib_swap));
+
+DECLARE_HOOK(android_vh_save_cpu_resume,
+	TP_PROTO(u64 *addr, u64 phys_addr),
+	TP_ARGS(addr, phys_addr));
+
+DECLARE_HOOK(android_vh_save_hib_resume_bdev,
+	TP_PROTO(struct file *hib_resume_bdev_file),
+	TP_ARGS(hib_resume_bdev_file));
+
+#endif /* _TRACE_HOOK_S2D_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>

kernel/power/swap.c
@@ -29,6 +29,7 @@
 #include <linux/kthread.h>
 #include <linux/crc32.h>
 #include <linux/ktime.h>
+#include <trace/hooks/bl_hib.h>
 
 #include "power.h"
 
@@ -1569,6 +1570,7 @@ int swsusp_check(bool exclusive)
 	hib_resume_bdev_file = bdev_file_open_by_dev(swsusp_resume_device,
 				BLK_OPEN_READ, holder, NULL);
 	if (!IS_ERR(hib_resume_bdev_file)) {
+		trace_android_vh_save_hib_resume_bdev(hib_resume_bdev_file);
 		clear_page(swsusp_header);
 		error = hib_submit_io(REQ_OP_READ, swsusp_resume_block,
 					swsusp_header, NULL);