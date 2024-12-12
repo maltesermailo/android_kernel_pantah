11680ec4dfb8895252fff779b3e058f33e981afe
Justin Jiang <justinjiang@vivo.corp-partner.google.com>
2024-12-12 12:19:26 +0800

    ANDROID: add hook for send signal debug
    The hook is added to get the system memory states at send_signal time.
    It is used for problem analysis. Additionally adjust the memory
    reclaim strategy to improve the system's memory states.
    
    Bug: 384379368
    Bug: 383646535
    Change-Id: Ia2cec73e4588f4dd2f07dd47eb3375e19df894f5
    Signed-off-by: Justin Jiang <justinjiang@vivo.corp-partner.google.com>
    (cherry picked from commit 3bdcda6c9ffaa721012e7c6f046dcda9d2f33ac1)
    (cherry picked from commit d7df2fd537c7a1d8a67b1b33fb54a7b64fa448cf)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -542,3 +542,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swapmem_gather_add_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swapmem_gather_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_oom_swapmem_gather_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_oom_swapmem_gather_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_send_signal_locked);
