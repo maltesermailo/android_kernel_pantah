02d1f161a5ad0ff2f276434685c62b679c69a8f7
xiaofeng <xiaofeng5@xiaomi.com>
2024-12-13 10:20:36 +0800

    ANDROID: GKI: add vendor hook to reclaim cold pagecache generated by bufferio in advance.
    Add three vendor_hooks
        android_vh_copy_page_to_iter(): record times a page copied to userspace by read syscall
        android_vh_copy_page_from_iter_atomic(): record times a page copied from userspace by write syscall
        android_vh_destroy_inode(): notify us the inode corresponding to a file is to be destroy
    
    Export symbol shrink_inactive_list to provide a interface to reclaim pagecache in advance.Cause we will move cold pagecache to LRU inactive list tail.
    
    Bug: 383857059
    Change-Id: I799be49b8c94037e4e5c525220e8358b2a214f71
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -605,3 +605,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_direct_reclaim_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_queue_work);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_wake_idle_worker);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dma_buf_release);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_copy_page_to_iter);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_copy_page_from_iter_atomic);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_destroy_inode);
