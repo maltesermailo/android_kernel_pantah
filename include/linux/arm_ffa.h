898a672067e9d75ab171ba407614ae4d6f990060
Sebastian Ene <sebastianene@google.com>
2024-12-18 14:24:27 +0000

    ANDROID: KVM: arm64: Support direct messages for protected guests
    Allow direct messages to be forwarded from guest VMs. Validate the
    source endpoint of the caller before sending the message to SPMC
    to prevent impersonation.
    Make sure that the host uses the ID 1 and the hypervisor uses the
    ID 0 as per the FF-A spec, to prevent the host from impersonating
    the hypervisor.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: I96da79f8f87f4dc64228d2a53957dcff2c733667
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- include/linux/arm_ffa.h
+++ include/linux/arm_ffa.h
@@ -268,6 +268,8 @@ bool ffa_partition_check_property(struct ffa_device *dev, u32 property)
 #define FFA_VM_CREATION_MSG	(BIT(31) | (BIT(2)))
 #define FFA_VM_DESTRUCTION_MSG  (FFA_VM_CREATION_MSG | BIT(1))
 
+#define FFA_SRC_ENDPOINT_MASK	GENMASK(31, 16)
+
 /* For use with FFA_MSG_SEND_DIRECT_{REQ,RESP} which pass data via registers */
 struct ffa_send_direct_data {
 	unsigned long data0; /* w3/x3 */
