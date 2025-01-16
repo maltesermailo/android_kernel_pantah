e595969dbb5928bcfa60b6f8edb79946f5a62ce9
yipeng xiang <yipengxiang@honor.corp-partner.google.com>
2025-01-16 16:56:48 +0800

    ANDROID: mm: add vendor hook for async psi
    Add vendor hook to ignore async process PSI
    
    Bug: 390314846
    Change-Id: I6914517ee8196339f3a225d52fb46ebb294fa380
    Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -223,3 +223,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_flush_work_wait_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_enable_thermal_genl_check);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_unref_folios_to_pcp_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_async_psi_bypass);
