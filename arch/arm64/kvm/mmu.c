456a8d5e73fa4c11826f0c796f89f8db1c4a51bc
Vincent Donnefort <vdonnefort@google.com>
2025-01-21 10:46:23 +0000

    ANDROID: KVM: arm64: Introduce kvm-arm.protected_prefault parameter
    On a protected-guest page-fault, preemptively fault a bigger region.
    This region is defined by the protected_prefault parameter which is an
    order.
    
    Bug: 357781595
    Bug: 361026567
    Change-Id: I57565399f37ef1bc3f30098b82ba03ad4fb7cff7
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -1835,7 +1835,7 @@ static int __pkvm_host_donate_guest_sglist(struct kvm *kvm, struct list_head *pp
 			nr_ppages++;
 
 			/* Limit the time spent at EL2 */
-			if (nr_ppages >= 32)
+			if (nr_ppages >= (1 << max(pkvm_prefault, 5)))
 				break;
 		}
 
@@ -1997,6 +1997,34 @@ int pkvm_mem_abort_range(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa, size_t si
 	srcu_read_unlock(&vcpu->kvm->srcu, idx);
 
 	return err;
+}
+
+static u16 pkvm_prefault __read_mostly;
+
+static int __init early_pkvm_prefault_cfg(char *buf)
+{
+	int ret = kstrtou16(buf, 10, &pkvm_prefault);
+
+	if (ret)
+		return ret;
+
+	pkvm_prefault = min(pkvm_prefault, 9);
+}
+early_param("kvm-arm.protected_prefault", early_pkvm_prefault_cfg);
+
+int pkvm_mem_abort_prefault(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
+			    struct kvm_memory_slot *memslot)
+{
+	phys_addr_t memslot_start = memslot->base_gfn << PAGE_SHIFT;
+	size_t size = (1 << pkvm_prefault) << PAGE_SHIFT;
+
+	fault_ipa = ALIGN_DOWN(fault_ipa, size);
+	if (fault_ipa < memslot_start) {
+		size -= memslot_start - fault_ipa;
+		fault_ipa = memslot_start;
+	}
+
+	return pkvm_mem_abort(vcpu, fault_ipa, size, memslot);
 }
 
 static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
@@ -2477,7 +2505,7 @@ int kvm_handle_guest_abort(struct kvm_vcpu *vcpu)
 	}
 
 	if (kvm_vm_is_protected(vcpu->kvm))
-		ret = pkvm_mem_abort(vcpu, fault_ipa, PAGE_SIZE, memslot);
+		ret = pkvm_mem_abort_prefault(vcpu, fault_ipa, memslot);
 	else
 		ret = user_mem_abort(vcpu, fault_ipa, nested, memslot,
 				     esr_fsc_is_permission_fault(esr));
