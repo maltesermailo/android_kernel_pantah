92f3ce840775b341a57cb4f6767ca651e26a1c24
yipeng xiang <yipengxiang@honor.corp-partner.google.com>
2025-01-21 10:23:50 +0800

    ANDROID: mm: add vendor hook for reserve highatomic page
    Customized pages are not allowed to be reserved as highatomic
    
    Bug: 391128435
    
    Change-Id: Id6cf871325ee9f27de45b1f5588737a11651f971
    Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -67,6 +67,9 @@ DECLARE_HOOK(android_vh_free_one_page_bypass,
 DECLARE_HOOK(android_vh_migration_target_bypass,
 	TP_PROTO(struct page *page, bool *bypass),
 	TP_ARGS(page, bypass));
+DECLARE_HOOK(android_vh_reserve_highatomic_bypass,
+	TP_PROTO(struct page *page, bool *bypass),
+	TP_ARGS(page, bypass));
 
 struct page_vma_mapped_walk;
 DECLARE_HOOK(android_vh_slab_alloc_node,
