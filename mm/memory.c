BayLibre/android_kernel_common

ANDROID: Allow vendor modules perform more operations on memleak detect
Export arch_vma_name, anon_vma_name, vm_normal_page, walk_page_range, pmd_clear_bad,
__pmd_trans_huge_lock,__pte_offset_map_lock functions. Allow vendor modules perform
more operations on memleak detect and debug.It can detect multiple types of memleak,
such as Native,ION,Slub and Vmalloc. Shows detail size of memleak, and shows the stack.

Bug: 342305999
Change-Id: I118efa570e660d0696bf081d00b670760d01bec1
Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>

mm/memory.c
@@ -649,6 +649,7 @@ check_pfn:
 out:
 	return pfn_to_page(pfn);
 }
+EXPORT_SYMBOL_GPL(vm_normal_page);
 
 struct folio *vm_normal_folio(struct vm_area_struct *vma, unsigned long addr,
 			    pte_t pte)

maltesermailo/android_kernel_pantah

Export arch_vma_name, _vm_normal_page, pmd_clear_bad,
__pmd_trans_huge_lock functions. Allow vendor modules perform more
operations on memleak detect and debug. It can detect multiple types
of memleak,such as Native,ION,Slub and Vmalloc. Shows detail size of
memleak, and shows the stack.
Bug: 392028527
Bug: 379962310
Bug: 365506692
(cherry picked from commit 21e76b82444db3635eacb360f0d0206a069de4a8)
(cherry picked from commit ce0653fd2cb3b91553870938edd1b854c4975c72)
(cherry picked from commit e05e6574f729d534de2b795facb3b98c321ae0ce)

mm/memory.c
@@ -698,6 +698,7 @@ check_pfn:
 out:
 	return pfn_to_page(pfn);
 }
+EXPORT_SYMBOL_GPL(_vm_normal_page);
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 struct page *vm_normal_page_pmd(struct vm_area_struct *vma, unsigned long addr,