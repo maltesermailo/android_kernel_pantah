c139ee0698980200dbba861f6178478d9ba68306
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-02-03 13:07:16 -0800

    Revert "ANDROID: zram: allow zram to allocate CMA pages"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/3f1cec23-dab7-4ad8-81b2-eacdf5a29fec). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Bug: 394117013
    Change-Id: I1a32ee559bccb4b483a64c4ef4b983508c94ee3c
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1578,14 +1578,13 @@ compress_again:
 				__GFP_KSWAPD_RECLAIM |
 				__GFP_NOWARN |
 				__GFP_HIGHMEM |
-				__GFP_MOVABLE |
-				__GFP_CMA);
+				__GFP_MOVABLE);
 	if (IS_ERR_VALUE(handle)) {
 		zcomp_stream_put(zram->comps[ZRAM_PRIMARY_COMP]);
 		atomic64_inc(&zram->stats.writestall);
 		handle = zs_malloc(zram->mem_pool, comp_len,
 				GFP_NOIO | __GFP_HIGHMEM |
-				__GFP_MOVABLE | __GFP_CMA);
+				__GFP_MOVABLE);
 		if (IS_ERR_VALUE(handle))
 			return PTR_ERR((void *)handle);
 
