c9d11f79d918cd44dcb0d90e65fd0c0eaea45774
zhanghui <zhanghui31@xiaomi.com>
2025-02-14 16:23:03 +0800

    ANDROID: mm: add a new vendor hook in filemap_map_pages
    In the current vendor hook, if next_uptodate_folio returns NULL, the
    first_pgoff is set to zero, and the last_pgoff is set to start_pgoff.
    Therefore, the collection range is from 0 to the start_pgoff.
    
    |-----------|------------|-------------|------------------|
    0      start_pgoff  first_pgoff    last_pgoff         end_pgoff
    
    We want to collect the first_pgoff to last_pgoff, so we have to add a
    new vendor hook.
    
    Bug: 511864907
    Change-Id: I19d54c601e2ffc5de5ec2dafcd43fbdcdc84b0d2
    Signed-off-by: zhanghui <zhanghui31@xiaomi.com>
    (cherry picked from commit 2a0173a4773f377a208d79f1926ef08cdd20766a)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -469,9 +469,9 @@ DECLARE_HOOK(android_vh_filemap_adjust_folio_flags,
 		pgoff_t index),
 	TP_ARGS(mapping, folio, index));
 DECLARE_HOOK(android_vh_filemap_map_pages,
-	TP_PROTO(struct file *file, pgoff_t first_pgoff,
+	TP_PROTO(struct file *file, pgoff_t orig_start_pgoff, pgoff_t first_pgoff,
 		pgoff_t last_pgoff, vm_fault_t ret),
-	TP_ARGS(file, first_pgoff, last_pgoff, ret));
+	TP_ARGS(file, orig_start_pgoff, first_pgoff, last_pgoff, ret));
 DECLARE_HOOK(android_vh_zs_shrinker_adjust,
 	TP_PROTO(unsigned long *pages_to_free),
 	TP_ARGS(pages_to_free));
