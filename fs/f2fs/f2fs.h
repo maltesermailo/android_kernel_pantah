9bcf1ac4755acd6ec19cb3b79055000ea9a9a31a
Chao Yu <chaseyu@google.com>
2025-02-18 15:19:52 +0800

    ANDROID: f2fs: introduce sanity_check sysfs entry
    init.rc can enable sanity check of f2fs via writing 1 to this new sysfs
    entry.
    
    Bug: 383968963
    Change-Id: I1900e110b44793b5c3528622da799474218feb76
    Signed-off-by: Chao Yu <chaseyu@google.com>
    (cherry picked from commit 7b80db2aae726fe9e8fe4f13ca90a51fa6c5dfd9)

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1828,6 +1828,7 @@ struct f2fs_sb_info {
 	spinlock_t iostat_lat_lock;
 	struct iostat_lat_info *iostat_io_lat;
 #endif
+	unsigned int sanity_check;
 };
 
 /* Definitions to access f2fs_sb_info */
