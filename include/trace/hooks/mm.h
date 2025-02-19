ef1b476661c9c03d2b8eb3c8821b06cf1d0ef572
Charan Teja Kalla <quic_charante@quicinc.com>
2025-02-19 16:08:04 +0530

    ANDROID: mm/shmem: fix trace hook used to get the shmem folio
    Before mTHP for shmem is added, there used different API for huge and
    order-0 folios. With mTHP, shmem_alloc_folio() is used for both and is
    differentiated at the order passed.
    
    To make better decission at the client, pass the order.
    
    Bug: 397599006
    Change-Id: Idfc0fc82a8c6443fc6259d8b2f05a2d91d60ab38
    Signed-off-by: Charan Teja Kalla <quic_charante@quicinc.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -14,9 +14,8 @@ struct folio;
 struct folio_batch;
 
 DECLARE_RESTRICTED_HOOK(android_rvh_shmem_get_folio,
-			TP_PROTO(struct shmem_inode_info *info, struct folio **folio),
-			TP_ARGS(info, folio), 2);
-
+			TP_PROTO(struct shmem_inode_info *info, struct folio **folio, int order),
+			TP_ARGS(info, folio, order), 3);
 /*
 
 DECLARE_RESTRICTED_HOOK(android_rvh_set_skip_swapcache_flags,
