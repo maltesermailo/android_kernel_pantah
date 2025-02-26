1ce6133e99ea37bb4fcbe06eeebf0ba7ad80b4b4
Andrei Homescu <ahomescu@google.com>
2025-02-26 14:29:18 +0000

    ANDROID: KVM: Return EAGAIN on VM destruction errors
    Trusty sends RETRY when the VM destruction messages
    needs to be repeated. This needs to be converted to
    EAGAIN so the host retries the message. For now, we
    convert all errors to that code.
    
    Bug: 280886201
    Change-Id: I1795f1b7c61349bf5ad9424bcae045c01464addb
    Signed-off-by: Andrei Homescu <ahomescu@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1336,8 +1336,10 @@ int kvm_reclaim_ffa_guest_pages(struct pkvm_hyp_vm *vm, pkvm_handle_t handle)
 		goto unlock;
 
 	ret = kvm_notify_vm_availability(vm_handle, FFA_VM_DESTRUCTION_MSG);
-	if (ret != FFA_RET_SUCCESS)
+	if (ret != FFA_RET_SUCCESS) {
+		ret = -EAGAIN;
 		goto unlock;
+	}
 
 	list_for_each_entry_safe(transfer, tmp, &ffa_buf->xfer_list, node) {
 		ffa_mem_reclaim(&res,
