ac248357668c48ee1f88abd16faf47b0027176c5
Chenghao Zhao <zhaochenghao@honor.com>
2025-03-18 20:13:24 +0800

    ANDROID: GKI: net: add vendor hook to get the correct classid for net cls
    android_vh_receive_sock -- The sock classid will be modified by current
    task via SCM_RIGHTS, despite the sock's owning process having been assigned
    to a specified cgroup, so we restore the classid with the original process's
    classid.
    
    android_vh_task_get_classid -- In cases where packet transmission involves
    non-serving softirq, execution may occur within the Wi-Fi driver's RX thread
    context. This requires retrieving the classid from the sock associated with
    the sk_buff structure rather than deriving it from the task context.
    
    _trace_android_vh_task_get_classid() -- trace_android_vh_task_get_classid
    called in include/net/cls_cgroup.h will result to build error and visibility
    issue of some struct. So we create wrapper func:_trace_android_vh_task_get_classid
    for include/net/cls_cgroup.h to call.
    
    Bug: 404485061
    Change-Id: I519bcece243f271133bcae2b71e66d54446ebde2
    Signed-off-by: Chenghao Zhao <zhaochenghao@honor.com>
    (cherry picked from commit 17b3b0979f0e61048b85b9b3fee244892941a604)

--- include/trace/hooks/net.h
+++ include/trace/hooks/net.h
@@ -82,6 +82,10 @@ DECLARE_HOOK(android_vh_tcp_rcv_established_slow_path,
 	TP_PROTO(struct sock *sk), TP_ARGS(sk));
 DECLARE_HOOK(android_vh_tcp_rcv_rtt_update,
 	TP_PROTO(struct tcp_sock *tp, u32 sample, int win_dep), TP_ARGS(tp, sample, win_dep));
+DECLARE_HOOK(android_vh_receive_sock,
+	TP_PROTO(struct sock *sk), TP_ARGS(sk));
+DECLARE_HOOK(android_vh_task_get_classid,
+	TP_PROTO(const struct sk_buff *skb, u32 *classid), TP_ARGS(skb, classid));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_NET_VH_H */
