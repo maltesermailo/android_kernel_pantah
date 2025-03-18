4863ebf04b87717068f1f8a724aa36bca3396749
Takaya Saeki <takayas@chromium.org>
2025-03-18 08:31:39 +0000

    UPSTREAM: selinux: support wildcard match in genfscon
    Currently, genfscon only supports string prefix match to label files.
    Thus, labeling numerous dynamic sysfs entries requires many specific
    path rules. For example, labeling device paths such as
    `/sys/devices/pci0000:00/0000:00:03.1/<...>/0000:04:00.1/wakeup`
    requires listing all specific PCI paths, which is challenging to
    maintain. While user-space restorecon can handle these paths with
    regular expression rules, relabeling thousands of paths under sysfs
    after it is mounted is inefficient compared to using genfscon.
    
    This commit adds wildcard matching to genfscon to make rules more
    efficient and expressive. This new behavior is enabled by
    genfs_seclabel_wildcard capability. With this capability, genfscon does
    wildcard matching instead of prefix matching. When multiple wildcard
    rules match against a path, then the longest rule (determined by the
    length of the rule string) will be applied. If multiple rules of the
    same length match, the first matching rule encountered in the given
    genfscon policy will be applied. Users are encouraged to write longer,
    more explicit path rules to avoid relying on this behavior.
    
    This change resulted in nice real-world performance improvements. For
    example, boot times on test Android devices were reduced by 15%. This
    improvement is due to the elimination of the "restorecon -R /sys" step
    during boot, which takes more than two seconds in the worst case.
    
    Signed-off-by: Takaya Saeki <takayas@chromium.org>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    Bug: 455849277
    Bug: 428091536
    Bug: 454745976
    Change-Id: Icf60e899654e0cfcc8e3b9a7ea8b6927bf958549
    (cherry picked from commit 8716451a4e57cc82f3656d7a71b67d3b5831ef3f)
    Signed-off-by: Giuliano Procida <gprocida@google.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -48,6 +48,7 @@
 #include <linux/audit.h>
 #include <linux/vmalloc.h>
 #include <linux/lsm_hooks.h>
+#include <linux/parser.h>
 #include <net/netlabel.h>
 
 #include "flask.h"
@@ -2861,6 +2862,7 @@ static inline int __security_genfs_sid(struct selinux_policy *policy,
 	struct genfs *genfs;
 	struct ocontext *c;
 	int cmp = 0;
+	bool wildcard;
 
 	while (path[0] == '/' && path[1] == '/')
 		path++;
@@ -2877,11 +2879,20 @@ static inline int __security_genfs_sid(struct selinux_policy *policy,
 	if (!genfs || cmp)
 		return -ENOENT;
 
+	wildcard = ebitmap_get_bit(&policy->policydb.policycaps,
+				   POLICYDB_CAP_GENFS_SECLABEL_WILDCARD);
 	for (c = genfs->head; c; c = c->next) {
-		size_t len = strlen(c->u.name);
-		if ((!c->v.sclass || sclass == c->v.sclass) &&
-		    (strncmp(c->u.name, path, len) == 0))
-			break;
+		if (!c->v.sclass || sclass == c->v.sclass) {
+			if (wildcard) {
+				if (match_wildcard(c->u.name, path))
+					break;
+			} else {
+				size_t len = strlen(c->u.name);
+
+				if ((strncmp(c->u.name, path, len)) == 0)
+					break;
+			}
+		}
 	}
 
 	if (!c)
