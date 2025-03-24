c54d232c1641e7151e7c61ee8b6844bb1ad78cb6
Qianfeng Rong <rongqianfeng@vivo.corp-partner.google.com>
2025-03-24 20:05:01 +0800

    ANDROID: vendor hooks: Add hooks to adjust page alloc water mark
    When allocating memory, we can adjust the water level to open a fast
    allocation channel for some important foreground processes, while
    allowing less important background processes to wait.
    
    Bug: 405889864
    
    Change-Id: I71d709c8c782aa614d2c9e1d3150ce14a0106acb
    Signed-off-by: Qianfeng Rong <rongqianfeng@vivo.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -448,3 +448,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_kcompactd_cpu_online);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_direct_reclaim_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_direct_reclaim_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_read_fault);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_alloc_pages_adjust_wmark);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_alloc_pages_reset_wmark);
