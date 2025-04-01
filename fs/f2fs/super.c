2929206cc02e7513ac0efaeba0284e8958687a09
Rui Chen <chenrui9@honor.com>
2025-04-01 10:34:14 +0800

    ANDROID: GKI: Add vendor hook define for f2fs
    Add hook to support a runtime hot/cold data separation method
    for f2fs, aiming to improve the accuracy of data temperature classification and reduce garbage collection overhead after long-term data updates.
    
    Bug: 404731793
    Change-Id: Iaa4ff3490e43fe66cf0ec7fea2708f81013a2dbf
    Signed-off-by: Rui Chen <chenrui9@honor.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1687,6 +1687,8 @@ static void f2fs_put_super(struct super_block *sb)
 	 */
 	f2fs_destroy_stats(sbi);
 
+	trace_android_vh_f2fs_hc_manager_destroy(sbi);
+
 	/* destroy f2fs internal modules */
 	f2fs_destroy_node_manager(sbi);
 	f2fs_destroy_segment_manager(sbi);
@@ -2545,6 +2547,8 @@ static int f2fs_remount(struct super_block *sb, int *flags, char *data)
 			goto restore_checkpoint;
 		}
 	}
+
+	trace_android_vh_f2fs_hc_manager_restart(sbi);
 
 skip:
 #ifdef CONFIG_QUOTA
@@ -4846,6 +4850,7 @@ reset_checkpoint:
 	clear_sbi_flag(sbi, SBI_CP_DISABLED_QUICK);
 
 	cleancache_init_fs(sb);
+	trace_android_vh_f2fs_hc_manager_init(sbi);
 	return 0;
 
 sync_free_meta:
--- include/trace/hooks/fs.h
+++ include/trace/hooks/fs.h
@@ -10,6 +10,8 @@
 #include <trace/hooks/vendor_hooks.h>
 struct f2fs_sb_info;
 struct va_format;
+struct f2fs_io_info;
+enum log_type;
 
 DECLARE_RESTRICTED_HOOK(android_rvh_f2fs_down_read,
 	TP_PROTO(wait_queue_head_t *read_waiters, struct rw_semaphore *rwsem, bool *skip),
@@ -26,6 +28,26 @@ DECLARE_HOOK(android_vh_f2fs_restore_priority,
 DECLARE_HOOK(android_vh_f2fs_printk,
 	TP_PROTO(struct f2fs_sb_info *sbi, struct va_format *vaf, int level, bool limit_rate),
 	TP_ARGS(sbi, vaf, level, limit_rate));
+
+DECLARE_HOOK(android_vh_f2fs_hc_manager_destroy,
+	TP_PROTO(struct f2fs_sb_info *sbi),
+	TP_ARGS(sbi));
+
+DECLARE_HOOK(android_vh_f2fs_hotness_decide,
+	TP_PROTO(struct f2fs_io_info *fio, enum log_type *type),
+	TP_ARGS(fio, type));
+
+DECLARE_HOOK(android_vh_f2fs_hotness_maintain,
+	TP_PROTO(struct f2fs_io_info *fio, enum log_type *type),
+	TP_ARGS(fio, type));
+
+DECLARE_HOOK(android_vh_f2fs_hc_manager_restart,
+	TP_PROTO(struct f2fs_sb_info *sbi),
+	TP_ARGS(sbi));
+
+DECLARE_HOOK(android_vh_f2fs_hc_manager_init,
+	TP_PROTO(struct f2fs_sb_info *sbi),
+	TP_ARGS(sbi));
 
 DECLARE_HOOK(android_vh_wb_dirty_limits,
 	TP_PROTO(unsigned long *thresh, struct bdi_writeback *wb),
