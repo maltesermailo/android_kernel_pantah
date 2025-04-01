46fe26c5cc0c0bc80c323c1b54d1d88e791e47ca
Andrei Homescu <ahomescu@google.com>
2025-04-01 23:52:27 +0000

    ANDROID: KVM: Handle SMCs trapped to EL2
    Catch SMCs and forward them to the HVC handler
    so we support both SMC and HVC conduits for
    calls that follow the SMCCC.
    
    Bug: 379677575
    Change-Id: Ie90dff8811469658a0768583993f942265a53c31
    Signed-off-by: Andrei Homescu <ahomescu@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -1739,3 +1739,31 @@ bool kvm_handle_pvm_hvc64(struct kvm_vcpu *vcpu, u64 *exit_code)
 	smccc_set_retval(vcpu, val[0], val[1], val[2], val[3]);
 	return true;
 }
+
+/*
+ * Handler for protected VM SMC calls.
+ *
+ * Returns true if the hypervisor has handled the exit, and control should go
+ * back to the guest, or false if it hasn't.
+ */
+bool kvm_handle_pvm_smc64(struct kvm_vcpu *vcpu, u64 *exit_code)
+{
+	bool res;
+
+	res = kvm_handle_pvm_hvc64(vcpu, exit_code);
+
+	/*
+	 * From the Architecture Reference Manual:
+	 *   If an SMC instruction executed at Non-secure EL1 is trapped to EL2
+	 *   because HCR_EL2.TSC is 1, the exception is a Trap exception, not
+	 *   a Secure Monitor Call exception, and so SPSR_ELx.SS is set to 1, not 0.
+	 *   [...] Exceptions whose preferred return address is the address of
+	 *   the instruction to be stepped.
+	 *
+	 * This means that the PC is on the SMC instruction, and we need to
+	 * advance it manually to the next instruction.
+	 */
+	__kvm_skip_instr(vcpu);
+
+	return res;
+}
