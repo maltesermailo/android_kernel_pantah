33eadf8635513d4e80d0b7e3553fd352b0a5dd84
Max Wang <max.wang@unisoc.com>
2025-04-07 13:51:57 +0800

    ANDROID: fix missing hrtimers_cpu_starting in CPU Online process when the device is awakened from a deep state
    When merging android13-5.15-178, we did MSR(Monkey+Sleep+Reboot)test with the userdebug version, a kernel crash occured.
    The following is the call statck of the CPU core that hung:
    
    PID:4241 TASK:ffffff80db04b780 CPU:4 COMMOND:"binder:452_3"
    __switch_to at ffffffc00813b7ec
    __schedule at ffffffc0093dbbf8
    schedule at ffffffc0093dc188
    usleep_range_state at ffffffc0093e4844
    sipa_sys_do_power_on_cb_v3 at ffffffc0016ad194[sipa_sys]
    sipa_sys_pd_power_on at ffffffc0016ac75c[sipa_sys]
    genpd_sync_power_on at ffffffc008afc37c
    gend_resume_noirq at ffffffc008af994
    dpm_run_callback at ffffffc008af3028
    __device_resume_noirq at ffffffc008af2c2c
    dpm_noirq_resume_devices at ffffffc008af03ac
    dpm_resume_noirq at ffffffc008af02e4
    suspend_devices_and_enter at ffffffc0082a1694
    enter_state at ffffffc0082a1d78
    pm_suspend at ffffffc0082a1c64
    
    Locally, we have identified the steps to reproduct the issuse by putting the device into deep
    mode, and the phenomenon occurs when the device is awakened. Or we can just reproduce the issue with
    the following two steps: for example, echo 0 > /sys/devices/system/cpu/cpu4/online, plug-out cpu4, then
    echo 1 > /sys/devices/system/cpu/cpu4/online, plug-in cpu4.
    
    We have checked the following two commits and the CPU hotplug code flow
    1. https://android.googlesource.com/kernel/common/+/6fcbcc6c8e52650749692c7613cbe71bf601670d
    2. https://android.googlesource.com/kernel/common/+/15b453db41d36184cf0ccc21e7df624014ab6a1a
    
    We found that in android13-5.15-178, the newly added hrtimers_cpu_starting method is only exeuted in hrtimers_init,
    which is only called in start_kernel. However, in the CPU hotplug process the deep sleep wakeup involved, when executing
    the CPU online process, hrtimers_cpu_startintg is not invoked. Previously, in version before android13-5.15-178, hrtimers_cpu_startintg
    was located in hrtimer_prepare_cpu, which was called during the CPU online flow when the state reached [CPUHP_HRTIMER_PREPARE].
    Therefore, it can be seen that in android13-5.15-178, hrtimers_cpu_starting is missed when a device waken form deep sleep,
    while hrtimers_dead_cpu is invoked in CPU offline flow normally.
    
    We need to place hrtimer_cpu_starting into an appropriate process to ensure that in can also be executed during the CPU online process.
    
    Bug: 407861080
    Change-Id: I8f66a9e0098e4eb37ab09070e1acdff0414bc0c8
    Signed-off-by: Max Wang <max.wang@unisoc.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1757,7 +1757,7 @@ static struct cpuhp_step cpuhp_hp_states[] = {
 	[CPUHP_HRTIMERS_PREPARE] = {
 		.name			= "hrtimers:prepare",
 		.startup.single		= hrtimers_prepare_cpu,
-		.teardown.single	= hrtimers_dead_cpu,
+		.teardown.single	= NULL,
 	},
 	[CPUHP_SMPCFD_PREPARE] = {
 		.name			= "smpcfd:prepare",
@@ -1823,6 +1823,11 @@ static struct cpuhp_step cpuhp_hp_states[] = {
 		.name			= "smpcfd:dying",
 		.startup.single		= NULL,
 		.teardown.single	= smpcfd_dying_cpu,
+	},
+	[CPUHP_AP_HRTIMERS_DYING] = {
+		.name			= "hrtimers:dying",
+		.startup.single		= hrtimers_cpu_starting,
+		.teardown.single	= hrtimers_cpu_dying,
 	},
 	/* Entry state on starting. Interrupts enabled from here on. Transient
 	 * state for synchronsization */
