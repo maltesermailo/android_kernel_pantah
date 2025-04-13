3101d2618c56f7c1b2aa8566883102aec7ed5f2d
Deepa Dinamani <deepadinamani@google.com>
2025-04-13 16:53:35 -0700

    ANDROID: experiments
    WIP: DO NOT SUBMIT
    
    Add sysfs knobs to toggle various platform experiments
    
    Bug: 410302304
    Test: build and boot with virtual device
    Change-Id: I894f9c9cea4bfaa39e37500bf4efbff70fb7a580
    Signed-off-by: Deepa Dinamani <deepadinamani@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -84,6 +84,7 @@
 #include <trace/hooks/typec.h>
 #include <trace/events/android_vendor_lmk.h>
 #include <trace/hooks/swapfile.h>
+#include <trace/hooks/experiments.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
