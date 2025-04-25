35619342b9d620a48b3b867da488ca804b1e23c2
Huan Yang <link@vivo.com>
2025-04-25 11:19:25 +0800

    UPSTREAM: mm/memcg: use kmem_cache when alloc memcg pernode info
    When tracing mem_cgroup_per_node allocations with kmalloc ftrace:
    
    kmalloc: call_site=mem_cgroup_css_alloc+0x1d8/0x5b4 ptr=00000000d798700c
        bytes_req=2896 bytes_alloc=4096 gfp_flags=GFP_KERNEL|__GFP_ZERO node=0
        accounted=false
    
    This reveals the slab allocator provides 4096B chunks for 2896B
    mem_cgroup_per_node due to:
    
    1. The slab allocator predefines bucket sizes from 64B to 8096B
    2. The mem_cgroup allocation size (2312B) falls between the 2KB and 4KB
       slabs
    3. The allocator rounds up to the nearest larger slab (4KB), resulting in
       ~1KB wasted memory per memcg alloc - per node.
    
    This patch introduces a dedicated kmem_cache for mem_cgroup structs,
    achieving precise memory allocation. Post-patch ftrace verification shows:
    
    kmem_cache_alloc: call_site=mem_cgroup_css_alloc+0x1b8/0x5d4
        ptr=000000002989e63a bytes_req=2896 bytes_alloc=2944
        gfp_flags=GFP_KERNEL|__GFP_ZERO node=0 accounted=false
    
    Each mem_cgroup_per_node alloc 2944bytes(include hw cacheline align),
    compare to 4096, it avoid waste.
    
    Link: https://lkml.kernel.org/r/20250425031935.76411-4-link@vivo.com
    Signed-off-by: Huan Yang <link@vivo.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Francesco Valla <francesco@valla.it>
    Cc: guoweikang <guoweikang.kernel@gmail.com>
    Cc: Huang Shijie <shijie@os.amperecomputing.com>
    Cc: KP Singh <kpsingh@kernel.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: "Paul E . McKenney" <paulmck@kernel.org>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Raul E Rangel <rrangel@chromium.org>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: "Uladzislau Rezki (Sony)" <urezki@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 1b6a58e205ed0bbeeeca46388f0649f322b04f06)
    Change-Id: I83a4f675dd8b539a8a40617cc1c1f10e3588e955
    Bug: 417296244
    Signed-off-by: T.J. Mercier <tjmercier@google.com>
    (cherry picked from commit 125f87a148b7c514e99e998707d7ce1a5f5291f9)

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -95,6 +95,9 @@ static bool cgroup_memory_nokmem __ro_after_init;
 /* BPF memory accounting disabled? */
 static bool cgroup_memory_nobpf __ro_after_init;
 
+static struct kmem_cache *memcg_cachep;
+static struct kmem_cache *memcg_pn_cachep;
+
 #ifdef CONFIG_CGROUP_WRITEBACK
 static DECLARE_WAIT_QUEUE_HEAD(memcg_cgwb_frn_waitq);
 #endif
@@ -5378,7 +5381,8 @@ static int alloc_mem_cgroup_per_node_info(struct mem_cgroup *memcg, int node)
 {
 	struct mem_cgroup_per_node *pn;
 
-	pn = kzalloc_node(sizeof(*pn), GFP_KERNEL, node);
+	pn = kmem_cache_alloc_node(memcg_pn_cachep, GFP_KERNEL | __GFP_ZERO,
+				   node);
 	if (!pn)
 		return 1;
 
@@ -7625,6 +7629,13 @@ static int __init mem_cgroup_init(void)
 	for_each_possible_cpu(cpu)
 		INIT_WORK(&per_cpu_ptr(&memcg_stock, cpu)->work,
 			  drain_local_stock);
+
+	memcg_size = struct_size_t(struct mem_cgroup, nodeinfo, nr_node_ids);
+	memcg_cachep = kmem_cache_create("mem_cgroup", memcg_size, 0,
+					 SLAB_PANIC | SLAB_HWCACHE_ALIGN, NULL);
+
+	memcg_pn_cachep = KMEM_CACHE(mem_cgroup_per_node,
+				     SLAB_PANIC | SLAB_HWCACHE_ALIGN);
 
 	for_each_node(node) {
 		struct mem_cgroup_tree_per_node *rtpn;
