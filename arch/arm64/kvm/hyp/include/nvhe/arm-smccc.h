55c1a1409964ea43e3f85783ca200de3eb71c9b5
Per Larsen <perlarsen@google.com>
2025-05-02 00:16:15 +0000

    ANDROID: KVM: arm64: Add SMCCC 1.2 calls to hyp events
    Add a new macro to ensure that SMCs can be traced no matter what calling
    convention they happen to use.
    
    Bug: 280886201
    Change-Id: I2f837263f64fc34e6562105b3eeea6895edc7461
    Signed-off-by: Per Larsen <perlarsen@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/arm-smccc.h
+++ arch/arm64/kvm/hyp/include/nvhe/arm-smccc.h
@@ -11,3 +11,15 @@
 		__arm_smccc_1_1(SMCCC_SMC_INST, __VA_ARGS__);	\
 		__hyp_enter();					\
 	} while (0)
+
+/*
+ * arm_smccc_1_1_smc is a macro around __arm_smccc_1_1 but arm_smccc_1_2_smc is
+ * a function so we cannot follow the pattern used to wrap arm_smccc_1_1_smc.
+ * Hence the nvhe prefix.
+ */
+#define nvhe_arm_smccc_1_2_smc(args, regs)                     \
+       do {                                                    \
+               __hyp_exit();                                   \
+               arm_smccc_1_2_smc(args, regs);                  \
+	       __hyp_enter();                                  \
+       } while (0)
