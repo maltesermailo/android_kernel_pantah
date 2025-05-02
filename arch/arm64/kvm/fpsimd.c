3375e7b30560640d9e5c0fab7d5991f3351df4b5
Fuad Tabba <tabba@google.com>
2025-05-02 09:19:58 +0100

    ANDROID: KVM: arm64: Don't save/flush fpsimd/sve state at host in pKVM
    pKVM saves and restores the host fpsimd/sve state at the hypervisor. No
    need to duplicate the effort and also save it at the host.
    
    Bug: 357781595
    Bug: 411040189
    Change-Id: If0a5a8d2eb2e45eade3b29cecf43d9f129ed314a
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/fpsimd.c
+++ arch/arm64/kvm/fpsimd.c
@@ -62,8 +62,10 @@ void kvm_arch_vcpu_load_fp(struct kvm_vcpu *vcpu)
 	 * When the host may use SME, fpsimd_save_and_flush_cpu_state() ensures
 	 * that PSTATE.{SM,ZA} == {0,0}.
 	 */
-	fpsimd_save_and_flush_cpu_state();
-	*host_data_ptr(fp_owner) = FP_STATE_FREE;
+	if (!is_protected_kvm_enabled()) {
+		fpsimd_save_and_flush_cpu_state();
+		*host_data_ptr(fp_owner) = FP_STATE_FREE;
+	}
 
 	/*
 	 * If normal guests gain SME support, maintain this behavior for pKVM
