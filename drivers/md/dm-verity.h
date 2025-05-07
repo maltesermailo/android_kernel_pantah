0a9d23323faa17f8f6901ad2618a4764a5a3fee4
Eran Messeri <eranm@google.com>
2025-05-07 14:11:04 +0100

    dm-verity: Remove use of BH workqueue
    Remove the use of the BH workqueue. Instead, perform verification
    in-line when the right conditions are met (block size is less than 8k,
    etc). Queue work in the verity workqueue if verification should not be
    performed in-line.
    
    Additionally, rename the "in_bh" / "use_bh" fields to indicate the
    work is done in-line, not in the "bottom half" (as a synonym to
    softirq). In certain situations (e.g. dm-verity on top of a loopback
    device) the verification will not be done in a softirq context but in a
    task context.
    
    ANDROID: Will be upstreamed soon.
    Signed-off-by: Eran Messeri <eranm@google.com>
    Change-Id: Ic3330a58df1ce6021f5eadace02c58ad814479ec

--- drivers/md/dm-verity.h
+++ drivers/md/dm-verity.h
@@ -60,7 +60,7 @@ struct dm_verity {
 	unsigned char levels;	/* the number of tree levels */
 	unsigned char version;
 	bool hash_failed:1;	/* set if hash of any block failed */
-	bool use_bh_wq:1;	/* try to verify in BH wq before normal work-queue */
+	bool verify_inline:1;	/* try to verify inline before normal work-queue */
 	unsigned char mb_max_msgs; /* max multibuffer hashing interleaving factor */
 	unsigned int digest_size;	/* digest size for the current hash algorithm */
 	unsigned int hash_reqsize; /* the size of temporary space for crypto */
@@ -101,11 +101,11 @@ struct dm_verity_io {
 
 	sector_t block;
 	unsigned int n_blocks;
-	bool in_bh;
+	/* true if the block is verified in-line rather than a workqueue. */
+	bool verifying_inline;
 	bool had_mismatch;
 
 	struct work_struct work;
-	struct work_struct bh_work;
 
 	u8 tmp_digest[HASH_MAX_DIGESTSIZE];
 
