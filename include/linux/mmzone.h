f644d55434ac73806a466791307da411d995f423
Suren Baghdasaryan <surenb@google.com>
2025-05-13 23:00:03 +0000

    ANDROID: GKI: Add ABI padding for kcompressd feature
    kcompressd is being upstreamed by a partner at [1]. We will likely need
    to backport if when it's accepted upstream.
    
    [1] https://lore.kernel.org/lkml/20250430082651.3152444-1-qun-wei.lin@mediatek.com/#r
    
    Bug: 417475166
    Change-Id: I75d0497ad38ab9a90913e4417d2f2c9a66144335
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -1466,6 +1466,7 @@ typedef struct pglist_data {
 #ifdef CONFIG_MEMORY_FAILURE
 	struct memory_failure_stats mf_stats;
 #endif
+	ANDROID_BACKPORT_RESERVE(1);
 } pg_data_t;
 
 #define node_present_pages(nid)	(NODE_DATA(nid)->node_present_pages)
