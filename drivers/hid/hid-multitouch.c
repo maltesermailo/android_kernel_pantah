a9f88d31653eaba026f902f0476d8ea3c73759b6
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2025-05-28 15:16:35 +0530

    UPSTREAM: HID: multitouch: make mt_set_mode() less cryptic
    mt_set_mode() accepts 2 boolean switches indicating whether the device
    (if it follows Windows Precision Touchpad specification) should report
    hardware buttons and/or surface contacts. For a casual reader it is
    completely not clear, as they look at the call site, which exact mode
    is being requested.
    
    Define report_mode enum and change mt_set_mode() to accept is as
    an argument instead. This allows to write:
    
            mt_set_modes(hdev, HID_LATENCY_NORMAL, TOUCHPAD_REPORT_ALL);
    
    or
    
            mt_set_modes(hdev, HID_LATENCY_HIGH, TOUCHPAD_REPORT_BUTTONS);
    
    which makes intent much more clear.
    
    Link: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [cherry-picked from commit-id: e8a0581914bd2e28f7af8d333ddc73fd78b1ef84]
    
    Bug: None
    Test: ISH firmware is loaded, mosaic sensor card enumerates and the Sensor HAL is up.
    
    Change-Id: Ib0c6eb16ec083639fa9fe5db55f97a4e2111b1ed
    Reviewed-by: Benjamin Tissoires <bentiss@kernel.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Link: https://patch.msgid.link/Zx_hBvg5Qa3KU3ta@google.com
    Signed-off-by: Benjamin Tissoires <bentiss@kernel.org>
    Signed-off-by: rnaidu <ramya.v.naidu@intel.com>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -31,6 +31,7 @@
  * [1] https://gitlab.freedesktop.org/libevdev/hid-tools
  */
 
+#include <linux/bits.h>
 #include <linux/device.h>
 #include <linux/delay.h>
 #include <linux/dmi.h>
@@ -84,6 +85,13 @@ MODULE_LICENSE("GPL");
 enum latency_mode {
 	HID_LATENCY_NORMAL = 0,
 	HID_LATENCY_HIGH = 1,
+};
+
+enum report_mode {
+       TOUCHPAD_REPORT_NONE = 0,
+       TOUCHPAD_REPORT_BUTTONS = BIT(0),
+       TOUCHPAD_REPORT_CONTACTS = BIT(1),
+       TOUCHPAD_REPORT_ALL = TOUCHPAD_REPORT_BUTTONS | TOUCHPAD_REPORT_CONTACTS,
 };
 
 #define MT_IO_FLAGS_RUNNING		0
@@ -1673,8 +1681,7 @@ static bool mt_need_to_apply_feature(struct hid_device *hdev,
 				     struct hid_field *field,
 				     struct hid_usage *usage,
 				     enum latency_mode latency,
-				     bool surface_switch,
-				     bool button_switch,
+				     enum report_mode report_mode,
 				     bool *inputmode_found)
 {
 	struct mt_device *td = hid_get_drvdata(hdev);
@@ -1729,11 +1736,11 @@ static bool mt_need_to_apply_feature(struct hid_device *hdev,
 		return true;
 
 	case HID_DG_SURFACESWITCH:
-		field->value[index] = surface_switch;
+		field->value[index] = !!(report_mode & TOUCHPAD_REPORT_CONTACTS);
 		return true;
 
 	case HID_DG_BUTTONSWITCH:
-		field->value[index] = button_switch;
+		field->value[index] = !!(report_mode & TOUCHPAD_REPORT_BUTTONS);
 		return true;
 	}
 
@@ -1741,7 +1748,7 @@ static bool mt_need_to_apply_feature(struct hid_device *hdev,
 }
 
 static void mt_set_modes(struct hid_device *hdev, enum latency_mode latency,
-			 bool surface_switch, bool button_switch)
+			 enum report_mode report_mode)
 {
 	struct hid_report_enum *rep_enum;
 	struct hid_report *rep;
@@ -1766,8 +1773,7 @@ static void mt_set_modes(struct hid_device *hdev, enum latency_mode latency,
 							     rep->field[i],
 							     usage,
 							     latency,
-							     surface_switch,
-							     button_switch,
+							     report_mode,
 							     &inputmode_found))
 					update_report = true;
 			}
@@ -2017,7 +2023,7 @@ static int mt_probe(struct hid_device *hdev, const struct hid_device_id *id)
 		dev_warn(&hdev->dev, "Cannot allocate sysfs group for %s\n",
 				hdev->name);
 
-	mt_set_modes(hdev, HID_LATENCY_NORMAL, true, true);
+	mt_set_modes(hdev, HID_LATENCY_NORMAL, TOUCHPAD_REPORT_ALL);
 
 	return 0;
 }
@@ -2035,9 +2041,9 @@ static int mt_suspend(struct hid_device *hdev, pm_message_t state)
 	/* High latency is desirable for power savings during S3/S0ix */
 	if ((td->mtclass.quirks & MT_QUIRK_DISABLE_WAKEUP) ||
 	    !hid_hw_may_wakeup(hdev))
-		mt_set_modes(hdev, HID_LATENCY_HIGH, false, false);
+		mt_set_modes(hdev, HID_LATENCY_HIGH, TOUCHPAD_REPORT_NONE);
 	else
-		mt_set_modes(hdev, HID_LATENCY_HIGH, true, true);
+		mt_set_modes(hdev, HID_LATENCY_HIGH, TOUCHPAD_REPORT_ALL);
 	mutex_unlock(&td->mode_mutex);
 
 	return 0;
@@ -2050,7 +2056,7 @@ static int mt_reset_resume(struct hid_device *hdev)
 	mt_release_contacts(hdev);
 
 	mutex_lock(&td->mode_mutex);
-	mt_set_modes(hdev, HID_LATENCY_NORMAL, true, true);
+	mt_set_modes(hdev, HID_LATENCY_NORMAL, TOUCHPAD_REPORT_ALL);
 	mutex_unlock(&td->mode_mutex);
 
 	return 0;
@@ -2067,7 +2073,7 @@ static int mt_resume(struct hid_device *hdev)
 	hid_hw_idle(hdev, 0, 0, HID_REQ_SET_IDLE);
 
 	mutex_lock(&td->mode_mutex);
-	mt_set_modes(hdev, HID_LATENCY_NORMAL, true, true);
+	mt_set_modes(hdev, HID_LATENCY_NORMAL, TOUCHPAD_REPORT_ALL);
 	mutex_unlock(&td->mode_mutex);
 
 	return 0;
