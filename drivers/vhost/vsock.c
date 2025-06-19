669b31e76ce667c052aca1f39a40037e66c06d69
Will Deacon <will@kernel.org>
2025-06-19 16:52:33 +0100

    ANDROID: vhost/vsock: Allocate nonlinear SKBs for handling large receive buffers
    When receiving a packet from a guest, vhost_vsock_handle_tx_kick()
    calls vhost_vsock_alloc_skb() to allocate and fill an SKB with the
    receive data. Unfortunately, these are always linear allocations and can
    therefore result in significant pressure on kmalloc() considering that
    the maximum packet size (VIRTIO_VSOCK_MAX_PKT_BUF_SIZE +
    VIRTIO_VSOCK_SKB_HEADROOM) is a little over 64KiB.
    
    Rework the vsock SKB allocation so that, for buffers larger than a page,
    a nonlinear SKB is allocated instead with the packet header in the SKB
    and the receive data in the fragment pages.
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Bug: 421244320
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: I4212a8daf9f19b5bbffc06ce93338c823de7bb19

--- drivers/vhost/vsock.c
+++ drivers/vhost/vsock.c
@@ -344,11 +344,16 @@ vhost_vsock_alloc_skb(struct vhost_virtqueue *vq,
 
 	len = iov_length(vq->iov, out);
 
-	if (len > VIRTIO_VSOCK_MAX_PKT_BUF_SIZE + VIRTIO_VSOCK_SKB_HEADROOM)
+	if (len < VIRTIO_VSOCK_SKB_HEADROOM ||
+	    len > VIRTIO_VSOCK_MAX_PKT_BUF_SIZE + VIRTIO_VSOCK_SKB_HEADROOM)
 		return NULL;
 
 	/* len contains both payload and hdr */
-	skb = virtio_vsock_alloc_skb(len, GFP_KERNEL);
+	if (len > SKB_WITH_OVERHEAD(PAGE_SIZE))
+		skb = virtio_vsock_alloc_skb_with_frags(len, GFP_KERNEL);
+	else
+		skb = virtio_vsock_alloc_skb(len, GFP_KERNEL);
+
 	if (!skb)
 		return NULL;
 
@@ -377,10 +382,8 @@ vhost_vsock_alloc_skb(struct vhost_virtqueue *vq,
 
 	virtio_vsock_skb_rx_put(skb);
 
-	nbytes = copy_from_iter(skb->data, payload_len, &iov_iter);
-	if (nbytes != payload_len) {
-		vq_err(vq, "Expected %zu byte payload, got %zu bytes\n",
-		       payload_len, nbytes);
+	if (skb_copy_datagram_from_iter(skb, 0, &iov_iter, payload_len)) {
+		vq_err(vq, "Failed to copy %zu byte payload\n", payload_len);
 		kfree_skb(skb);
 		return NULL;
 	}
