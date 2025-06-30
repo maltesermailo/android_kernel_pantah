3edfd311668663fe90cbbf56208632ec8f2c91e8
Jason Le <jason.v.le@intel.corp-partner.google.com>
2025-06-30 15:10:55 -0700

    ANDROID: pkvm: x86: Leave ATS enabled when enabling pKVM.
    ATS enabling is required for fatcat SOC.
    Bug: 422532017
    Test: Boot on fatcat with pKVM enabled.
    
    Change-Id: I29cd7ad69cc833c586c96319f2c5b1250517e3c3
    Signed-off-by: Jason Le <jason.v.le@intel.corp-partner.google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu.c
@@ -501,9 +501,6 @@ static bool sync_shadow_context_entry(struct id_sync_data *sdata)
 		if (sdata->guest_ptep && sdata->shadow_pa) {
 			tmp.hi = guest_ce->hi;
 			tmp.lo = sdata->shadow_pa | (guest_ce->lo & 0xfff);
-
-			/* Clear DTE to make sure device TLB is disabled for security */
-			context_sm_clear_dte(&tmp);
 		}
 	} else {
 		/*
