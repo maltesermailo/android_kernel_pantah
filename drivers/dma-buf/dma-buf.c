67f448b3596476557c60f92cc043f4ddc9d4c92b
T.J. Mercier <tjmercier@google.com>
2025-07-01 00:51:35 +0000

    ANDROID: Add dmabuf RSS trace event
    Dmabuf RSS is associated with a task, or group of tasks sharing the same
    mm_struct and files_struct. Any time the RSS counter is modified for a
    task, or group of tasks, emit a trace event with the current value of
    the dmabuf RSS counter.
    
    This allows for fast tracking of per-process dmabuf RSS by userspace
    analysis tools like Perfetto, compared to periodically obtaining
    per-process dmabuf RSS from procfs.
    
    Bug: 424646615
    Change-Id: I74434dddacc342918cb52b1b9e2fa6679e332764
    Signed-off-by: T.J. Mercier <tjmercier@google.com>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -31,6 +31,7 @@
 #include <uapi/linux/dma-buf.h>
 #include <uapi/linux/magic.h>
 
+#include <trace/events/kmem.h>
 #include <trace/hooks/dmabuf.h>
 
 #include "dma-buf-sysfs-stats.h"
@@ -190,6 +191,7 @@ static int new_task_dmabuf_record(struct task_struct *task, struct dma_buf *dmab
 		return -ENOMEM;
 
 	rss = atomic64_add_return(dmabuf->size, &task->dmabuf_info->rss);
+	trace_dmabuf_rss_stat(rss, dmabuf->size, dmabuf);
 	/*
 	 * task->dmabuf_info->lock protects against concurrent writers, so no
 	 * worries about stale rss_hwm between the read and write, and we don't
@@ -257,6 +259,7 @@ int dma_buf_account_task(struct dma_buf *dmabuf, struct task_struct *task)
 void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_struct *task)
 {
 	struct task_dma_buf_record *rec;
+	s64 rss;
 
 	WARN_ON(!dmabuf);
 	WARN_ON(!task);
@@ -270,7 +273,8 @@ void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_struct *task)
 	if (--rec->refcnt == 0) {
 		list_del(&rec->node);
 		kfree(rec);
-		atomic64_sub(dmabuf->size, &task->dmabuf_info->rss);
+		rss = atomic64_sub_return(dmabuf->size, &task->dmabuf_info->rss);
+		trace_dmabuf_rss_stat(rss, -dmabuf->size, dmabuf);
 		atomic64_dec(&dmabuf->num_unique_refs);
 	}
 err:
