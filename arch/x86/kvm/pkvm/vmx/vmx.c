9a6de413dc4b1be6c1ec2ddc4fb8b10c4c28e5e7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-07-01 13:54:17 +0800

    ANDROID: pkvm: vmx: Flush l1d for the guest in the pkvm hypervisor
    The l1d flushing before entering the guest was done by the host if this
    is required. As the host is untrusted, move the l1d flushing to the pkvm
    hypervisor so that the flushing can be guaranteed. When a guest vcpu is
    loaded by the host, mark its l1d flushing flag as true so that the l1d
    contents can be flushed before this guest vcpu runs.
    
    Bug: 420681877
    Upstream-Task: 402758258
    
    Change-Id: I7fda3d8d6dcae04d64f69cc7740f29dc321d7cf3
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/vmx/vmx.c
+++ arch/x86/kvm/pkvm/vmx/vmx.c
@@ -6670,6 +6670,7 @@ static noinstr void vmx_vcpu_enter_exit(struct kvm_vcpu *vcpu,
 
 #ifndef __PKVM_HYP__ /* For pkvm hypervisor, leave these to host KVM */
 	guest_state_enter_irqoff();
+#endif
 
 	/*
 	 * L1D Flush includes CPU buffer clear to mitigate MDS, but VERW
@@ -6682,7 +6683,6 @@ static noinstr void vmx_vcpu_enter_exit(struct kvm_vcpu *vcpu,
 	else if (static_branch_unlikely(&mmio_stale_data_clear) &&
 		 kvm_arch_has_assigned_device(vcpu->kvm))
 		mds_clear_cpu_buffers();
-#endif
 
 	vmx_disable_fb_clear(vmx);
 
