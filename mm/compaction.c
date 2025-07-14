ff25d6763d793575a2639a7f13b82d41b5700996
Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>
2025-07-14 16:05:00 +0800

    ANDROID: vendor_hook: Added hook for memory reclaim tuning
    The following hooks are added for memory reclaim tuning:
    
    android_vh_mm_customize_file_is_tiny - For a specific process, we want only
    anonymous pages to be reclaimed. This hook allows vendors to modify the value
    of file_is_tiny to affect the reclaim behavior.
    
    android_vh_mm_customize_pgdat_balanced - When there are multiple zones, it is
    not always reasonable to perform indirect reclaim only when all zones do not
    meet the watermark. This hook allows vendors to customize pgdat_balanced to use
    their own strategy to determine whether to perform and stop indirect collection.
    
    android_vh_mm_customize_reclaim_idx - When there are multiple zones, vendors use
    this hook to customize which zones can be reclaimed.
    
    android_vh_mm_customize_zone_can_compact - When there are multiple zones, vendors
    use this hook to customize which zones can be compacted.
    
    Bug: 431672372
    Bug: 505636392
    Change-Id: Ia4ce31ccc23da0e88187d1d550fcb20037e3025b
    Signed-off-by: Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>
    (cherry picked from commit 6c25b93a26385e5ab85750db953d2fedf5754e0e)

--- mm/compaction.c
+++ mm/compaction.c
@@ -2855,6 +2855,11 @@ enum compact_result try_to_compact_pages(gfp_t gfp_mask, unsigned int order,
 	for_each_zone_zonelist_nodemask(zone, z, ac->zonelist,
 					ac->highest_zoneidx, ac->nodemask) {
 		enum compact_result status;
+		bool can_compact = true;
+
+		trace_android_vh_mm_customize_zone_can_compact(zone, &can_compact);
+		if (!can_compact)
+			continue;
 
 		if (cpusets_enabled() &&
 			(alloc_flags & ALLOC_CPUSET) &&
@@ -2922,8 +2927,14 @@ void compact_node_async(int nid)
 	};
 
 	for (zoneid = 0; zoneid < MAX_NR_ZONES; zoneid++) {
+		bool can_compact = true;
+
 		zone = &pgdat->node_zones[zoneid];
 		if (!populated_zone(zone))
+			continue;
+
+		trace_android_vh_mm_customize_zone_can_compact(zone, &can_compact);
+		if (!can_compact)
 			continue;
 
 		if (fatal_signal_pending(current))
@@ -2961,8 +2972,14 @@ static int compact_node(pg_data_t *pgdat, bool proactive)
 	};
 
 	for (zoneid = 0; zoneid < MAX_NR_ZONES; zoneid++) {
+		bool can_compact = true;
+
 		zone = &pgdat->node_zones[zoneid];
 		if (!populated_zone(zone))
+			continue;
+
+		trace_android_vh_mm_customize_zone_can_compact(zone, &can_compact);
+		if (!can_compact)
 			continue;
 
 		if (fatal_signal_pending(current))
@@ -3093,9 +3110,14 @@ static bool kcompactd_node_suitable(pg_data_t *pgdat)
 		ALLOC_WMARK_HIGH : ALLOC_WMARK_MIN;
 
 	for (zoneid = 0; zoneid <= highest_zoneidx; zoneid++) {
+		bool can_compact = true;
 		zone = &pgdat->node_zones[zoneid];
 
 		if (!populated_zone(zone))
+			continue;
+
+		trace_android_vh_mm_customize_zone_can_compact(zone, &can_compact);
+		if (!can_compact)
 			continue;
 
 		ret = compaction_suit_allocation_order(zone,
@@ -3134,9 +3156,14 @@ static void kcompactd_do_work(pg_data_t *pgdat)
 
 	for (zoneid = 0; zoneid <= cc.highest_zoneidx; zoneid++) {
 		int status;
+		bool can_compact = true;
 
 		zone = &pgdat->node_zones[zoneid];
 		if (!populated_zone(zone))
+			continue;
+
+		trace_android_vh_mm_customize_zone_can_compact(zone, &can_compact);
+		if (!can_compact)
 			continue;
 
 		if (compaction_deferred(zone, cc.order))
