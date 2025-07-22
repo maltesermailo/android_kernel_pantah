393c4f3a8b8a4fdfa00cfcf3e6dd7c1694ff7969
Randall Huang <huangrandall@google.com>
2025-07-22 15:13:02 +0800

    print debug string
    Change-Id: Iee038223680f976989670dcce4d81914f67e094f
    Signed-off-by: Randall Huang <huangrandall@google.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -8795,6 +8795,7 @@ static int ufshcd_probe_hba(struct ufs_hba *hba, bool init_dev_params)
 	struct Scsi_Host *host = hba->host;
 
 	hba->ufshcd_state = UFSHCD_STATE_RESET;
+        pr_info("Randall test build\n");
 
 	ret = ufshcd_link_startup(hba);
 	if (ret)
