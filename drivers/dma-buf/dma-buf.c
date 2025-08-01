bc38793be794596ebfdeea8ff3a779a416a41814
Suren Baghdasaryan <surenb@google.com>
2025-08-01 22:54:08 +0000

    ANDROID: add command line parameter for dmabuf accounting
    Introduce a kernel command line parameter to allow dmabuf accounting to
    be turned off. "dmabuf_accounting" kernel command line parameter is a
    boolean that can control accounting at boot time. By default accounting
    is enabled.
    
    Bug: 424648392
    Bug: 489619376
    Change-Id: Ia203bc9a4dd90339e4c05a8c5fb4702cf2507f67
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -38,6 +38,8 @@
 
 #include "dma-buf-sysfs-stats.h"
 
+DEFINE_STATIC_KEY_TRUE(dmabuf_accounting_key);
+
 static DEFINE_MUTEX(dmabuf_list_mutex);
 static LIST_HEAD(dmabuf_list);
 
@@ -377,6 +379,9 @@ int dma_buf_account_task(struct dma_buf *dmabuf, struct task_dma_buf_info *dmabu
 {
 	struct task_dma_buf_record *rec;
 
+	if (!static_key_enabled(&dmabuf_accounting_key))
+		return 0;
+
 	if (!dmabuf_info)
 		return 0;
 
@@ -412,6 +417,9 @@ int dma_buf_account_task(struct dma_buf *dmabuf, struct task_dma_buf_info *dmabu
 void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_dma_buf_info *dmabuf_info)
 {
 	struct task_dma_buf_record *rec;
+
+	if (!static_key_enabled(&dmabuf_accounting_key))
+		return;
 
 	if (!dmabuf_info)
 		return;
@@ -521,6 +529,9 @@ int copy_dmabuf_info(u64 clone_flags, struct task_struct *task)
 	struct task_dma_buf_info *child_dmabuf_info;
 	bool share_vm = clone_flags & CLONE_VM;
 	bool share_fs = clone_flags & CLONE_FILES;
+
+	if (!static_key_enabled(&dmabuf_accounting_key))
+		return 0;
 
 	/* kthreads are not supported */
 	if (task->flags & PF_KTHREAD) {
@@ -2277,6 +2288,24 @@ static inline void dma_buf_uninit_debugfs(void)
 {
 }
 #endif
+
+static int __init setup_early_dmabuf_accounting(char *str)
+{
+	bool enable;
+
+	if (kstrtobool(str, &enable))
+		return -EINVAL;
+
+	if (enable != static_key_enabled(&dmabuf_accounting_key)) {
+		if (enable)
+			static_branch_enable(&dmabuf_accounting_key);
+		else
+			static_branch_disable(&dmabuf_accounting_key);
+	}
+
+	return 0;
+}
+early_param("dmabuf_accounting", setup_early_dmabuf_accounting);
 
 static int __init dma_buf_init(void)
 {
