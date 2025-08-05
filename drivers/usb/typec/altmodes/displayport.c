50789732f18694b485aa6ca4cc9c3ad1df3088d1
Andrei Kuchynski <akuchynski@chromium.org>
2025-08-05 09:18:25 +0000

    test
    Change-Id: I533f9f6729f504972cb1b0cc16c84d218efd0f19
    Signed-off-by: Andrei Kuchynski <akuchynski@google.com>

--- drivers/usb/typec/altmodes/displayport.c
+++ drivers/usb/typec/altmodes/displayport.c
@@ -783,8 +783,10 @@ int dp_altmode_probe(struct typec_altmode *alt)
 	if (plug)
 		typec_altmode_set_drvdata(plug, dp);
 
+#if 0
 	dp->state = plug ? DP_STATE_ENTER_PRIME : DP_STATE_ENTER;
 	schedule_work(&dp->work);
+#endif
 
 	return 0;
 }
