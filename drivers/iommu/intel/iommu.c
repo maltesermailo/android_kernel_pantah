BayLibre/android_kernel_common

ANDROID: pkvm: x86: flush iotlb within PKVM when doing domain_mapping
We need to flush iotlb after a domain map operation and this could be
done in pkvm thereby avoiding one extra vmexit from host driver.

Bug: 391542119
Test: Build and Boot

Change-Id: I7e26e7174810348823a29c4494f306b821949e39
Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/iommu.c
@@ -4769,8 +4769,10 @@ static bool risky_device(struct pci_dev *pdev)
 static int intel_iommu_iotlb_sync_map(struct iommu_domain *domain,
 				      unsigned long iova, size_t size)
 {
-	cache_tag_flush_range_np(to_dmar_domain(domain), iova, iova + size - 1);
+	if (IS_ENABLED(CONFIG_PKVM_INTEL_PVIOMMU) && pkvm_ia_enabled())
+		return 0;
 
+	cache_tag_flush_range_np(to_dmar_domain(domain), iova, iova + size - 1);
 	return 0;
 }
 

maltesermailo/android_kernel_pantah

drivers/iommu/intel/iommu.c
@@ -4826,8 +4826,10 @@ static bool risky_device(struct pci_dev *pdev)
 static int intel_iommu_iotlb_sync_map(struct iommu_domain *domain,
 				      unsigned long iova, size_t size)
 {
-	cache_tag_flush_range_np(to_dmar_domain(domain), iova, iova + size - 1);
+	if (IS_ENABLED(CONFIG_PKVM_INTEL_PVIOMMU) && pkvm_enabled())
+		return 0;
 
+	cache_tag_flush_range_np(to_dmar_domain(domain), iova, iova + size - 1);
 	return 0;
 }
 