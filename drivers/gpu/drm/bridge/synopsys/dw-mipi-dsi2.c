5f3ad73495fb730bc55e6b78d3628b1b1e8a04f4
Derick Hong <derickhong@google.com>
2025-08-18 02:29:18 -0700

    Add debug print for MIPI DSI2 escape clock calculation.
    Change-Id: Ied00785250a051753dfaa25635598ffa2e23ab26
    Signed-off-by: Derick Hong <derickhong@google.com>

--- drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi2.c
+++ drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi2.c
@@ -310,6 +310,8 @@ static void dw_mipi_dsi2_phy_clk_mode_cfg(struct dw_mipi_dsi2 *dsi2)
 	val |= NON_CONTINUOUS_CLK;
 
 	/* The maximum value of the escape clock frequency is 20MHz */
+	dev_err(dsi2->dev, "[DEBUG_LOG]: sys_clk=%u, esc_clk_div=%u, %lu\n",
+					sys_clk, esc_clk_div, PHY_LPTX_CLK_DIV(esc_clk_div));
 	sys_clk = clk_get_rate(dsi2->sys_clk) / USEC_PER_SEC;
 	esc_clk_div = DIV_ROUND_UP(sys_clk, 20 * 2);
 	val |= PHY_LPTX_CLK_DIV(esc_clk_div);
