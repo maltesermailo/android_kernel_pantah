BayLibre/android_kernel_common

BACKPORT: FROMLIST: usb: typec: Implement automated mode selection
This patch introduces new sysfs attributes to enable user control over
Type-C automated mode selection and provide negotiation feedback.

`mode_selection` attribute shows a prioritized list of supported modes
with the currently entered mode bracketed. Writing boolean 0 or 1 to
this attribute starts or stops the mode selection process,
respectively.

`entry_result`, `usb4_entry_result` read-only attributes show the
result of the last mode selection attempt for a specific mode.

Signed-off-by: Andrei Kuchynski <akuchynski@chromium.org>

Bug: 382333876
Upstream-Task:
Link: https://lore.kernel.org/all/20250804090340.3062182-6-akuchynski@chromium.org/
Change-Id: Ib3d41583ea8bcafd22d634c2aed134a78ccfac24
Signed-off-by: Andrei Kuchynski <akuchynski@google.com>

drivers/usb/typec/class.h
@@ -6,6 +6,8 @@
 #include <linux/device.h>
 #include <linux/usb/typec.h>
 #include <linux/android_kabi.h>
+#include <linux/usb/typec_altmode.h>
+#include <linux/kfifo.h>
 
 struct typec_mux;
 struct typec_switch;
@@ -29,6 +31,8 @@ struct typec_cable {
 	ANDROID_KABI_RESERVE(1);
 };
 
+struct mode_selection_state;
+
 struct typec_partner {
 	struct device			dev;
 	unsigned int			usb_pd:1;
@@ -42,6 +46,12 @@ struct typec_partner {
 	u8				usb_capability;
 
 	struct usb_power_delivery	*pd;
+
+	struct delayed_work mode_selection_work;
+	DECLARE_KFIFO(mode_sequence, struct mode_selection_state *,
+			roundup_pow_of_two(TYPEC_MODE_MAX));
+	struct mutex mode_sequence_lock;
+	struct mode_selection_state *active_mode;
 
 	void (*attach)(struct typec_partner *partner, struct device *dev);
 	void (*deattach)(struct typec_partner *partner, struct device *dev);
@@ -116,5 +126,7 @@ void typec_unlink_ports(struct typec_port *connector);
 static inline int typec_link_ports(struct typec_port *connector) { return 0; }
 static inline void typec_unlink_ports(struct typec_port *connector) { }
 #endif
+
+#define ALTERNATE_MODE_DEVICE_TYPE_NAME "typec_alternate_mode"
 
 #endif /* __USB_TYPEC_CLASS__ */

maltesermailo/android_kernel_pantah

Link:

drivers/usb/typec/class.h
@@ -6,6 +6,8 @@
 #include <linux/device.h>
 #include <linux/usb/typec.h>
 #include <linux/android_kabi.h>
+#include <linux/usb/typec_altmode.h>
+#include <linux/kfifo.h>
 
 struct typec_mux;
 struct typec_switch;
@@ -29,6 +31,8 @@ struct typec_cable {
 	ANDROID_KABI_RESERVE(1);
 };
 
+struct mode_selection_state;
+
 struct typec_partner {
 	struct device			dev;
 	unsigned int			usb_pd:1;
@@ -42,6 +46,12 @@ struct typec_partner {
 	u8				usb_capability;
 
 	struct usb_power_delivery	*pd;
+
+	struct delayed_work mode_selection_work;
+	DECLARE_KFIFO(mode_sequence, struct mode_selection_state *,
+			roundup_pow_of_two(TYPEC_ALTMODE_MAX));
+	struct mutex mode_sequence_lock;
+	struct mode_selection_state *active_mode;
 
 	void (*attach)(struct typec_partner *partner, struct device *dev);
 	void (*deattach)(struct typec_partner *partner, struct device *dev);
@@ -116,5 +126,7 @@ void typec_unlink_ports(struct typec_port *connector);
 static inline int typec_link_ports(struct typec_port *connector) { return 0; }
 static inline void typec_unlink_ports(struct typec_port *connector) { }
 #endif
+
+#define ALTERNATE_MODE_DEVICE_TYPE_NAME "typec_alternate_mode"
 
 #endif /* __USB_TYPEC_CLASS__ */