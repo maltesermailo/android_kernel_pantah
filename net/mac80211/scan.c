36e093134b9fbf5015b7e6d4eeade0b0fe390f18
Ken Sun <kensun@google.com>
2025-08-28 02:24:58 +0000

    Revert "UPSTREAM: wifi: cfg80211: rename UHB to 6 GHz"
    This reverts commit 9225bb806f4ad34665852e5332aee30b11529f69.
    
    Bug: 397869489
    Change-Id: I05e5c96912816ceffccc608eff45e6fe877561ee
    Signed-off-by: Ken Sun <kensun@google.com>

--- net/mac80211/scan.c
+++ net/mac80211/scan.c
@@ -9,7 +9,7 @@
  * Copyright 2007, Michael Wu <flamingice@sourmilk.net>
  * Copyright 2013-2015  Intel Mobile Communications GmbH
  * Copyright 2016-2017  Intel Deutschland GmbH
- * Copyright (C) 2018-2024 Intel Corporation
+ * Copyright (C) 2018-2023 Intel Corporation
  */
 
 #include <linux/if_arp.h>
--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -2160,9 +2160,9 @@ static bool cfg80211_6ghz_power_type_valid(const u8 *ie, size_t ielen,
 		case IEEE80211_6GHZ_CTRL_REG_LPI_AP:
 			return true;
 		case IEEE80211_6GHZ_CTRL_REG_SP_AP:
-			return !(flags & IEEE80211_CHAN_NO_6GHZ_AFC_CLIENT);
+			return !(flags & IEEE80211_CHAN_NO_UHB_AFC_CLIENT);
 		case IEEE80211_6GHZ_CTRL_REG_VLP_AP:
-			return !(flags & IEEE80211_CHAN_NO_6GHZ_VLP_CLIENT);
+			return !(flags & IEEE80211_CHAN_NO_UHB_VLP_CLIENT);
 		}
 	}
 	return false;
@@ -2205,7 +2205,7 @@ cfg80211_inform_single_bss_data(struct wiphy *wiphy,
 					    channel->flags)) {
 		data->use_for = 0;
 		data->cannot_use_reasons =
-			NL80211_BSS_CANNOT_USE_6GHZ_PWR_MISMATCH;
+			NL80211_BSS_CANNOT_USE_UHB_PWR_MISMATCH;
 	}
 
 	memcpy(tmp.pub.bssid, data->bssid, ETH_ALEN);
