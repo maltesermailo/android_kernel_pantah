ae6cc989c531b52ff27e2bc10cd677ce5fd12ed1
Pedro Falcato <pfalcato@suse.de>
2025-09-03 14:59:58 +0200

    BACKPORT: UPSTREAM: maple_tree: Use kfree_rcu in ma_free_rcu
    kfree_rcu is an optimized version of call_rcu + kfree. It used to not be
    possible to call it on non-kmalloc objects, but this restriction was
    lifted ever since SLOB was dropped from the kernel, and since commit
    6c6c47b063b5 ("mm, slab: call kvfree_rcu_barrier() from kmem_cache_destroy()").
    
    Thus, replace call_rcu + mt_free_rcu with kfree_rcu.
    Bug: 500919123
    Signed-off-by: Pedro Falcato <pfalcato@suse.de>
    Reviewed-by: Harry Yoo <harry.yoo@oracle.com>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    (cherry picked from commit 9b60811cb3b42fe9a8e3f0aa3e007cd9cc814a78)
    Signed-off-by: Lei Dong <lei.dong@unisoc.com>
    Change-Id: I0a3ded8d9ba896dbba8ca4bdcbd531030586aceb

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -179,13 +179,6 @@ static inline void mt_free_bulk(size_t size, void __rcu **nodes)
 	kmem_cache_free_bulk(maple_node_cache, size, (void **)nodes);
 }
 
-static void mt_free_rcu(struct rcu_head *head)
-{
-	struct maple_node *node = container_of(head, struct maple_node, rcu);
-
-	kmem_cache_free(maple_node_cache, node);
-}
-
 /*
  * ma_free_rcu() - Use rcu callback to free a maple node
  * @node: The node to free
@@ -196,7 +189,7 @@ static void mt_free_rcu(struct rcu_head *head)
 static void ma_free_rcu(struct maple_node *node)
 {
 	WARN_ON(node->parent != ma_parent_ptr(node));
-	call_rcu(&node->rcu, mt_free_rcu);
+	kfree_rcu(node, rcu);
 }
 
 static void mas_set_height(struct ma_state *mas)
@@ -5243,7 +5236,7 @@ static void mt_free_walk(struct rcu_head *head)
 	mt_free_bulk(node->slot_len, slots);
 
 free_leaf:
-	mt_free_rcu(&node->rcu);
+	mt_free_one(node);
 }
 
 static inline void __rcu **mte_destroy_descend(struct maple_enode **enode,
@@ -5327,7 +5320,7 @@ next:
 
 free_leaf:
 	if (free)
-		mt_free_rcu(&node->rcu);
+		mt_free_one(node);
 	else
 		mt_clear_meta(mt, node, node->type);
 }
