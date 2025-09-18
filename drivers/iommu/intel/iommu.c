820e19e87b2c2043097d073975a6b786b724b2a5
Vineeth Pillai <vineethrp@google.com>
2025-09-18 14:56:53 -0400

    ANDROID: iommu/vt-d: Paravirtualize iommu domain management
    When pKVM is enabled with pvIOMMU, use the iommu domain management
    hypercalls.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I42e42a57fe36e9640db3252002e7df88f73d0bc5
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -1389,6 +1389,11 @@ static void domain_exit(struct dmar_domain *domain)
 
 		domain_unmap(domain, 0, DOMAIN_MAX_PFN(domain->gaw), &freelist);
 		iommu_put_pages_list(&freelist);
+
+		if (pkvm_pviommu_enabled()) {
+			pkvm_hc_iommu_domain_free(virt_to_phys(domain->pgd));
+			iommu_free_page(domain->pgd);
+		}
 	}
 
 	if (WARN_ON(!list_empty(&domain->devices)))
@@ -3298,6 +3303,28 @@ static int iommu_superpage_capability(struct intel_iommu *iommu, bool first_stag
 	return fls(cap_super_page_val(iommu->cap));
 }
 
+static int pv_paging_domain_alloc(struct device_domain_info *info, struct dmar_domain *domain)
+{
+	struct pkvm_domain_param param = {
+		.phys = info->iommu->reg_phys,
+		.bdf = PCI_DEVID(info->bus, info->devfn),
+		.use_first_level = domain->use_first_level,
+		.pgd_gpa = virt_to_phys(domain->pgd),
+		.gaw = domain->gaw,
+		.agaw = domain->agaw,
+		.max_addr = domain->max_addr,
+		.iommu_coherency = domain->iommu_coherency,
+		.iommu_superpage = domain->iommu_superpage,
+	};
+	int ret = pkvm_hc_iommu_domain_alloc(&param);
+
+	if (ret)
+		pr_err("%s: pv domain alloc failed for device[%x:%x.%x] ret=%d\n", __func__,
+		       info->bus, PCI_SLOT(info->devfn), PCI_FUNC(info->devfn), ret);
+
+	return ret;
+}
+
 static struct dmar_domain *paging_domain_alloc(struct device *dev, bool first_stage)
 {
 	struct device_domain_info *info = dev_iommu_priv_get(dev);
@@ -3354,7 +3381,18 @@ static struct dmar_domain *paging_domain_alloc(struct device *dev, bool first_st
 		kfree(domain);
 		return ERR_PTR(-ENOMEM);
 	}
-	domain_flush_cache(domain, domain->pgd, PAGE_SIZE);
+
+	if (pkvm_pviommu_enabled()) {
+		int ret = pv_paging_domain_alloc(info, domain);
+
+		if (ret) {
+			iommu_free_page(domain->pgd);
+			kfree(domain);
+			domain = ERR_PTR(ret);
+		}
+	} else {
+		domain_flush_cache(domain, domain->pgd, PAGE_SIZE);
+	}
 
 	return domain;
 }
