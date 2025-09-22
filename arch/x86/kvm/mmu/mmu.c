BayLibre/android_kernel_common

ANDROID: pkvm: x86: fill memcache with host backed pages
pKVM-high, during guest page fault handling, allocates pages required for
creating/extending mapping in page tables managed by pKVM-low and
topup's memcache. pKVM-low next refills its memcache which host backed
pages, and donates them to itself so later on they can be used for
page table allocation.

This patch is heavily inspired by pKVM-ARM stage2 memcache.

Bug: 391541180
Test: Run various VMs on Brya and Qemu

Change-Id: I5eee0a1e78d475be2c888a5b8d70770490d1b26a
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/mmu/mmu.c
@@ -4743,6 +4743,43 @@ out_unlock:
 #endif
 
 #ifdef CONFIG_PKVM_INTEL
+static void *pkvm_mc_alloc_fn(void *flags, unsigned long order)
+{
+	unsigned long __flags = (unsigned long)flags;
+	gfp_t gfp_mask;
+	void *addr;
+
+	gfp_mask = __flags & HYP_MEMCACHE_ACCOUNT_KMEMCG ?
+		   GFP_KERNEL_ACCOUNT : GFP_KERNEL;
+
+	addr = (void *)__get_free_pages(gfp_mask, order);
+
+	if (addr && __flags & HYP_MEMCACHE_ACCOUNT_STAGE2)
+		kvm_account_pgtable_pages(addr, 1);
+
+	return addr;
+}
+
+static phys_addr_t host_pa(void *addr)
+{
+	return virt_to_phys((volatile void *) addr);
+}
+
+static int topup_pkvm_memcache(struct pkvm_memcache *mc, unsigned long
+			       min_pages, unsigned long order)
+{
+	unsigned long flags = mc->flags;
+
+	if (order > PAGE_SHIFT)
+		return -E2BIG;
+
+	return __topup_pkvm_memcache(mc, min_pages, pkvm_mc_alloc_fn,
+				     host_pa, (void *)flags, order);
+}
+
+//TODO: JAZ: fix it
+#define min_mc_pages 5
+
 static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 {
 	int r;
@@ -4761,6 +4798,10 @@ static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	if (likely(fault->slot)) {
 		gfn_t base_gfn;
 		gfn_t nr_pages;
+
+		r = topup_pkvm_memcache(&vcpu->arch.stage2_mc, min_mc_pages, 0);
+		if (r)
+			return -ENOMEM;
 
 		kvm_mmu_hugepage_adjust(vcpu, fault);
 
@@ -5640,6 +5681,8 @@ static void init_kvm_tdp_mmu(struct kvm_vcpu *vcpu,
 
 	reset_guest_paging_metadata(vcpu, context);
 	reset_tdp_shadow_zero_bits_mask(context);
+
+	init_pkvm_stage2_memcache(&vcpu->arch.stage2_mc);
 }
 
 static void shadow_mmu_init_context(struct kvm_vcpu *vcpu, struct kvm_mmu *context,

maltesermailo/android_kernel_pantah

topup's memcache. pKVM-low next refills its memcache with host backed
Test: Build, boot and run various VMs
Upstream-Task: 402758258

arch/x86/kvm/mmu/mmu.c
@@ -684,6 +684,10 @@ static void mmu_free_memory_caches(struct kvm_vcpu *vcpu)
 	kvm_mmu_free_memory_cache(&vcpu->arch.mmu_shadow_page_cache);
 	kvm_mmu_free_memory_cache(&vcpu->arch.mmu_shadowed_info_cache);
 	kvm_mmu_free_memory_cache(&vcpu->arch.mmu_page_header_cache);
+
+#ifdef CONFIG_PKVM_INTEL
+	free_pkvm_memcache(&vcpu->arch.stage2_mc);
+#endif
 }
 
 static void mmu_free_pte_list_desc(struct pte_list_desc *pte_list_desc)
@@ -4782,8 +4786,11 @@ out_unlock:
 #endif
 
 #ifdef CONFIG_PKVM_INTEL
-static void pkvm_mc_free_fn(void *addr, void *unused)
+static void pkvm_mc_free_fn(void *addr, void *flags)
 {
+	if ((unsigned long)flags & HYP_MEMCACHE_ACCOUNT_STAGE2)
+		kvm_account_pgtable_pages(addr, -1);
+
 	free_page((unsigned long)addr);
 }
 
@@ -4795,7 +4802,41 @@ static void *kvm_host_va(phys_addr_t phys)
 void free_pkvm_memcache(struct pkvm_memcache *mc)
 {
 	__free_pkvm_memcache(mc, pkvm_mc_free_fn,
-			     kvm_host_va, NULL);
+			     kvm_host_va, (void *)mc->flags);
+}
+
+static void *pkvm_mc_alloc_fn(void *flags, unsigned long order)
+{
+	unsigned long __flags = (unsigned long)flags;
+	gfp_t gfp_mask;
+	void *addr;
+
+	gfp_mask = __flags & HYP_MEMCACHE_ACCOUNT_KMEMCG ?
+		   GFP_KERNEL_ACCOUNT : GFP_KERNEL;
+
+	addr = (void *)__get_free_pages(gfp_mask, order);
+
+	if (addr && __flags & HYP_MEMCACHE_ACCOUNT_STAGE2)
+		kvm_account_pgtable_pages(addr, 1);
+
+	return addr;
+}
+
+static phys_addr_t host_pa(void *addr)
+{
+	return virt_to_phys((volatile void *) addr);
+}
+
+int topup_pkvm_memcache(struct pkvm_memcache *mc, unsigned long min_pages,
+			unsigned long order)
+{
+	unsigned long flags = mc->flags;
+
+	if (order > PAGE_SHIFT)
+		return -E2BIG;
+
+	return __topup_pkvm_memcache(mc, min_pages, pkvm_mc_alloc_fn,
+				     host_pa, (void *)flags, order);
 }
 
 static int pkvm_pin_page(struct kvm *kvm, struct kvm_page_fault *fault)
@@ -4829,6 +4870,12 @@ static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	gfn_t nr_pages;
 	int r;
 
+	/*
+	 * Minimum pages required to install stage-2 translation. We've
+	 * pre-allocating the entry page table during per-vm pool creation
+	 */
+	u8 min_mc_pages = kvm_mmu_get_max_tdp_level() - 1;
+
 	r = kvm_faultin_pfn(vcpu, fault, ACC_ALL);
 	if (r != RET_PF_CONTINUE) {
 		/* MMIO emulation works for non-protected VMs only. */
@@ -4854,6 +4901,10 @@ static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 
 	base_gfn = gfn_round_for_level(fault->gfn, fault->goal_level);
 	nr_pages = KVM_PAGES_PER_HPAGE(fault->goal_level);
+
+	r = topup_pkvm_memcache(&vcpu->arch.stage2_mc, min_mc_pages, 0);
+	if (r)
+		goto out_unlock;
 
 	r = kvm_call_pkvm(vm_mmu_map, vcpu,
 			  base_gfn << PAGE_SHIFT, fault->pfn << PAGE_SHIFT,