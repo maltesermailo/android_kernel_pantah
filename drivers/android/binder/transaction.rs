c5d696dd4c5f88d6974fbe0c8b1c716e93ff2c04
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-09-24 19:07:59 -0700

    Revert "ANDROID: rust_binder: avoid cloning `DArc` in `find_from`"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/8f90ffb5-ccbc-4699-8452-970a8d3494df). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ia276277f537e3f88164e4d630ee39e11ed9279e7
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- drivers/android/binder/transaction.rs
+++ drivers/android/binder/transaction.rs
@@ -239,11 +239,11 @@ impl Transaction {
     }
 
     /// Searches in the transaction stack for a transaction originating at the given thread.
-    pub(crate) fn find_from(&self, thread: &Thread) -> Option<&DArc<Transaction>> {
+    pub(crate) fn find_from(&self, thread: &Thread) -> Option<DArc<Transaction>> {
         let mut it = &self.from_parent;
         while let Some(transaction) = it {
             if core::ptr::eq(thread, transaction.from.as_ref()) {
-                return Some(transaction);
+                return Some(transaction.clone());
             }
 
             it = &transaction.from_parent;
