851af0d72052746483dc7264931dfa9f6d0f0806
gao xu <gaoxu2@honor.corp-partner.google.com>
2025-09-25 17:21:33 +0800

    ANDROID: mm/swap_cgroup: remove global swap cgroup lock from swap_cgroup_cmpxchg
    Adapt swap_cgroup_cmpxchg to no longer rely on the global swap cgroup lock,
    in line with patch d7fdd8244671 ("mm/swap_cgroup: remove global swap cgroup
    lock").
    
    The Linux community has removed swap_cgroup_cmpxchg entirely in patch
    ddafdd294f58 ("mm/swap_cgroup: remove swap_cgroup_cmpxchg"), which depends on
    patch 6b611388b626 ("memcg-v1: remove charge move code"). As these patches
    break the ABI, we must retain and modify swap_cgroup_cmpxchg instead.
    
    Bug: 123456
    Change-Id: I7ca33c1461345de595619e32e6029df082cbb013
    Signed-off-by: gao xu <gaoxu2@honor.corp-partner.google.com>

--- mm/swap_cgroup.c
+++ mm/swap_cgroup.c
@@ -63,20 +63,38 @@ static unsigned short __swap_cgroup_id_xchg(struct swap_cgroup *map,
 unsigned short swap_cgroup_cmpxchg(swp_entry_t ent,
 					unsigned short old, unsigned short new)
 {
-	struct swap_cgroup_ctrl *ctrl;
 	struct swap_cgroup *sc;
-	unsigned long flags;
-	unsigned short retval;
+	struct swap_cgroup *map;
+	unsigned short retval = 0;
+	unsigned short old_id;
+	int retry_cnt = 5;
+	pgoff_t offset;
+	unsigned int new_ids, old_ids;
+	unsigned int shift;
+	bool ret;
 
-	sc = lookup_swap_cgroup(ent, &ctrl);
+	map = swap_cgroup_ctrl[swp_type(ent)].map;
+	offset = swp_offset(ent);
+	sc = &map[offset / ID_PER_SC];
+	old_ids = atomic_read(&sc->ids);
+	shift = (offset % ID_PER_SC) * ID_SHIFT;
+	old_id = (old_ids >> shift) & ID_MASK;
+	if (old_id != old) {
+		pr_err("the id of sc:%u is not equal to old:%u\n", old_id, old);
+		goto out;
+	}
 
-	spin_lock_irqsave(&ctrl->lock, flags);
-	retval = sc->id;
-	if (retval == old)
-		sc->id = new;
-	else
-		retval = 0;
-	spin_unlock_irqrestore(&ctrl->lock, flags);
+	new_ids = (old_ids & ~(ID_MASK << shift));
+	new_ids |= ((unsigned int)new) << shift;
+	do {
+		ret = atomic_try_cmpxchg(&sc->ids, &old_ids, new_ids);
+		if (ret) {
+			retval = old;
+			break;
+		}
+	} while (retry_cnt--);
+
+out:
 	return retval;
 }
 
