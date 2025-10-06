f33da5ab0c451365c205fd53ca385d888dd0640d
Arselan Alvi <arselan.alvi@intel.com>
2025-10-06 13:12:29 -0700

    ANDROID: Fixing Android Kernel compilation
    1.Export 'ttm_tt_populate' without if condition
    "IS_ENABLED(CONFIG_DRM_TTM_KUNIT_TEST)" in ttm_tt.c
    
    2.Remove duplicate definition of "ttm_tt_is_swapped" in ttm_tt.h
    
    Change-Id: I555f76606d51d7e92a3354e705258193d7a80a71
    Signed-off-by:  Arselan Alvi <arselan.alvi@intel.corp-partner.google.com>

--- include/drm/ttm/ttm_tt.h
+++ include/drm/ttm/ttm_tt.h
@@ -145,11 +145,6 @@ static inline bool ttm_tt_is_populated(struct ttm_tt *tt)
 	return tt->page_flags & TTM_TT_FLAG_PRIV_POPULATED;
 }
 
-static inline bool ttm_tt_is_swapped(const struct ttm_tt *tt)
-{
-	return tt->page_flags & TTM_TT_FLAG_SWAPPED;
-}
-
 /**
  * ttm_tt_is_swapped() - Whether the ttm_tt is swapped out or backed up
  * @tt: The struct ttm_tt.
