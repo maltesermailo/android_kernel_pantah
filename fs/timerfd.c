b5b8f1ee24b8cdf40cb6dd1a41493015bc5865f5
Konrad Korczyński <kkorczynski@google.com>
2025-10-13 07:50:19 -0700

    ANDROID: fs: Add vendor hooks for ep_create_wakeup_source & timerfd_create
    timerfd doesn't create any wakelocks, but eventpoll can.  When it does,
    it names them after the underlying file descriptor, and since all
    timerfd file descriptors are named "[timerfd]" (which saves memory on
    systems like desktops with potentially many timerfd instances), all
    wakesources created as a result of using the eventpoll-on-timerfd idiom
    are called... "[timerfd]".
    
    However, it becomes impossible to tell which "[timerfd]" wakesource is
    affliated with which process and hence troubleshooting is difficult.
    
    Adding vendor hooks to allow vendor to assign appropriate names to
    timerfd descriptors and eventoll wakesource.
    
    Bug: 155142106
    Bug: 415767459
    Signed-off-by: Manish Varma <varmam@google.com>
    (cherry picked from https://android-review.googlesource.com/q/commit:0ff110fbb309be385126a42ac9f7004ba9b0644e)
    Signed-off-by: Darren Hsu <darrenhsu@google.com>
    (cherry picked from https://android-review.googlesource.com/q/commit:719fc80624522769ef8d7dacd73a5a1ec4f402cf)
    Merged-In: I330a42ab48bed4b26d5eb2f636925c66061165ec
    Change-Id: I330a42ab48bed4b26d5eb2f636925c66061165ec
    Signed-off-by: Konrad Korczyński <kkorczynski@google.com>
    (cherry picked from commit 3fb238239b57fb6b96e3395144ff057f2c7d66d1)
    Signed-off-by: Konrad Korczyński <kkorczynski@google.com>
    (cherry picked from commit 03396e4bfe3a698ebac790ce8f196ad2c4392333)

--- fs/timerfd.c
+++ fs/timerfd.c
@@ -28,6 +28,8 @@
 #include <linux/rcupdate.h>
 #include <linux/time_namespace.h>
 
+#include <trace/hooks/fs.h>
+
 struct timerfd_ctx {
 	union {
 		struct hrtimer tmr;
@@ -413,6 +415,8 @@ SYSCALL_DEFINE2(timerfd_create, int, clockid, int, flags)
 	struct timerfd_ctx *ctx;
 	struct file *file;
 
+	char file_name_buf[32];
+
 	/* Check the TFD_* constants for consistency.  */
 	BUILD_BUG_ON(TFD_CLOEXEC != O_CLOEXEC);
 	BUILD_BUG_ON(TFD_NONBLOCK != O_NONBLOCK);
@@ -454,7 +458,10 @@ SYSCALL_DEFINE2(timerfd_create, int, clockid, int, flags)
 		return ufd;
 	}
 
-	file = anon_inode_getfile("[timerfd]", &timerfd_fops, ctx,
+	strscpy(file_name_buf, "[timerfd]", sizeof(file_name_buf));
+	trace_android_vh_timerfd_create(file_name_buf, sizeof(file_name_buf));
+
+	file = anon_inode_getfile(file_name_buf, &timerfd_fops, ctx,
 				    O_RDWR | (flags & TFD_SHARED_FCNTL_FLAGS));
 	if (IS_ERR(file)) {
 		put_unused_fd(ufd);
@@ -467,7 +474,7 @@ SYSCALL_DEFINE2(timerfd_create, int, clockid, int, flags)
 	return ufd;
 }
 
-static int do_timerfd_settime(int ufd, int flags, 
+static int do_timerfd_settime(int ufd, int flags,
 		const struct itimerspec64 *new,
 		struct itimerspec64 *old)
 {
