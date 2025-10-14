786ac2dd4b629c1d394a062af002a3777f276219
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-10-14 15:48:36 -0700

    Revert "ANDROID: partially re-enable KABI macros for VDSO and EF..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/4298a75c-877b-47a5-9da7-b1e49dd5d87a). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I0cdfa4fdc8bd7a7f58ffe0f8907ad28726af5b22
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 400972353
    Bug: 445902333

--- include/linux/android_kabi.h
+++ include/linux/android_kabi.h
@@ -70,6 +70,9 @@
 				__stringify(_new));			\
 	}
 
+#if defined(BUILD_VDSO) || defined(__DISABLE_EXPORTS)
+#define _ANDROID_KABI_REPLACE(_orig, _new)	_new
+#else
 #define _ANDROID_KABI_REPLACE(_orig, _new)		      \
 	union {						      \
 		_new;					      \
@@ -78,6 +81,7 @@
 		};					      \
 		_ANDROID_KABI_NORMAL_SIZE_ALIGN(_orig, _new); \
 	}
+#endif
 
 
 /*
