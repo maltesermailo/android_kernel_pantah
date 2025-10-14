14352d537b1e5b76f24933ec0df5c79dd8cd9aa3
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-10-14 01:59:35 -0700

    Revert "ANDROID: KVM: arm64: Fix kvm_hyp_memcache::mapping leak"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/c1cb4ada-6179-41ee-9e50-8a3c14484ba6). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I3bce7775fef983e8424847540e38841b1434c22e
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 357781595
    Bug: 449003536

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -164,12 +164,11 @@ int topup_hyp_memcache_gfp(struct kvm_hyp_memcache *mc, unsigned long min_pages,
 static inline void init_hyp_memcache(struct kvm_hyp_memcache *mc)
 {
 	memset(mc, 0, sizeof(*mc));
-	mc->mapping = ZERO_SIZE_PTR; /* Prevent allocation, solely useful for stage2 memcache */
 }
 
 static inline void init_hyp_stage2_memcache(struct kvm_hyp_memcache *mc)
 {
-	memset(mc, 0, sizeof(*mc));
+	init_hyp_memcache(mc);
 	mc->flags = HYP_MEMCACHE_ACCOUNT_KMEMCG | HYP_MEMCACHE_ACCOUNT_STAGE2;
 }
 
