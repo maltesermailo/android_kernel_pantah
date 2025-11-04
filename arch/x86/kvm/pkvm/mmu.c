BayLibre/android_kernel_common

ANDROID: pkvm: x86: handle memcache refill during page sharing
When the protected guest shares the pages with host additional mapping
may be required. In such case there may be not enough memory in memcache
which is needed for extending guest page tables. In such case return to
host and refill the memcache after which re-issue guest page sharing
operation.

Bug: 391541180
Test: Run various VMs on Brya and Qemu

Change-Id: I27a222b25feb255251218a13a3c19c149314997f
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -225,7 +225,7 @@ static int refill_memcache(struct pkvm_memcache *mc, unsigned long min_pages,
 	return ret;
 }
 
-static int pkvm_refill_memcache(struct pkvm_vcpu *pkvm_vcpu)
+int pkvm_refill_memcache(struct pkvm_vcpu *pkvm_vcpu)
 {
 	struct kvm_vcpu *vcpu = to_kvm_vcpu(pkvm_vcpu);
 

maltesermailo/android_kernel_pantah

which is needed for extending guest page tables. If it happens return to
Bug: 453743944
Test: Build, boot and run various VMs
Upstream-Task: 402758258

arch/x86/kvm/pkvm/mmu.c
@@ -224,7 +224,7 @@ static int refill_memcache(struct pkvm_memcache *mc, unsigned long min_pages,
 	return ret;
 }
 
-static int pkvm_refill_mmu_memcache(struct pkvm_vcpu *pkvm_vcpu)
+int pkvm_refill_mmu_memcache(struct pkvm_vcpu *pkvm_vcpu)
 {
 	struct kvm_vcpu *vcpu = to_kvm_vcpu(pkvm_vcpu);
 	struct pkvm_memcache *host_mc;
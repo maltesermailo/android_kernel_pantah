d962cce2e62a52e0f4a29faf815edaed579c0339
Grzegorz Jaszczyk <jaszczyk@google.com>
2025-07-18 14:55:56 +0000

    ANDROID: pkvm: x86: handle memcache refill during page sharing
    When the protected guest shares the pages with host additional mapping
    may be required. In such case there may be not enough memory in memcache
    which is needed for extending guest page tables. If it happens return to
    host and refill the memcache after which re-issue guest page sharing
    operation.
    
    Bug: 391541180
    Bug: 453743944
    Test: Build, boot and run various VMs
    Upstream-Task: 402758258
    
    Change-Id: I27a222b25feb255251218a13a3c19c149314997f
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -10035,6 +10035,7 @@ static int complete_hypercall_exit(struct kvm_vcpu *vcpu)
 	return kvm_skip_emulated_instruction(vcpu);
 }
 
+#ifdef CONFIG_PKVM_INTEL
 static int pkvm_start_secondary_vcpu(struct kvm *kvm, int apic_id)
 {
 	struct kvm_vcpu *vcpu = kvm_get_vcpu_by_id(kvm, apic_id);
@@ -10060,9 +10061,23 @@ static int pkvm_start_secondary_vcpu(struct kvm *kvm, int apic_id)
 	return 0;
 }
 
+static int handle_memcache_refill(struct kvm_vcpu *vcpu, int refill_size)
+{
+	int ret;
+
+	ret = topup_pkvm_memcache(&vcpu->arch.pkvm_vcpu.guest_mmu_memcache,
+				  refill_size);
+	if (ret)
+		return ret;
+
+	/* handled */
+	return 1;
+}
+
 static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 {
 	unsigned long nr, a0, a1, a2;
+	int refill_size;
 	int ret;
 
 	nr = kvm_rax_read(vcpu);
@@ -10088,6 +10103,10 @@ static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 		kvm_rax_write(vcpu, ret);
 		ret = 1;
 		break;
+	case PKVM_GHC_SHARE_MEM:
+		refill_size = vcpu->arch.pkvm_vcpu.req_param;
+		ret = handle_memcache_refill(vcpu, refill_size);
+		break;
 	default:
 		ret = 1;
 		break;
@@ -10095,6 +10114,7 @@ static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 
 	return ret;
 }
+#endif
 
 unsigned long __kvm_emulate_hypercall(struct kvm_vcpu *vcpu, unsigned long nr,
 				      unsigned long a0, unsigned long a1,
@@ -10194,8 +10214,10 @@ int kvm_emulate_hypercall(struct kvm_vcpu *vcpu)
 	int op_64_bit;
 	int cpl;
 
+#ifdef CONFIG_PKVM_INTEL
 	if (pkvm_is_protected_vcpu(vcpu))
 		return kvm_pkvm_hypercall(vcpu);
+#endif
 
 	if (kvm_xen_hypercall_enabled(vcpu->kvm))
 		return kvm_xen_hypercall(vcpu);
