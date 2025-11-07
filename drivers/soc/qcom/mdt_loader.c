fa7f5b5889193ae45addadc9da3caf640cd617c7
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-07 15:41:36 -0800

    Revert "Merge 6.1.153 into android14-6.1-lts"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/22d5d2d8-e953-4d89-a254-f9c83db4fe07). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I7a65af6982353f2a75712a9d3af6382eb0491fc3
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- drivers/soc/qcom/mdt_loader.c
+++ drivers/soc/qcom/mdt_loader.c
@@ -32,20 +32,18 @@ static bool mdt_header_valid(const struct firmware *fw)
 		return false;
 
 	if (ehdr->e_phentsize != sizeof(struct elf32_phdr))
-		return false;
+		return -EINVAL;
 
 	phend = size_add(size_mul(sizeof(struct elf32_phdr), ehdr->e_phnum), ehdr->e_phoff);
 	if (phend > fw->size)
 		return false;
 
-	if (ehdr->e_shentsize || ehdr->e_shnum) {
-		if (ehdr->e_shentsize != sizeof(struct elf32_shdr))
-			return false;
+	if (ehdr->e_shentsize != sizeof(struct elf32_shdr))
+		return -EINVAL;
 
-		shend = size_add(size_mul(sizeof(struct elf32_shdr), ehdr->e_shnum), ehdr->e_shoff);
-		if (shend > fw->size)
-			return false;
-	}
+	shend = size_add(size_mul(sizeof(struct elf32_shdr), ehdr->e_shnum), ehdr->e_shoff);
+	if (shend > fw->size)
+		return false;
 
 	return true;
 }
