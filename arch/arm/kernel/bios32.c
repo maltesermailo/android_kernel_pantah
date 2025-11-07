d38238ce077f32e0f0f2ec0e0117183229947bf3
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-06 18:39:51 -0800

    Revert "Merge 7c8dcac8d72d ("Merge tag 'v6.18-p2' of git://git.k..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/8eaf22e9-8350-48b1-a220-83cfe42129b1). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ia6af413d8cef2987ae1c544e18fc87027c31831d
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- arch/arm/kernel/bios32.c
+++ arch/arm/kernel/bios32.c
@@ -10,7 +10,6 @@
 #include <linux/kernel.h>
 #include <linux/pci.h>
 #include <linux/slab.h>
-#include <linux/string_choices.h>
 #include <linux/init.h>
 #include <linux/io.h>
 
@@ -338,8 +337,8 @@ void pcibios_fixup_bus(struct pci_bus *bus)
 	/*
 	 * Report what we did for this bus
 	 */
-	pr_info("PCI: bus%d: Fast back to back transfers %s\n",
-		bus->number, str_enabled_disabled(features & PCI_COMMAND_FAST_BACK));
+	pr_info("PCI: bus%d: Fast back to back transfers %sabled\n",
+		bus->number, (features & PCI_COMMAND_FAST_BACK) ? "en" : "dis");
 }
 EXPORT_SYMBOL(pcibios_fixup_bus);
 
