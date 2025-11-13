e6d5205a61fa2240fa456f6801facc50cea7ce83
T.J. Mercier <tjmercier@google.com>
2025-11-12 18:17:43 -0800

    ANDROID: Don't use current pointer for per-process dmabuf accounting
    If a child task is created with a partial sharing relationship with the
    parent (e.g. CLONE_FILES && !CLONE_VM), AND that child task closes a
    dmabuf in the partially shared (files/mm) structure, then the
    dma_buf_unaccount_task operation will do nothing since tasks created
    with partial sharing relationships do not have associated
    task_dma_buf_info accounting structures associated with them. The dmabuf
    will be closed, but the accounting information in the parent will still
    have a record of the dead buffer. If nothing else happens, the result is
    just incorrect accounting.
    
    Later, if a fork is performed the accounting information is copied into
    the child, and there a UAF occurs when we dereference the dead dmabuf in
    the parent's accounting information in copy_dmabuf_info.
    
    Fix this by tracking task_dma_buf_info in mm_struct and files_struct,
    which will be used instead of the current task's task_dma_buf_info
    pointer, even for unaccount operations via partial sharing.
    
    Fixes: 060742b3d06d ("ANDROID: Track per-process dmabuf RSS")
    Bug: 459757379
    Bug: 461520624
    Bug: 465237667
    Change-Id: Idaa4c39de396eb8261c018a816fb549356a2a5cc
    Signed-off-by: T.J. Mercier <tjmercier@google.com>
    (cherry picked from commit 5dd8a05ba11a36d91c244d3e482a18a275e5d8ad)
    Signed-off-by: Lee Jones <joneslee@google.com>

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -803,10 +803,11 @@ int dma_buf_get_flags(struct dma_buf *dmabuf, unsigned long *flags);
 #ifdef CONFIG_DMA_SHARED_BUFFER
 
 int is_dma_buf_file(struct file *file);
-int dma_buf_account_task(struct dma_buf *dmabuf, struct task_struct *task);
-void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_struct *task);
+int dma_buf_account_task(struct dma_buf *dmabuf, struct task_dma_buf_info *dmabuf_info);
+void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_dma_buf_info *dmabuf_info);
 int copy_dmabuf_info(u64 clone_flags, struct task_struct *task);
-void put_dmabuf_info(struct task_struct *task);
+void put_dmabuf_info(struct task_dma_buf_info *dmabuf_info);
+int dma_buf_begin_new_exec(struct files_struct *old_files);
 
 #else /* CONFIG_DMA_SHARED_BUFFER */
 
@@ -818,6 +819,7 @@ static inline void dma_buf_unaccount_task(struct dma_buf *dmabuf,
 static inline int copy_dmabuf_info(u64 clone_flags,
 				   struct task_struct *task) { return 0; }
 static inline void put_dmabuf_info(struct task_struct *task) {}
+int dma_buf_begin_new_exec(struct files_struct *old_files) { return 0; }
 
 #endif /* CONFIG_DMA_SHARED_BUFFER */
 #endif /* __DMA_BUF_H__ */
