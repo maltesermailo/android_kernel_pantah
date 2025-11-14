120c9625667b4561d0824d2d9b9d38c0e9d4e1d2
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-13 18:25:29 -0800

    Revert "ANDROID: vendor_hooks: add hook in __dev_pm_qos_update_r..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/f40d45a9-31d1-423d-be94-0d3d0b5587fb). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Icdbaf5ddc79ecd2fdabd3167fa72f5332ec5d8ac
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 440488476

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -695,4 +695,3 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_pgdat_balanced);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_file_is_tiny);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_nr_pcp_alloc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pcp_alloc_factor_adjust);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fas_gpu_qos_update_tracer);
