8d2411ca351ae52b431d6f8cfbdd6a2874446596
Vincent Donnefort <vdonnefort@google.com>
2025-11-14 15:48:43 +0000

    ANDROID: KVM: arm64: KVM_GET_ONE_REG/KVM_SET_ONE_REG for p-guests
    KVM can forward HVCs to the VMM (KVM_EXIT_HYPERCALL). However, to read
    read the content of the HVC arguments, it must use KVM_GET_ONE_REG and
    KVM_SET_ONE_REG to write the return value. Currently those two ioctl are
    rejected for protected VMs. Relax slightly this limitation when a
    hypercall is forwarded for the 4 first general-purpose registers.
    
    Bug: 460349550
    Bug: 461757236
    Change-Id: I85c36e1cd4f85162da689373f535d7bd36a9aeab
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1740,6 +1740,37 @@ static int kvm_arm_vcpu_set_events(struct kvm_vcpu *vcpu,
 	return __kvm_arm_vcpu_set_events(vcpu, events);
 }
 
+static bool kvm_pvm_one_reg_allowed(struct kvm_vcpu *vcpu, struct kvm_one_reg *reg)
+{
+	u64 off;
+
+	if (!vcpu_get_flag(vcpu, VCPU_PKVM_FINALIZED))
+		return true;
+
+	/* For protected VMs, SET_ONE_REG|GET_ONE_REG only make sense for forwarded guest HVCs */
+	if (vcpu->run->exit_reason != KVM_EXIT_HYPERCALL)
+		return false;
+
+	switch (vcpu->run->hypercall.nr) {
+	/*
+	 * It is expected from the VMM to perform the power cycle, most likely with
+	 * VFIO_DEVICE_FEATURE_LOW_POWER_ENTRY/EXIT
+	 */
+	case ARM_SMCCC_VENDOR_HYP_KVM_DEV_REQ_PWR_FUNC_ID:
+		break;
+	default:
+		return false;
+	}
+
+	if ((reg->id & KVM_REG_ARM_COPROC_MASK) != KVM_REG_ARM_CORE)
+		return false;
+
+	off = reg->id & ~(KVM_REG_ARCH_MASK | KVM_REG_SIZE_MASK | KVM_REG_ARM_CORE);
+
+	/* Only regs[0] to regs[3] matter to HVCs */
+	return off <= KVM_REG_ARM_CORE_REG(regs.regs[3]);
+}
+
 long kvm_arch_vcpu_ioctl(struct file *filp,
 			 unsigned int ioctl, unsigned long arg)
 {
@@ -1767,12 +1798,12 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 		if (unlikely(!kvm_vcpu_initialized(vcpu)))
 			break;
 
-		r = -EPERM;
-		if (unlikely(vcpu_is_protected(vcpu) && vcpu_get_flag(vcpu, VCPU_PKVM_FINALIZED)))
-			break;
-
 		r = -EFAULT;
 		if (copy_from_user(&reg, argp, sizeof(reg)))
+			break;
+
+		r = -EPERM;
+		if (vcpu_is_protected(vcpu) && !kvm_pvm_one_reg_allowed(vcpu, &reg))
 			break;
 
 		/*
