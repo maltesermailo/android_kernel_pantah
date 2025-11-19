BayLibre/android_kernel_common

ANDROID: iommu/vt-d: Paravirtualize queued invalidation
Bug: 391542119
Upstream-Task: 402758258
Test: Build and Boot

Change-Id: I52a302a8bd5614ff82342f95f7001e346b81f5bd
Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/iommu_pkvm.h
@@ -108,4 +108,10 @@ static inline long pkvm_hc_cache_tag_unassign(struct pkvm_cache_tag_param *param
 	return pkvm_iommu_hypercall(PKVM_HC_IOMMU_CACHE_UNASSIGN,
 			cache_tag_param, param);
 }
+
+static inline int pkvm_hc_qi_submit_sync(unsigned long reg_phys, unsigned long desc,
+		unsigned int count)
+{
+	return kvm_hypercall3(PKVM_HC_SUBMIT_QI, reg_phys, desc, count);
+}
 #endif

maltesermailo/android_kernel_pantah

drivers/iommu/intel/iommu_pkvm.h
@@ -101,4 +101,10 @@ static inline long pkvm_hc_iommu_unmap_pages(unsigned long pgd_gpa, unsigned lon
 {
 	return pkvm_hypercall(iommu_unmap_pages, pgd_gpa, start_pfn, last_pfn);
 }
+
+static inline int pkvm_hc_qi_submit_sync(unsigned long reg_phys, unsigned long desc,
+		unsigned int count)
+{
+	return pkvm_hypercall(iommu_submit_qi, reg_phys, desc, count);
+}
 #endif