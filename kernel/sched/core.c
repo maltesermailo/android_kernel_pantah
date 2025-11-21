837462303f6dd32dd4a82a275ea0f16c266e59b3
Yu-Yu Chen <yuyulydia@google.com>
2025-11-21 09:38:56 +0000

    sched: Export show_state_filter
    Bug: 456333108
    Test: None
    Change-Id: Icef6d3a6dc340effe3f24eeb400f6eff2c41c281
    Signed-off-by: Yu-Yu Chen <yuyulydia@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8791,6 +8791,7 @@ void show_state_filter(unsigned int state_filter)
 	if (!state_filter)
 		debug_show_all_locks();
 }
+EXPORT_SYMBOL_GPL(show_state_filter);
 
 /**
  * init_idle - set up an idle thread for a given CPU
