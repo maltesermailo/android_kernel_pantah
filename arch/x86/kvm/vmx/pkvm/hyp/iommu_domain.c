371b8952359ccc44972e3ff1293ede88325e0093
Vineeth Pillai <vineethrp@google.com>
2025-11-21 20:12:55 -0500

    ANDROID: pkvm: x86: Perform iotlb flush unconditionally on unmap
    Host iommu driver has a performance optimization to delay iotlb cache
    flush on unmap. This breaks pkvm security guarantee because the pages
    freed by unmap may be donated to pvms and could be used to store
    sensitve data. Until a flush happens the iotlb cache may contain stale
    entries pointing to these pages and enables the devide to read those
    pages.
    
    So flush iotlb cache unconditionally on unmap for now. We could revisit
    this later to figure out how to securely delay the flush for
    performance.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I4adfcf2d97cf37818c32b71067d1fb3f444180b9
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_domain.c
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_domain.c
@@ -693,10 +693,14 @@ static void domain_unmap(struct pkvm_iommu_domain *domain, unsigned long start_p
 unsigned long pkvm_iommu_domain_unmap(unsigned long pgd_gpa, unsigned long start_pfn,
 		unsigned long last_pfn)
 {
+	unsigned long start = start_pfn << VTD_PAGE_SHIFT;
+	unsigned long end = last_pfn << VTD_PAGE_SHIFT;
 	union pkvm_iommu_page_donation *donation;
 	struct pkvm_iommu_domain *domain;
+	int nr_pages;
 
 	donation = this_cpu_read(iommu_page_donation);
+	nr_pages = donation->nr_pages;
 
 	domain = pkvm_get_iommu_domain(host_gpa2hpa(pgd_gpa));
 	if (!domain) {
@@ -707,6 +711,20 @@ unsigned long pkvm_iommu_domain_unmap(unsigned long pgd_gpa, unsigned long start
 
 	pkvm_spin_lock(&domain->lock);
 	domain_unmap(domain, start_pfn, last_pfn, donation);
+
+	/*
+	 * Regardless of the DMA mode used by host, we perform iotlb flush on
+	 * unmap. Unmapped pages may be donated to a pvm and pvm could use
+	 * it to store sensitve data. Until a flush happens, stale entries in
+	 * cache could enable a device to read those pages which might contain
+	 * sensitive data. So perform flush unconditionally.
+	 */
+	/*
+	 * No new pages released during unmap implies only the leaf
+	 * PTEs were updated. Set IH=1(Invalidation Hint) in that case.
+	 */
+	pkvm_cache_tag_flush_range(domain, start, end,
+				nr_pages == donation->nr_pages);
 	pkvm_spin_unlock(&domain->lock);
 	pkvm_put_iommu_domain(domain);
 
