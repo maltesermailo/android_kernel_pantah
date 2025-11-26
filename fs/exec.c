9bf23b478280b49a35d57ba161e39bfd50c907a2
T.J. Mercier <tjmercier@google.com>
2025-11-26 10:58:52 -0800

    ANDROID: ABI: refresh, CRC fixups and allowed breaks
    We need to add a new pointer to mm_struct, but there is only 1 ABI
    reservation. Use the mm_struct ABI reservation to point to a new
    mm_struct_abi_extend which contains a task_dma_buf_info pointer, and can
    be extended with other fields in the future.
    
    Since mm_struct_abi_extend is meant to be available for extension in the
    future, vendor modules may NOT access this pointer as the size of the
    struct cannot be expected to be stable.
    
    Change-Id: I1707f1184c7e97869c9e4eaff0bcdde13b3725df

--- fs/exec.c
+++ fs/exec.c
@@ -1322,7 +1322,7 @@ int begin_new_exec(struct linux_binprm * bprm)
 	 */
 	if (IS_ENABLED(CONFIG_DMA_SHARED_BUFFER)) {
 		refcount_inc(&current->dmabuf_info->refcnt);
-		me->mm->dmabuf_info = current->dmabuf_info;
+		me->mm->abi_extend->dmabuf_info = current->dmabuf_info;
 	}
 
 #ifdef CONFIG_POSIX_TIMERS
