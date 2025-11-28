cfcc395a9ca4e8ff85731af118c10fdec59c9436
Cong Zhang <cong.zhang@oss.qualcomm.com>
2025-11-28 15:27:44 +0800

    FROMLIST: blk-mq: Abort suspend when wakeup events are pending
    During system suspend, wakeup capable IRQs for block device can be
    delayed, which can cause blk_mq_hctx_notify_offline() to hang
    indefinitely while waiting for pending request to complete.
    Skip the request waiting loop and abort suspend when wakeup events are
    pending to prevent the deadlock.
    
    Bug: 465275370
    Change-Id: I5eb6b271cb4a3c0c552f3dbecc5246ae27e10254
    Link: https://lore.kernel.org/all/20251202-blkmq_skip_waiting-v1-1-f73d8a977ce0@oss.qualcomm.com/
    Fixes: bf0beec0607d ("blk-mq: drain I/O when all CPUs in a hctx are offline")
    Signed-off-by: Cong Zhang <cong.zhang@oss.qualcomm.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -23,6 +23,7 @@
 #include <linux/cache.h>
 #include <linux/sched/topology.h>
 #include <linux/sched/signal.h>
+#include <linux/suspend.h>
 #include <linux/delay.h>
 #include <linux/crash_dump.h>
 #include <linux/prefetch.h>
@@ -3679,6 +3680,7 @@ static int blk_mq_hctx_notify_offline(unsigned int cpu, struct hlist_node *node)
 {
 	struct blk_mq_hw_ctx *hctx = hlist_entry_safe(node,
 			struct blk_mq_hw_ctx, cpuhp_online);
+	int ret = 0;
 
 	if (blk_mq_hctx_has_online_cpu(hctx, cpu))
 		return 0;
@@ -3699,12 +3701,18 @@ static int blk_mq_hctx_notify_offline(unsigned int cpu, struct hlist_node *node)
 	 * frozen and there are no requests.
 	 */
 	if (percpu_ref_tryget(&hctx->queue->q_usage_counter)) {
-		while (blk_mq_hctx_has_requests(hctx))
+		while (blk_mq_hctx_has_requests(hctx)) {
+			if (pm_wakeup_pending()) {
+				clear_bit(BLK_MQ_S_INACTIVE, &hctx->state);
+				ret = -EBUSY;
+				break;
+			}
 			msleep(5);
+		}
 		percpu_ref_put(&hctx->queue->q_usage_counter);
 	}
 
-	return 0;
+	return ret;
 }
 
 /*
