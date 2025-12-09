8e5751284c517425b641eed665ef5508b47b3995
Vineeth Pillai <vineethrp@google.com>
2025-12-09 09:21:25 -0500

    DONOTMERGE: Enable iommu debugging
    Change-Id: Ia90f5e25f3a30efe2dc207358fa230c061d0d868
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/debug.h
+++ arch/x86/kvm/vmx/pkvm/hyp/debug.h
@@ -6,7 +6,7 @@
 #ifndef _PKVM_DEBUG_H_
 #define _PKVM_DEBUG_H_
 
-#define pkvm_dbg(f, x...)	pr_debug(f, ## x)
+#define pkvm_dbg(f, x...)	pr_info(f, ## x)
 #define pkvm_info(f, x...)	pr_info(f, ## x)
 #define pkvm_err(f, x...)	pr_err(f, ## x)
 
