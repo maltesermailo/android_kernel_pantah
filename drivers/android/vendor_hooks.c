c33372fcaf2095199eed19e95e92fddea8bd0c3e
fanshaohua <fanshaohua@xiaomi.corp-partner.google.com>
2025-12-11 15:44:43 +0800

    ANDROID: vendor_hooks: Add vendor hooks for rcu
    Add hooks in rcu to mark the begin and end for expedited grace period and each rcu boost
    
    Bug: 467835301
    Bug: 493943094
    Change-Id: I74c0cebbbf27ea2c2dd3fc9a0b2527ac121629ed
    Signed-off-by: fanshaohua <fanshaohua@xiaomi.corp-partner.google.com>
    (cherry picked from commit 419dd9ec591526d218610da5bd65324faf663774)
    Bug: 503208507
    Signed-off-by: jingyiming <jingyiming@xiaomi.corp-partner.google.com>
    (cherry picked from commit fbcfe3567c2a1238c94f3a1162144b61e41291d5)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -76,6 +76,7 @@
 #include <trace/hooks/sound.h>
 #include <trace/hooks/fuse.h>
 #include <trace/events/android_vendor_lmk.h>
+#include <trace/hooks/rcu.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -478,6 +479,10 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_add_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fuse_request_send_ext);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fuse_request_end_ext);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fuse_request_fetch);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sync_rcu_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sync_rcu_wait_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rcu_boost_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rcu_boost_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_async_mmap_readahead);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_sync_mmap_readahead);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_pr_set_vma_name_bypass);
