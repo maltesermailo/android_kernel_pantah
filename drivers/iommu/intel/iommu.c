26e327ff5905e76bf411b4a7af501c11e906c704
Vineeth Pillai <vineethrp@google.com>
2025-12-14 17:41:47 -0500

    ANDROID: iommu/vt-d: Avoid iotlb flush when pvIOMMU enabled
    pKVM flushes the iotlb cache unconditionally on unmap. This is to close
    a security gap where device will be able to read contents of pages
    donated to pvm that was previously mapped for dma and later unammped but
    not yet flushed. So iommu driver don't need to flush it again.
    
    pKVM also flushes iotlb after a map if the hardware mandates it. So
    iommu driver need not perform iotlb flush after map as well.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: If5dd04b227021bd936da97c0af5463bd29b129aa
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -1908,6 +1908,10 @@ static bool dev_is_real_dma_subdevice(struct device *dev)
 static bool domain_need_iotlb_sync_map(struct dmar_domain *domain,
 				       struct intel_iommu *iommu)
 {
+	/* pKVM performs iotlb flush after map if required */
+	if (pkvm_pviommu_enabled())
+		return false;
+
 	if (cap_caching_mode(iommu->cap) && !domain->use_first_level)
 		return true;
 
@@ -3716,8 +3720,16 @@ static size_t intel_iommu_unmap_pages(struct iommu_domain *domain,
 static void intel_iommu_tlb_sync(struct iommu_domain *domain,
 				 struct iommu_iotlb_gather *gather)
 {
-	cache_tag_flush_range(to_dmar_domain(domain), gather->start,
-			      gather->end, list_empty(&gather->freelist));
+	/*
+	 * pKVM unconditionally performs the flush on unmap to close a security
+	 * gap where device will be able to read contents of a donated page which
+	 * was previously mapped for dma and then unmapped but iotlb not yet
+	 * flushed.
+	 * So, since pKVM already flushed the iotlb, don't bother doing it here.
+	 */
+	if (!pkvm_pviommu_enabled())
+		cache_tag_flush_range(to_dmar_domain(domain), gather->start,
+				      gather->end, list_empty(&gather->freelist));
 	iommu_put_pages_list(&gather->freelist);
 }
 
@@ -3802,8 +3814,13 @@ static bool intel_iommu_capable(struct device *dev, enum iommu_cap cap)
 
 	switch (cap) {
 	case IOMMU_CAP_CACHE_COHERENCY:
-	case IOMMU_CAP_DEFERRED_FLUSH:
 		return true;
+	case IOMMU_CAP_DEFERRED_FLUSH:
+		/*
+		 * pKVM enforces immediate flush and hence
+		 * deferred flush capability is not required.
+		 */
+		return !pkvm_pviommu_enabled();
 	case IOMMU_CAP_PRE_BOOT_PROTECTION:
 		return dmar_platform_optin();
 	case IOMMU_CAP_ENFORCE_CACHE_COHERENCY:
