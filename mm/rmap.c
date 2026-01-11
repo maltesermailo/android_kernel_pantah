2a07e823eda5a4de2be6118fdce0a1039f1df80c
Wenchao Hao <haowenchao@xiaomi.corp-partner.google.com>
2026-01-11 17:38:30 +0800

    ANDROID: vendor_hooks: Add 2 vendor hook in rmap.c
    Add 2 vendor hooks in rmap.c to trace folio's map count and perform
    some special policy.
    
    Bug:474606104
    
    Change-Id: I1805b34a2d0aeba6e40bf1e29f3fe31ae4f79089
    Signed-off-by: Wenchao Hao <haowenchao@xiaomi.corp-partner.google.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -1443,6 +1443,8 @@ static __always_inline void __folio_add_file_rmap(struct folio *folio,
 
 	VM_WARN_ON_FOLIO(folio_test_anon(folio), folio);
 
+	trace_android_vh_folio_add_file_rmap(folio, page, nr_pages, level);
+
 	nr = __folio_add_rmap(folio, page, nr_pages, level, &nr_pmdmapped);
 	__folio_mod_stat(folio, nr, nr_pmdmapped);
 
@@ -1562,6 +1564,7 @@ static __always_inline void __folio_remove_rmap(struct folio *folio,
 	 * it's only reliable while mapped.
 	 */
 
+	trace_android_vh_folio_remove_rmap(folio, page, nr_pages, level);
 	munlock_vma_folio(folio, vma);
 }
 
