21689c578183e774a8060f586f3b6ff582186f3e
Paras Sharma <paras.sharma@oss.qualcomm.com>
2026-01-12 17:39:25 +0530

    ANDROID: abi_gki_aarch64_qcom: Add PCIe PTM related symbol and config
    Add PCIe PTM related symbol to abi_gki_aarch64_qcom:
    pci_enable_ptm
    
    Enable PCIe PTM related config:
    CONFIG_PCIE_PTM
    
    Refactor pci_dev layout to avoid KMI break by relocating PTM related
    fields into existing padding.
    
    This is needed for enabling PCIe PTM.
    
    Change-Id: Ia4ac435270c29234a8dc59ee454de27018dd206a
    Signed-off-by: Paras Sharma <paras.sharma@oss.qualcomm.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -465,7 +465,17 @@ struct pci_dev {
 	unsigned int	no_vf_scan:1;		/* Don't scan for VFs after IOV enablement */
 	unsigned int	no_command_memory:1;	/* No PCI_COMMAND_MEMORY */
 	unsigned int	rom_bar_overlap:1;	/* ROM BAR disable broken */
+
+#ifdef CONFIG_PCIE_PTM
+	unsigned int	ptm_root:1;
+	unsigned int	ptm_enabled:1;
+#endif
 	pci_dev_flags_t dev_flags;
+
+#ifdef CONFIG_PCIE_PTM
+	u16		ptm_cap;		/* PTM Capability */
+	u8		ptm_granularity;
+#endif
 	atomic_t	enable_cnt;	/* pci_enable_device has been called */
 
 	u32		saved_config_space[16]; /* Config space saved at suspend time */
@@ -476,12 +486,6 @@ struct pci_dev {
 
 #ifdef CONFIG_HOTPLUG_PCI_PCIE
 	unsigned int	broken_cmd_compl:1;	/* No compl for some cmds */
-#endif
-#ifdef CONFIG_PCIE_PTM
-	u16		ptm_cap;		/* PTM Capability */
-	unsigned int	ptm_root:1;
-	unsigned int	ptm_enabled:1;
-	u8		ptm_granularity;
 #endif
 #ifdef CONFIG_PCI_MSI
 	void __iomem	*msix_base;
