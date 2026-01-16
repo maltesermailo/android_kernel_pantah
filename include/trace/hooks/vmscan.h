aaf561b6f664839a188131b9a76557cdf9364f97
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-01-16 10:34:45 -0800

    Revert "BACKPORT: ANDROID: vendor_hook: Add hooks to maintain fo..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/c320090a-4df0-45db-8a92-250a9d32a909). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I37a55eea79f840fac44d0bc97323850fe9f6b295
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -43,12 +43,6 @@ DECLARE_HOOK(android_vh_should_continue_reclaim,
 DECLARE_HOOK(android_vh_file_is_tiny_bypass,
 	TP_PROTO(bool file_is_tiny, bool *bypass),
 	TP_ARGS(file_is_tiny, bypass));
-DECLARE_HOOK(android_vh_handle_folio_writeback,
-	TP_PROTO(struct folio *folio, bool *bypass),
-	TP_ARGS(folio, bypass));
-DECLARE_HOOK(android_vh_reclaim_before_kswapd,
-	TP_PROTO(unsigned long *nr_reclaimed),
-	TP_ARGS(nr_reclaimed));
 DECLARE_HOOK(android_vh_check_folio_look_around_ref,
 	TP_PROTO(struct folio *folio, int *skip),
 	TP_ARGS(folio, skip));
