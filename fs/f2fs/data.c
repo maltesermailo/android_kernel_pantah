e71d9e9b0fb617a2482ed21cb8efc56e9f3dd1fc
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-01-29 13:51:39 -0800

    Revert "FROMGIT: f2fs: trace elapsed time for node_change lock"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/3ec151c8-dc66-41bc-80b1-8b208db1de8d). Please review this revert and approve if it looks good to you.
    
    Change-Id: Ifc9ab6d3f358523cf071ac1159e62125a76424f4
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 408202989

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1479,7 +1479,7 @@ static void f2fs_map_lock(struct f2fs_sb_info *sbi,
 				int flag)
 {
 	if (flag == F2FS_GET_BLOCK_PRE_AIO)
-		f2fs_down_read_trace(&sbi->node_change, lc);
+		f2fs_down_read(&sbi->node_change);
 	else
 		f2fs_lock_op(sbi, lc);
 }
@@ -1489,7 +1489,7 @@ static void f2fs_map_unlock(struct f2fs_sb_info *sbi,
 				int flag)
 {
 	if (flag == F2FS_GET_BLOCK_PRE_AIO)
-		f2fs_up_read_trace(&sbi->node_change, lc);
+		f2fs_up_read(&sbi->node_change);
 	else
 		f2fs_unlock_op(sbi, lc);
 }
