89d2aa58a7ae37d0fbfa67fb49513edbcd0f5c22
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-01-29 13:51:39 -0800

    Revert "FROMGIT: f2fs: trace elapsed time for node_write lock"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/3ec151c8-dc66-41bc-80b1-8b208db1de8d). Please review this revert and approve if it looks good to you.
    
    Change-Id: I53c8ebe5d54c4672b21b938649ed1745ed327b95
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 408202989

--- include/trace/events/f2fs.h
+++ include/trace/events/f2fs.h
@@ -178,8 +178,7 @@ TRACE_DEFINE_ENUM(EX_BLOCK_AGE);
 #define show_lock_name(lock)						\
 	__print_symbolic(lock,						\
 		{ LOCK_NAME_CP_RWSEM,		"cp_rwsem" },		\
-		{ LOCK_NAME_NODE_CHANGE,	"node_change" },	\
-		{ LOCK_NAME_NODE_WRITE,		"node_write" })
+		{ LOCK_NAME_NODE_CHANGE,	"node_change" })
 
 struct f2fs_sb_info;
 struct f2fs_io_info;
