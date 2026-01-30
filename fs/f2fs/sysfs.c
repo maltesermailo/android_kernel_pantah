e9b0d44be79d3dfe5c8f0a9b85c61fe28be37209
Chao Yu <chao@kernel.org>
2026-01-30 21:28:08 +0800

    BACKPORT: UPSTREAM: f2fs: fix lock priority inversion issue
    If userspace thread has held f2fs rw semaphore, due to its low priority,
    it could be runnable or preempted state for long time, during the time,
    it will block high priority thread which is trying to grab the same rw
    semaphore, e.g. cp_rwsem, io_rwsem...
    
    To fix such issue, let's detect thread's priority when it tries to grab
    f2fs_rwsem lock, if the priority is lower than a priority threshold, let's
    uplift the priority before it enters into critical region of lock, and
    restore the priority after it leaves from critical region.
    
    Meanwhile, introducing two new sysfs nodes:
    - /sys/fs/f2fs/<disk>/adjust_lock_priority, it is used to control whether
    the functionality is enable or not.
    ==========     ==================
    Flag_Value     Flag_Description
    ==========     ==================
    0x00000000     Disabled (default)
    0x00000001     cp_rwsem
    0x00000002     node_change
    0x00000004     node_write
    0x00000008     gc_lock
    0x00000010     cp_global
    0x00000020     io_rwsem
    ==========     ==================
    - /sys/fs/f2fs/<disk>/lock_duration_priority, it is used to control
    priority threshold.
    
    Bug: 408202989
    Change-Id: Icde023cdc46dd7e9be16635cc6f047c9101051fa
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 07de55cbf5762cb4a7e9e0db7aba5c10c8cfe079)
    [Chao: fix the conflict in sysfs.c]
    Signed-off-by: Chao Yu <chaseyu@google.com>

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -794,6 +794,20 @@ out:
 		return count;
 	}
 
+	if (!strcmp(a->attr.name, "adjust_lock_priority")) {
+		if (t >= BIT(LOCK_NAME_MAX - 1))
+			return -EINVAL;
+		sbi->adjust_lock_priority = t;
+		return count;
+	}
+
+	if (!strcmp(a->attr.name, "lock_duration_priority")) {
+		if (t < NICE_TO_PRIO(MIN_NICE) || t > NICE_TO_PRIO(MAX_NICE))
+			return -EINVAL;
+		sbi->lock_duration_priority = t;
+		return count;
+	}
+
 	__sbi_store_value(a, sbi, ptr + a->offset, t);
 
 	return count;
@@ -1067,6 +1081,8 @@ F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, warm_data_age_threshold, warm_data_age_thre
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, last_age_weight, last_age_weight);
 
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, max_lock_elapsed_time, max_lock_elapsed_time);
+F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, lock_duration_priority, lock_duration_priority);
+F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, adjust_lock_priority, adjust_lock_priority);
 
 #define ATTR_LIST(name) (&f2fs_attr_##name.attr)
 static struct attribute *f2fs_attrs[] = {
@@ -1172,6 +1188,8 @@ static struct attribute *f2fs_attrs[] = {
 	ATTR_LIST(warm_data_age_threshold),
 	ATTR_LIST(last_age_weight),
 	ATTR_LIST(max_lock_elapsed_time),
+	ATTR_LIST(lock_duration_priority),
+	ATTR_LIST(adjust_lock_priority),
 	NULL,
 };
 ATTRIBUTE_GROUPS(f2fs);
