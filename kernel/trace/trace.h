8cc1f143832c0878d5712d72e776dc231ff3efc1
Steven Rostedt <rostedt@goodmis.org>
2026-02-07 22:24:20 -0500

    BACKPORT: tracing: Move __trace_buffer_{un}lock_*() functions to trace.h
    The file trace.c has become a catchall for most things tracing. Start
    making it smaller by breaking out various aspects into their own files.
    
    Move the __always_inline functions __trace_buffer_lock_reserve(),
    __trace_buffer_unlock_commit() and trace_event_setup() into trace.h.
    
    The trace.c file will be split up and these functions will be used in more
    than one of these files. As they are already __always_inline they can
    easily be moved into the trace.h header file.
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Link: https://patch.msgid.link/20260208032449.813550600@kernel.org
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    (cherry picked from commit 0e730bc067e7a790d61344dbf6d9dfdce7f99ea3)
    
    Conflicts:
       kernel/trace/trace.h
    
    Bug: 466891640
    Test: Build and boot test with persistent ring buffer
    
    Change-Id: I4bce733d45593f9a67285a3b65992d17f9d50d9d
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>

--- kernel/trace/trace.h
+++ kernel/trace/trace.h
@@ -1490,6 +1490,47 @@ void trace_buffered_event_enable(void);
 
 void early_enable_events(struct trace_array *tr, char *buf, bool disable_first);
 
+static __always_inline void
+trace_event_setup(struct ring_buffer_event *event,
+		  int type, unsigned int trace_ctx)
+{
+	struct trace_entry *ent = ring_buffer_event_data(event);
+
+	tracing_generic_entry_update(ent, type, trace_ctx);
+}
+
+static __always_inline struct ring_buffer_event *
+__trace_buffer_lock_reserve(struct trace_buffer *buffer,
+			  int type,
+			  unsigned long len,
+			  unsigned int trace_ctx)
+{
+	struct ring_buffer_event *event;
+
+	event = ring_buffer_lock_reserve(buffer, len);
+	if (event != NULL)
+		trace_event_setup(event, type, trace_ctx);
+
+	return event;
+}
+
+static __always_inline void
+__buffer_unlock_commit(struct trace_buffer *buffer, struct ring_buffer_event *event)
+{
+	__this_cpu_write(trace_taskinfo_save, true);
+
+	/* If this is the temp buffer, we need to commit fully */
+	if (this_cpu_read(trace_buffered_event) == event) {
+		/* Length is in event->array[0] */
+		ring_buffer_write(buffer, event->array[0], &event->array[1]);
+		/* Release the temp buffer */
+		this_cpu_dec(trace_buffered_event_cnt);
+		/* ring_buffer_unlock_commit() enables preemption */
+		preempt_enable_notrace();
+	} else
+		ring_buffer_unlock_commit(buffer);
+}
+
 static inline void
 __trace_event_discard_commit(struct trace_buffer *buffer,
 			     struct ring_buffer_event *event)
