ee57cd14c82d696a356610d276e2199b5296425c
Alice Ryhl <aliceryhl@google.com>
2026-02-18 11:53:27 +0000

    BACKPORT: FROMLIST: rust_binder: avoid reading the written value in offsets array
    When sending a transaction, its offsets array is first copied into the
    target proc's vma, and then the values are read back from there. This is
    normally fine because the vma is a read-only mapping, so the target
    process cannot change the value under us.
    
    However, if the target process somehow gains the ability to write to its
    own vma, it could change the offset before it's read back, causing the
    kernel to misinterpret what the sender meant. If the sender happens to
    send a payload with a specific shape, this could in the worst case lead
    to the receiver being able to privilege escalate into the sender.
    
    The intent is that gaining the ability to change the read-only vma of
    your own process should not be exploitable, so remove this TOCTOU read
    even though it's unexploitable without another Binder bug.
    
    Cc: stable@vger.kernel.org
    Fixes: eafedbc7c050 ("rust_binder: add Rust Binder driver")
    Reported-by: Jann Horn <jannh@google.com>
    Reviewed-by: Jann Horn <jannh@google.com>
    Link: https://lore.kernel.org/r/20260218-binder-vma-check-v2-2-60f9d695a990@google.com
    Bug: 485023800
    [aliceryhl: adjust UserSlice creation due to UserPtr missing]
    Change-Id: I5da1e2dab72951663ad9803c61e0a2be206245fd
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/thread.rs
+++ drivers/android/binder/thread.rs
@@ -1137,10 +1137,8 @@ impl Thread {
 
         // Copy offsets if there are any.
         if offsets_size > 0 {
-            {
-                let mut reader = UserSlice::new(trd_data_ptr.offsets as _, offsets_size).reader();
-                alloc.copy_into(&mut reader, aligned_data_size, offsets_size)?;
-            }
+            let mut offsets_reader =
+                UserSlice::new(trd_data_ptr.offsets as _, offsets_size).reader();
 
             let offsets_start = aligned_data_size;
             let offsets_end = aligned_data_size + offsets_size;
@@ -1161,11 +1159,9 @@ impl Thread {
                 .step_by(size_of::<u64>())
                 .enumerate()
             {
-                let offset: usize = view
-                    .alloc
-                    .read::<u64>(index_offset)?
-                    .try_into()
-                    .map_err(|_| EINVAL)?;
+                let offset = offsets_reader.read::<u64>()?;
+                view.alloc.write(index_offset, &offset)?;
+                let offset: usize = offset.try_into().map_err(|_| EINVAL)?;
 
                 if offset < end_of_previous_object || !is_aligned(offset, size_of::<u32>()) {
                     pr_warn!("Got transaction with invalid offset.");
