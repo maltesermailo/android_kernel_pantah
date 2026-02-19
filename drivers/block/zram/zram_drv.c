593c8d68cbe87b63fd710e27e9ac0105c616764f
Richard Chang <richardycc@google.com>
2026-02-19 17:35:44 +0000

    ANDROID: zram: introduce ZRAM_PREFETCH_CACHE flag
    The prefetch cache currently relies on XArray lookups to determine if a
    slot's backing device block index (blk_idx) is cached. This introduces
    performance overhead due to XArray's internal locking and search
    latency, especially during frequent state checks.
    
    To mitigate global lock contention and reduce lookup overhead, introduce
    the ZRAM_PREFETCH_CACHE flag to explicitly track slots with cached
    blk_idx. This provides a per-slot state that simplifies and accelerates
    checks in several paths without unnecessary XArray operations.
    
    Bug: 340137613
    Change-Id: I9cff1647da0f35c0a4a9e0a5ef3d3f2498c924d8
    Signed-off-by: Richard Chang <richardycc@google.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1061,7 +1061,8 @@ static int restore_bdev_page(struct zram *zram, struct page *page, u32 index)
 	 * changed via slot_free(). To avoid the race, we need to check ZRAM_WB
 	 * again.
 	 */
-	if (!zram_test_flag(zram, index, ZRAM_WB)) {
+	if (!zram_test_flag(zram, index, ZRAM_WB) ||
+	    zram_test_flag(zram, index, ZRAM_PREFETCH_CACHE)) {
 		zram_slot_unlock(zram, index);
 		return -EIO;
 	}
@@ -1106,6 +1107,12 @@ static int restore_bdev_page(struct zram *zram, struct page *page, u32 index)
 		return -ENOMEM;
 	}
 	WARN_ON(xa_is_value(old_val));
+
+	/*
+	 * Set ZRAM_PREFETCH_CACHE flag before zram_free_page to avoid
+	 * releasing the blk_idx
+	 */
+	zram_set_flag(zram, index, ZRAM_PREFETCH_CACHE);
 
 	zram_free_page(zram, index);
 #ifdef CONFIG_ZRAM_TRACK_ENTRY_ACTIME
@@ -1336,10 +1343,13 @@ int zram_writeback_slots(struct zram *zram,
 			goto next;
 
 		/* Reuse the blk_idx if it is found in the prefetch cache. */
-		val = xa_erase(&zram->prefetch_cache, index);
-		if (xa_is_value(val)) {
-			zram_rebind_bdev_block(zram, index, xa_to_value(val));
-			goto next;
+		if (zram_test_flag(zram, index, ZRAM_PREFETCH_CACHE)) {
+			zram_clear_flag(zram, index, ZRAM_PREFETCH_CACHE);
+			val = xa_erase(&zram->prefetch_cache, index);
+			if (xa_is_value(val)) {
+				zram_rebind_bdev_block(zram, index, xa_to_value(val));
+				goto next;
+			}
 		}
 
 		if (zram->wb_compressed)
@@ -2349,7 +2359,7 @@ static void zram_free_page(struct zram *zram, size_t index)
 	if (zram_test_flag(zram, index, ZRAM_WB)) {
 		zram_clear_flag(zram, index, ZRAM_WB);
 #if defined CONFIG_ZRAM_WRITEBACK
-		if (!xa_load(&zram->prefetch_cache, index))
+		if (!zram_test_flag(zram, index, ZRAM_PREFETCH_CACHE))
 #endif
 			zram_release_bdev_block(zram,
 						zram_get_handle(zram, index));
@@ -3013,7 +3023,7 @@ static ssize_t recompress_store(struct device *dev,
 		 * fault by using the wrong decompression algorithm. So we skip
 		 * such slots during recompression.
 		 */
-		if (xa_load(&zram->prefetch_cache, pps->index))
+		if (zram_test_flag(zram, pps->index, ZRAM_PREFETCH_CACHE))
 			goto next;
 #endif
 
@@ -3169,9 +3179,6 @@ static void zram_slot_free_notify(struct block_device *bdev,
 				unsigned long index)
 {
 	struct zram *zram;
-#if defined CONFIG_ZRAM_WRITEBACK
-	void *val;
-#endif
 
 	zram = bdev->bd_disk->private_data;
 
@@ -3183,9 +3190,14 @@ static void zram_slot_free_notify(struct block_device *bdev,
 
 	zram_free_page(zram, index);
 #if defined CONFIG_ZRAM_WRITEBACK
-	val = xa_erase(&zram->prefetch_cache, index);
-	if (xa_is_value(val))
-		zram_release_bdev_block(zram, xa_to_value(val));
+	if (zram_test_flag(zram, index, ZRAM_PREFETCH_CACHE)) {
+		void *val;
+
+		zram_clear_flag(zram, index, ZRAM_PREFETCH_CACHE);
+		val = xa_erase(&zram->prefetch_cache, index);
+		if (xa_is_value(val))
+			zram_release_bdev_block(zram, xa_to_value(val));
+	}
 #endif
 	zram_slot_unlock(zram, index);
 }
