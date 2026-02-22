86c142ba9ad67711c94026acaf42910132548442
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-02-22 11:25:38 -0800

    Revert "UPSTREAM: f2fs: fix missing discard for active segments"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/c5630ffb-fbd2-4de6-ba89-28a67b5057cd). Please review this revert and approve if it looks good to you.
    
    Change-Id: Ibac35928573b910395629c2d73a082df3d1d7e47
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 455453371

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -2096,9 +2096,7 @@ static bool add_discard_addrs(struct f2fs_sb_info *sbi, struct cp_control *cpc,
 		return false;
 
 	if (!force) {
-		if (!f2fs_realtime_discard_enable(sbi) ||
-			(!se->valid_blocks &&
-				!IS_CURSEG(sbi, cpc->trim_start)) ||
+		if (!f2fs_realtime_discard_enable(sbi) || !se->valid_blocks ||
 			SM_I(sbi)->dcc_info->nr_discards >=
 				SM_I(sbi)->dcc_info->max_discards)
 			return false;
