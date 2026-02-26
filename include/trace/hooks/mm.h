ebbe40df14e24639d736fa93c5283f3822aa3c79
wudihui <wudihui@xiaomi.corp-partner.google.com>
2026-02-26 11:25:33 +0800

    ANDROID: GKI: add vendor hooks for memory compaction and cma free.
    Add vendor hooks to __compact_finished to prevent unnecessary compaction.
    
    Add vendor hooks to __cma_release to collect CMA statistics and place freed CMA memory into a pool.
    
    Bug: 483845255
    
    Change-Id: I8179d197322af04b1828a7480485cfcb6bb576cc
    Signed-off-by: wudihui <wudihui@xiaomi.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -192,6 +192,11 @@ DECLARE_HOOK(android_vh_si_meminfo_adjust_shmem,
 DECLARE_HOOK(android_vh_compact_finished,
 	TP_PROTO(bool *abort_compact),
 	TP_ARGS(abort_compact));
+
+struct compact_control;
+DECLARE_HOOK(android_vh_compact_bypass,
+	TP_PROTO(struct compact_control *cc, bool *bypass),
+	TP_ARGS(cc, bypass));
 DECLARE_HOOK(android_vh_madvise_cold_or_pageout_abort,
 	TP_PROTO(struct vm_area_struct *vma, bool *abort_madvise),
 	TP_ARGS(vma, abort_madvise));
@@ -277,6 +282,14 @@ DECLARE_HOOK(android_vh_thaw_killed_process,
 DECLARE_HOOK(android_vh_cma_alloc_retry,
 	TP_PROTO(char *name, int *retry),
 	TP_ARGS(name, retry));
+DECLARE_HOOK(android_vh_cma_release_bypass,
+	TP_PROTO(struct cma *cma, const struct page *pages, unsigned long count,
+		bool *bypass),
+	TP_ARGS(cma, pages, count, bypass));
+DECLARE_HOOK(android_vh_cma_alloc_end,
+	TP_PROTO(struct cma *cma, unsigned long pfn, const struct page *pages,
+		unsigned long count, unsigned int align, int ret),
+	TP_ARGS(cma, pfn, pages, count, align, ret));
 DECLARE_HOOK(android_vh_wp_page_reuse,
 	TP_PROTO(struct vm_fault *vmf, struct folio *folio),
 	TP_ARGS(vmf, folio));
