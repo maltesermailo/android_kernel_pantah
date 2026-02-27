8f1c9b8e9598299508777cb8d84633a04e40277d
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2026-02-27 18:20:27 +0900

    ANDROID: FROMLIST: ring-buffer: Flush and stop persistent ring buffer on panic
    On real hardware, panic and machine reboot may not flush hardware cache
    to memory. This means the persistent ring buffer, which relies on a
    coherent state of memory, may not have its events written to the buffer
    and they may be lost. Moreover, there may be inconsistency with the
    counters which are used for validation of the integrity of the
    persistent ring buffer which may cause all data to be discarded.
    
    To avoid this issue, stop recording of the ring buffer on panic and
    flush the cache of the ring buffer's memory.
    
    Fixes: e645535a954a ("tracing: Add option to use memmapped memory for trace boot instance")
    Cc: stable@vger.kernel.org
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    (am from https://patchwork.kernel.org/project/linux-trace-kernel/patch/177218402749.1988514.15768451704953140678.stgit@mhiramat.tok.corp.google.com/)
    (also found at https://marc.info/?i=177218402749.1988514.15768451704953140678.stgit@mhiramat.tok.corp.google.com)
    [mhiramat: Resolved minor conflcts in arch/alpha/include/asm/Kbuild,
     arch/hexagon/include/asm/Kbuild, arch/riscv/include/asm/Kbuild,
     arch/sh/include/asm/Kbuild, arch/sparc/include/asm/Kbuild and
     kernel/trace/ring_buffer.c ]
    
    Bug: 466891640
    Test: Build and boot test with persistent ring buffer
    
    Change-Id: I4ab50f2aead8017e8684a5e1b1fd40ff5da9fbd0
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -6,6 +6,7 @@
  */
 #include <linux/page_size_compat_defs.h>
 #include <linux/trace_recursion.h>
+#include <linux/panic_notifier.h>
 #include <linux/trace_events.h>
 #include <linux/ring_buffer.h>
 #include <linux/trace_clock.h>
@@ -30,6 +31,7 @@
 #include <linux/oom.h>
 #include <linux/mm.h>
 
+#include <asm/ring_buffer.h>
 #include <asm/local64.h>
 #include <asm/local.h>
 #include <asm/setup.h>
@@ -523,6 +525,7 @@ struct trace_buffer {
 
 	unsigned long			range_addr_start;
 	unsigned long			range_addr_end;
+	struct notifier_block		flush_nb;
 
 	struct ring_buffer_meta		*meta;
 
@@ -2480,6 +2483,16 @@ static void rb_free_cpu_buffer(struct ring_buffer_per_cpu *cpu_buffer)
 	kfree(cpu_buffer);
 }
 
+/* Stop recording on a persistent buffer and flush cache if needed. */
+static int rb_flush_buffer_cb(struct notifier_block *nb, unsigned long event, void *data)
+{
+	struct trace_buffer *buffer = container_of(nb, struct trace_buffer, flush_nb);
+
+	ring_buffer_record_off(buffer);
+	arch_ring_buffer_flush_range(buffer->range_addr_start, buffer->range_addr_end);
+	return NOTIFY_DONE;
+}
+
 static struct trace_buffer *alloc_buffer(unsigned long size, unsigned flags,
 					 int order, unsigned long start,
 					 unsigned long end,
@@ -2605,6 +2618,12 @@ static struct trace_buffer *alloc_buffer(unsigned long size, unsigned flags,
 
 	mutex_init(&buffer->mutex);
 
+	/* Persistent ring buffer needs to flush cache before reboot. */
+	if (start & end) {
+		buffer->flush_nb.notifier_call = rb_flush_buffer_cb;
+		atomic_notifier_chain_register(&panic_notifier_list, &buffer->flush_nb);
+	}
+
 	return buffer;
 
  fail_free_buffers:
@@ -2694,6 +2713,9 @@ void
 ring_buffer_free(struct trace_buffer *buffer)
 {
 	int cpu;
+
+	if (buffer->range_addr_start && buffer->range_addr_end)
+		atomic_notifier_chain_unregister(&panic_notifier_list, &buffer->flush_nb);
 
 	cpuhp_state_remove_instance(CPUHP_TRACE_RB_PREPARE, &buffer->node);
 
