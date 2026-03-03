e1f2074e3e4645c9b41520d563a4baabe6417a41
Alice Ryhl <aliceryhl@google.com>
2026-03-03 13:54:10 +0000

    ANDROID: rust_binder: future proof rust_binder_process_task()
    A currently in-progress change to reduce memory usage of dead processes
    means that process->task may become NULL on deferred_release(). Thus,
    document for vendor hooks that they should be careful when reading this
    field.
    
    Bug: 486143390
    Change-Id: I7795548d0d80f4192ee18c625d15ee7467e28473
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/rust_binder.h
+++ drivers/android/binder/rust_binder.h
@@ -133,9 +133,16 @@ static inline s32 rust_binder_thread_id(rust_binder_thread t)
 	return * (s32 *) (t + RUST_BINDER_LAYOUT.th.id);
 }
 
+/*
+ * Binder process deferred_release may set this field to NULL, so unless you
+ * are called from ioctl context on the process itself, this may be null and
+ * should be used under rcu_read_lock().
+ */
 static inline struct task_struct *rust_binder_process_task(rust_binder_process t)
 {
-	return * (struct task_struct **) (t + RUST_BINDER_LAYOUT.p.task);
+	struct task_struct **task = (struct task_struct **) (t + RUST_BINDER_LAYOUT.p.task);
+
+	return READ_ONCE(*task);
 }
 
 static inline size_t rust_binder_node_debug_id(rust_binder_node t)
