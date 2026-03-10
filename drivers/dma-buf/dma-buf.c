60a5ccb038aeb23cff4fbeb27e7dc97eb9e139c2
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-03-10 13:14:33 -0700

    Revert "ANDROID: Add dmabuf RSS trace event"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/90191813-f770-4592-9fd2-f7b25b298ae3). Please review this revert and approve if it looks good to you.
    
    Bug: 491532154
    Bug: 491533679
    Change-Id: I391217c4446ae2163a3275494be09c71db7cbce5
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 424646615
    Bug: 489619376

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -33,8 +33,6 @@
 #include <uapi/linux/dma-buf.h>
 #include <uapi/linux/magic.h>
 
-#include <trace/events/kmem.h>
-
 #include "dma-buf-sysfs-stats.h"
 
 #include <trace/hooks/dmabuf.h>
@@ -374,7 +372,6 @@ static void add_task_dmabuf_record(struct task_dma_buf_info *dmabuf_info,
 	dmabuf_info->rss += dmabuf->size;
 	if (dmabuf_info->rss > dmabuf_info->rss_hwm)
 		dmabuf_info->rss_hwm = dmabuf_info->rss;
-	trace_dmabuf_rss_stat(dmabuf_info->rss, dmabuf->size, dmabuf);
 }
 
 /**
@@ -442,7 +439,6 @@ void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_struct *task)
 			list_del(&rec->node);
 			dmabuf_info->dmabuf_count--;
 			dmabuf_info->rss -= dmabuf->size;
-			trace_dmabuf_rss_stat(dmabuf_info->rss, -dmabuf->size, dmabuf);
 		} else {
 			rec = NULL;
 		}
