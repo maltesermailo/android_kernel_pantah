3e550ce03d4d030a91f8212bd24feda758ecfe3b
Mostafa Saleh <smostafa@google.com>
2026-03-11 15:43:08 +0000

    ANDROID: KVM: arm64: Support KASAN in pKVM
    To support KASAN in the hypervisor, every memory access is
    instrumented through the compiler by generating callbacks
    on every read/write or using complier intrinsics as memcpy.
    
    These handlers need to be implemented by the hypervisor to
    check if software is allowed to access this memory.
    
    Some differences between the kernel and hypervisor kasan:
    - The hypervisor only supports the callback based one which is
      similar to CONFIG_KASAN_OUTLINE + CONFIG_KASAN_GENERIC
    - The hypervisor only tracks if an address is allowed or not and
      doesn't check for read/write accesss separately.
    - No reports printed from the hypervisor, just a panic.
    
    This patch only adds the callbacks with the main function
    alloc_check_range() is empty, that is added later for the hypervisor
    heap allocator which is the only supported allocator in KASAN.
    
    Change-Id: I6094c617b7be538210b36d6f9f7b2b86093c578e
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

---
+++ arch/arm64/kvm/hyp/include/nvhe/kasan.h
@@ -0,0 +1,62 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __NVHE_KASAN_KASAN_H
+#define __NVHE_KASAN_KASAN_H
+
+/*
+ * Exported functions for interfaces called from assembly or from generated
+ * code. Declared here to avoid warnings about missing declarations.
+ */
+
+void __asan_register_globals(void *globals, ssize_t size);
+void __asan_unregister_globals(void *globals, ssize_t size);
+void __asan_handle_no_return(void);
+
+void __asan_load1(void *);
+void __asan_store1(void *);
+void __asan_load2(void *);
+void __asan_store2(void *);
+void __asan_load4(void *);
+void __asan_store4(void *);
+void __asan_load8(void *);
+void __asan_store8(void *);
+void __asan_load16(void *);
+void __asan_store16(void *);
+void __asan_loadN(void *, ssize_t size);
+void __asan_storeN(void *, ssize_t size);
+
+void __asan_load1_noabort(void *);
+void __asan_store1_noabort(void *);
+void __asan_load2_noabort(void *);
+void __asan_store2_noabort(void *);
+void __asan_load4_noabort(void *);
+void __asan_store4_noabort(void *);
+void __asan_load8_noabort(void *);
+void __asan_store8_noabort(void *);
+void __asan_load16_noabort(void *);
+void __asan_store16_noabort(void *);
+void __asan_loadN_noabort(void *, ssize_t size);
+void __asan_storeN_noabort(void *, ssize_t size);
+
+void __asan_report_load1_noabort(void *);
+void __asan_report_store1_noabort(void *);
+void __asan_report_load2_noabort(void *);
+void __asan_report_store2_noabort(void *);
+void __asan_report_load4_noabort(void *);
+void __asan_report_store4_noabort(void *);
+void __asan_report_load8_noabort(void *);
+void __asan_report_store8_noabort(void *);
+void __asan_report_load16_noabort(void *);
+void __asan_report_store16_noabort(void *);
+void __asan_report_load_n_noabort(void *, ssize_t size);
+void __asan_report_store_n_noabort(void *, ssize_t size);
+
+void *__asan_memset(void *addr, int c, ssize_t len);
+void *__asan_memcpy(void *dest, const void *src, ssize_t len);
+
+static inline bool hyp_alloc_check_range(const volatile void *p, size_t size)
+{
+	/* Main function to do checks, To be implemented next */
+	return true;
+}
+
+#endif /* __NVHE_KASAN_KASAN_H */
