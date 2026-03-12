14b2bf7403f498a0e435d9b3b77f385915e7d042
Mostafa Saleh <smostafa@google.com>
2026-03-12 16:47:08 +0000

    ANDROID: kvm: arm64: iommu: Add CMA donate/reclaim
    Allow the host to topup iommu CMA pool and support allocating and
    reclaiming from it from EL2 IOMMU modules.
    
    Bug: 383901011
    Bug: 277989609
    Bug: 357781595
    Change-Id: I82b1eb9647233ada650271e9f38d96bff5abd250
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -35,6 +35,8 @@ static bool iommu_pools_ready;
 
 phys_addr_t cma_base;
 size_t cma_size;
+static struct hyp_pool iommu_cma_pool;
+static const u8 pmd_order = PMD_SHIFT - PAGE_SHIFT;
 
 /*
  * We support multiple drivers for the host kernel, but only one for the guest,
@@ -64,12 +66,56 @@ static void kvm_iommu_drv_unlock(void)
 	hyp_read_unlock(&kvm_iommu_reg_lock);
 }
 
+static bool kvm_iommu_donate_from_cma(phys_addr_t phys, unsigned long order)
+{
+	phys_addr_t end = phys + PAGE_SIZE * (1 << order);
+
+	if (end <= phys)
+		return false;
+
+	if (order != pmd_order)
+		return false;
+
+	if (!IS_ALIGNED(phys, PMD_SIZE))
+		return false;
+
+	if (phys < cma_base || end > cma_base + cma_size)
+		return false;
+
+	return true;
+}
+
 static int kvm_iommu_refill(struct kvm_hyp_memcache *host_mc)
 {
+	struct kvm_hyp_memcache tmp_mc = *host_mc;
+
 	if (!iommu_pools_ready)
 		return -EINVAL;
 
-	return refill_hyp_pool(&iommu_host_pool, host_mc);
+	while (tmp_mc.nr_pages) {
+		unsigned long order = FIELD_GET(~PAGE_MASK, tmp_mc.head);
+		phys_addr_t phys = tmp_mc.head & PAGE_MASK;
+		struct hyp_pool *pool = &iommu_host_pool;
+		u64 nr_pages;
+		void *addr;
+
+		if (check_shl_overflow(1UL, order, &nr_pages) ||
+		    !IS_ALIGNED(phys, PAGE_SIZE << order))
+			return -EINVAL;
+
+		addr = admit_host_page(&tmp_mc, order);
+		if (IS_ERR_OR_NULL(addr))
+			return addr ? PTR_ERR(addr) : -EINVAL;
+
+		*host_mc = tmp_mc;
+
+		if (kvm_iommu_donate_from_cma(phys, order))
+			pool = &iommu_cma_pool;
+
+		WARN_ON(hyp_pool_admit(pool, hyp_virt_to_page(addr), order));
+	}
+
+	return 0;
 }
 
 static void kvm_iommu_reclaim(struct kvm_hyp_memcache *host_mc, int target)
@@ -163,6 +209,50 @@ static int kvm_iommu_snapshot_host_stage2(struct kvm_iommu_ops *ops)
 	return ret;
 }
 
+static int kvm_iommu_init_cma_pool(void)
+{
+	phys_addr_t cma_pfn = hyp_phys_to_pfn(cma_base);
+	size_t cma_nr_pages = cma_size >> PAGE_SHIFT;
+	int ret, i;
+
+	if (!cma_size)
+		return 0;
+
+	if (!is_power_of_2(cma_size))
+		return -EINVAL;
+
+	/*
+	 * Donate the pages temporarily, at this point the CMA is not created
+	 * yet in EL1 so the memory is reserved.
+	 */
+	ret = __pkvm_host_donate_hyp(cma_pfn, cma_nr_pages);
+	if (ret)
+		return ret;
+
+	ret = hyp_pool_init(&iommu_cma_pool, cma_pfn, cma_nr_pages, 0);
+	if (ret)
+		goto out_donate_host;
+
+	__hyp_pool_set_range_reclaimable(&iommu_cma_pool);
+
+	/*
+	 * Allocate and immediately reclaim and donate the pages back
+	 * to the host so it can init the CMA pool.
+	 */
+	for (i = 0 ; i < cma_nr_pages ; ++i) {
+		void *p;
+
+		p = hyp_alloc_pages(&iommu_cma_pool, 0);
+		WARN_ON(!p);
+
+		WARN_ON(hyp_pool_reclaim(&iommu_cma_pool, hyp_virt_to_page(p), 0, true));
+	}
+
+out_donate_host:
+	WARN_ON(__pkvm_hyp_donate_host(cma_pfn, cma_nr_pages));
+	return ret;
+}
+
 int kvm_iommu_init(void *pool_base, size_t nr_pages)
 {
 	int ret;
@@ -177,6 +267,8 @@ int kvm_iommu_init(void *pool_base, size_t nr_pages)
 	ret = hyp_pool_init_empty(&iommu_host_pool, 64);
 	if (ret)
 		return ret;
+
+	ret = kvm_iommu_init_cma_pool();
 
 	iommu_pools_ready = true;
 	return ret;
@@ -253,6 +345,12 @@ static void *__kvm_iommu_donate_pages(struct hyp_pool *pool,
 	struct pkvm_hyp_vcpu *hyp_vcpu = __get_vcpu();
 	void *p;
 
+	if (cma_size && !hyp_vcpu) {
+		p = hyp_alloc_pages(&iommu_cma_pool, order);
+		if (p)
+			return p;
+	}
+
 	p = hyp_alloc_pages(pool, order);
 	if (p)
 		return p;
@@ -295,8 +393,11 @@ void kvm_iommu_reclaim_pages(void *p, u8 order)
 {
 	struct pkvm_hyp_vcpu *hyp_vcpu = __get_vcpu();
 	struct hyp_pool *pool;
+	phys_addr_t phys = hyp_virt_to_phys(p);
 
-	if (hyp_vcpu)
+	if ((phys >= cma_base) && (phys < cma_base + cma_size))
+		pool = &iommu_cma_pool;
+	else if (hyp_vcpu)
 		pool = &pkvm_hyp_vcpu_to_hyp_vm(hyp_vcpu)->iommu_pool;
 	else
 		pool = &iommu_host_pool;
