310205ff6897ac0083b59cbe477c46ffd73c9c2d
Mostafa Saleh <smostafa@google.com>
2026-03-16 21:16:00 +0000

    ANDROID: KVM: arm64: iommu: Add awareness for cma pool in shrinker
    Integrate the logic to free pages from the CMA pool in the shrinker
    HVC reclaim interface.
    
    Bug: 383901011
    Bug: 277989609
    Bug: 357781595
    Change-Id: I92e4c1d2ca36cbd990a2ad4be4304536e9980430
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -123,18 +123,42 @@ static int kvm_iommu_refill(struct kvm_hyp_memcache *host_mc)
 
 static void kvm_iommu_reclaim(struct kvm_hyp_memcache *host_mc, int target)
 {
+	unsigned long prev_nr_pages = host_mc->nr_pages;
+	unsigned long block_pages = 1 << pmd_order;
+
 	if (!iommu_pools_ready)
 		return;
 
 	reclaim_hyp_pool(&iommu_host_pool, host_mc, target, 0, false);
+
+	if (!cma_size)
+		return;
+
+	if (check_sub_overflow(target,
+			       host_mc->nr_pages - prev_nr_pages,
+			       &target))
+		return;
+
+	if (target < block_pages)
+		return;
+
+	reclaim_hyp_pool(&iommu_cma_pool, host_mc,
+			 ALIGN_DOWN(target, block_pages), pmd_order, false);
 }
 
 static int kvm_iommu_reclaimable(void)
 {
+	unsigned long reclaimable;
+
 	if (!iommu_pools_ready)
 		return 0;
 
-	return hyp_pool_free_pages(&iommu_host_pool);
+	reclaimable = hyp_pool_free_pages(&iommu_host_pool);
+
+	if (cma_size)
+		reclaimable += hyp_pool_reclaimable(&iommu_cma_pool, pmd_order) *
+				PMD_SIZE;
+	return reclaimable;
 }
 
 static struct kvm_iommu_ops *get_drv(pkvm_handle_t drv_id)
