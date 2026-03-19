56fe33c02d2f7d3498a1a9bcdcf09adcfbe90a64
Sarvesh Kalwit <sarveshkalwit@google.com>
2026-03-19 14:40:28 -0700

    Bluetooth: btmtk: Apply SAR power setting for MT7925
    This change adds a specific HCI command to set the BT SAR power to 8 dBm for the MT7925 device (dev_id 0x7925). This is a temporary measure to address SAR power limitations.
    
    Change-Id: Ib76060992e3ae41aa34ce27519a741d60de2996b
    Signed-off-by: Sarvesh Kalwit <sarveshkalwit@google.com>

--- drivers/bluetooth/btmtk.c
+++ drivers/bluetooth/btmtk.c
@@ -1325,6 +1325,7 @@ int btmtk_usb_setup(struct hci_dev *hdev)
 		break;
 	case 0x7922:
 	case 0x7925:
+
 	case 0x7961:
 		btmtk_fw_get_filename(fw_bin_name, sizeof(fw_bin_name), dev_id,
 				      fw_version, fw_flavor);
@@ -1358,6 +1359,49 @@ int btmtk_usb_setup(struct hci_dev *hdev)
 
 		hci_set_msft_opcode(hdev, 0xFD30);
 		hci_set_aosp_capable(hdev);
+
+		// TODO(b/494324772): Remove after implementing the long-term solution
+		// for setting SAR values
+		if (dev_id == 0x7925) {
+			// Adjust the BT SAR power to 8 dBm.
+			const u8 MT7925_BT_SAR_POWER = 0x08;
+			u8 sar_power_cmd[] = { 0x03,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       0x00,
+					       0x00,
+					       0x00,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       MT7925_BT_SAR_POWER,
+					       0x00 };
+
+			skb = __hci_cmd_sync(hdev, 0xfc2d,
+					     sizeof(sar_power_cmd),
+					     sar_power_cmd, HCI_INIT_TIMEOUT);
+			if (IS_ERR(skb)) {
+				err = PTR_ERR(skb);
+				bt_dev_err(
+					hdev,
+					"Failed to apply SAR power setting (%d)",
+					err);
+				return err;
+			}
+			kfree_skb(skb);
+		}
 
 		/* Set up ISO interface after protocol enabled */
 		if (test_bit(BTMTK_ISOPKT_OVER_INTR, &btmtk_data->flags)) {
