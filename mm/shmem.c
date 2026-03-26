8da2b05b4a3a661c2b59e832aefa7e514d26c47b
Isaac J. Manjarres <isaacmanjarres@google.com>
2026-03-26 01:17:50 +0000

    ANDROID: mm/memfd-ashmem-shim: Align ioctl handler name with Rust version
    Name the ioctl handler with the same name as the Rust version. This
    allows the logic in mm/shmem.c to be simplified, as the linker will
    either link the call to C version of the ioctl handler or the Rust
    version, and both are mutually exclusive.
    
    Bug: 495579795
    Change-Id: I06f1987b593546f20c98d5a45aeab8de47cdcad9
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -92,10 +92,6 @@ static struct vfsmount *shm_mnt __ro_after_init;
 
 #define BLOCKS_PER_PAGE  (PAGE_SIZE/512)
 
-#ifdef CONFIG_MEMFD_ASHMEM_SHIM
-#include "memfd-ashmem-shim.h"
-#endif
-
 #define VM_ACCT(size)    (PAGE_ALIGN(size) >> PAGE_SHIFT)
 
 /* Pretend that each entry is of this size in directory's i_size */
@@ -4890,7 +4886,7 @@ static const struct address_space_operations shmem_aops = {
 	.error_remove_folio = shmem_error_remove_folio,
 };
 
-#ifdef CONFIG_ASHMEM_RUST
+#ifdef CONFIG_ASHMEM
 extern long ashmem_memfd_ioctl(struct file *file, unsigned int cmd,
 			       unsigned long arg);
 #endif
@@ -4908,14 +4904,7 @@ static const struct file_operations shmem_file_operations = {
 	.splice_write	= iter_file_splice_write,
 	.fallocate	= shmem_fallocate,
 #endif
-#ifdef CONFIG_MEMFD_ASHMEM_SHIM
-	.unlocked_ioctl	= memfd_ashmem_shim_ioctl,
-#ifdef CONFIG_COMPAT
-	.compat_ioctl	= memfd_ashmem_shim_compat_ioctl,
-
-#endif
-#endif
-#ifdef CONFIG_ASHMEM_RUST
+#ifdef CONFIG_ASHMEM
 	.unlocked_ioctl	= ashmem_memfd_ioctl,
 #ifdef CONFIG_COMPAT
 	.compat_ioctl	= ashmem_memfd_ioctl,
