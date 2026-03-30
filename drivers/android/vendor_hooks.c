BayLibre/android_kernel_common

ANDROID: mm: differentiate folio origins to improve reclaim decisions
Differentiate the origin of folios (pages) in memory—whether they were
allocated through madvise hints, explicit user read calls, page faults,
or the kernel’s readahead mechanism—and leverage this classification
during memory reclaim. By tracking the source of each folio, the kernel
can make more informed eviction decisions and improve overall memory
management efficiency.

Bug: 442743231
Change-Id: If7c275952f6336dde81d366b8b67722e5996c724
Signed-off-by: Yang Yang <yang.yang@vivo.com>

drivers/android/vendor_hooks.c
@@ -493,6 +493,10 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lruvec_del_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_add_lazyfree_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_async_mmap_readahead);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_free_page);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_page_cache_ra_unbounded);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_force_page_cache_ra);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_fault);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_read_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_update_page_mapcount);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_add_page_to_lrulist);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_del_page_from_lrulist);

maltesermailo/android_kernel_pantah

Bug: 491963506
Change-Id: I37fd2161a9c70f3ee45290a478c280a258ac5250
(cherry picked from commit a02fd9c488a4d0ae562d72608d6f5e7f72d1c8a3)
Signed-off-by: huanpeng xin <xinhuanpeng@xiaomi.com>

drivers/android/vendor_hooks.c
@@ -120,6 +120,10 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_opt_spin_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_opt_spin_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_can_spin_on_owner);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_fault_folio_locked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_read_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_force_page_cache_ra);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_page_cache_ra_unbounded);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_wait_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_wait_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_wait_finish);