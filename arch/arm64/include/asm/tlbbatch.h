3db68e733f6d62a51db2cd920e43218d8c7fe43e
Catalin Marinas <catalin.marinas@arm.com>
2026-04-21 11:00:17 +0100

    BACKPORT: arm64: errata: Work around early CME DVMSync acknowledgement
    commit 0baba94a9779c13c857f6efc55807e6a45b1d4e4 upstream.
    
    C1-Pro acknowledges DVMSync messages before completing the SME/CME
    memory accesses. Work around this by issuing an IPI to the affected CPUs
    if they are running in EL0 with SME enabled.
    
    Note that we avoid the local DSB in the IPI handler as the kernel runs
    with SCTLR_EL1.IESB=1. This is sufficient to complete SME memory
    accesses at EL0 on taking an exception to EL1. On the return to user
    path, no barrier is necessary either. See the comment in
    sme_set_active() and the more detailed explanation in the link below.
    
    To avoid a potential IPI flood from malicious applications (e.g.
    madvise(MADV_PAGEOUT) in a tight loop), track where a process is active
    via mm_cpumask() and only interrupt those CPUs.
    
    Link: https://lore.kernel.org/r/ablEXwhfKyJW1i7l@J2N7QTR9R3
    Cc: Will Deacon <will@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Mark Brown <broonie@kernel.org>
    Reviewed-by: Will Deacon <will@kernel.org>
    Bug: 448672457
    Change-Id: I3a19bd12ba5067ab85fea142163b69cee40f4cd4
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/include/asm/tlbbatch.h
+++ arch/arm64/include/asm/tlbbatch.h
@@ -3,13 +3,19 @@
 #define _ARCH_ARM64_TLBBATCH_H
 
 #include <linux/android_kabi.h>
+#include <linux/cpumask.h>
 
 struct arch_tlbflush_unmap_batch {
+#ifdef CONFIG_ARM64_ERRATUM_4193714
 	/*
-	 * For arm64, HW can do tlb shootdown, so we don't
-	 * need to record cpumask for sending IPI
+	 * Track CPUs that need SME DVMSync on completion of this batch.
+	 * Otherwise, the arm64 HW can do tlb shootdown, so we don't need to
+	 * record cpumask for sending IPI
 	 */
+	ANDROID_KABI_USE(1, cpumask_var_t cpumask);
+#else
 	ANDROID_KABI_RESERVE(1);
+#endif
 };
 
 #endif /* _ARCH_ARM64_TLBBATCH_H */
