GrapheneOS/kernel_common-6.12

commit 113c5d9ef7ecfcc4728b6ccef0a5ee38d0e84866

ANDROID: wrapfd: allow page-unaligned load size
When loading a file into wrapfd, the size of the file might not be page
aligned. We still should be able to load it.
Pass the requested size to the content loader instead of page-aligned
value and let specific content loader make the page alignment adjustments.
Fix the checks for load completion when load size is not page-aligned.
Update tests to use unaligned file size.

Bug: 480234490
Change-Id: I91c8643b62217e85e3010c8d1bf8d6c3d2af96d7
Signed-off-by: Suren Baghdasaryan <surenb@google.com>

drivers/android/wrapfd.c
@@ -109,7 +109,7 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 	dmabuf_content = container_of(content, struct wrap_content_dmabuf,
 				      content);
 
-	if (check_add_overflow(buf_offs, len, &end))
+	if (check_add_overflow(buf_offs, PAGE_ALIGN(len), &end))
 		return -EINVAL;
 
 	if (end > dmabuf_content->dmabuf->size)
@@ -134,13 +134,13 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 	kiocb.ki_pos = file_offs;
 	kiocb.ki_flags |= IOCB_DIRECT;
 
-	while (len) {
-		loff_t count = min_t(loff_t, MAX_RW_COUNT, len);
+	while (len > 0) {
+		loff_t count = min_t(loff_t, MAX_RW_COUNT, PAGE_ALIGN(len));
 
 		iov.iov_len = count;
 		iov_iter_kvec(&iter, ITER_DEST, &iov, 1, iov.iov_len);
 		bytes_read = 0;
-		while (bytes_read < count) {
+		while (len > 0 && bytes_read < count) {
 			ssize_t sz = vfs_iocb_iter_read(file, &kiocb, &iter);
 
 			if (sz <= 0) {
@@ -148,9 +148,9 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 				goto err_unmap;
 			}
 			bytes_read += sz;
+			len -= sz;
 		}
 		iov.iov_base += count;
-		len -= count;
 	}
 err_unmap:
 	dma_buf_vunmap(dmabuf_content->dmabuf, &map);
@@ -702,9 +702,6 @@ static int wrap_file_load(struct wrap_ctx *ctx,
 		ret = -EINVAL;
 		goto put_file;
 	}
-
-	/* Align the size to the page boundary */
-	wrapfd_load.len = PAGE_ALIGN(wrapfd_load.len);
 
 	if (check_add_overflow(wrapfd_load.file_offs, wrapfd_load.len,
 			       &end)) {

tools/testing/selftests/drivers/wrapfd/wrapfd.c
@@ -150,7 +150,8 @@ FIXTURE_SETUP(wrapfd_tests)
 		SKIP(return, "Skipping all tests as non-root");
 
 	self->page_size = (size_t)sysconf(_SC_PAGESIZE);
-	self->size = self->page_size * FILE_SZ_PAGES;
+	/* Intentionally make the file size page unaligned */
+	self->size = self->page_size * FILE_SZ_PAGES - self->page_size / 2;
 
 	self->dev_fd = open("/dev/wrapfd", O_RDONLY);
 	ASSERT_TRUE(self->dev_fd >= 0);

maltesermailo/android_kernel_pantah

commit b48cf7cddb242f042a63814ab3e20550b1dfb8ff

ANDROID: drivers/wrapfd: Fix unaligned size loading issues
Commit 113c5d9ef7ecf ("ANDROID: wrapfd: allow page-unaligned load size")
accidentally introduced two issues:

When using IOCB_DIRECT, reads happen in units of the block size. If the
extent of the read results in reading more than what was requested, it
will cause the len variable to be negative. This is reported as a
failure to the caller, despite the content being read successfully
because the existing logic treats this scenario as an unanticipated EOF.

The second issue is that since reads happen in units of the block size
directly into the user-provided buffer, if the extent of the read within
the last page of the buffer is smaller than what the caller requested,
it will overwrite the data after the length that they requested.

Fix the first issue can by recognizing unanticipated EOFs as len
being a positive number. Fix the second issue by allocating a bounce
page for the last page of the read, so that only the required amount
of data can be copied to the user-provided buffer from the bounce page.

Add testcases for these scenarios as well.

Bug: 480234490
Fixes: 113c5d9ef7ecf ("ANDROID: wrapfd: allow page-unaligned load size")
Change-Id: I3b352e7be69d8db19a5e616b2a8f8c0c30c77fa6
Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

drivers/android/wrapfd.c
@@ -98,27 +98,42 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 			       loff_t file_offs, loff_t buf_offs, loff_t len)
 {
 	struct wrap_content_dmabuf *dmabuf_content;
+	void *bounce_page = NULL;
 	struct iosys_map map;
 	struct iov_iter iter;
 	struct kiocb kiocb;
-	loff_t bytes_read;
-	struct kvec iov;
-	loff_t end;
-	int ret;
+	size_t bounce_len;
+	struct kvec iov[2];
+	loff_t end, aligned_end;
+	int ret, nr_segs = 1;
 
 	dmabuf_content = container_of(content, struct wrap_content_dmabuf,
 				      content);
 
-	if (check_add_overflow(buf_offs, PAGE_ALIGN(len), &end))
+	if (check_add_overflow(buf_offs, PAGE_ALIGN(len), &aligned_end))
 		return -EINVAL;
 
-	if (end > dmabuf_content->dmabuf->size)
+	if (aligned_end > dmabuf_content->dmabuf->size)
 		return -EINVAL;
+
+	/*
+	 * If the end is not page-aligned, then the read into the last page of the range will
+	 * overwrite any data past the range. Allocate a bounce page for the last page, and handle
+	 * that separately.
+	 */
+	end = buf_offs + len;
+	if (!IS_ALIGNED(end, PAGE_SIZE)) {
+		bounce_page = (void *)__get_free_page(GFP_KERNEL);
+		if (!bounce_page)
+			return -ENOMEM;
+		bounce_len = offset_in_page(end);
+		nr_segs++;
+	}
 
 	ret = dma_buf_begin_cpu_access(dmabuf_content->dmabuf,
 				       DMA_BIDIRECTIONAL);
 	if (ret)
-		return ret;
+		goto err_free_page;
 
 	ret = dma_buf_vmap(dmabuf_content->dmabuf, &map);
 	if (ret)
@@ -129,39 +144,59 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 		goto err_unmap;
 	}
 
-	iov.iov_base = (u8 *)map.vaddr + buf_offs;
+	iov[0].iov_base = (u8 *)map.vaddr + buf_offs;
+	iov[0].iov_len = PAGE_ALIGN(len);
+	/*
+	 * Read the last page of the extent from the file into the bounce page so we can copy just
+	 * what we need later.
+	 */
+	if (nr_segs == 2) {
+		iov[0].iov_len -= PAGE_SIZE;
+		iov[1].iov_base = bounce_page;
+		iov[1].iov_len = PAGE_SIZE;
+	}
+
 	init_sync_kiocb(&kiocb, file);
 	kiocb.ki_pos = file_offs;
 	kiocb.ki_flags |= IOCB_DIRECT;
+	iov_iter_kvec(&iter, ITER_DEST, iov, nr_segs, PAGE_ALIGN(len));
 
 	while (len > 0) {
 		loff_t count = min_t(loff_t, MAX_RW_COUNT, PAGE_ALIGN(len));
 
-		iov.iov_len = count;
-		iov_iter_kvec(&iter, ITER_DEST, &iov, 1, iov.iov_len);
-		bytes_read = 0;
-		while (len > 0 && bytes_read < count) {
+		iter.count = count;
+		while (len > 0 && iov_iter_count(&iter)) {
 			ssize_t sz = vfs_iocb_iter_read(file, &kiocb, &iter);
 
 			if (sz <= 0) {
 				ret = sz;
 				goto err_unmap;
 			}
-			bytes_read += sz;
 			len -= sz;
+			iov_iter_advance(&iter, sz);
 		}
-		iov.iov_base += count;
 	}
+
+	/* Copy just what we need from the bounce buffer. */
+	if (nr_segs == 2)
+		memcpy(PTR_ALIGN_DOWN(map.vaddr + end, PAGE_SIZE), bounce_page, bounce_len);
+
 err_unmap:
 	dma_buf_vunmap(dmabuf_content->dmabuf, &map);
 err_end_access:
 	dma_buf_end_cpu_access(dmabuf_content->dmabuf, DMA_BIDIRECTIONAL);
+err_free_page:
+	free_page((unsigned long)bounce_page);
 
 	if (ret < 0)
 		return ret;
 
-	if (len)
-		return -EINVAL; /* File was too short / early EOF */
+	/*
+	 * File was too short / early EOF. Test explicitly for a positive value, as len can be
+	 * negative in cases where the page-aligned size of the final read is larger than len.
+	 */
+	if (len > 0)
+		return -EINVAL;
 
 	return 0;
 }

tools/testing/selftests/drivers/wrapfd/wrapfd.c
@@ -183,15 +183,15 @@ FIXTURE_TEARDOWN(wrapfd_tests)
 }
 
 static int cmp_content(struct __test_metadata *_metadata,
-		       FIXTURE_DATA(wrapfd_tests) *self, int wrapfd)
+		       FIXTURE_DATA(wrapfd_tests) *self, int wrapfd, size_t size)
 {
 	char *ptr;
 	int ret;
 
-	ptr = mmap(NULL, self->size, PROT_READ, MAP_SHARED, wrapfd, 0);
+	ptr = mmap(NULL, size, PROT_READ, MAP_SHARED, wrapfd, 0);
 	ASSERT_NE(ptr, MAP_FAILED);
-	ret = memcmp(self->content, ptr, self->size);
-	ASSERT_EQ(munmap(ptr, self->size), 0);
+	ret = memcmp(self->content, ptr, size);
+	ASSERT_EQ(munmap(ptr, size), 0);
 
 	return ret;
 }
@@ -235,17 +235,32 @@ static void test_wrap(struct __test_metadata *_metadata,
 static void test_load(struct __test_metadata *_metadata,
 		      FIXTURE_DATA(wrapfd_tests) *self, int fd)
 {
-	int wrapfd;
+	char *ptr, *ptr2;
+	int wrapfd, i;
 
 	/* Load the file content first */
 	wrapfd = wrapfd_wrap(self->dev_fd, fd, PROT_READ | PROT_WRITE);
 	ASSERT_TRUE(wrapfd >= 0);
 	ASSERT_EQ(wrapfd_acquire_ownership(wrapfd), 0);
 
+	/* Test a sub-page load and ensure only the requested part was written to. */
 	clear_content(_metadata, self, wrapfd);
-	ASSERT_NE(cmp_content(_metadata, self, wrapfd), 0);
+	ptr = mmap(NULL, self->page_size, PROT_READ | PROT_WRITE, MAP_SHARED, wrapfd, 0);
+	ASSERT_NE(ptr, MAP_FAILED);
+	ptr2 = malloc(self->page_size / 2);
+	ASSERT_NE(ptr, NULL);
+	memset(ptr2, 0xaa, self->page_size / 2);
+	memcpy(&ptr[self->page_size / 2], ptr2, self->page_size / 2);
+	ASSERT_EQ(wrapfd_load(wrapfd, self->fd, 0, 0, self->page_size / 2), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->page_size / 2), 0);
+	ASSERT_EQ(memcmp(&ptr[self->page_size / 2], ptr2, self->page_size / 2), 0);
+	ASSERT_EQ(munmap(ptr, self->page_size), 0);
+	free(ptr2);
+
+	clear_content(_metadata, self, wrapfd);
+	ASSERT_NE(cmp_content(_metadata, self, wrapfd, self->size), 0);
 	ASSERT_EQ(wrapfd_load(wrapfd, self->fd, 0, 0, self->size), 0);
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->size), 0);
 	/* TODO: test more load offsets */
 
 	ASSERT_EQ(wrapfd_release_ownership(wrapfd), 0);
@@ -261,7 +276,7 @@ static void test_wrap_rdonly(struct __test_metadata *_metadata,
 	ASSERT_TRUE(wrapfd >= 0);
 
 	/* Check content of the buffer */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->size), 0);
 
 	/* Try mapping as writable */
 	ASSERT_EQ(mmap(NULL, self->size, PROT_READ | PROT_WRITE,
@@ -281,7 +296,7 @@ static void test_wrap_rdwr(struct __test_metadata *_metadata,
 	ASSERT_TRUE(wrapfd >= 0);
 
 	/* Check content of the buffer before modification */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->size), 0);
 
 	/* Modify buffer content */
 	ptr = mmap(NULL, self->size, PROT_READ | PROT_WRITE, MAP_SHARED,
@@ -290,13 +305,13 @@ static void test_wrap_rdwr(struct __test_metadata *_metadata,
 	ptr[0]++;
 
 	/* Check content of the buffer after modification */
-	ASSERT_NE(cmp_content(_metadata, self, wrapfd), 0);
+	ASSERT_NE(cmp_content(_metadata, self, wrapfd, self->size), 0);
 
 	/* Restore buffer content */
 	ptr[0]--;
 
 	/* Confirm the final content */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->size), 0);
 
 	ASSERT_EQ(munmap(ptr, self->size), 0);
 
@@ -340,7 +355,7 @@ static void test_wrap_remap(struct __test_metadata *_metadata,
 	ASSERT_TRUE(wrapfd >= 0);
 
 	/* Check content of the buffer before modification */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->size), 0);
 
 	/* Modify buffer content */
 	ptr = mmap(NULL, self->size, PROT_READ | PROT_WRITE, MAP_SHARED,
@@ -374,7 +389,7 @@ static void test_wrap_fork(struct __test_metadata *_metadata,
 	ASSERT_TRUE(wrapfd >= 0);
 
 	/* Check content of the buffer before modification */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->size), 0);
 
 	/* Modify buffer content */
 	ptr = mmap(NULL, self->size, PROT_READ | PROT_WRITE, MAP_SHARED,
@@ -411,8 +426,8 @@ static void test_dup(struct __test_metadata *_metadata,
 	ASSERT_TRUE(wrapfd2 >= 0);
 
 	/* Check content of the buffer using both fds */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd), 0);
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd2), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd, self->size), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd2, self->size), 0);
 
 	close(wrapfd2);
 	close(wrapfd);
@@ -490,7 +505,7 @@ static void test_rewrap(struct __test_metadata *_metadata,
 		    state == WRAPFD_CONTENT_EMPTY);
 
 	/* Check rewrapped content */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd2), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd2, self->size), 0);
 
 	/* Take ownership of the rewrapped buffer */
 	ASSERT_EQ(wrapfd_acquire_ownership(wrapfd2), 0);
@@ -509,7 +524,7 @@ static void test_rewrap(struct __test_metadata *_metadata,
 	ASSERT_TRUE(ptr == MAP_FAILED && errno == EACCES);
 
 	/* Check rewrapped content */
-	ASSERT_EQ(cmp_content(_metadata, self, wrapfd3), 0);
+	ASSERT_EQ(cmp_content(_metadata, self, wrapfd3, self->size), 0);
 
 	/* Try mapping the original empty wrap file */
 	ptr = mmap(NULL, self->size, PROT_READ, MAP_SHARED,