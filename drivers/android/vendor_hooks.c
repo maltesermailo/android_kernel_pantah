3c3ceeb52e88c190da25ceba928f1ee0d4d3855f
qiwu.chen <qiwu.chen@transsion.com>
2026-04-23 17:35:38 +0800

    ANDROID: mm: add vendor hook to activate/unactivate folios
    Add hooks for folio activation and deactivation.
    The vendor hook helps us determine whether to activate folios in certain cases.
    
    Bug: 507212714
    Bug: 417133503
    Bug: 462138379
    Change-Id: I5cecd1661a8817a0aa06f4d72271dd7b82d75f6a
    Signed-off-by: qiwu.chen <qiwu.chen@transsion.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -511,3 +511,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fas_gpu_qos_update_tracer);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_nr_pcp_alloc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pcp_alloc_factor_adjust);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_read_swap_cache_async_timeout);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_add_lru_folio_activate);
