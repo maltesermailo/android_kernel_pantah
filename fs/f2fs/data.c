1835f7262c7d4981b9697a5295818b9cc16b3536
Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>
2026-04-23 17:05:40 +0800

    ANDROID: vendor_hook: Added hooks for optimize f2fs buffered-io
    The following hooks have been added to optimize f2fs buffered-io:
    
    android_rvh_f2fs_buffer_write - This hook is used to optimize buffered
    write behavior in specific scenarios to reduce stuttering in critical
    applications caused by write latency.
    
    android_rvh_f2fs_move_data_block_bypass - This hook was added because
    the vendor wanted to specify certain files that wouldn't be moved during
    garbage collection (GC) to improve the I/O performance of those files.
    
    android_rvh_f2fs_ra_data_block_bypass - This hook is used to skip
    reading certain special files encountered during GC, thus preventing
    stuttering caused by background GC in the foreground of critical
    applications.
    
    android_rvh_f2fs_write_cache_pages - This hook is used to optimize
    stuttering in critical applications caused by writing back a large
    number of dirty pages in specific scenarios.
    
    android_vh_f2fs_customized_ioctl - This hook is used to customize f2fs'
    response to specific ioctl commands, adding OEM-customized ioctl
    commands.
    
    android_vh_f2fs_dnode_set_blkaddr - This hook is used to observe the
    behavior of setting `blkaddr` on the dnode, which helps to locate issues
    related to corrupted block addresses.
    
    android_vh_f2fs_inode_may_compress - This hook is used to reject
    compression attempts on specific critical files, thereby improving the
    read and write performance of these critical files.
    
    Bug: 505636392
    Change-Id: I500ab5750e11daad11101be0247b9a724ff7a2c5
    Signed-off-by: Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1181,6 +1181,8 @@ static void __set_data_blkaddr(struct dnode_of_data *dn, block_t blkaddr)
 {
 	__le32 *addr = get_dnode_addr(dn->inode, dn->node_folio);
 
+	trace_android_vh_f2fs_dnode_set_blkaddr(dn->inode, blkaddr);
+
 	dn->data_blkaddr = blkaddr;
 	addr[dn->ofs_in_node] = cpu_to_le32(dn->data_blkaddr);
 }
@@ -3583,6 +3585,7 @@ static int __f2fs_write_data_pages(struct address_space *mapping,
 	struct blk_plug plug;
 	int ret;
 	bool locked = false;
+	bool wb_done = false;
 
 	/* skip writing if there is no dirty page in this inode */
 	if (!get_dirty_pages(inode) && wbc->sync_mode == WB_SYNC_NONE)
@@ -3622,7 +3625,14 @@ static int __f2fs_write_data_pages(struct address_space *mapping,
 	account_writeback(inode, true);
 
 	blk_start_plug(&plug);
+
+	trace_android_rvh_f2fs_write_cache_pages(mapping, wbc, &ret, &wb_done);
+	if (wb_done)
+		goto blk_finish;
+
 	ret = f2fs_write_cache_pages(mapping, wbc, io_type);
+
+blk_finish:
 	blk_finish_plug(&plug);
 
 	account_writeback(inode, false);
