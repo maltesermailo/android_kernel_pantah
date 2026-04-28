29d6595032b35e18f365df5cd1d907d3ada5217e
Siddharth Nayyar <sidnayyar@google.com>
2026-04-28 15:30:29 +0000

    ANDROID: new symbol list test
    Bug: 461821577
    Change-Id: I53b3ff8602d3581fe96f8f43d32ab57dba6672de
    Signed-off-by: Siddharth Nayyar <sidnayyar@google.com>

--- fs/smb/client/cifsfs.c
+++ fs/smb/client/cifsfs.c
@@ -2064,6 +2064,7 @@ exit_cifs(void)
 
 MODULE_AUTHOR("Steve French");
 MODULE_LICENSE("GPL");	/* combination of LGPL + GPL source behaves as GPL */
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 MODULE_DESCRIPTION
 	("VFS to access SMB3 servers e.g. Samba, Macs, Azure and Windows (and "
 	"also older servers complying with the SNIA CIFS Specification)");
