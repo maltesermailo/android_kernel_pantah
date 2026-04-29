505dd036245a19b4cf88796ed225b740b012669b
Dongdong zhang <zhangdongdong5@xiaomi.com>
2026-04-29 11:05:30 +0800

    ANDROID: mm: add android_vh_folio_start_writeback hook
    Add a vendor hook in __folio_start_writeback() to notify modules when a
    folio begins writeback. This is a companion to the existing
    android_vh_folio_end_writeback hook and together they allow vendor
    modules to accurately track the writeback lifecycle of file folios,
    enabling better reclaim decisions.
    
    Bug: 506184275
    Change-Id: I24b458ac0694bbeb0686c1eada84a0ce0e4f60f5
    Signed-off-by: Dongdong zhang <zhangdongdong5@xiaomi.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -826,6 +826,9 @@ DECLARE_HOOK(android_vh_pick_swap_device_bypass,
 DECLARE_HOOK(android_vh_folio_end_writeback,
 	TP_PROTO(struct folio *folio),
 	TP_ARGS(folio));
+DECLARE_HOOK(android_vh_folio_start_writeback,
+	TP_PROTO(struct folio *folio),
+	TP_ARGS(folio));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
