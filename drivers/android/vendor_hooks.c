4bbd94c1f2b87d7a7574c09d28d298090a71cf6c
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-04-28 18:20:26 -0700

    Revert "ANDROID: vendor hooks: add vendor hooks for do_new_mount"
    This revert was created by Android Culprit Assistant as part of session http://go/aca-get/eeab258b-0656-4b44-acca-531718e0c5ae. Test  fails at build 15037183 and passes with this revert (go/abtd/run/pending_run_id). Please review this revert and approve if it looks good to you.
    
    Bug: 493813259
    Change-Id: Idf4ed47125bbaa582fa2791a8641a64976b9946d
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -66,7 +66,6 @@
 #include <trace/hooks/signal.h>
 #include <trace/hooks/psci.h>
 #include <trace/hooks/psi.h>
-#include <trace/hooks/blk.h>
 #include <trace/hooks/bl_hib.h>
 #include <trace/hooks/regmap.h>
 #include <trace/hooks/compaction.h>
@@ -489,7 +488,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swapmem_gather_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_oom_evaluate_task_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_oom_swapmem_gather_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_oom_swapmem_gather_finish);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_new_mount_fc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_read_fault);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_read);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_map_pages);
