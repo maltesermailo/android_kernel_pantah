54b2e10fd4df7a0f0d90d40f454afbcb715fa5bf
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-05-04 14:54:16 -0700

    Revert "UPSTREAM: f2fs: fix fiemap boundary handling when read e..."
    This revert was created by Android Culprit Assistant as part of session http://go/aca-get/8752437e-c58e-4b3c-87a9-0be71e97ca9d. Test  fails at build 15339857 and passes with this revert (go/abtd/run/pending_run_id). Please review this revert and approve if it looks good to you.
    
    Change-Id: Ib040ff640d61690801d4512ef3f39b94a52ae1df
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 486017190

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1647,26 +1647,8 @@ int f2fs_map_blocks(struct inode *inode, struct f2fs_map_blocks *map, int flag)
 	lfs_dio_write = (flag == F2FS_GET_BLOCK_DIO && f2fs_lfs_mode(sbi) &&
 				map->m_may_create);
 
-	if (!map->m_may_create && f2fs_map_blocks_cached(inode, map, flag)) {
-		struct extent_info ei;
-
-		/*
-		 * 1. If map->m_multidev_dio is true, map->m_pblk cannot be
-		 * waitted by f2fs_wait_on_block_writeback_range() and are not
-		 * mergeable.
-		 * 2. If pgofs hits the read extent cache, it means the mapping
-		 * is already cached in the extent cache, but it is not
-		 * mergeable, and there is no need to query the mapping again
-		 * via f2fs_get_dnode_of_data().
-		 */
-		pgofs =	(pgoff_t)map->m_lblk + map->m_len;
-		if (map->m_len == maxblocks ||
-			map->m_multidev_dio ||
-			f2fs_lookup_read_extent_cache(inode, pgofs, &ei))
-			goto out;
-		ofs = map->m_len;
-		goto map_more;
-	}
+	if (!map->m_may_create && f2fs_map_blocks_cached(inode, map, flag))
+		goto out;
 
 	map->m_bdev = inode->i_sb->s_bdev;
 	map->m_multidev_dio =
@@ -1677,8 +1659,7 @@ int f2fs_map_blocks(struct inode *inode, struct f2fs_map_blocks *map, int flag)
 
 	/* it only supports block size == page size */
 	pgofs =	(pgoff_t)map->m_lblk;
-map_more:
-	end = (pgoff_t)map->m_lblk + maxblocks;
+	end = pgofs + maxblocks;
 
 	if (flag == F2FS_GET_BLOCK_PRECACHE)
 		mode = LOOKUP_NODE_RA;
