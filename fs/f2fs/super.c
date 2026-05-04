efedabe7e34694fdab5f6e0ff31d1f5001c16e2c
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-05-04 14:54:16 -0700

    Revert "UPSTREAM: f2fs: allow empty mount string for Opt_usr|grp..."
    This revert was created by Android Culprit Assistant as part of session http://go/aca-get/8752437e-c58e-4b3c-87a9-0be71e97ca9d. Test  fails at build 15339857 and passes with this revert (go/abtd/run/pending_run_id). Please review this revert and approve if it looks good to you.
    
    Change-Id: If76832753d8bb60a77f427b23c35228e8d99a04d
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 486017190

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -340,12 +340,9 @@ static const struct fs_parameter_spec f2fs_param_specs[] = {
 	fsparam_flag("usrquota", Opt_usrquota),
 	fsparam_flag("grpquota", Opt_grpquota),
 	fsparam_flag("prjquota", Opt_prjquota),
-	fsparam_string("usrjquota", Opt_usrjquota),
-	fsparam_flag("usrjquota", Opt_usrjquota),
-	fsparam_string("grpjquota", Opt_grpjquota),
-	fsparam_flag("grpjquota", Opt_grpjquota),
-	fsparam_string("prjjquota", Opt_prjjquota),
-	fsparam_flag("prjjquota", Opt_prjjquota),
+	fsparam_string_empty("usrjquota", Opt_usrjquota),
+	fsparam_string_empty("grpjquota", Opt_grpjquota),
+	fsparam_string_empty("prjjquota", Opt_prjjquota),
 	fsparam_flag("nat_bits", Opt_nat_bits),
 	fsparam_enum("jqfmt", Opt_jqfmt, f2fs_param_jqfmt),
 	fsparam_enum("alloc_mode", Opt_alloc, f2fs_param_alloc_mode),
@@ -988,26 +985,26 @@ static int f2fs_parse_param(struct fs_context *fc, struct fs_parameter *param)
 		ctx_set_opt(ctx, F2FS_MOUNT_PRJQUOTA);
 		break;
 	case Opt_usrjquota:
-		if (param->type == fs_value_is_string && *param->string)
-			ret = f2fs_note_qf_name(fc, USRQUOTA, param);
-		else
+		if (!*param->string)
 			ret = f2fs_unnote_qf_name(fc, USRQUOTA);
+		else
+			ret = f2fs_note_qf_name(fc, USRQUOTA, param);
 		if (ret)
 			return ret;
 		break;
 	case Opt_grpjquota:
-		if (param->type == fs_value_is_string && *param->string)
-			ret = f2fs_note_qf_name(fc, GRPQUOTA, param);
-		else
+		if (!*param->string)
 			ret = f2fs_unnote_qf_name(fc, GRPQUOTA);
+		else
+			ret = f2fs_note_qf_name(fc, GRPQUOTA, param);
 		if (ret)
 			return ret;
 		break;
 	case Opt_prjjquota:
-		if (param->type == fs_value_is_string && *param->string)
-			ret = f2fs_note_qf_name(fc, PRJQUOTA, param);
-		else
+		if (!*param->string)
 			ret = f2fs_unnote_qf_name(fc, PRJQUOTA);
+		else
+			ret = f2fs_note_qf_name(fc, PRJQUOTA, param);
 		if (ret)
 			return ret;
 		break;
