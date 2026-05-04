ea74d6bebd62ac0cad214be469379e6c52fd322a
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-05-04 14:54:16 -0700

    Revert "UPSTREAM: f2fs: fix to do sanity check on dcc->discard_c..."
    This revert was created by Android Culprit Assistant as part of session http://go/aca-get/8752437e-c58e-4b3c-87a9-0be71e97ca9d. Test  fails at build 15339857 and passes with this revert (go/abtd/run/pending_run_id). Please review this revert and approve if it looks good to you.
    
    Change-Id: Ib456c0dfbb80fe53108e91d71439e9afb36276ec
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 486017190

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2015,7 +2015,7 @@ static void f2fs_put_super(struct super_block *sb)
 	}
 
 	/* be sure to wait for any on-going discard commands */
-	done = f2fs_issue_discard_timeout(sbi, true);
+	done = f2fs_issue_discard_timeout(sbi);
 	if (f2fs_realtime_discard_enable(sbi) && !sbi->discard_blks && done) {
 		struct cp_control cpc = {
 			.reason = CP_UMOUNT | CP_TRIMMED,
@@ -2158,7 +2158,7 @@ static int f2fs_unfreeze(struct super_block *sb)
 	 * will recover after removal of snapshot.
 	 */
 	if (test_opt(sbi, DISCARD) && !f2fs_hw_support_discard(sbi))
-		f2fs_issue_discard_timeout(sbi, true);
+		f2fs_issue_discard_timeout(sbi);
 
 	clear_sbi_flag(F2FS_SB(sb), SBI_IS_FREEZING);
 	return 0;
@@ -2963,12 +2963,7 @@ static int __f2fs_remount(struct fs_context *fc, struct super_block *sb)
 			need_stop_discard = true;
 		} else {
 			f2fs_stop_discard_thread(sbi);
-			/*
-			 * f2fs_ioc_fitrim() won't race w/ "remount ro"
-			 * so it's safe to check discard_cmd_cnt in
-			 * f2fs_issue_discard_timeout().
-			 */
-			f2fs_issue_discard_timeout(sbi, flags & SB_RDONLY);
+			f2fs_issue_discard_timeout(sbi);
 			need_restart_discard = true;
 		}
 	}
