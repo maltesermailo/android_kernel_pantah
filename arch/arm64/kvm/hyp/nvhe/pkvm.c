ffcca9eb72b6446acb965695876c58a992f7f6d5
Madhu Ananthula <mananthu@qti.qualcomm.com>
2026-05-05 18:21:36 +0530

    ANDROID: KVM: arm64: Refactor initializing SVE state in pKVM
    Easier to read. Streamlines error handling.
    
    No functional change intended.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Bug: 278749606
    Change-Id: I406503d48311d8eb608c6a3d7260fdd6d78f9e62
    Signed-off-by: Madhu Ananthula <mananthu@qti.qualcomm.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -533,24 +533,32 @@ static void init_pkvm_hyp_vm(struct kvm *host_kvm, struct pkvm_hyp_vm *hyp_vm,
 static int init_pkvm_hyp_vcpu_sve(struct pkvm_hyp_vcpu *hyp_vcpu,
 				  struct kvm_vcpu *host_vcpu)
 {
+	unsigned int sve_max_vl = READ_ONCE(host_vcpu->arch.sve_max_vl);
+	void *sve_state = kern_hyp_va(READ_ONCE(host_vcpu->arch.sve_state));
 	size_t sve_state_size;
-	void *sve_state;
+	int ret = 0;
 
-	hyp_vcpu->vcpu.arch.sve_state = READ_ONCE(host_vcpu->arch.sve_state);
-	hyp_vcpu->vcpu.arch.sve_max_vl = READ_ONCE(host_vcpu->arch.sve_max_vl);
-
-	sve_state = kern_hyp_va(hyp_vcpu->vcpu.arch.sve_state);
+	/* Temporarily set sve_max_vl to compute state size */
+	hyp_vcpu->vcpu.arch.sve_max_vl = sve_max_vl;
 	sve_state_size = vcpu_sve_state_size(&hyp_vcpu->vcpu);
 
-	if (!hyp_vcpu->vcpu.arch.sve_state || !sve_state_size ||
-	    hyp_pin_shared_mem(sve_state, sve_state + sve_state_size)) {
-		clear_bit(KVM_ARM_VCPU_SVE, hyp_vcpu->vcpu.arch.features);
-		hyp_vcpu->vcpu.arch.sve_state = NULL;
-		hyp_vcpu->vcpu.arch.sve_max_vl = 0;
-		return -EINVAL;
+	if (!sve_state || !sve_state_size) {
+		ret = -EINVAL;
+		goto err;
 	}
 
+	ret = hyp_pin_shared_mem(sve_state, sve_state + sve_state_size);
+	if (ret)
+		goto err;
+
+	hyp_vcpu->vcpu.arch.sve_state = sve_state;
+	hyp_vcpu->vcpu.arch.sve_max_vl = sve_max_vl;
+
 	return 0;
+err:
+	clear_bit(KVM_ARM_VCPU_SVE, hyp_vcpu->vcpu.arch.features);
+	hyp_vcpu->vcpu.arch.sve_max_vl = 0;
+	return ret;
 }
 
 static int init_pkvm_hyp_vcpu(struct pkvm_hyp_vcpu *hyp_vcpu,
