c99e95d7fda9cc3a4b01d950b1dade6c19a68f85
Isaac J. Manjarres <isaacmanjarres@google.com>
2026-05-09 00:37:02 +0000

    NOMERGE: ANDROID: drivers/wrapfd: Add support for forcing buffered I/O
    Add a debugfs node to force buffered I/O dynamically without having to
    rebuild the kernel. This can be used for profiling buffered I/O against
    our current approach using direct I/O.
    
    Change-Id: I7b223372bb786e33442fb438711affea96f72839
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- drivers/android/wrapfd.c
+++ drivers/android/wrapfd.c
@@ -8,6 +8,7 @@
 
 #include <linux/anon_inodes.h>
 #include <linux/bvec.h>
+#include <linux/debugfs.h>
 #include <linux/dma-buf.h>
 #include <linux/fdtable.h>
 #include <linux/file.h>
@@ -27,6 +28,7 @@
 struct wrap_ctx;
 struct wrap_content;
 static const struct file_operations wrap_fops;
+static atomic_t force_buffered_io;
 
 struct wrap_content_operations {
 	int (*create_wrap)(struct wrap_content *content, struct wrap_ctx *ctx);
@@ -111,6 +113,7 @@ struct dmabuf_load_param {
 	size_t direct_read_len;
 	size_t bytes_read;
 	size_t file_read_len;
+	bool use_buffered_io;
 	struct kiocb kiocb;
 	struct iov_iter iter;
 	/* At most 4 segments: beginning, middle, and 2 at the end. */
@@ -145,7 +148,7 @@ static void prepare_iocb_request(struct dmabuf_load_param *param, struct file *f
 
 	init_sync_kiocb(&param->kiocb, file);
 	param->kiocb.ki_pos = PAGE_ALIGN_DOWN(param->file_offs);
-	param->kiocb.ki_flags |= IOCB_DIRECT;
+	param->kiocb.ki_flags |= param->use_buffered_io ? 0 : IOCB_DIRECT;
 	iov_iter_kvec(&param->iter, ITER_DEST, param->iov, nr_segs, param->file_read_len);
 }
 
@@ -162,6 +165,7 @@ static int dmabuf_content_load_prepare(struct file *file, struct dma_buf *dmabuf
 	loff_t buf_end;
 	struct iosys_map map;
 	int i, ret;
+	bool use_buffered_io = !!atomic_read(&force_buffered_io);
 
 	if (check_add_overflow(buf_offs, len, &buf_end))
 		return -EINVAL;
@@ -169,13 +173,14 @@ static int dmabuf_content_load_prepare(struct file *file, struct dma_buf *dmabuf
 	if (buf_end > dmabuf->size)
 		return -EINVAL;
 
-	file_read_len = PAGE_ALIGN(file_offs + len) - PAGE_ALIGN_DOWN(file_offs);
+	file_read_len = use_buffered_io ? len : 
+	                                  PAGE_ALIGN(file_offs + len) - PAGE_ALIGN_DOWN(file_offs);
 
 	/*
 	 * If the file offset is unaligned, then writing it into the first page of the user's buffer
 	 * could overwrite some of their data. A similar scenario can happen with the buffer offset.
 	 */
-	if (!PAGE_ALIGNED(file_offs | buf_offs)) {
+	if (!use_buffered_io && !PAGE_ALIGNED(file_offs | buf_offs)) {
 		/*
 		 * Start directly reading into the buffer at the next page from where the data on
 		 * the bounce page will ultimately live. This avoids having to shift the data that
@@ -192,7 +197,10 @@ static int dmabuf_content_load_prepare(struct file *file, struct dma_buf *dmabuf
 	 * the range we're supposed to write into, and since direct I/O is done in units of pages,
 	 * ensure that there is at least a page to read.
 	 */
-	if ((direct_read_start_offs < buf_end) && ((buf_end - direct_read_start_offs) >= PAGE_SIZE))
+	if (use_buffered_io)
+		direct_read_len = file_read_len;
+	else if ((direct_read_start_offs < buf_end) &&
+		 ((buf_end - direct_read_start_offs) >= PAGE_SIZE))
 		direct_read_len = PAGE_ALIGN_DOWN(buf_end) - direct_read_start_offs;
 
 	/*
@@ -245,6 +253,7 @@ static int dmabuf_content_load_prepare(struct file *file, struct dma_buf *dmabuf
 	param->direct_read_len = direct_read_len;
 	param->len = len;
 	param->file_read_len = file_read_len;
+	param->use_buffered_io = use_buffered_io;
 	prepare_iocb_request(param, file);
 	return 0;
 
@@ -266,7 +275,8 @@ static void dmabuf_content_load_complete(struct dmabuf_load_param *param)
 	size_t tot_len, len;
 	int i;
 
-	if (param->bytes_read < (offset_in_page(param->file_offs) + param->len))
+	if (param->bytes_read <
+	    (param->use_buffered_io ? param->len : (offset_in_page(param->file_offs) + param->len)))
 		goto out;
 
 	tot_len = param->len;
@@ -333,6 +343,8 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 
 	while (param.bytes_read < param.file_read_len) {
 		param.iter.count = min_t(size_t, MAX_RW_COUNT,
+					 param.use_buffered_io ?
+					 param.file_read_len - param.bytes_read :
 					 PAGE_ALIGN(param.file_read_len - param.bytes_read));
 
 		bytes_read = vfs_iocb_iter_read(file, &param.kiocb, &param.iter);
@@ -351,7 +363,7 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 		return ret;
 
 	/* File was too short / early EOF. */
-	return param.bytes_read < offset_in_page(file_offs) + len ? -EINVAL : 0;
+	return param.bytes_read < (param.use_buffered_io ? len : offset_in_page(file_offs) + len) ? -EINVAL : 0;
 }
 
 static struct wrap_content *
@@ -1482,6 +1494,55 @@ static struct miscdevice wrapfd_misc = {
 	.fops = &wrapfd_dev_fops,
 };
 
+#ifdef CONFIG_DEBUG_FS
+static struct dentry *wrapfd_debugfs_dir;
+
+static int force_buffered_io_show(struct seq_file *s, void *unused)
+{
+	seq_printf(s, "%d\n", atomic_read(&force_buffered_io));
+	return 0;
+}
+
+static ssize_t force_buffered_io_write(struct file *file, const char __user *userbuf, size_t count,
+				       loff_t *ppos)
+{
+	int ret, val;
+
+	ret = kstrtoint_from_user(userbuf, count, 10, &val);
+	if (ret)
+		return ret;
+
+	if (val != 0 && val != 1)
+		return -EINVAL;
+
+	atomic_set(&force_buffered_io, val);
+	return count;
+}
+DEFINE_SHOW_STORE_ATTRIBUTE(force_buffered_io);
+
+static void wrapfd_debugfs_init(void)
+{
+	struct dentry *d;
+
+	d = debugfs_create_dir("wrapfd", NULL);
+	if (IS_ERR(d))
+		return;
+
+	wrapfd_debugfs_dir = d;
+
+	d = debugfs_create_file("force-buffered-io", 0444, wrapfd_debugfs_dir, NULL,
+				&force_buffered_io_fops);
+	if (IS_ERR(d)) {
+		debugfs_remove_recursive(wrapfd_debugfs_dir);
+		wrapfd_debugfs_dir = NULL;
+	}
+}
+#else
+static void wrapfd_debugfs_init(void)
+{
+}
+#endif
+
 static int __init wrapfd_init(void)
 {
 	int ret;
@@ -1491,6 +1552,8 @@ static int __init wrapfd_init(void)
 		pr_err("failed to register misc device!\n");
 		return ret;
 	}
+
+	wrapfd_debugfs_init();
 
 	return 0;
 }
