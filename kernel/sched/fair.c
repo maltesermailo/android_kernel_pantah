2743630d0ea0328c74d58b43e52a1f7852a2e998
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-05-13 07:51:56 -0700

    Revert "UPSTREAM: sched/fair: Introduce and use the vruntime_cmp..."
    This revert was created by Android Culprit Assistant as part of session http://go/aca-get/57c79b8f-2d47-44f7-80f2-5f5c09c1bc78. Test v2/android-gki/main_aosp_cf_x86_64_test_mapping_presubmit fails at build 15129376 and passes with this revert (go/abtd/run/pending_run_id). Please review this revert and approve if it looks good to you.
    
    Change-Id: I2cd5eb9d86528c18d721a83a774c5e93406ae69b
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -559,48 +559,10 @@ void account_cfs_rq_runtime(struct cfs_rq *cfs_rq, u64 delta_exec);
  * Scheduling class tree data structure manipulation methods:
  */
 
-extern void __BUILD_BUG_vruntime_cmp(void);
-
-/* Use __builtin_strcmp() because of __HAVE_ARCH_STRCMP: */
-
-#define vruntime_cmp(A, CMP_STR, B) ({				\
-	int __res = 0;						\
-								\
-	if (!__builtin_strcmp(CMP_STR, "<")) {			\
-		__res = ((s64)((A)-(B)) < 0);			\
-	} else if (!__builtin_strcmp(CMP_STR, "<=")) {		\
-		__res = ((s64)((A)-(B)) <= 0);			\
-	} else if (!__builtin_strcmp(CMP_STR, ">")) {		\
-		__res = ((s64)((A)-(B)) > 0);			\
-	} else if (!__builtin_strcmp(CMP_STR, ">=")) {		\
-		__res = ((s64)((A)-(B)) >= 0);			\
-	} else {						\
-		/* Unknown operator throws linker error: */	\
-		__BUILD_BUG_vruntime_cmp();			\
-	}							\
-								\
-	__res;							\
-})
-
-extern void __BUILD_BUG_vruntime_op(void);
-
-#define vruntime_op(A, OP_STR, B) ({				\
-	s64 __res = 0;						\
-								\
-	if (!__builtin_strcmp(OP_STR, "-")) {			\
-		__res = (s64)((A)-(B));				\
-	} else {						\
-		/* Unknown operator throws linker error: */	\
-		__BUILD_BUG_vruntime_op();			\
-	}							\
-								\
-	__res;						\
-})
-
-
 static inline __maybe_unused u64 max_vruntime(u64 max_vruntime, u64 vruntime)
 {
-	if (vruntime_cmp(vruntime, ">", max_vruntime))
+	s64 delta = (s64)(vruntime - max_vruntime);
+	if (delta > 0)
 		max_vruntime = vruntime;
 
 	return max_vruntime;
@@ -608,7 +570,8 @@ static inline __maybe_unused u64 max_vruntime(u64 max_vruntime, u64 vruntime)
 
 static inline __maybe_unused u64 min_vruntime(u64 min_vruntime, u64 vruntime)
 {
-	if (vruntime_cmp(vruntime, "<", min_vruntime))
+	s64 delta = (s64)(vruntime - min_vruntime);
+	if (delta < 0)
 		min_vruntime = vruntime;
 
 	return min_vruntime;
@@ -621,12 +584,12 @@ static inline bool entity_before(const struct sched_entity *a,
 	 * Tiebreak on vruntime seems unnecessary since it can
 	 * hardly happen.
 	 */
-	return vruntime_cmp(a->deadline, "<", b->deadline);
+	return (s64)(a->deadline - b->deadline) < 0;
 }
 
 static inline s64 entity_key(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
-	return vruntime_op(se->vruntime, "-", cfs_rq->zero_vruntime);
+	return (s64)(se->vruntime - cfs_rq->zero_vruntime);
 }
 
 #define __node_2_se(node) \
@@ -804,7 +767,7 @@ static int vruntime_eligible(struct cfs_rq *cfs_rq, u64 vruntime)
 		load += weight;
 	}
 
-	return avg >= vruntime_op(vruntime, "-", cfs_rq->zero_vruntime) * load;
+	return avg >= (s64)(vruntime - cfs_rq->zero_vruntime) * load;
 }
 
 int entity_eligible(struct cfs_rq *cfs_rq, struct sched_entity *se)
@@ -815,7 +778,7 @@ int entity_eligible(struct cfs_rq *cfs_rq, struct sched_entity *se)
 static void update_zero_vruntime(struct cfs_rq *cfs_rq)
 {
 	u64 vruntime = avg_vruntime(cfs_rq);
-	s64 delta = vruntime_op(vruntime, "-", cfs_rq->zero_vruntime);
+	s64 delta = (s64)(vruntime - cfs_rq->zero_vruntime);
 
 	sum_w_vruntime_update(cfs_rq, delta);
 
@@ -842,12 +805,13 @@ static inline bool __entity_less(struct rb_node *a, const struct rb_node *b)
 	return entity_before(__node_2_se(a), __node_2_se(b));
 }
 
+#define vruntime_gt(field, lse, rse) ({ (s64)((lse)->field - (rse)->field) > 0; })
+
 static inline void __min_vruntime_update(struct sched_entity *se, struct rb_node *node)
 {
 	if (node) {
 		struct sched_entity *rse = __node_2_se(node);
-
-		if (vruntime_cmp(se->min_vruntime, ">", rse->min_vruntime))
+		if (vruntime_gt(min_vruntime, se, rse))
 			se->min_vruntime = rse->min_vruntime;
 	}
 }
@@ -960,7 +924,7 @@ static inline void update_protect_slice(struct cfs_rq *cfs_rq, struct sched_enti
 
 static inline bool protect_slice(struct sched_entity *se)
 {
-	return vruntime_cmp(se->vruntime, "<", se->vprot);
+	return ((s64)(se->vprot - se->vruntime) > 0);
 }
 
 static inline void cancel_protect_slice(struct sched_entity *se)
@@ -1093,7 +1057,7 @@ static bool update_deadline(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	if (skip_preempt)
 		return false;
 
-	if (vruntime_cmp(se->vruntime, "<", se->deadline))
+	if ((s64)(se->vruntime - se->deadline) < 0)
 		return false;
 
 	/*
@@ -13431,8 +13395,8 @@ bool cfs_prio_less(const struct task_struct *a, const struct task_struct *b,
 	 * zero_vruntime_fi, which would have been updated in prior calls
 	 * to se_fi_update().
 	 */
-	delta = vruntime_op(sea->vruntime, "-", seb->vruntime) +
-		vruntime_op(cfs_rqb->zero_vruntime_fi, "-", cfs_rqa->zero_vruntime_fi);
+	delta = (s64)(sea->vruntime - seb->vruntime) +
+		(s64)(cfs_rqb->zero_vruntime_fi - cfs_rqa->zero_vruntime_fi);
 
 	return delta > 0;
 }
